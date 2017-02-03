ff650cc884e205792f57919be162dee9cd6b49e1
Robert Stoll <rstoll@tutteli.ch>
2017-02-03 14:12:25 +0100

    added lazy evaluating assert - rewrote AssertionFactoryNullableSpec and using check instead of it

--- src/ch/tutteli/assertk/AssertK.kt
+++ src/ch/tutteli/assertk/AssertK.kt
@@ -8,8 +8,28 @@ fun <T : Any> assert(subject: T): IAssertionFactory<T>
 fun <T : Any?> assert(subject: T): IAssertionFactoryNullable<T>
     = AssertionFactory.newNullable("assert", subject)
 
+inline fun <T : Any> assert(subject: T, createAssertions: IAssertionFactory<T>.() -> Unit)
+    = createAndCheckAssertions("assert", subject, createAssertions)
+
 fun expect(act: () -> Unit)
     = ThrowableFluent.create("expect to throw", act)
+
+/**
+ * Use this function to create custom 'expect' functions which lazy evaluate the given assertions.
+ */
+inline fun <T : Any> createAndCheckAssertions(assertionVerb: String, subject: T, createAssertions: IAssertionFactory<T>.() -> Unit) {
+    val factory = AssertionFactory.new(assertionVerb, subject)
+    factory.createAssertions()
+    factory.checkAssertions()
+}
+
+fun <T : Any> IAssertionFactoryNullable<T?>.isNotNull(createAssertions: IAssertionFactory<T>.() -> Unit) {
+    if (subject != null) {
+        createAndCheckAssertions(assertionVerb, subject!!, createAssertions)
+    } else {
+        AssertionFactory.failWithCustomSubject(assertionVerb, "null", listOf("is not" to "null"))
+    }
+}
 
 fun <T : Any> IAssertionFactoryNullable<T?>.isNotNull(): IAssertionFactory<T> {
     if (subject != null) {
@@ -21,6 +41,18 @@ fun <T : Any> IAssertionFactoryNullable<T?>.isNotNull(): IAssertionFactory<T> {
 
 fun <T : Any> IAssertionFactory<T>.toBe(expected: T)
     = createAndAddAssertion("to be", expected, { subject == expected })
+
+fun IAssertionFactory<Int>.isSmallerThan(expected: Int)
+    = createAndAddAssertion("is smaller than", expected, { subject < expected })
+
+fun IAssertionFactory<Int>.isSmallerOrEquals(expected: Int)
+    = createAndAddAssertion("is smaller or equals", expected, { subject <= expected })
+
+fun IAssertionFactory<Int>.isGreaterThan(expected: Int)
+    = createAndAddAssertion("is greater than", expected, { subject > expected })
+
+fun IAssertionFactory<Int>.isGreaterOrEquals(expected: Int)
+    = createAndAddAssertion("is greater or equals", expected, { subject >= expected })
 
 fun IAssertionFactory<CharSequence>.contains(expected: CharSequence)
     = createAndAddAssertion("contains", expected, { subject.contains(expected) })
