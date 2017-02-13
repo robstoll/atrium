1a48aebfb297166d75d0b6196bb9b36d3bafc192
Robert Stoll <rstoll@tutteli.ch>
2017-02-13 22:53:58 +0100

    introducing IAssertionChecker - including default implementation ThrowingAssertionChecker

---
+++ src/ch/tutteli/assertk/ThrowingAssertionChecker.kt
@@ -0,0 +1,27 @@
+package ch.tutteli.assertk
+
+class ThrowingAssertionChecker(private val reporter: IReporter) : IAssertionChecker {
+
+    override fun check(assertionVerb: String, subject: Any, assertions: List<IAssertion>) {
+        val sb = StringBuilder()
+        var allHold = true
+        assertions.forEach {
+            allHold = allHold && it.holds()
+            reporter.format(sb, it)
+        }
+        if (!allHold) {
+            throw AssertionError(sb.toString())
+        }
+    }
+
+    override fun fail(assertionVerb: String, subject: Any, assertion: IAssertion) {
+        if (assertion.holds()) throw IllegalArgumentException("the given assertion should fail: $assertion")
+        check(assertionVerb, subject, listOf(assertion))
+    }
+
+    override fun failWithCustomSubject(assertionVerb: String, subject: String, assertion: IAssertion) {
+        if (assertion.holds()) throw IllegalArgumentException("the given assertion should fail: $assertion")
+        check(assertionVerb, subject, listOf(assertion))
+    }
+
+}
---
+++ test/ch/tutteli/assertk/ThrowingAssertionCheckerSpec.kt
@@ -0,0 +1,57 @@
+package ch.tutteli.assertk
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+
+class ThrowingAssertionCheckerSpec : Spek({
+
+    val assertionVerb = "assertionVerb"
+    val reporterResponse = "hello"
+    val reporter = mock<IReporter> {
+        on { format(any<StringBuilder>(), any<IAssertion>()) }.thenAnswer {
+            (it.arguments[0] as StringBuilder).append(reporterResponse)
+        }
+    }
+    val testee = ThrowingAssertionChecker(reporter)
+
+    describe("fail") {
+        it("throws an IllegalArgumentException if the given assertion holds") {
+            expect {
+                testee.fail(assertionVerb, 1, DescriptionExpectedAssertion("description", "1", { true }))
+            }.toThrow<IllegalArgumentException> {
+                assert(subject.message).isNotNull().startsWith("the given assertion should fail:")
+            }
+        }
+
+        it("throws an AssertionError with the message formatted by the reporter") {
+            expect {
+                testee.fail(assertionVerb, "1", DescriptionExpectedAssertion("to be", "0", { false }))
+            }.toThrow<AssertionError> {
+                assert(subject.message).isNotNull().toBe(reporterResponse)
+            }
+        }
+    }
+
+    describe("check") {
+
+        it("does not throw an AssertionError if all assertions hold") {
+            testee.check(assertionVerb, 1, listOf(
+                DescriptionExpectedAssertion("a", "a", { true }),
+                DescriptionExpectedAssertion("a", "b", { true })
+            ))
+        }
+
+        it("throws an AssertionError with the message formatted by the reporter if one assertion does not hold") {
+            expect {
+                testee.check(assertionVerb, 1, listOf(
+                    DescriptionExpectedAssertion("a", "a", { true }),
+                    DescriptionExpectedAssertion("a", "b", { false })
+                ))
+            }.toThrow<AssertionError> {
+                assert(subject.message).isNotNull().toBe(reporterResponse + reporterResponse)
+            }
+        }
+    }
+})
