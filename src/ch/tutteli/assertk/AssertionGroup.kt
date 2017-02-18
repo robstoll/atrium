5365fe0c53d779eab506ba1528e87ca868f26d13
Robert Stoll <rstoll@tutteli.ch>
2017-02-18 21:59:50 +0100

    lazy assertion messages

--- src/ch/tutteli/assertk/AssertionGroup.kt
+++ src/ch/tutteli/assertk/AssertionGroup.kt
@@ -1,6 +1,9 @@
 package ch.tutteli.assertk
 
-open class AssertionGroup(override final val name: String, override final val subject: Any, val assertions: List<IAssertion>) : IAssertionGroup {
+open class AssertionGroup(override final val name: String, override final val subject: Any, assertions: List<IAssertion>) : IAssertionGroup {
+    private val lazyMessages: List<Message> by lazy {
+        assertions.flatten { it.messages() }
+    }
 
-    override fun messages() = assertions.flatten { it.messages() }
+    override fun messages() = lazyMessages
 }
--- src/ch/tutteli/assertk/ExceptionThrownAssertion.kt
+++ src/ch/tutteli/assertk/ExceptionThrownAssertion.kt
@@ -1,10 +1,12 @@
 package ch.tutteli.assertk
 
-class ExceptionThrownAssertion<T : Throwable>(private val throwable: Throwable?, private val expectedType: Class<T>) : IAssertion {
-
-
-    override fun messages() = when (throwable) {
-        null -> listOf(Message("exception was", null, false))
-        else -> listOf(Message("exception was", throwable.javaClass, expectedType.isAssignableFrom(throwable.javaClass)))
+class ExceptionThrownAssertion<T : Throwable>(throwable: Throwable?, expectedType: Class<T>) : IAssertion {
+    private val lazyMessages: List<Message> by lazy {
+        when (throwable) {
+            null -> listOf(Message("exception was", null, false))
+            else -> listOf(Message("exception was", throwable.javaClass, expectedType.isAssignableFrom(throwable.javaClass)))
+        }
     }
+
+    override fun messages() = lazyMessages
 }
