9781b3f354a4c8d2552f7faa9e8b7e3f06906938
Robert Stoll <rstoll@tutteli.ch>
2017-02-18 22:49:12 +0100

    IAssertionCheckerDelegateFail
    In preparation for feature/sub-subject assertions I moved the fail methods to an interface which then can be implemented by a FeatureAssertionChecker as well

---
+++ test/ch/tutteli/assertk/IAssertionCheckerDelegateFailSpec.kt
@@ -0,0 +1,35 @@
+package ch.tutteli.assertk
+
+import com.nhaarman.mockito_kotlin.spy
+import com.nhaarman.mockito_kotlin.verify
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+
+class IAssertionCheckerDelegateFailSpec : Spek({
+
+    val assertionVerb = "assertionVerb"
+
+    val testee = spy<IAssertionCheckerDelegateFail>(Dummy())
+    describe("fail") {
+        it("throws an IllegalArgumentException if the given assertion holds") {
+            expect {
+                testee.fail(assertionVerb, 1, DescriptionExpectedAssertion("description", 1, { true }))
+            }.toThrow<IllegalArgumentException> {
+                //TODO check message
+            }
+        }
+
+        it("delegates to check") {
+            val assertion = DescriptionExpectedAssertion("description", 1, { false })
+            testee.fail(assertionVerb, 1, assertion)
+            verify(testee).check(assertionVerb, 1, listOf(assertion))
+        }
+    }
+}){
+    open class Dummy: IAssertionCheckerDelegateFail{
+        override fun check(assertionVerb: String, subject: Any, assertions: List<IAssertion>) {
+            //do nothing
+        }
+
+    }
+}
