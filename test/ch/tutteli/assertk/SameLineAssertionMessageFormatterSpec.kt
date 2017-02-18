3fe4f511611d0d945256cfa837175460830fa52b
Robert Stoll <rstoll@tutteli.ch>
2017-02-18 22:13:10 +0100

    Test for GroupAssertion formatting
    - and for assertions with multiple messages

--- test/ch/tutteli/assertk/SameLineAssertionMessageFormatterSpec.kt
+++ test/ch/tutteli/assertk/SameLineAssertionMessageFormatterSpec.kt
@@ -19,6 +19,21 @@ class SameLineAssertionMessageFormatterSpec : Spek({
             assert(sb.toString()).toBe("bla: bli")
         }
 
+        val separator = System.getProperty("line.separator")!!
+        it("uses the system line separator if there are multiple messages") {
+            testee.format(sb, object : IAssertion {
+                override fun messages() = listOf(Message("a", "b", false), Message("c", "d", false))
+            }, alwaysTrueFilter)
+            assert(sb.toString()).contains("a: b${separator}c: d")
+        }
+
+        it("includes the group name and its representation in case of a ${IAssertionGroup::class.java.name}") {
+            testee.format(sb, AssertionGroup("assert", "subject", listOf(
+                DescriptionExpectedAssertion("bla", "bli", { false }),
+                DescriptionExpectedAssertion("hello", "bye", { false })
+            )), alwaysTrueFilter)
+            assert(sb.toString()).startsWith("assert: subject${separator}bla: bli${separator}hello: bye")
+        }
 
     }
 })
