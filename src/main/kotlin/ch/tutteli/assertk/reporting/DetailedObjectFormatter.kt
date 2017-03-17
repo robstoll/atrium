42b7531d2069e0bd31434c5e7c8486a2d607c751
Robert Stoll <rstoll@tutteli.ch>
2017-03-17 21:51:23 +0100

    object formatting: only string without type
    it should be obvious in reporting if one deals with a quoted CharSequence such as StringBuilder etc. or a quoted String. Hence add the type to the result in addition if it is not a string but another CharSequence

--- src/main/kotlin/ch/tutteli/assertk/reporting/DetailedObjectFormatter.kt
+++ src/main/kotlin/ch/tutteli/assertk/reporting/DetailedObjectFormatter.kt
@@ -5,16 +5,22 @@ class DetailedObjectFormatter : IObjectFormatter {
     override fun format(any: Any?): String = when (any) {
         null -> "null"
         is RawString -> any.string
+        is String -> format(any)
         is CharSequence -> format(any)
         is Class<*> -> format(any)
-        else -> any.toString() + classNameAndIdentity(any)
+        else -> any.toString() + INDENT + classNameAndIdentity(any)
     }
 
-    private fun format(charSequence: CharSequence) = "\"$charSequence\"" + "   " + identityHash(charSequence)
+    private fun format(string: String) = "\"$string\"" + INDENT + identityHash(string)
+    private fun format(charSequence: CharSequence) = "\"$charSequence\"" + INDENT + classNameAndIdentity(charSequence)
     private fun <T> format(clazz: Class<T>) = "${clazz.simpleName} (${clazz.name})"
 
     private fun classNameAndIdentity(any: Any)
-        = "   (" + any::class.java.name + identityHash(any) + ")"
+        = "(${any::class.java.name} ${identityHash(any)})"
 
-    private fun identityHash(any: Any) = "<" + System.identityHashCode(any) + ">"
+    private fun identityHash(any: Any) = "<${System.identityHashCode(any)}>"
+
+    companion object {
+        private const val INDENT: String = "   "
+    }
 }
