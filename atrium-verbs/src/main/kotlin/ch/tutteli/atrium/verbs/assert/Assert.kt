2445fa03cbac9e7cd0eb4b104daeec1eb535e801
Robert Stoll <rstoll@tutteli.ch>
2017-04-01 11:00:04 +0200

    complement assert and assertThat with throwable

--- atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assert/Assert.kt
+++ atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assert/Assert.kt
@@ -3,6 +3,7 @@ package ch.tutteli.atrium.verbs.assert
 import ch.tutteli.atrium.creating.AssertionPlantFactory
 import ch.tutteli.atrium.creating.IAssertionPlant
 import ch.tutteli.atrium.creating.IAssertionPlantNullable
+import ch.tutteli.atrium.creating.ThrowableFluent
 
 fun <T : Any> assert(subject: T): IAssertionPlant<T>
     = AssertionPlantFactory.newCheckImmediately("assert", subject)
@@ -12,3 +13,6 @@ fun <T : Any?> assert(subject: T): IAssertionPlantNullable<T>
 
 inline fun <T : Any> assert(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
     = AssertionPlantFactory.newCheckLazilyAtTheEnd("assert", subject, createAssertions)
+
+fun assert(act: () -> Unit): ThrowableFluent
+    = AssertionPlantFactory.throwableFluent("expect the thrown exception", act)
