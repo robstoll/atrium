238dbce620b480264faee50d1ecac2b6ba018fb0
Robert Stoll <rstoll@tutteli.ch>
2017-04-02 20:15:01 +0200

    rename and split up AssertKSpec
    - rename to NarrowingAssertionsSpec
    - move throwable part to own file ThrowableAssertionsSpec

--- atrium-assertions/src/test/kotlin/ch/tutteli/atrium/NarrowingAssertionsSpec.kt
+++ atrium-assertions/src/test/kotlin/ch/tutteli/atrium/NarrowingAssertionsSpec.kt
@@ -7,7 +7,7 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 
-class AssertKSpec : Spek({
+class NarrowingAssertionsSpec : Spek({
     describe("fun isNotNull") {
 
         checkNarrowingNullableAssertion<Int?>("it throws an AssertionError if the subject is null", { isNotNull ->
@@ -97,26 +97,5 @@ class AssertKSpec : Spek({
         }
     }
 
-    describe("fun `message` (for Throwable)") {
-        checkNarrowingAssertion<Throwable>("it throws an AssertionError if the ${Throwable::message.name} is null", { message ->
-            val throwable = IllegalArgumentException()
-            expect {
-                assert(throwable).message()
-            }.toThrow<AssertionError>()
-        }, { message }, { message {} })
 
-
-        context("it allows to define an assertion for the ${Throwable::message.name} if it is not null") {
-            val throwable = IllegalArgumentException("oh no")
-            checkNarrowingAssertion<Throwable>("it throws an AssertionError if the assertion does not hold", { messageWithCheck ->
-                expect {
-                    assert(throwable).messageWithCheck()
-                }.toThrow<AssertionError>()
-            }, { message.contains("hello") }, { message { contains("hello") } })
-
-            checkNarrowingAssertion<Throwable>("it does not throw an exception if the assertion holds", { messageWithCheck ->
-                assert(throwable).messageWithCheck()
-            }, { message.contains("oh") }, { message { contains("oh") } })
-        }
-    }
 })
---
+++ atrium-assertions/src/test/kotlin/ch/tutteli/atrium/ThrowableAssertionsSpec.kt
@@ -0,0 +1,31 @@
+package ch.tutteli.atrium
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+
+class ThrowableAssertionsSpec : Spek({
+
+    describe("fun `message` (for Throwable)") {
+        checkNarrowingAssertion<Throwable>("it throws an AssertionError if the ${Throwable::message.name} is null", { message ->
+            val throwable = IllegalArgumentException()
+            expect {
+                assert(throwable).message()
+            }.toThrow<AssertionError>()
+        }, { message }, { message {} })
+
+
+        context("it allows to define an assertion for the ${Throwable::message.name} if it is not null") {
+            val throwable = IllegalArgumentException("oh no")
+            checkNarrowingAssertion<Throwable>("it throws an AssertionError if the assertion does not hold", { messageWithCheck ->
+                expect {
+                    assert(throwable).messageWithCheck()
+                }.toThrow<AssertionError>()
+            }, { message.contains("hello") }, { message { contains("hello") } })
+
+            checkNarrowingAssertion<Throwable>("it does not throw an exception if the assertion holds", { messageWithCheck ->
+                assert(throwable).messageWithCheck()
+            }, { message.contains("oh") }, { message { contains("oh") } })
+        }
+    }
+})
