7dbd7fb8769d254b3059bf7a361922fb0ba9e37b
Robert Stoll <rstoll@tutteli.ch>
2017-04-06 22:39:54 +0200

    remove return type from assertion verbs

--- atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assertthat/AssertThat.kt
+++ atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assertthat/AssertThat.kt
@@ -2,19 +2,17 @@ package ch.tutteli.atrium.verbs.assertthat
 
 import ch.tutteli.atrium.AtriumFactory
 import ch.tutteli.atrium.creating.IAssertionPlant
-import ch.tutteli.atrium.creating.IAssertionPlantNullable
-import ch.tutteli.atrium.creating.ThrowableFluent
 import ch.tutteli.atrium.creating.newCheckLazilyAtTheEnd
 import ch.tutteli.atrium.verbs.AtriumReporterSupplier
 
-fun <T : Any> assertThat(subject: T): IAssertionPlant<T>
+fun <T : Any> assertThat(subject: T)
     = AtriumFactory.newCheckImmediately("assert that", subject, AtriumReporterSupplier.REPORTER)
 
-fun <T : Any?> assertThat(subject: T): IAssertionPlantNullable<T>
+fun <T : Any?> assertThat(subject: T)
     = AtriumFactory.newNullable("assert that", subject, AtriumReporterSupplier.REPORTER)
 
 inline fun <T : Any> assertThat(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
     = AtriumFactory.newCheckLazilyAtTheEnd("assert that", subject, AtriumReporterSupplier.REPORTER, createAssertions)
 
-fun assertThat(act: () -> Unit): ThrowableFluent
+fun assertThat(act: () -> Unit)
     = AtriumFactory.newThrowableFluent("assert that the thrown exception", act, AtriumReporterSupplier.REPORTER)
--- atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/expect/Expect.kt
+++ atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/expect/Expect.kt
@@ -2,19 +2,17 @@ package ch.tutteli.atrium.verbs.expect
 
 import ch.tutteli.atrium.AtriumFactory
 import ch.tutteli.atrium.creating.IAssertionPlant
-import ch.tutteli.atrium.creating.IAssertionPlantNullable
-import ch.tutteli.atrium.creating.ThrowableFluent
 import ch.tutteli.atrium.creating.newCheckLazilyAtTheEnd
 import ch.tutteli.atrium.verbs.AtriumReporterSupplier
 
-fun <T : Any> expect(subject: T): IAssertionPlant<T>
+fun <T : Any> expect(subject: T)
     = AtriumFactory.newCheckImmediately("expect", subject, AtriumReporterSupplier.REPORTER)
 
-fun <T : Any?> expect(subject: T): IAssertionPlantNullable<T>
+fun <T : Any?> expect(subject: T)
     = AtriumFactory.newNullable("expect", subject, AtriumReporterSupplier.REPORTER)
 
 inline fun <T : Any> expect(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
     = AtriumFactory.newCheckLazilyAtTheEnd("expect", subject, AtriumReporterSupplier.REPORTER, createAssertions)
 
-fun expect(act: () -> Unit): ThrowableFluent
+fun expect(act: () -> Unit)
     = AtriumFactory.newThrowableFluent("expect the thrown exception", act, AtriumReporterSupplier.REPORTER)
