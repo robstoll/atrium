cf852b4cacf38c4fe9c1ae03661c7f055649abea
Robert Stoll <rstoll@tutteli.ch>
2017-04-14 12:28:40 +0200

    improve atrium-verbs doc

--- atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assert/Assert.kt
+++ atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assert/Assert.kt
@@ -1,18 +1,47 @@
 package ch.tutteli.atrium.verbs.assert
 
 import ch.tutteli.atrium.AtriumFactory
+import ch.tutteli.atrium.assertions.IAssertion
 import ch.tutteli.atrium.creating.IAssertionPlant
+import ch.tutteli.atrium.creating.IAssertionPlantNullable
+import ch.tutteli.atrium.creating.ThrowableFluent
 import ch.tutteli.atrium.creating.newCheckLazilyAtTheEnd
 import ch.tutteli.atrium.verbs.AtriumReporterSupplier
 
+/**
+ * Creates an [IAssertionPlant] for [subject] which immediately evaluates [IAssertion]s.
+ *
+ * @return The newly created plant.
+ *
+ * @see AtriumFactory.newCheckImmediately
+ */
 fun <T : Any> assert(subject: T)
     = AtriumFactory.newCheckImmediately("assert", subject, AtriumReporterSupplier.REPORTER)
 
+/**
+ * Creates an [IAssertionPlantNullable] for [subject].
+ *
+ * @return The newly created plant.
+ *
+ * @see AtriumFactory.newNullable
+ */
 fun <T : Any?> assert(subject: T)
     = AtriumFactory.newNullable("assert", subject, AtriumReporterSupplier.REPORTER)
 
+/**
+ * Creates an [IAssertionPlant] for [subject] which lazily evaluates [IAssertion]s.
+ *
+ * @return The newly created plant.
+ *
+ * @see newCheckLazilyAtTheEnd
+ */
 inline fun <T : Any> assert(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
     = AtriumFactory.newCheckLazilyAtTheEnd("assert", subject, AtriumReporterSupplier.REPORTER, createAssertions)
 
+/**
+ * Creates an [ThrowableFluent] for the given function [act].
+ *
+ * @return The newly created [ThrowableFluent].
+ */
 fun assert(act: () -> Unit)
     = AtriumFactory.newThrowableFluent("assert the thrown exception", act, AtriumReporterSupplier.REPORTER)
--- atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/expect/Expect.kt
+++ atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/expect/Expect.kt
@@ -1,18 +1,47 @@
 package ch.tutteli.atrium.verbs.expect
 
 import ch.tutteli.atrium.AtriumFactory
+import ch.tutteli.atrium.assertions.IAssertion
 import ch.tutteli.atrium.creating.IAssertionPlant
+import ch.tutteli.atrium.creating.IAssertionPlantNullable
+import ch.tutteli.atrium.creating.ThrowableFluent
 import ch.tutteli.atrium.creating.newCheckLazilyAtTheEnd
 import ch.tutteli.atrium.verbs.AtriumReporterSupplier
 
+/**
+ * Creates an [IAssertionPlant] for [subject] which immediately evaluates [IAssertion]s.
+ *
+ * @return The newly created plant.
+ *
+ * @see AtriumFactory.newCheckImmediately
+ */
 fun <T : Any> expect(subject: T)
     = AtriumFactory.newCheckImmediately("expect", subject, AtriumReporterSupplier.REPORTER)
 
+/**
+ * Creates an [IAssertionPlantNullable] for [subject].
+ *
+ * @return The newly created plant.
+ *
+ * @see AtriumFactory.newNullable
+ */
 fun <T : Any?> expect(subject: T)
     = AtriumFactory.newNullable("expect", subject, AtriumReporterSupplier.REPORTER)
 
+/**
+ * Creates an [IAssertionPlant] for [subject] which lazily evaluates [IAssertion]s.
+ *
+ * @return The newly created plant.
+ *
+ * @see newCheckLazilyAtTheEnd
+ */
 inline fun <T : Any> expect(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
     = AtriumFactory.newCheckLazilyAtTheEnd("expect", subject, AtriumReporterSupplier.REPORTER, createAssertions)
 
+/**
+ * Creates an [ThrowableFluent] for the given function [act].
+ *
+ * @return The newly created [ThrowableFluent].
+ */
 fun expect(act: () -> Unit)
     = AtriumFactory.newThrowableFluent("expect the thrown exception", act, AtriumReporterSupplier.REPORTER)
