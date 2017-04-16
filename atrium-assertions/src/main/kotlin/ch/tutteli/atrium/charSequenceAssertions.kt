56f816cf9a33f4e009d6724079931416341611c6
Robert Stoll <rstoll@tutteli.ch>
2017-04-16 22:40:32 +0200

    add fun isNotEmpty

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/charSequenceAssertions.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/charSequenceAssertions.kt
@@ -13,6 +13,16 @@ fun <T : CharSequence> IAssertionPlant<T>.contains(expected: CharSequence)
     = createAndAddAssertion("contains", expected, { subject.contains(expected) })
 
 /**
+ * Makes the assertion that [IAssertionPlant.subject] does not contain the [expected] [CharSequence].
+ *
+ * @return This plant to support a fluent-style API.
+ * @throws AssertionError Might throw an [AssertionError] if the made assertion does not hold.
+ */
+fun <T : CharSequence> IAssertionPlant<T>.containsNot(expected: CharSequence)
+    = createAndAddAssertion("does not contain", expected, { !subject.contains(expected) })
+
+
+/**
  * Makes the assertion that [IAssertionPlant.subject] contains the [expected] [CharSequence]
  * and [otherExpected] [CharSequence]s (if defined).
  *
@@ -26,28 +36,6 @@ fun <T : CharSequence> IAssertionPlant<T>.contains(expected: CharSequence, varar
 }
 
 /**
- * Makes the assertion that [IAssertionPlant.subject] contains [expected]'s [toString] representation
- * and the [toString] representation of the [otherExpected] (if defined).
- *
- * @return This plant to support a fluent-style API.
- * @throws AssertionError Might throw an [AssertionError] if the made assertion does not hold.
- */
-fun <T : CharSequence> IAssertionPlant<T>.contains(expected: Any, vararg otherExpected: Any): IAssertionPlant<T> {
-    val plant = contains(expected.toString())
-    otherExpected.forEach { contains(it.toString()) }
-    return plant
-}
-
-/**
- * Makes the assertion that [IAssertionPlant.subject] does not contain the [expected] [CharSequence].
- *
- * @return This plant to support a fluent-style API.
- * @throws AssertionError Might throw an [AssertionError] if the made assertion does not hold.
- */
-fun <T : CharSequence> IAssertionPlant<T>.containsNot(expected: CharSequence)
-    = createAndAddAssertion("does not contain", expected, { !subject.contains(expected) })
-
-/**
  * Makes the assertion that [IAssertionPlant.subject] does not contain the [expected] [CharSequence]
  * and neither one of the [otherExpected] [CharSequence]s (if defined).
  *
@@ -57,6 +45,20 @@ fun <T : CharSequence> IAssertionPlant<T>.containsNot(expected: CharSequence)
 fun <T : CharSequence> IAssertionPlant<T>.containsNot(expected: CharSequence, vararg otherExpected: CharSequence): IAssertionPlant<T> {
     val plant = containsNot(expected)
     otherExpected.forEach { containsNot(it) }
+    return plant
+}
+
+
+/**
+ * Makes the assertion that [IAssertionPlant.subject] contains [expected]'s [toString] representation
+ * and the [toString] representation of the [otherExpected] (if defined).
+ *
+ * @return This plant to support a fluent-style API.
+ * @throws AssertionError Might throw an [AssertionError] if the made assertion does not hold.
+ */
+fun <T : CharSequence> IAssertionPlant<T>.contains(expected: Any, vararg otherExpected: Any): IAssertionPlant<T> {
+    val plant = contains(expected.toString())
+    otherExpected.forEach { contains(it.toString()) }
     return plant
 }
 
@@ -100,3 +102,12 @@ fun <T : CharSequence> IAssertionPlant<T>.endsWith(expected: CharSequence)
  */
 fun <T : CharSequence> IAssertionPlant<T>.isEmpty()
     = createAndAddAssertion("is", RawString("empty"), { subject.isEmpty() })
+
+/**
+ * Makes the assertion that [IAssertionPlant.subject] [CharSequence].[kotlin.text.isNotEmpty].
+ *
+ * @return This plant to support a fluent-style API.
+ * @throws AssertionError Might throw an [AssertionError] if the made assertion does not hold.
+ */
+fun <T : CharSequence> IAssertionPlant<T>.isNotEmpty()
+    = createAndAddAssertion("is not", RawString("empty"), { subject.isNotEmpty() })
