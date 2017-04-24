522bcc434c290ef62f5831f171642bfd18885a13
Robert Stoll <rstoll@tutteli.ch>
2017-04-24 20:56:38 +0200

    using concrete classes in specs
    - use the constructor of the concrete classes in specs instead of AtriumFactory::new...
    - introduce a testeeFactory in ThrowableFluentSpec
      - fix dependencies -> in case the expected exception is thrown
        - twice the same checkToThrow description
        - immediate and lazy are different, immediate makes 2 calls, lazy only 1

--- atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/creating/AssertionPlantNullableSpec.kt
+++ atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/creating/AssertionPlantNullableSpec.kt
@@ -1,7 +1,6 @@
 package ch.tutteli.atrium.creating
 
 import ch.tutteli.atrium.AssertionVerbFactory
-import ch.tutteli.atrium.AtriumFactory
 
 object AssertionPlantNullableSpec : ch.tutteli.atrium.spec.creating.AssertionPlantNullableSpec(
-        AssertionVerbFactory, AtriumFactory::newNullable)
+        AssertionVerbFactory, ::AssertionPlantNullable)
