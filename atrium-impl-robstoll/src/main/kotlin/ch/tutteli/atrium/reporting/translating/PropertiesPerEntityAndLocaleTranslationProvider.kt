f7097c1465b3b99b31a2d6845f2a790be2663fde
Robert Stoll <rstoll@tutteli.ch>
2017-06-05 20:57:32 +0200

    move PropertiesBasedTranslationProviderSpec to specs
    - rename it to TranslationProviderSpec
    - rewrite it to expect a factory for the assertion verbs
    - remodel the spec, create tests for primary locale, for second alternative and for fallback

--- atrium-impl-robstoll/src/main/kotlin/ch/tutteli/atrium/reporting/translating/PropertiesPerEntityAndLocaleTranslationProvider.kt
+++ atrium-impl-robstoll/src/main/kotlin/ch/tutteli/atrium/reporting/translating/PropertiesPerEntityAndLocaleTranslationProvider.kt
@@ -8,7 +8,7 @@ import java.util.*
  *
  * For instance, the translations for `ch.tutteli.atrium.DescriptionAnyAssertion` and the [Locale] `de_CH` are
  * stored in a properties file named `DescriptionAnyAssertion_de_CH.properties` in the folder `/ch/tutteli/atrium/`
- * (same mechanism as in [ResourceBundle] applies here).
+ * (compatible with the behaviour of [ResourceBundle]).
  *
  * An entry in such a file would look like as follows:
  * `TO_BE = a translation for TO_BE`
@@ -16,9 +16,13 @@ import java.util.*
 class PropertiesPerEntityAndLocaleTranslationProvider : PropertiesBasedTranslationProvider<String>() {
 
     override fun get(translatable: ITranslatable, locale: Locale): String? {
-        val qualifiedName = translatable::class.qualifiedName
-        //using _ here to be compatible with ResourceBundle
-        val key = qualifiedName + "_" + locale.toString()
+        val qualifiedName = translatable::class.qualifiedName!!
+        val key = if (locale != Locale.ROOT) {
+            //using _ here to be compatible with ResourceBundle
+            qualifiedName + "_" + locale.toString()
+        } else {
+            qualifiedName
+        }
         val translations = getOrLoadProperties(key, key, { qualifiedName + ITranslatable.ID_SEPARATOR + it })
         return translations[translatable.id]
     }
--- atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/reporting/translating/PropertiesPerEntityAndLocaleTranslationProviderSpec.kt
+++ atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/reporting/translating/PropertiesPerEntityAndLocaleTranslationProviderSpec.kt
@@ -1,11 +1,14 @@
 package ch.tutteli.atrium.reporting.translating
 
+import ch.tutteli.atrium.AssertionVerbFactory
 import ch.tutteli.atrium.reporting.ReporterBuilder
+import ch.tutteli.atrium.spec.reporting.translating.TranslationProviderSpec
 import java.util.*
 
-object PropertiesPerEntityAndLocaleTranslationProviderSpec : PropertiesBasedTranslationProviderSpec(
+object PropertiesPerEntityAndLocaleTranslationProviderSpec : TranslationProviderSpec(
+    AssertionVerbFactory,
     ReporterBuilder
-        .withTranslations(PropertiesPerEntityAndLocaleTranslationProvider(), Locale("de", "CH", "Sensler"))
+        .withTranslations(PropertiesPerEntityAndLocaleTranslationProvider(), Locale("de", "CH"), Locale("fr"))
         .withDetailedObjectFormatter()
         .withSameLineAssertionMessageFormatter()
         .buildOnlyFailureReporting()
