f9455cc90f44e287f1549e599933b7378e096323
Robert Stoll <rstoll@tutteli.ch>
2017-06-10 23:53:52 +0200

    `its` should delegate to `property`
    - property is the more general term and thus the implementation should be there

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/featureAssertions.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/featureAssertions.kt
@@ -12,20 +12,20 @@ import kotlin.reflect.KProperty0
  * Creates an [IAssertionPlant] which immediately evaluates [IAssertion]s using the given [property] as
  * [subject][IAssertionPlant.subject].
  *
- * Delegates to [its].
- *
  * @return An [IAssertionPlant] for the given [property], using an [AtriumFactory.newFeatureAssertionChecker].
  *
  * @See IAtriumFactory.newCheckImmediately
  */
-fun <T : Any, TProperty : Any> IAssertionPlant<T>.property(property: KProperty0<TProperty>)
-    = its(property)
+fun <T : Any, TProperty : Any> IAssertionPlant<T>.property(property: KProperty0<TProperty>) : IAssertionPlant<TProperty>
+    = AtriumFactory.newCheckImmediately(createCommonFieldsForFeatureFactory(property))
 
 /**
  * Creates an [IAssertionPlant] which lazily evaluates [IAssertion]s using the given [property] as
  * [subject][IAssertionPlant.subject].
  *
- * Delegates to [its], more details are given there.
+ * The given [createAssertions] function is called after the plant has been created. It could create
+ * [IAssertion]s for the given [property] which are lazily evaluated by the newly created [IAssertionPlant]
+ * after the call to [createAssertions] is made.
  *
  * @return An [IAssertionPlant] for the given [property], using an [AtriumFactory.newFeatureAssertionChecker].
  *
@@ -34,38 +34,37 @@ fun <T : Any, TProperty : Any> IAssertionPlant<T>.property(property: KProperty0<
  *
  * @see [IAtriumFactory.newCheckLazily]
  */
-fun <T : Any, TProperty : Any> IAssertionPlant<T>.property(property: KProperty0<TProperty>, createAssertions: IAssertionPlant<TProperty>.() -> Unit)
-    = its(property, createAssertions)
+fun <T : Any, TProperty : Any> IAssertionPlant<T>.property(property: KProperty0<TProperty>, createAssertions: IAssertionPlant<TProperty>.() -> Unit) : IAssertionPlant<TProperty>
+    = AtriumFactory.newCheckLazily(createCommonFieldsForFeatureFactory(property))
+    .createAssertionsAndCheckThem(createAssertions)
 
 
 /**
  * Creates an [IAssertionPlantNullable] using the given [property] as [subject][IAssertionPlantNullable.subject].
  *
- * Delegates to [its].
- *
  * @return An [IAssertionPlant] for the given [property], using an [AtriumFactory.newNullable].
  */
-fun <T : Any, TFeature : Any?> IAssertionPlant<T>.property(property: KProperty0<TFeature>)
-    = its(property)
+fun <T : Any, TProperty : Any?> IAssertionPlant<T>.property(property: KProperty0<TProperty>) : IAssertionPlantNullable<TProperty>
+    = AtriumFactory.newNullable(createCommonFieldsForFeatureFactory(property))
 
 /**
  * Creates an [IAssertionPlant] which immediately evaluates [IAssertion]s using the given [feature] as
  * [subject][IAssertionPlant.subject].
+ *
+ * Delegates to [property].
  *
  * @return An [IAssertionPlant] for the given [feature], using an [AtriumFactory.newFeatureAssertionChecker].
  *
  * @See IAtriumFactory.newCheckImmediately
  */
 fun <T : Any, TFeature : Any> IAssertionPlant<T>.its(feature: KProperty0<TFeature>): IAssertionPlant<TFeature>
-    = AtriumFactory.newCheckImmediately(createCommonFieldsForFeatureFactory(feature))
+    = property(feature)
 
 /**
  * Creates an [IAssertionPlant] which lazily evaluates [IAssertion]s using the given [feature] as
  * [subject][IAssertionPlant.subject].
  *
- * The given [createAssertions] function is called after the plant has been created. It could create
- * [IAssertion]s for the given [feature] which are lazily evaluated by the newly created [IAssertionPlant]
- * after the call to [createAssertions] is made.
+ * Delegates to [property], more details are given there.
  *
  * @return An [IAssertionPlant] for the given [feature], using an [AtriumFactory.newFeatureAssertionChecker].
  *
@@ -74,17 +73,19 @@ fun <T : Any, TFeature : Any> IAssertionPlant<T>.its(feature: KProperty0<TFeatur
  *
  * @see [IAtriumFactory.newCheckLazily]
  */
-fun <T : Any, TFeature : Any> IAssertionPlant<T>.its(feature: KProperty0<TFeature>, createAssertions: IAssertionPlant<TFeature>.() -> Unit): IAssertionPlant<TFeature>
-    = AtriumFactory.newCheckLazily(createCommonFieldsForFeatureFactory(feature))
-    .createAssertionsAndCheckThem(createAssertions)
+fun <T : Any, TFeature : Any> IAssertionPlant<T>.its(feature: KProperty0<TFeature>, createAssertions: IAssertionPlant<TFeature>.() -> Unit)
+    = property(feature, createAssertions)
 
 /**
  * Creates an [IAssertionPlantNullable] using the given [feature] as [subject][IAssertionPlantNullable.subject].
  *
+ * Delegates to [property].
+ *
  * @return An [IAssertionPlant] for the given [feature], using an [AtriumFactory.newNullable].
  */
-fun <T : Any, TFeature : Any?> IAssertionPlant<T>.its(feature: KProperty0<TFeature>): IAssertionPlantNullable<TFeature>
-    = AtriumFactory.newNullable(createCommonFieldsForFeatureFactory(feature))
+fun <T : Any, TFeature : Any?> IAssertionPlant<T>.its(feature: KProperty0<TFeature>)
+    = property(feature)
 
 private fun <T : Any, TFeature : Any?> IAssertionPlant<T>.createCommonFieldsForFeatureFactory(feature: KProperty0<TFeature>)
     = IAssertionPlantWithCommonFields.CommonFields(Untranslatable(feature.name), feature.get(), AtriumFactory.newFeatureAssertionChecker(this))
+
