a2eb5e1b4c6e9536a4959d5e35a0ffa468f83966
Robert Stoll <rstoll@tutteli.ch>
2017-06-11 20:56:05 +0200

    update README with property and returnValueOf
    - restructure Examples, split it up in sub sections
    - rewrite `Write own Assertion Functions` example, use ITranslatable

--- atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/atriumVerbs.kt
+++ atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/atriumVerbs.kt
@@ -3,6 +3,8 @@ package ch.tutteli.atrium
 import ch.tutteli.atrium.AssertionVerb.ASSERT
 import ch.tutteli.atrium.AssertionVerb.EXPECT_THROWN
 import ch.tutteli.atrium.creating.IAssertionPlant
+import ch.tutteli.atrium.reporting.IObjectFormatter
+import ch.tutteli.atrium.reporting.IReporter
 import ch.tutteli.atrium.reporting.ReporterBuilder
 import ch.tutteli.atrium.reporting.translating.ISimpleTranslatable
 import ch.tutteli.atrium.spec.IAssertionVerbFactory
@@ -34,12 +36,19 @@ internal object AtriumReporterSupplier {
     }
 }
 
+/**
+ * You need to add atrium-spec to your dependencies in order to be able to reuse the VerbSpec.
+ */
 internal object VerbSpec : ch.tutteli.atrium.spec.verbs.VerbSpec(
     "assert" to { subject -> assert(subject) },
     "assert" to { subject, createAssertions -> assert(subject, createAssertions) },
     "assert" to { subject -> assert(subject) },
     "expect" to { act -> expect { act() } })
 
+/**
+ * Only required if you implement a custom component (for instance an own [IReporter], [IObjectFormatter] etc.)
+ * and you want to reuse a specification from atrium-spec to test your custom component against it.
+ */
 internal object AssertionVerbFactory : IAssertionVerbFactory {
     override fun <T : Any> checkImmediately(subject: T) = assert(subject)
     override fun <T : Any> checkLazily(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
