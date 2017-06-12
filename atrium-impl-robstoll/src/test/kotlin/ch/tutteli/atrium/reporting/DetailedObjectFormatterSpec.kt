7119bb8542ae40a3df0f1bcc242adae16cbb1246
Robert Stoll <rstoll@tutteli.ch>
2017-06-12 22:41:18 +0200

    write Spec for TranslatableRawString
    - define how a DetailedObjectFormatter has to format a TranslatableRawString

--- atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/reporting/DetailedObjectFormatterSpec.kt
+++ atrium-impl-robstoll/src/test/kotlin/ch/tutteli/atrium/reporting/DetailedObjectFormatterSpec.kt
@@ -1,10 +1,15 @@
 package ch.tutteli.atrium.reporting
 
+import ch.tutteli.atrium.AssertionVerb
 import ch.tutteli.atrium.assert
 import ch.tutteli.atrium.creating.IAssertionPlant
 import ch.tutteli.atrium.reporting.DetailedObjectFormatter.Companion.INDENT
+import ch.tutteli.atrium.reporting.translating.ITranslator
+import ch.tutteli.atrium.reporting.translating.TranslatableRawString
 import ch.tutteli.atrium.reporting.translating.UsingDefaultTranslator
 import ch.tutteli.atrium.toBe
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
@@ -22,28 +27,40 @@ object DetailedObjectFormatterSpec : Spek({
             }
         }
 
-        on("a ${RawString::class.java.simpleName}") {
+        on("a ${RawString::class.simpleName}") {
             val result = testee.format(RawString("hello"))
             it("returns the containing string") {
                 assert(result).toBe("hello")
             }
         }
 
-        on("class type") {
-            val result = testee.format(DetailedObjectFormatterSpec::class.java)
-            it("returns its simpleName and name in parenthesis") {
-                val clazz = DetailedObjectFormatterSpec::class.java
-                assert(result).toBe("${clazz.simpleName} (${clazz.name})")
+        on("a ${TranslatableRawString::class.simpleName}"){
+            val translation = "es gilt"
+            val translator = mock<ITranslator>{
+                on{translate(AssertionVerb.ASSERT)} doReturn translation
+            }
+            val testeeWithMockedTranslation = DetailedObjectFormatter(translator)
+            val result = testeeWithMockedTranslation.format(TranslatableRawString(AssertionVerb.ASSERT))
+            it("returns the translated string"){
+                assert(result).toBe(translation)
             }
         }
 
-        on("a ${String::class.java.simpleName}") {
-            it("returns two quotes including identity hash if empty ${String::class.java.simpleName}") {
+        on("class type") {
+            val result = testee.format(DetailedObjectFormatterSpec::class.java)
+            it("returns its simpleName and name in parenthesis") {
+                val clazz = DetailedObjectFormatterSpec::class
+                assert(result).toBe("${clazz.simpleName} (${clazz.qualifiedName})")
+            }
+        }
+
+        on("a ${String::class.simpleName}") {
+            it("returns two quotes including identity hash if empty ${String::class.simpleName}") {
                 val string = ""
                 val result = testee.format(string)
                 assert(result).toBe("\"\"" + INDENT + "<${System.identityHashCode(string)}>")
             }
-            it("returns ${String::class.java.simpleName} in quotes including identity hash") {
+            it("returns ${String::class.simpleName} in quotes including identity hash") {
                 val string = "atrium"
                 val result = testee.format(string)
                 assert(result).toBe("\"$string\"" + INDENT + "<${System.identityHashCode(string)}>")
@@ -52,18 +69,18 @@ object DetailedObjectFormatterSpec : Spek({
 
         val typeNameAndHash = "including type name and identity hash"
 
-        on("a ${CharSequence::class.java.simpleName} besides ${String::class.java.simpleName}") {
-            it("returns two quotes $typeNameAndHash if empty ${CharSequence::class.java.simpleName}") {
+        on("a ${CharSequence::class.simpleName} besides ${String::class.simpleName}") {
+            it("returns two quotes $typeNameAndHash if empty ${CharSequence::class.simpleName}") {
                 val value = StringBuilder("")
                 val result = testee.format(value)
                 assert(result).toBe("\"\"" + INDENT
-                    + "(${value::class.java.name} <${System.identityHashCode(value)}>)")
+                    + "(${value::class.qualifiedName} <${System.identityHashCode(value)}>)")
             }
-            it("returns ${CharSequence::class.java.simpleName} in quotes $typeNameAndHash") {
+            it("returns ${CharSequence::class.simpleName} in quotes $typeNameAndHash") {
                 val value = StringBuilder("atrium")
                 val result = testee.format(value)
                 assert(result).toBe("\"$value\"" + INDENT
-                    + "(${value::class.java.name} <${System.identityHashCode(value)}>)")
+                    + "(${value::class.qualifiedName} <${System.identityHashCode(value)}>)")
             }
         }
 
