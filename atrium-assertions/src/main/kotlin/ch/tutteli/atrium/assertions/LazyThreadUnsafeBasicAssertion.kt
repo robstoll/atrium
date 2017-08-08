5d09be5c7c0e3596f3677fbe66d2d66a67a29d22
Robert Stoll <rstoll@tutteli.ch>
2017-08-08 23:55:50 +0200

    fix Lazy...BasicAssertion's eager properties
    there is no sense of having something lazy if the properties are
    implemented eagerly and thus are called during creation of the object
    
    - use `get()` instead of `=` for the properties
    - move LazyThreadUnsafeBasicAssertion to atrium-assertions, should not
      have been in API as we can see. It was not trivial enough
    - write a regression test

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/LazyThreadUnsafeBasicAssertion.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/LazyThreadUnsafeBasicAssertion.kt
@@ -3,17 +3,15 @@ package ch.tutteli.atrium.assertions
 /**
  * Represents an assertion which is evaluated lazily where the lazy loading is not thread safe.
  *
- * @property assertionCreator The factory function which is used for lazy loading.
- *
  * @constructor Represents an assertion which is evaluated lazily where the lazy loading is not thread safe.
  * @param assertionCreator The factory function which is used for lazy loading.
  */
-class LazyThreadUnsafeBasicAssertion(private val assertionCreator: () -> IBasicAssertion) : IBasicAssertion {
+class LazyThreadUnsafeBasicAssertion(assertionCreator: () -> IBasicAssertion) : IBasicAssertion {
     private val basicAssertion by lazy(LazyThreadSafetyMode.NONE) {
         assertionCreator()
     }
 
-    override val expected = basicAssertion.expected
-    override val description = basicAssertion.description
+    override val expected get() = basicAssertion.expected
+    override val description get() = basicAssertion.description
     override fun holds() = basicAssertion.holds()
 }
---
+++ atrium-assertions/src/test/kotlin/ch/tutteli/atrium/assertions/LazyThreadUnsafeBasicAssertionSpec.kt
@@ -0,0 +1,55 @@
+package ch.tutteli.atrium.assertions
+
+import ch.tutteli.atrium.assert
+import ch.tutteli.atrium.reporting.translating.Untranslatable
+import ch.tutteli.atrium.toBe
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+object LazyThreadUnsafeBasicAssertionSpec : Spek({
+
+    describe("creating it") {
+        var count: Int = 0
+        val testee = LazyThreadUnsafeBasicAssertion {
+            ++count
+            BasicAssertion(Untranslatable("a"), 2, false)
+        }
+        test("does not evaluate anything") {
+            assert(count).toBe(0)
+        }
+        test("adding it to a list does not evaluate anything") {
+            listOf(testee)
+            assert(count).toBe(0)
+        }
+        on("invoking ${testee::holds.name}") {
+            val resultHolds = testee.holds()
+
+            it("evaluates it") {
+                assert(count).toBe(1)
+            }
+
+            it("returns holds() of the underlying ${BasicAssertion::class.simpleName}") {
+                assert(resultHolds).toBe(false)
+            }
+        }
+
+        on("invoking ${testee::holds.name} and then ${testee::expected.name}") {
+            val resultHolds = testee.holds()
+            val resultExpected = testee.expected
+
+            it("evaluates it only once") {
+                assert(count).toBe(1)
+            }
+
+            it("returns holds() of the underlying ${BasicAssertion::class.simpleName}") {
+                assert(resultHolds).toBe(false)
+            }
+
+            it("returns expected of the underlying ${BasicAssertion::class.simpleName}") {
+                assert(resultExpected).toBe(2)
+            }
+        }
+    }
+})
