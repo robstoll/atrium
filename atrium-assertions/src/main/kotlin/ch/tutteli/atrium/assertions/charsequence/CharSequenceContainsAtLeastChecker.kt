639b9a64b0c18e8f58ecc489b77e335dbcf62afd
Robert Stoll <rstoll@tutteli.ch>
2017-08-08 20:41:22 +0200

    simplify IChecker, remove type parameter
    I doubt it is needed in an extension and if so we can re-add it at
    that point

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/charsequence/CharSequenceContainsAtLeastChecker.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/charsequence/CharSequenceContainsAtLeastChecker.kt
@@ -4,11 +4,11 @@ import ch.tutteli.atrium.assertions.BasicAssertion
 import ch.tutteli.atrium.assertions.DescriptionCharSequenceAssertion
 import ch.tutteli.atrium.assertions.IAssertion
 
-class CharSequenceContainsAtLeastChecker<T : CharSequence>(
+class CharSequenceContainsAtLeastChecker(
     times: Int,
     nameContainsNotFun: String,
     nameAtLeastFun: String
-) : CharSequenceContainsChecker<T>(times, nameContainsNotFun, nameAtLeastFun) {
+) : CharSequenceContainsChecker(times, nameContainsNotFun, nameAtLeastFun) {
 
     override fun createAssertion(foundNumberOfTimes: Int): IAssertion
         = BasicAssertion(DescriptionCharSequenceAssertion.AT_LEAST, times, foundNumberOfTimes >= times)
