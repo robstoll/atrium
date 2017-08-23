ea58655e070b39248e978f3e370385d5e407b6d3
Robert Stoll <rstoll@tutteli.ch>
2017-08-23 21:58:32 +0200

    move Char...CheckerBuilder::addAssertion to assertions
    The extension function can be implemented directly in
    CharSequenceContainsCheckerBuilder

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/charsequence/contains/builders/CharSequenceContainsCheckerBuilder.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/charsequence/contains/builders/CharSequenceContainsCheckerBuilder.kt
@@ -1,11 +1,20 @@
 package ch.tutteli.atrium.assertions.charsequence.contains.builders
 
 
+import ch.tutteli.atrium.assertions.charsequence.contains.CharSequenceContainsAssertionCreator
 import ch.tutteli.atrium.assertions.charsequence.contains.CharSequenceContainsAssertionCreator.IChecker
 import ch.tutteli.atrium.assertions.charsequence.contains.CharSequenceContainsAssertionCreator.IDecorator
+import ch.tutteli.atrium.assertions.charsequence.contains.CharSequenceContainsAssertionCreator.ISearcher
+import ch.tutteli.atrium.creating.IAssertionPlant
 
 abstract class CharSequenceContainsCheckerBuilder<T : CharSequence, D: IDecorator>(
     val containsBuilder: CharSequenceContainsBuilder<T, D>
 ) {
     abstract val checkers: List<IChecker>
+
+    fun addAssertion(searcher: ISearcher<D>, expected: Any, otherExpected: Array<out Any>): IAssertionPlant<T> {
+        val assertionGroup = CharSequenceContainsAssertionCreator<T, D>(containsBuilder.decorator, searcher, checkers)
+            .create(containsBuilder.plant, expected, *otherExpected)
+        return containsBuilder.plant.addAssertion(assertionGroup)
+    }
 }
