6be0caaf067a5b20548cee5af512e5088ee6a688
Robert Stoll <rstoll@tutteli.ch>
2017-08-26 10:53:30 +0200

    simplify CharSequenceContainsSpecBase, use *

--- atrium-assertions-code-completion-de_CH/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/CharSequenceContainsSpecBase.kt
+++ atrium-assertions-code-completion-de_CH/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/CharSequenceContainsSpecBase.kt
@@ -1,7 +1,6 @@
 package ch.tutteli.atrium.api.cc.de_CH
 
 import ch.tutteli.atrium.api.cc.de_CH.assertions.charsequence.contains.builders.CharSequenceContainsAtLeastCheckerBuilder
-import ch.tutteli.atrium.assertions.charsequence.contains.CharSequenceContainsAssertionCreator
 import ch.tutteli.atrium.assertions.charsequence.contains.builders.CharSequenceContainsBuilder
 import ch.tutteli.atrium.assertions.charsequence.contains.builders.CharSequenceContainsCheckerBuilder
 import ch.tutteli.atrium.assertions.charsequence.contains.decorators.CharSequenceContainsNoOpDecorator
@@ -9,14 +8,14 @@ import ch.tutteli.atrium.creating.IAssertionPlant
 import kotlin.reflect.KProperty
 
 abstract class CharSequenceContainsSpecBase {
-    private val containsProp: KProperty<CharSequenceContainsBuilder<String, CharSequenceContainsNoOpDecorator>> = IAssertionPlant<String>::enthaelt
+    private val containsProp: KProperty<*> = IAssertionPlant<String>::enthaelt
     protected val contains = containsProp.name
     protected val containsNot = IAssertionPlant<String>::enthaeltNicht.name
-    protected val atLeast = CharSequenceContainsBuilder<String, CharSequenceContainsAssertionCreator.IDecorator>::zumindest.name
-    protected val butAtMost = CharSequenceContainsAtLeastCheckerBuilder<String, CharSequenceContainsAssertionCreator.IDecorator>::aberHoechstens.name
-    protected val exactly = CharSequenceContainsBuilder<String, CharSequenceContainsAssertionCreator.IDecorator>::genau.name
-    protected val atMost = CharSequenceContainsBuilder<String, CharSequenceContainsAssertionCreator.IDecorator>::hoechstens.name
-    protected val notOrAtMost = CharSequenceContainsBuilder<String, CharSequenceContainsAssertionCreator.IDecorator>::nichtOderHoechstens.name
-    protected val regex = CharSequenceContainsCheckerBuilder<String, CharSequenceContainsNoOpDecorator>::regex.name
-    protected val ignoringCase = CharSequenceContainsBuilder<String, CharSequenceContainsNoOpDecorator>::ignoriereGrossKleinschreibung.name
+    protected val atLeast = CharSequenceContainsBuilder<*, *>::zumindest.name
+    protected val butAtMost = CharSequenceContainsAtLeastCheckerBuilder<*, *>::aberHoechstens.name
+    protected val exactly = CharSequenceContainsBuilder<*, *>::genau.name
+    protected val atMost = CharSequenceContainsBuilder<*, *>::hoechstens.name
+    protected val notOrAtMost = CharSequenceContainsBuilder<*, *>::nichtOderHoechstens.name
+    protected val regex = CharSequenceContainsCheckerBuilder<*, CharSequenceContainsNoOpDecorator>::regex.name
+    protected val ignoringCase = CharSequenceContainsBuilder<*, CharSequenceContainsNoOpDecorator>::ignoriereGrossKleinschreibung.name
 }
