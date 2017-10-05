75dea70083297688b3b78aa888755eb0efc5bf0c
Robert Stoll <rstoll@tutteli.ch>
2017-10-05 21:34:33 +0200

    remove ErrorMsg, integrate into AtriumFactory

--- atrium-api-late-binding/src/main/kotlin/ch/tutteli/atrium/ErrorMsg.kt
+++
@@ -1,6 +0,0 @@
-package ch.tutteli.atrium
-
-internal object ErrorMsg {
-    const val ERROR_MSG = "The atrium-api-late-binding should only be used as a compileOnly dependency, " +
-        "meaning as a substitute for a real implementation"
-}
