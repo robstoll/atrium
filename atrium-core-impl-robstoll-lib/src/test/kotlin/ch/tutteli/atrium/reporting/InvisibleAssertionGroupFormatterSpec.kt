239cfa8a7ae0bfecc52def8729a8dd861e2baf44
Robert Stoll <rstoll@tutteli.ch>
2017-10-09 12:21:59 +0200

    apply AssertionFormatterSpec to formatters

--- atrium-core-impl-robstoll-lib/src/test/kotlin/ch/tutteli/atrium/reporting/InvisibleAssertionGroupFormatterSpec.kt
+++ atrium-core-impl-robstoll-lib/src/test/kotlin/ch/tutteli/atrium/reporting/InvisibleAssertionGroupFormatterSpec.kt
@@ -1,7 +1,18 @@
 package ch.tutteli.atrium.reporting
 
 import ch.tutteli.atrium.AssertionVerbFactory
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.include
 
-object InvisibleAssertionGroupFormatterSpec : ch.tutteli.atrium.spec.reporting.InvisibleAssertionGroupFormatterSpec(
-    AssertionVerbFactory, ::InvisibleAssertionGroupFormatter
-)
+object InvisibleAssertionGroupFormatterSpec : Spek({
+    include(AtriumsInvisibleAssertionGroupFormatterSpecSpec)
+    include(AtriumsAssertionFormatterSpec)
+}) {
+    object AtriumsInvisibleAssertionGroupFormatterSpecSpec : ch.tutteli.atrium.spec.reporting.InvisibleAssertionGroupFormatterSpec(
+        AssertionVerbFactory, ::InvisibleAssertionGroupFormatter, "[Atrium's InvisibleGroup...Spec] ")
+
+    object AtriumsAssertionFormatterSpec : ch.tutteli.atrium.spec.reporting.AssertionFormatterSpec(
+        AssertionVerbFactory,
+        { assertionFormatterController, _, _ -> InvisibleAssertionGroupFormatter(assertionFormatterController) },
+        "[Atrium's AssertionFormatterSpec] ")
+}
--- atrium-core-impl-robstoll-lib/src/test/kotlin/ch/tutteli/atrium/reporting/TextFeatureAssertionGroupFormatterSpec.kt
+++ atrium-core-impl-robstoll-lib/src/test/kotlin/ch/tutteli/atrium/reporting/TextFeatureAssertionGroupFormatterSpec.kt
@@ -1,10 +1,27 @@
 package ch.tutteli.atrium.reporting
 
 import ch.tutteli.atrium.AssertionVerbFactory
+import ch.tutteli.atrium.reporting.translating.ITranslator
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.include
 
-object TextFeatureAssertionGroupFormatterSpec : ch.tutteli.atrium.spec.reporting.TextFeatureAssertionGroupFormatterSpec(
-    AssertionVerbFactory,
-    { arrow, featureBulletPoint, controller, objectFormatter, translator ->
-        TextFeatureAssertionGroupFormatter(arrow, featureBulletPoint, controller, TextSameLineAssertionPairFormatter(objectFormatter, translator))
+class TextFeatureAssertionGroupFormatterSpec : Spek({
+    include(AtriumsTextFeatureAssertionGroupFormatterSpec)
+    include(AtriumsAssertionFormatterSpec)
+}) {
+    object AtriumsTextFeatureAssertionGroupFormatterSpec : ch.tutteli.atrium.spec.reporting.TextFeatureAssertionGroupFormatterSpec(
+        AssertionVerbFactory, factory(), "[Atrium's TextFeature...Spec] ")
+
+    object AtriumsAssertionFormatterSpec : ch.tutteli.atrium.spec.reporting.AssertionFormatterSpec(
+        AssertionVerbFactory, factoryWithArrow(), "[Atrium's AssertionFormatterSpec] ")
+
+    companion object {
+        internal fun factory() = { arrow: String, featureBulletPoint: String, controller: IAssertionFormatterController, objectFormatter: IObjectFormatter, translator: ITranslator ->
+            TextFeatureAssertionGroupFormatter(arrow, featureBulletPoint, controller, TextSameLineAssertionPairFormatter(objectFormatter, translator))
+        }
+
+        internal fun factoryWithArrow() = { assertionFormatterController: IAssertionFormatterController, objectFormatter: IObjectFormatter, translator: ITranslator ->
+            factory()("=>", "▪", assertionFormatterController, objectFormatter, translator)
+        }
     }
-)
+}
