c813f864b13b69eddcd27102f7f2d41d1dde5829
Robert Stoll <rstoll@tutteli.ch>
2017-10-09 12:21:59 +0200

    apply AssertionFormatterSpec to formatters

--- atrium-core-impl-robstoll-lib/src/test/kotlin/ch/tutteli/atrium/reporting/TextListAssertionGroupFormatterSpec.kt
+++ atrium-core-impl-robstoll-lib/src/test/kotlin/ch/tutteli/atrium/reporting/TextListAssertionGroupFormatterSpec.kt
@@ -1,10 +1,28 @@
 package ch.tutteli.atrium.reporting
 
 import ch.tutteli.atrium.AssertionVerbFactory
+import ch.tutteli.atrium.reporting.translating.ITranslator
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.include
 
-object TextListAssertionGroupFormatterSpec : ch.tutteli.atrium.spec.reporting.TextListAssertionGroupFormatterSpec(
-    AssertionVerbFactory,
-    { bulletPoint, controller, objectFormatter, translator ->
-        TextListAssertionGroupFormatter(bulletPoint, controller, TextSameLineAssertionPairFormatter(objectFormatter, translator))
+class TextListAssertionGroupFormatterSpec : Spek({
+    include(AtriumsTextFallbackAssertionFormatterSpec)
+    include(AtriumsAssertionFormatterSpec)
+}) {
+    object AtriumsTextFallbackAssertionFormatterSpec : ch.tutteli.atrium.spec.reporting.TextListAssertionGroupFormatterSpec(
+        AssertionVerbFactory, factory(), "[Atrium's TextList...Spec] ")
+
+    object AtriumsAssertionFormatterSpec : ch.tutteli.atrium.spec.reporting.AssertionFormatterSpec(
+        AssertionVerbFactory, factoryWithBullet(), "[Atrium's AssertionFormatterSpec] "
+    )
+
+    companion object {
+        internal fun factory() = { bulletPoint: String, assertionFormatterController: IAssertionFormatterController, objectFormatter: IObjectFormatter, translator: ITranslator ->
+            TextListAssertionGroupFormatter(bulletPoint, assertionFormatterController, TextSameLineAssertionPairFormatter(objectFormatter, translator))
+        }
+
+        internal fun factoryWithBullet() = { assertionFormatterController: IAssertionFormatterController, objectFormatter: IObjectFormatter, translator: ITranslator ->
+            factory()("▪", assertionFormatterController, objectFormatter, translator)
+        }
     }
-)
+}
