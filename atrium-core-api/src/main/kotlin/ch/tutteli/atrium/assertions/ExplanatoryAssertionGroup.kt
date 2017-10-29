475dadfb0ccb3e9623efae34317a477342988640
Robert Stoll <rstoll@tutteli.ch>
2017-10-29 11:24:46 +0100

    simplify creation of explanatory groups
    - introduce an ExplanatoryAssertionGroup which has type
      IExplanatoryAssertionGroupType and returns always true for holds
    - add factory method createWithExplanatoryAssertionGroup to
      IndentAssertionGroup
    - extend OnlyFailureReporterSpec with ExplanatoryAssertionGroup

---
+++ atrium-core-api/src/main/kotlin/ch/tutteli/atrium/assertions/ExplanatoryAssertionGroup.kt
@@ -0,0 +1,34 @@
+package ch.tutteli.atrium.assertions
+
+import ch.tutteli.atrium.reporting.IReporter
+import ch.tutteli.atrium.reporting.translating.ITranslatable
+
+/**
+ * Represents an [IAssertionGroup] with an [ExplanatoryAssertionGroupType], which means an [IReporter] should not
+ * show whether the [assertions] hold or not -- moreover [holds] always returns `true`.
+ *
+ * @constructor Represents an [IAssertionGroup] with an [ExplanatoryAssertionGroupType], which means an [IReporter]
+ *              should not show whether the [assertions] hold or not -- moreover [holds] always returns `true`.
+ * @param name The name of the group.
+ * @param subject The subject for which the [assertions] are defined.
+ * @param assertions The assertions of this group.
+ */
+class ExplanatoryAssertionGroup(
+    override val name: ITranslatable,
+    override val subject: Any,
+    override val assertions: List<IAssertion>) : IAssertionGroup {
+
+    /**
+     * [ExplanatoryAssertionGroupType]
+     */
+    override val type: IAssertionGroupType = ExplanatoryAssertionGroupType
+
+    override fun holds() = true
+
+    /**
+     * @suppress
+     */
+    override fun toString(): String {
+        return ExplanatoryAssertionGroup::class.simpleName + " " + assertions
+    }
+}
--- atrium-spec/src/main/kotlin/ch/tutteli/atrium/spec/reporting/OnlyFailureReporterSpec.kt
+++ atrium-spec/src/main/kotlin/ch/tutteli/atrium/spec/reporting/OnlyFailureReporterSpec.kt
@@ -68,20 +68,32 @@ abstract class OnlyFailureReporterSpec(
             }
         }
 
-        context("${IAssertionGroup::class.simpleName} is of type ${IExplanatoryAssertionGroupType::class.simpleName}") {
-            it("appends something if the assertion group holds") {
-                val explanatoryAssertionGroup = AssertionGroup(ExplanatoryAssertionGroupType, AssertionVerb.VERB, 1, listOf(assertion))
-                testee.format(explanatoryAssertionGroup, sb)
-                verbs.checkImmediately(sb).isNotEmpty()
-            }
+        val failingAssertion = object : IAssertion {
+            override fun holds() = false
+        }
 
-            it("appends something if the assertion group does not hold") {
-                val failingAssertion = object : IAssertion {
-                    override fun holds() = false
+        listOf(
+            Triple(
+                "${IAssertionGroup::class.simpleName} is of type ${IExplanatoryAssertionGroupType::class.simpleName}",
+                AssertionGroup(ExplanatoryAssertionGroupType, AssertionVerb.VERB, 1, listOf(assertion)),
+                AssertionGroup(ExplanatoryAssertionGroupType, AssertionVerb.VERB, 1, listOf(failingAssertion))
+            ),
+            Triple(
+                "${ExplanatoryAssertionGroup::class.simpleName}",
+                ExplanatoryAssertionGroup(AssertionVerb.VERB, 1, listOf(assertion)),
+                ExplanatoryAssertionGroup(AssertionVerb.VERB, 1, listOf(failingAssertion))
+            )
+        ).forEach { (description, holdingGroup, failingGroup) ->
+            context(description) {
+                it("appends something if the assertion group holds") {
+                    testee.format(holdingGroup, sb)
+                    verbs.checkImmediately(sb).isNotEmpty()
                 }
-                val explanatoryAssertionGroup = AssertionGroup(ExplanatoryAssertionGroupType, AssertionVerb.VERB, 1, listOf(failingAssertion))
-                testee.format(explanatoryAssertionGroup, sb)
-                verbs.checkImmediately(sb).isNotEmpty()
+
+                it("appends something if the assertion group does not hold") {
+                    testee.format(failingGroup, sb)
+                    verbs.checkImmediately(sb).isNotEmpty()
+                }
             }
         }
 
