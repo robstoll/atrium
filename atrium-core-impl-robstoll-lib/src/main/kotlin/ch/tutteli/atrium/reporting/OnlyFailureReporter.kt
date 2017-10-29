bb456be80018bcdc491f3c935a10bb39c1093f6d
Robert Stoll <rstoll@tutteli.ch>
2017-10-29 11:16:18 +0100

    OnlyFailureReporter has to report IExplanatoryAssertionGroups
    It does not matter whether an IAssertionGroup of type
    IExplanatoryAssertionGroups holds or not, it should always be displayed
    
    - change OnlyFailureReporter::assertionFilter accordingly
    - extend OnlyFailureReporterSpec accordingly
      - also extend it with an anonymous object of IBasicAssertion and
        an object of AssertionGroup

--- atrium-core-impl-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/reporting/OnlyFailureReporter.kt
+++ atrium-core-impl-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/reporting/OnlyFailureReporter.kt
@@ -1,6 +1,8 @@
 package ch.tutteli.atrium.reporting
 
 import ch.tutteli.atrium.assertions.IAssertion
+import ch.tutteli.atrium.assertions.IAssertionGroup
+import ch.tutteli.atrium.assertions.IExplanatoryAssertionGroupType
 
 /**
  * An [IReporter] which reports only failing assertions.
@@ -19,5 +21,9 @@ class OnlyFailureReporter(private val assertionFormatterFacade: IAssertionFormat
     override fun format(assertion: IAssertion, sb: StringBuilder)
         = assertionFormatterFacade.format(assertion, sb, this::assertionFilter)
 
-    private fun assertionFilter(assertion: IAssertion) = !assertion.holds()
+    private fun assertionFilter(assertion: IAssertion): Boolean {
+        return (assertion is IAssertionGroup && assertion.type is IExplanatoryAssertionGroupType)
+            || !assertion.holds()
+    }
+
 }
--- atrium-spec/src/main/kotlin/ch/tutteli/atrium/spec/reporting/OnlyFailureReporterSpec.kt
+++ atrium-spec/src/main/kotlin/ch/tutteli/atrium/spec/reporting/OnlyFailureReporterSpec.kt
@@ -2,6 +2,7 @@ package ch.tutteli.atrium.spec.reporting
 
 import ch.tutteli.atrium.AtriumFactory
 import ch.tutteli.atrium.api.cc.en_UK.isEmpty
+import ch.tutteli.atrium.api.cc.en_UK.isNotEmpty
 import ch.tutteli.atrium.assertions.*
 import ch.tutteli.atrium.assertions.DescriptionAnyAssertion.TO_BE
 import ch.tutteli.atrium.reporting.IAssertionFormatterFacade
@@ -40,23 +41,47 @@ abstract class OnlyFailureReporterSpec(
             override fun holds() = true
         }
         val basicAssertion = BasicAssertion(TO_BE, 0, true)
-        val assertionGroup = object : IAssertionGroup {
+        val basicAssertionAnonymous = object : IBasicAssertion {
+            override val expected = 1
+            override val description = AssertionVerb.VERB
+            override fun holds() = true
+        }
+
+        val assertionGroupAnonymous = object : IAssertionGroup {
             override val type = RootAssertionGroupType
             override val name = AssertionVerb.VERB
             override val subject = 0
-            override val assertions = listOf(assertion, basicAssertion)
+            override val assertions = listOf(assertion, basicAssertion, basicAssertionAnonymous)
         }
+        val assertionGroup = AssertionGroup(RootAssertionGroupType, AssertionVerb.VERB, 1, listOf(assertion, basicAssertion, basicAssertionAnonymous, assertionGroupAnonymous))
 
         mapOf(
-            IAssertion::class.java to assertion,
-            IBasicAssertion::class.java to basicAssertion,
-            IAssertionGroup::class.java to assertionGroup
-        ).forEach { (clazz, assertion) ->
-            it("does not append anything if ${clazz.simpleName} holds") {
+            "object: ${IAssertion::class.simpleName}" to assertion,
+            "object: ${IBasicAssertion::class.simpleName}" to basicAssertionAnonymous,
+            "${basicAssertion::class.simpleName}" to basicAssertion,
+            "object: ${IAssertionGroup::class.simpleName}" to assertionGroupAnonymous,
+            "${assertionGroup::class.simpleName}" to assertionGroup
+        ).forEach { (typeRepresentation, assertion) ->
+            it("does not append anything if $typeRepresentation holds") {
                 testee.format(assertion, sb)
-                verbs.checkLazily(sb) {
-                    isEmpty()
+                verbs.checkImmediately(sb).isEmpty()
+            }
+        }
+
+        context("${IAssertionGroup::class.simpleName} is of type ${IExplanatoryAssertionGroupType::class.simpleName}") {
+            it("appends something if the assertion group holds") {
+                val explanatoryAssertionGroup = AssertionGroup(ExplanatoryAssertionGroupType, AssertionVerb.VERB, 1, listOf(assertion))
+                testee.format(explanatoryAssertionGroup, sb)
+                verbs.checkImmediately(sb).isNotEmpty()
+            }
+
+            it("appends something if the assertion group does not hold") {
+                val failingAssertion = object : IAssertion {
+                    override fun holds() = false
                 }
+                val explanatoryAssertionGroup = AssertionGroup(ExplanatoryAssertionGroupType, AssertionVerb.VERB, 1, listOf(failingAssertion))
+                testee.format(explanatoryAssertionGroup, sb)
+                verbs.checkImmediately(sb).isNotEmpty()
             }
         }
 
