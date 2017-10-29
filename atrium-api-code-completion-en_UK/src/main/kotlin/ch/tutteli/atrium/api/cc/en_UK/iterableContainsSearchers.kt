b0db3bb5361129be59b1d2286ebf70109b355a52
Robert Stoll <rstoll@tutteli.ch>
2017-10-29 15:26:35 +0100

    separate api and impl in iterable contains fun
    The API should not bother what objects need to be created but merely
    use the functions from atrium-assertion to create assertions
    
    - add fun _objects to iterableAssertions
      (move it from IterableContainsCheckerBuilder)
    - use it in iterableContainsSearchers
      - add new addAssertion function to IterableContainsCheckerBuilder
        which eases the addition of assertions to the underlying plant

--- atrium-api-code-completion-en_UK/src/main/kotlin/ch/tutteli/atrium/api/cc/en_UK/iterableContainsSearchers.kt
+++ atrium-api-code-completion-en_UK/src/main/kotlin/ch/tutteli/atrium/api/cc/en_UK/iterableContainsSearchers.kt
@@ -1,12 +1,12 @@
 package ch.tutteli.atrium.api.cc.en_UK
 
+import ch.tutteli.atrium.assertions._objects
 import ch.tutteli.atrium.assertions.iterable.contains.builders.IterableContainsCheckerBuilder
 import ch.tutteli.atrium.assertions.iterable.contains.decorators.IterableContainsInAnyOrderDecorator
-import ch.tutteli.atrium.assertions.iterable.contains.searchers.IterableContainsInAnyOrderSearcher
 import ch.tutteli.atrium.creating.IAssertionPlant
 
 fun <E, T : Iterable<E>> IterableContainsCheckerBuilder<E, T, IterableContainsInAnyOrderDecorator>.value(expected: E): IAssertionPlant<T>
     = values(expected)
 
 fun <E, T : Iterable<E>> IterableContainsCheckerBuilder<E, T, IterableContainsInAnyOrderDecorator>.values(expected: E, vararg otherExpected: E): IAssertionPlant<T>
-    = addAssertion(IterableContainsInAnyOrderSearcher(), expected, otherExpected)
+    = addAssertion(_objects(this, expected, otherExpected))
--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/iterableAssertions.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/iterableAssertions.kt
@@ -1,5 +1,9 @@
 package ch.tutteli.atrium.assertions
 
+import ch.tutteli.atrium.assertions.iterable.contains.IterableContainsAssertionCreator
+import ch.tutteli.atrium.assertions.iterable.contains.builders.IterableContainsCheckerBuilder
+import ch.tutteli.atrium.assertions.iterable.contains.decorators.IterableContainsInAnyOrderDecorator
+import ch.tutteli.atrium.assertions.iterable.contains.searchers.IterableContainsInAnyOrderSearcher
 import ch.tutteli.atrium.creating.IAssertionPlant
 import ch.tutteli.atrium.reporting.RawString
 
@@ -11,4 +15,13 @@ fun <E, T : Iterable<E>> _containsNot(plant: IAssertionPlant<T>, expected: E, va
         })
     }
     return InvisibleAssertionGroup(assertions)
+}
+
+fun <E, T : Iterable<E>> _objects(
+    checker: IterableContainsCheckerBuilder<E, T, IterableContainsInAnyOrderDecorator>,
+    expected: E,
+    otherExpected: Array<out E>
+): IAssertion {
+    val creator = IterableContainsAssertionCreator(checker.containsBuilder.decorator, IterableContainsInAnyOrderSearcher<E, T>(), checker.checkers)
+    return creator.create(checker.containsBuilder.plant, expected, *otherExpected)
 }
