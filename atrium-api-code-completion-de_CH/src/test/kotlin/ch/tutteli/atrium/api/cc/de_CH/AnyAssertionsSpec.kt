9e1b08659ece576e86254fbcef7b0174329304d3
Robert Stoll <rstoll@tutteli.ch>
2017-11-13 22:36:30 +0100

    move isNull to atrium-assertions
    Assertion functions do not belong to atrium-core because they would
    appear in each API.
    
    - add _isNull to atrium-assertions
      - add isNull to atrium-api-cc-en_UK
      - add istNull to atrium-api-cc-de_CH
    - move specs

--- atrium-api-code-completion-de_CH/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/AnyAssertionsSpec.kt
+++ atrium-api-code-completion-de_CH/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/AnyAssertionsSpec.kt
@@ -2,6 +2,7 @@ package ch.tutteli.atrium.api.cc.de_CH
 
 import ch.tutteli.atrium.AssertionVerbFactory
 import ch.tutteli.atrium.creating.IAssertionPlant
+import ch.tutteli.atrium.creating.IAssertionPlantNullable
 import ch.tutteli.atrium.spec.assertions.AnyAssertionsSpec
 
 object AnyAssertionsSpec : ch.tutteli.atrium.spec.assertions.AnyAssertionsSpec(
@@ -11,7 +12,8 @@ object AnyAssertionsSpec : ch.tutteli.atrium.spec.assertions.AnyAssertionsSpec(
     IAssertionPlant<Int>::ist.name,
     IAssertionPlant<Int>::istNicht.name,
     IAssertionPlant<Int>::istSelbeInstanzWie.name,
-    IAssertionPlant<Int>::istNichtSelbeInstanzWie.name
+    IAssertionPlant<Int>::istNichtSelbeInstanzWie.name,
+    IAssertionPlantNullable<Int?>::istNull.name to IAssertionPlantNullable<Int?>::istNull
 ) {
     class AnyAssertionsSpecFunFactory<T : Any> : AnyAssertionsSpec.IAnyAssertionsSpecFunFactory<T> {
         override val toBe = IAssertionPlant<T>::ist
--- atrium-core-impl-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/creating/ReportingAssertionPlantNullable.kt
+++ atrium-core-impl-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/creating/ReportingAssertionPlantNullable.kt
@@ -1,8 +1,5 @@
 package ch.tutteli.atrium.creating
 
-import ch.tutteli.atrium.assertions.BasicAssertion
-import ch.tutteli.atrium.reporting.RawString
-
 /**
  * An [IAssertionPlant] for nullable types.
  *
@@ -15,17 +12,4 @@ class ReportingAssertionPlantNullable<out T : Any?>(
     commonFields: IAssertionPlantWithCommonFields.CommonFields<T>
 ) : BaseReportingAssertionPlant<T, IAssertionPlantNullable<T>>(commonFields), IReportingAssertionPlantNullable<T> {
     override val self = this
-
-    /**
-     * Makes the assertion that [subject] is `null`.
-     *
-     * @return Does not support a fluent API because: what else would you want to assert about `null` anyway?
-     *
-     * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
-     */
-    override fun isNull() {
-        if (subject != null) {
-            addAssertion(BasicAssertion(IAssertionPlantNullable.AssertionDescription, RawString.NULL, false))
-        }
-    }
 }
