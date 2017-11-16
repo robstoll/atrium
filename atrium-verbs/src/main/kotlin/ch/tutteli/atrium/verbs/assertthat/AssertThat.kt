7c667dac7e2b4a79372b8dce326128558f150de3
Robert Stoll <rstoll@tutteli.ch>
2017-11-16 19:31:02 +0100

    rewrite KDoc of assertion verbs, no lazy/immediate
    Mention instead, that one assertion verbs allows to add a group of
    assertions where the other just creates an IAssertionPlant (where
    one can still use the `and` function to create a group of assertions)

--- atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assertthat/AssertThat.kt
+++ atrium-verbs/src/main/kotlin/ch/tutteli/atrium/verbs/assertthat/AssertThat.kt
@@ -1,16 +1,18 @@
 package ch.tutteli.atrium.verbs.assertthat
 
 import ch.tutteli.atrium.AtriumFactory
+import ch.tutteli.atrium.IAtriumFactory
 import ch.tutteli.atrium.assertions.IAssertion
 import ch.tutteli.atrium.assertions.throwable.thrown.builders.ThrowableThrownBuilder
 import ch.tutteli.atrium.creating.IAssertionPlant
 import ch.tutteli.atrium.creating.IAssertionPlantNullable
+import ch.tutteli.atrium.reporting.IReporter
 import ch.tutteli.atrium.verbs.AssertionVerb.ASSERT_THAT
 import ch.tutteli.atrium.verbs.AssertionVerb.ASSERT_THAT_THROWN
 import ch.tutteli.atrium.verbs.AtriumReporterSupplier
 
 /**
- * Creates an [IAssertionPlant] for [subject] which immediately evaluates [IAssertion]s.
+ * Creates an [IAssertionPlant] for the given [subject].
  *
  * @return The newly created plant.
  *
@@ -20,17 +22,19 @@ fun <T : Any> assertThat(subject: T)
     = AtriumFactory.newReportingPlant(ASSERT_THAT, subject, AtriumReporterSupplier.REPORTER)
 
 /**
- * Creates an [IAssertionPlant] for [subject] which lazily evaluates [IAssertion]s.
+ * Creates an [IAssertionPlant] for the given [subject] and [IAssertionPlant.addAssertionsCreatedBy] the
+ * given [assertionCreator] lambda where the created [IAssertion]s are added as a group and usually (depending on
+ * the configured [IReporter]) reported as a whole.
  *
  * @return The newly created plant.
  *
- * @see AtriumFactory.newReportingPlantCheckLazilyAtTheEnd
+ * @see IAtriumFactory.newReportingPlantCheckLazilyAtTheEnd
  */
-fun <T : Any> assertThat(subject: T, createAssertions: IAssertionPlant<T>.() -> Unit)
-    = AtriumFactory.newReportingPlantCheckLazilyAtTheEnd(ASSERT_THAT, subject, AtriumReporterSupplier.REPORTER, createAssertions)
+fun <T : Any> assertThat(subject: T, assertionCreator: IAssertionPlant<T>.() -> Unit)
+    = AtriumFactory.newReportingPlantCheckLazilyAtTheEnd(ASSERT_THAT, subject, AtriumReporterSupplier.REPORTER, assertionCreator)
 
 /**
- * Creates an [IAssertionPlantNullable] for [subject].
+ * Creates an [IAssertionPlantNullable] for the given [subject] which might be `null`.
  *
  * @return The newly created plant.
  *
@@ -40,7 +44,7 @@ fun <T : Any?> assertThat(subject: T)
     = AtriumFactory.newReportingPlantNullable(ASSERT_THAT, subject, AtriumReporterSupplier.REPORTER)
 
 /**
- * Creates an [ThrowableThrownBuilder] for the given function [act].
+ * Creates an [ThrowableThrownBuilder] for the given function [act] which is expected to throw a [Throwable].
  *
  * @return The newly created [ThrowableThrownBuilder].
  */
