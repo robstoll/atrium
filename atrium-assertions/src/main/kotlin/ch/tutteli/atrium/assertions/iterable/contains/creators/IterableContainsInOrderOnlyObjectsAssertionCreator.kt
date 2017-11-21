cd34fbcbe0ccae9b55bf77668e485785c93aba16
Robert Stoll <rstoll@tutteli.ch>
2017-11-21 19:05:24 +0100

    reduce code dup. between inOrder entries and objects
    - introduce base class IterableContainsInOrderOnlyAssertionCreator
      - extend it in the existing classes

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/iterable/contains/creators/IterableContainsInOrderOnlyObjectsAssertionCreator.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/iterable/contains/creators/IterableContainsInOrderOnlyObjectsAssertionCreator.kt
@@ -1,64 +1,22 @@
 package ch.tutteli.atrium.assertions.iterable.contains.creators
 
-import ch.tutteli.atrium.assertions.*
-import ch.tutteli.atrium.assertions.iterable.contains.IIterableContains
+import ch.tutteli.atrium.assertions.BasicAssertion
+import ch.tutteli.atrium.assertions.DescriptionAnyAssertion
+import ch.tutteli.atrium.assertions.IAssertion
+import ch.tutteli.atrium.assertions.IAssertionGroup
 import ch.tutteli.atrium.assertions.iterable.contains.decorators.IterableContainsInOrderOnlyDecorator
-import ch.tutteli.atrium.creating.IAssertionPlant
 import ch.tutteli.atrium.reporting.RawString
-import ch.tutteli.atrium.reporting.translating.TranslatableRawString
-import ch.tutteli.atrium.reporting.translating.TranslatableWithArgs
-import ch.tutteli.atrium.reporting.translating.Untranslatable
 
 class IterableContainsInOrderOnlyObjectsAssertionCreator<E, T : Iterable<E>>(
-    private val decorator: IterableContainsInOrderOnlyDecorator
-) : IIterableContains.ICreator<T, E> {
+    decorator: IterableContainsInOrderOnlyDecorator
+) : IterableContainsInOrderOnlyAssertionCreator<E, T, E>(decorator) {
 
-    override fun createAssertionGroup(plant: IAssertionPlant<T>, expected: E, otherExpected: Array<out E>): IAssertionGroup {
-        return LazyThreadUnsafeAssertionGroup {
-            val assertions = mutableListOf<IAssertion>()
-            val allExpected = listOf(expected, *otherExpected)
-            val list = plant.subject.toList()
-            val itr = list.iterator()
-            allExpected.forEachIndexed { index, it ->
-                assertions.add(createEntryAssertion(itr, index, it))
-            }
-            assertions.add(createSizeFeatureAssertion(allExpected.size, list.size, itr))
+    override fun createEntryAssertion(iterableAsList: List<E>, expected: E, template: ((Boolean) -> IAssertion) -> IAssertionGroup): IAssertionGroup
+        = template(createEntryFeatureAssertion(expected))
 
-            val description = decorator.decorateDescription(DescriptionIterableAssertion.CONTAINS)
-            AssertionGroup(SummaryAssertionGroupType, description, RawString(""), assertions.toList())
-        }
-    }
+    private fun createEntryFeatureAssertion(expected: E): (Boolean) -> IAssertion
+        = { found -> BasicAssertion(DescriptionAnyAssertion.TO_BE, expected ?: RawString.NULL, found) }
 
-    private fun createEntryAssertion(itr: Iterator<E>, index: Int, expected: E): AssertionGroup {
-        val (found, entry) = if (itr.hasNext()) {
-            val actual = itr.next()
-            Pair(actual == expected, actual ?: RawString.NULL)
-        } else {
-            Pair(false, TranslatableRawString(DescriptionIterableAssertion.SIZE_EXCEEDED))
-        }
-        val description = TranslatableWithArgs(DescriptionIterableAssertion.ENTRY_WITH_INDEX, index)
-        return AssertionGroup(FeatureAssertionGroupType, description, entry, listOf(
-            BasicAssertion(DescriptionAnyAssertion.TO_BE, expected ?: RawString.NULL, found)
-        ))
-    }
-
-    private fun createSizeFeatureAssertion(expectedSize: Int, actualSize: Int, itr: Iterator<E>): IAssertionGroup {
-        val list = mutableListOf<E>()
-        while (itr.hasNext()) {
-            list.add(itr.next())
-        }
-        val featureAssertions = mutableListOf<IAssertion>()
-        featureAssertions.add(BasicAssertion(DescriptionAnyAssertion.TO_BE, RawString(expectedSize.toString()), { actualSize == expectedSize }))
-        if (actualSize > expectedSize) {
-            featureAssertions.add(LazyThreadUnsafeAssertionGroup {
-                val assertions = list.mapIndexed { index, it ->
-                    val description = TranslatableWithArgs(DescriptionIterableAssertion.ENTRY_WITH_INDEX, expectedSize + index)
-                    BasicAssertion(description, it ?: RawString.NULL, true)
-                }
-                val additionalEntries = AssertionGroup(ListAssertionGroupType, DescriptionIterableAssertion.WARNING_ADDITIONAL_ENTRIES, RawString(""), assertions)
-                ExplanatoryAssertionGroup(WarningAssertionGroupType, listOf(additionalEntries))
-            })
-        }
-        return AssertionGroup(FeatureAssertionGroupType, Untranslatable(list::size.name), RawString(actualSize.toString()), featureAssertions.toList())
-    }
+    override fun holds(actual: E, expected: E): Boolean
+        = actual == expected
 }
