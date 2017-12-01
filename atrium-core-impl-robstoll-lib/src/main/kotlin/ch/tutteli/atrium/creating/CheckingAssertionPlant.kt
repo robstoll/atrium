411536e3e659bfd1ab3bf555220beef93261265f
Robert Stoll <rstoll@tutteli.ch>
2017-12-02 00:40:13 +0100

    use require and check instead of throwing exceptions
    I was not aware of that kotlin has those two concepts, it surely makes
    more sense to use them

--- atrium-core-impl-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/creating/CheckingAssertionPlant.kt
+++ atrium-core-impl-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/creating/CheckingAssertionPlant.kt
@@ -12,7 +12,7 @@ class CheckingAssertionPlant<out T : Any>(
 
     override fun allAssertionsHold(): Boolean {
         val assertions = getAssertions()
-        if (assertions.isEmpty()) throw IllegalStateException("You need to create assertions first before checking whether they all hold.")
+        check(!assertions.isEmpty()) { "You need to create assertions first before checking whether they all hold." }
 
         try {
             return assertions.all { it.holds() }
