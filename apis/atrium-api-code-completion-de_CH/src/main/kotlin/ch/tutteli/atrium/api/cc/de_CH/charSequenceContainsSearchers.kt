34d45afd7ce65ff62239f42af9b40845be440cc1
Robert Stoll <rstoll@tutteli.ch>
2017-12-08 00:21:37 +0100

    refactor containsDefaultTranslation to builder
    - add searcher functions
    - refactor containsDefaultTranslation so that it uses builder

--- apis/atrium-api-code-completion-de_CH/src/main/kotlin/ch/tutteli/atrium/api/cc/de_CH/charSequenceContainsSearchers.kt
+++ apis/atrium-api-code-completion-de_CH/src/main/kotlin/ch/tutteli/atrium/api/cc/de_CH/charSequenceContainsSearchers.kt
@@ -1,13 +1,11 @@
 package ch.tutteli.atrium.api.cc.de_CH
 
-import ch.tutteli.atrium.assertions._containsRegex
-import ch.tutteli.atrium.assertions._containsRegexIgnoringCase
-import ch.tutteli.atrium.assertions._containsValues
-import ch.tutteli.atrium.assertions._containsValuesIgnoringCase
+import ch.tutteli.atrium.assertions.*
 import ch.tutteli.atrium.assertions.charsequence.contains.builders.CharSequenceContainsCheckerBuilder
 import ch.tutteli.atrium.assertions.charsequence.contains.searchbehaviours.CharSequenceContainsIgnoringCaseSearchBehaviour
 import ch.tutteli.atrium.assertions.charsequence.contains.searchbehaviours.CharSequenceContainsNoOpSearchBehaviour
 import ch.tutteli.atrium.creating.IAssertionPlant
+import ch.tutteli.atrium.reporting.translating.ITranslatable
 
 /**
  * Finishes the specification of the sophisticated `contains` assertion where the [expected] object shall be searched,
@@ -79,6 +77,54 @@ fun <T : CharSequence> CharSequenceContainsCheckerBuilder<T, CharSequenceContain
 @JvmName("valuesIgnoringCase")
 fun <T : CharSequence> CharSequenceContainsCheckerBuilder<T, CharSequenceContainsIgnoringCaseSearchBehaviour>.werte(expected: Any, vararg otherExpected: Any): IAssertionPlant<T>
     = addAssertion(_containsValuesIgnoringCase(this, expected, otherExpected))
+
+
+/**
+ * Finishes the specification of the sophisticated `contains` assertion where the [expected]'s
+ * [getDefault][ITranslatable.getDefault] representation as well as the [getDefault][ITranslatable.getDefault]
+ * representations of the [otherExpected] (if defined) shall be searched, using a non disjoint search.
+ *
+ * By non disjoint is meant that `'aa'` in `'aaaa'` is found three times and not only two times.
+ * Also notice, that it does not search for unique matches. Meaning, if the input of the search is `'a'` and the
+ * default translation of [expected] is defined as `'a'` and one default translation of the
+ * [otherExpected] is defined as `'a'` as well, then both match, even though they match the
+ * same sequence in the input of the search. Use an option such as [zumindest], [hoestens] and [genau] to control
+ * the number of occurrences you expect.
+ *
+ * Meaning you might want to use:
+ *   `enthaelt.genau(2).standardUebersetzungVon(IS)`
+ * instead of:
+ *   `enthaelt.zumindest(1).standardUebersetzungVon(IS, IS)`
+ *
+ * @return The [IAssertionPlant] for which the assertion was built to support a fluent API.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ */
+fun <T : CharSequence> CharSequenceContainsCheckerBuilder<T, CharSequenceContainsNoOpSearchBehaviour>.standardUebersetzungVon(expected: ITranslatable, vararg otherExpected: ITranslatable): IAssertionPlant<T>
+    = addAssertion(_containsDefaultTranslationOf(this, expected, otherExpected))
+
+/**
+ * Finishes the specification of the sophisticated `contains` assertion where the [expected]'s
+ * [getDefault][ITranslatable.getDefault] representation as well as the [getDefault][ITranslatable.getDefault]
+ * representations of the [otherExpected] (if defined) shall be searched (ignoring case), using a non disjoint search.
+ *
+ * By non disjoint is meant that `'aa'` in `'aaaa'` is found three times and not only two times.
+ * Also notice, that it does not search for unique matches. Meaning, if the input of the search is `'a'` and the
+ * default translation of [expected] is defined as `'a'` and one default translation of the
+ * [otherExpected] is defined as `'a'` as well, then both match, even though they match the
+ * same sequence in the input of the search. Use an option such as [zumindest], [hoestens] and [genau] to control
+ * the number of occurrences you expect.
+ *
+ * Meaning you might want to use:
+ *   `enthaelt.genau(2).standardUebersetzungVon(IS)`
+ * instead of:
+ *   `enthaelt.zumindest(1).standardUebersetzungVon(IS, IS)`
+ *
+ * @return The [IAssertionPlant] for which the assertion was built to support a fluent API.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ */
+@JvmName("defaultTranslationOfIgnoringCase")
+fun <T : CharSequence> CharSequenceContainsCheckerBuilder<T, CharSequenceContainsIgnoringCaseSearchBehaviour>.standardUebersetzungVon(expected: ITranslatable, vararg otherExpected: ITranslatable): IAssertionPlant<T>
+    = addAssertion(_containsDefaultTranslationOfIgnoringCase(this, expected, otherExpected))
 
 
 /**
