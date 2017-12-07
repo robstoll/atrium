664836f32f0a1ad56eab85e204f8be3d67c7251e
Robert Stoll <rstoll@tutteli.ch>
2017-12-08 00:26:25 +0100

    rename ContainsBuilder::decorator to searchBehaviour

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/basic/contains/builders/ContainsBuilder.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/basic/contains/builders/ContainsBuilder.kt
@@ -11,14 +11,14 @@ import ch.tutteli.atrium.creating.IAssertionPlant
  * @param S The type of the current [IContains.ISearchBehaviour].
  *
  * @property plant The [IAssertionPlant] for which the sophisticated `contains` assertions shall be built.
- * @property decorator The search behaviour which shall be applied to the input of the search.
+ * @property searchBehaviour The search behaviour which shall be applied to the input of the search.
  *
  * @constructor Represents the base class for builders representing the entry point of the process of building a
  *              sophisticated `contains` assertion.
  * @param plant The [IAssertionPlant] for which the sophisticated `contains` assertions shall be built.
- * @param decorator The search behaviour which shall be applied to the input of the search.
+ * @param searchBehaviour The search behaviour which shall be applied to the input of the search.
  */
 abstract class ContainsBuilder<out T : Any, S: IContains.ISearchBehaviour>(
     val plant: IAssertionPlant<T>,
-    val decorator: S
+    val searchBehaviour: S
 )
