614fa77a7317af6d7f9c201fb8b750f666f38501
Robert Stoll <rstoll@tutteli.ch>
2017-12-11 20:06:44 +0100

    rename `decorator` to `searchBehaviour`

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/iterable/contains/builders/IterableContainsBuilder.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/iterable/contains/builders/IterableContainsBuilder.kt
@@ -2,7 +2,6 @@ package ch.tutteli.atrium.assertions.iterable.contains.builders
 
 import ch.tutteli.atrium.assertions.IAssertion
 import ch.tutteli.atrium.assertions.basic.contains.builders.ContainsBuilder
-import ch.tutteli.atrium.assertions.charsequence.contains.ICharSequenceContains.ISearchBehaviour
 import ch.tutteli.atrium.assertions.iterable.contains.IIterableContains
 import ch.tutteli.atrium.creating.IAssertionPlant
 
@@ -17,8 +16,8 @@ import ch.tutteli.atrium.creating.IAssertionPlant
  *
  * @constructor Represents the entry point of the fluent API of sophisticated `contains` assertions.
  * @param plant The [IAssertionPlant] for which the sophisticated `contains` assertions shall be built.
- * @param decorator The decoration behaviour which shall be applied to the input of the search.
+ * @param searchBehaviour The search behaviour which shall be applied to the input of the search.
  */
 class IterableContainsBuilder<out E, out T : Iterable<E>, S : IIterableContains.ISearchBehaviour>(
-    plant: IAssertionPlant<T>, decorator: S
-) : ContainsBuilder<T, S>(plant, decorator)
+    plant: IAssertionPlant<T>, searchBehaviour: S
+) : ContainsBuilder<T, S>(plant, searchBehaviour)
