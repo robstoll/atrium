5990187c2ea6f12f672be70e4cc273df60e0f093
Robert Stoll <rstoll@tutteli.ch>
2018-01-02 14:24:50 +0100

    update KDoc of AssertionPlant, is out-dated

--- core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/creating/ReportingAssertionPlant.kt
+++ core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/creating/ReportingAssertionPlant.kt
@@ -3,8 +3,7 @@ package ch.tutteli.atrium.creating
 import ch.tutteli.atrium.assertions.Assertion
 
 /**
- * Represents a plant for [Assertion]s and offers the possibility to [check][checkAssertions] all
- * the [added][addAssertion] assertions which includes error reporting.
+ * Represents a plant for [Assertion]s and offers the possibility to [addAssertion]s and to report them.
  *
  * You can think of it as an [Assertion] factory which does more than just factoring
  * but also provides quality assurance capabilities.
