845c6c4ed845016ac64f3a401a9ced0be7e34c9d
Robert Stoll <rstoll@tutteli.ch>
2018-01-05 18:55:20 +0100

    workaround for overload reflection bug KT-17340
    One cannot use returnValueOf if the method has overloads. There is a bug
    https://youtrack.jetbrains.com/issue/KT-17340
    which obstructs it. Thus:
    - provide a method (not in the API, only as last resort) which only
      requires the name in addition and uses a functional type instead of
      KFunction
    - make a notice in README

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/featureAssertions.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/featureAssertions.kt
@@ -23,79 +23,133 @@ private fun <T : Any, TFeature : Any?> AssertionPlant<T>.createCommonFieldsForFe
 
 //Arg0
 fun <T : Any, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction0<TReturnValue>): AssertionPlant<TReturnValue>
-    = plant.createPlantForMethod(method)
+    = _method(plant, method.name, method)
+
+fun <T : Any, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: () -> TReturnValue): AssertionPlant<TReturnValue>
+    = plant.createPlantForMethod(name, method())
 
 fun <T : Any, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction0<TReturnValue>, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
-    = _returnValueOf(plant, method).addAssertionsCreatedBy(assertionCreator)
+    = _method(plant, method.name, method, assertionCreator)
+
+fun <T : Any, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: () -> TReturnValue, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
+    = _method(plant, name, method).addAssertionsCreatedBy(assertionCreator)
 
 fun <T : Any, TReturnValue : Any?> _returnValueOf(plant: AssertionPlant<T>, method: KFunction0<TReturnValue>): AssertionPlantNullable<TReturnValue>
-    = plant.createPlantForMethod(method)
+    = _method(plant, method.name, method)
+
+fun <T : Any, TReturnValue : Any?> _method(plant: AssertionPlant<T>, name: String, method: () -> TReturnValue): AssertionPlantNullable<TReturnValue>
+    = plant.createPlantForMethod(name, method())
 
 
 //Arg1
 fun <T : Any, T1 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction1<T1, TReturnValue>, arg1: T1): AssertionPlant<TReturnValue>
-    = plant.createPlantForMethod(method, arg1)
+    = _method(plant, method.name, method, arg1)
+
+fun <T : Any, T1 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1) -> TReturnValue, arg1: T1): AssertionPlant<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1), arg1)
 
 fun <T : Any, T1 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction1<T1, TReturnValue>, arg1: T1, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
-    = _returnValueOf(plant, method, arg1).addAssertionsCreatedBy(assertionCreator)
+    = _method(plant, method.name, method, arg1, assertionCreator)
+
+fun <T : Any, T1 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1) -> TReturnValue, arg1: T1, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
+    = _method(plant, name, method, arg1).addAssertionsCreatedBy(assertionCreator)
 
 fun <T : Any, T1 : Any?, TReturnValue : Any?> _returnValueOf(plant: AssertionPlant<T>, method: KFunction1<T1, TReturnValue>, arg1: T1): AssertionPlantNullable<TReturnValue>
-    = plant.createPlantForMethod(method, arg1)
+    = _method(plant, method.name, method, arg1)
+
+fun <T : Any, T1 : Any?, TReturnValue : Any?> _method(plant: AssertionPlant<T>, name: String, method: (T1) -> TReturnValue, arg1: T1): AssertionPlantNullable<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1), arg1)
 
 
 //Arg2
 fun <T : Any, T1 : Any?, T2 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction2<T1, T2, TReturnValue>, arg1: T1, arg2: T2): AssertionPlant<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2)
+    = _method(plant, method.name, method, arg1, arg2)
+
+fun <T : Any, T1 : Any?, T2 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2) -> TReturnValue, arg1: T1, arg2: T2): AssertionPlant<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2), arg1, arg2)
 
 fun <T : Any, T1 : Any?, T2 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction2<T1, T2, TReturnValue>, arg1: T1, arg2: T2, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
-    = _returnValueOf(plant, method, arg1, arg2).addAssertionsCreatedBy(assertionCreator)
+    = _method(plant, method.name, method, arg1, arg2, assertionCreator)
+
+fun <T : Any, T1 : Any?, T2 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2) -> TReturnValue, arg1: T1, arg2: T2, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
+    = _method(plant, name, method, arg1, arg2).addAssertionsCreatedBy(assertionCreator)
 
 fun <T : Any, T1 : Any?, T2 : Any?, TReturnValue : Any?> _returnValueOf(plant: AssertionPlant<T>, method: KFunction2<T1, T2, TReturnValue>, arg1: T1, arg2: T2): AssertionPlantNullable<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2)
+    = _method(plant, method.name, method, arg1, arg2)
+
+fun <T : Any, T1 : Any?, T2 : Any?, TReturnValue : Any?> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2) -> TReturnValue, arg1: T1, arg2: T2): AssertionPlantNullable<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2), arg1, arg2)
 
 
 //Arg3
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction3<T1, T2, T3, TReturnValue>, arg1: T1, arg2: T2, arg3: T3): AssertionPlant<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2, arg3)
+    = _method(plant, method.name, method, arg1, arg2, arg3)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3): AssertionPlant<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2, arg3), arg1, arg2, arg3)
 
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction3<T1, T2, T3, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
-    = _returnValueOf(plant, method, arg1, arg2, arg3).addAssertionsCreatedBy(assertionCreator)
+    = _method(plant, method.name, method, arg1, arg2, arg3, assertionCreator)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
+    = _method(plant, name, method, arg1, arg2, arg3).addAssertionsCreatedBy(assertionCreator)
 
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, TReturnValue : Any?> _returnValueOf(plant: AssertionPlant<T>, method: KFunction3<T1, T2, T3, TReturnValue>, arg1: T1, arg2: T2, arg3: T3): AssertionPlantNullable<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2, arg3)
+    = _method(plant, method.name, method, arg1, arg2, arg3)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, TReturnValue : Any?> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3): AssertionPlantNullable<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2, arg3), arg1, arg2, arg3)
 
 
 //Arg4
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction4<T1, T2, T3, T4, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, arg4: T4): AssertionPlant<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2, arg3, arg4)
+    = _method(plant, method.name, method, arg1, arg2, arg3, arg4)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3, T4) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, arg4: T4): AssertionPlant<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2, arg3, arg4), arg1, arg2, arg3, arg4)
 
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction4<T1, T2, T3, T4, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, arg4: T4, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
-    = _returnValueOf(plant, method, arg1, arg2, arg3, arg4).addAssertionsCreatedBy(assertionCreator)
+    = _method(plant, method.name, method, arg1, arg2, arg3, arg4, assertionCreator)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3, T4) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, arg4: T4, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
+    = _method(plant, name, method, arg1, arg2, arg3, arg4).addAssertionsCreatedBy(assertionCreator)
 
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, TReturnValue : Any?> _returnValueOf(plant: AssertionPlant<T>, method: KFunction4<T1, T2, T3, T4, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, arg4: T4): AssertionPlantNullable<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2, arg3, arg4)
+    = _method(plant, method.name, method, arg1, arg2, arg3, arg4)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, TReturnValue : Any?> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3, T4) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, arg4: T4): AssertionPlantNullable<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2, arg3, arg4), arg1, arg2, arg3, arg4)
 
 
 //Arg5
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, T5 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction5<T1, T2, T3, T4, T5, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): AssertionPlant<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2, arg3, arg4, arg5)
+    = _method(plant, method.name, method, arg1, arg2, arg3, arg4, arg5)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, T5 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3, T4, T5) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): AssertionPlant<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2, arg3, arg4, arg5), arg1, arg2, arg3, arg4, arg5)
 
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, T5 : Any?, TReturnValue : Any> _returnValueOf(plant: AssertionPlant<T>, method: KFunction5<T1, T2, T3, T4, T5, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
-    = _returnValueOf(plant, method, arg1, arg2, arg3, arg4, arg5).addAssertionsCreatedBy(assertionCreator)
+    = _method(plant, method.name, method, arg1, arg2, arg3, arg4, arg5, assertionCreator)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, T5 : Any?, TReturnValue : Any> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3, T4, T5) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, assertionCreator: AssertionPlant<TReturnValue>.() -> Unit): AssertionPlant<TReturnValue>
+    = _method(plant, name, method, arg1, arg2, arg3, arg4, arg5).addAssertionsCreatedBy(assertionCreator)
 
 fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, T5 : Any?, TReturnValue : Any?> _returnValueOf(plant: AssertionPlant<T>, method: KFunction5<T1, T2, T3, T4, T5, TReturnValue>, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): AssertionPlantNullable<TReturnValue>
-    = plant.createPlantForMethod(method, arg1, arg2, arg3, arg4, arg5)
+    = _method(plant, method.name, method, arg1, arg2, arg3, arg4, arg5)
+
+fun <T : Any, T1 : Any?, T2 : Any?, T3 : Any?, T4 : Any?, T5 : Any?, TReturnValue : Any?> _method(plant: AssertionPlant<T>, name: String, method: (T1, T2, T3, T4, T5) -> TReturnValue, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): AssertionPlantNullable<TReturnValue>
+    = plant.createPlantForMethod(name, method(arg1, arg2, arg3, arg4, arg5), arg1, arg2, arg3, arg4, arg5)
 
 
-private fun <T : Any, TReturnValue : Any> AssertionPlant<T>.createPlantForMethod(method: KFunction<TReturnValue>, vararg arguments: Any?)
-    = AtriumFactory.newReportingPlant(createCommonFieldsForFeatureFactory(method, arguments))
+private fun <T : Any, TReturnValue : Any> AssertionPlant<T>.createPlantForMethod(name: String, value: TReturnValue, vararg arguments: Any?)
+    = AtriumFactory.newReportingPlant(createCommonFieldsForFeatureFactory(name, value, arguments))
 
-private fun <T : Any, TReturnValue : Any?> AssertionPlant<T>.createPlantForMethod(method: KFunction<TReturnValue>, vararg arguments: Any?)
-    = AtriumFactory.newReportingPlantNullable(createCommonFieldsForFeatureFactory(method, arguments))
+private fun <T : Any, TReturnValue : Any?> AssertionPlant<T>.createPlantForMethod(name: String, value: TReturnValue, vararg arguments: Any?)
+    = AtriumFactory.newReportingPlantNullable(createCommonFieldsForFeatureFactory(name, value, arguments))
 
-private fun <T : Any, TReturnValue : Any?> AssertionPlant<T>.createCommonFieldsForFeatureFactory(method: KFunction<TReturnValue>, arguments: Array<out Any?>) =
+private fun <T : Any, TReturnValue : Any?> AssertionPlant<T>.createCommonFieldsForFeatureFactory(name: String, value: TReturnValue, arguments: Array<out Any?>) =
     AssertionPlantWithCommonFields.CommonFields(
-        Untranslatable(AtriumFactory.newMethodCallFormatter().format(method, arguments)),
-        method.call(*arguments),
+        Untranslatable(AtriumFactory.newMethodCallFormatter().format(name, arguments)),
+        value,
         AtriumFactory.newFeatureAssertionChecker(this),
         RawString.NULL)
--- core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/reporting/MethodCallFormatter.kt
+++ core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/reporting/MethodCallFormatter.kt
@@ -1,19 +1,17 @@
 package ch.tutteli.atrium.reporting
 
-import kotlin.reflect.KCallable
-
 /**
  * Responsible to format a method call in reporting.
  */
 interface MethodCallFormatter {
 
     /**
-     * Returns a lazy representation of the method call to [method] with the given [arguments].
+     * Returns a lazy representation of the method call to a method named [name] with the given [arguments].
      *
-     * @param method The method for which a call with the given [arguments] should be formatted.
+     * @param name The name of the method for which a call with the given [arguments] should be formatted.
      * @param arguments The arguments of the method call.
      *
      * @return An lambda containing the logic to build the representation.
      */
-    fun format(method: KCallable<*>, arguments: Array<out Any?>): () -> String
+    fun format(name: String, arguments: Array<out Any?>): () -> String
 }
