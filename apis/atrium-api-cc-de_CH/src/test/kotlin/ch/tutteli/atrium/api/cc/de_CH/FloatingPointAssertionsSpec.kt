b4aa8a37fe50702bbdfdac3731c444c584964d03
Robert Stoll <rstoll@tutteli.ch>
2018-01-11 23:09:26 +0100

    introduce toBeWithErrorTolerance for floating point numbers
    We will provide a sophisticated assertion builder later on (where one
    could implement other error ranges, e.g. only positive error, relative
    error and so on). Thus, we do not yet provide a shortcut function for
    the infix API because it will probably not look different than the
    sophisticated assertion builder. For now:
    
    - introduce toBeWithErrorTolerance for cc-en_UK and cc-de_CH
    - write spec
    - include an exact check in the error message but only in the error
      message

---
+++ apis/atrium-api-cc-de_CH/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/FloatingPointAssertionsSpec.kt
@@ -0,0 +1,38 @@
+package ch.tutteli.atrium.api.cc.de_CH
+
+import ch.tutteli.atrium.AssertionVerbFactory
+import ch.tutteli.atrium.creating.AssertionPlant
+import java.math.BigDecimal
+import kotlin.reflect.KFunction3
+
+class FloatingPointAssertionsSpec : ch.tutteli.atrium.spec.assertions.FloatingPointAssertionsSpec(
+    AssertionVerbFactory,
+    toBeWithErrorToleranceFloatPair(),
+    toBeWithErrorToleranceDoublePair(),
+    toBeWithErrorToleranceBigDecimalPair()
+
+) {
+    companion object {
+
+        private val toBeWithErrorToleranceFloatFun: KFunction3<AssertionPlant<Float>, Float, Float, AssertionPlant<Float>> = AssertionPlant<Float>::istMitFehlerToleranz
+        fun toBeWithErrorToleranceFloatPair()
+            = "${toBeWithErrorToleranceFloatFun.name} for Float" to Companion::toBeWithErrorToleranceFloat
+
+        private fun toBeWithErrorToleranceFloat(plant: AssertionPlant<Float>, expected: Float, tolerance: Float)
+            = plant.istMitFehlerToleranz(expected, tolerance)
+
+        private val toBeWithErrorToleranceDoubleFun: KFunction3<AssertionPlant<Double>, Double, Double, AssertionPlant<Double>> = AssertionPlant<Double>::istMitFehlerToleranz
+        fun toBeWithErrorToleranceDoublePair()
+            = "${toBeWithErrorToleranceDoubleFun.name} for Double" to Companion::toBeWithErrorToleranceDouble
+
+        private fun toBeWithErrorToleranceDouble(plant: AssertionPlant<Double>, expected: Double, tolerance: Double)
+            = plant.istMitFehlerToleranz(expected, tolerance)
+
+        private val toBeWithErrorToleranceBigDecimalFun: KFunction3<AssertionPlant<BigDecimal>, BigDecimal, BigDecimal, AssertionPlant<BigDecimal>> = AssertionPlant<BigDecimal>::istMitFehlerToleranz
+        fun toBeWithErrorToleranceBigDecimalPair()
+            = "${toBeWithErrorToleranceBigDecimalFun.name} for BigDecimal" to Companion::toBeWithErrorToleranceBigDecimal
+
+        private fun toBeWithErrorToleranceBigDecimal(plant: AssertionPlant<BigDecimal>, expected: BigDecimal, tolerance: BigDecimal)
+            = plant.istMitFehlerToleranz(expected, tolerance)
+    }
+}
