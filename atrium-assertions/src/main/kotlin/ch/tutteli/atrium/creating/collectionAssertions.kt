013b11a5310a96b959544cbb6a66402615ea57d4
Robert Stoll <rstoll@tutteli.ch>
2018-02-05 18:19:09 +0100

    re-add deprecated Collection assertions

---
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/creating/collectionAssertions.kt
@@ -0,0 +1,16 @@
+@file:JvmName("CollectionAnyAssertions")
+package ch.tutteli.atrium.creating
+
+import ch.tutteli.atrium.assertions.Assertion
+
+@Deprecated("use CollectionAssertions.hasSize instead, will be removed with 1.0.0", ReplaceWith("CollectionAssertions.hasSize(plant, size)"))
+fun <T : Collection<*>> _hasSize(plant: AssertionPlant<T>, size: Int): Assertion
+    = CollectionAssertions.hasSize(plant, size)
+
+@Deprecated("use CollectionAssertions.isEmpty instead, will be removed with 1.0.0", ReplaceWith("CollectionAssertions.isEmpty(plant)"))
+fun <T : Collection<*>> _isEmpty(plant: AssertionPlant<T>): Assertion
+    = CollectionAssertions.isEmpty(plant)
+
+@Deprecated("use CollectionAssertions.isNotEmpty instead, will be removed with 1.0.0", ReplaceWith("CollectionAssertions.isNotEmpty(plant)"))
+fun <T : Collection<*>> _isNotEmpty(plant: AssertionPlant<T>): Assertion
+    = CollectionAssertions.isNotEmpty(plant)
