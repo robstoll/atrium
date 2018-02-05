948e9e21d832ccb163eaafb47c892e51abdfc531
Robert Stoll <rstoll@tutteli.ch>
2018-02-05 22:15:33 +0100

    deprecate old type transformations

--- atrium-assertions/src/main/kotlin/ch/tutteli/atrium/creating/typeTransformationAssertions.kt
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/creating/typeTransformationAssertions.kt
@@ -4,14 +4,17 @@ package ch.tutteli.atrium.creating
 import ch.tutteli.atrium.reporting.translating.Translatable
 import kotlin.reflect.KClass
 
+@Deprecated("use TypeTransformationAssertions.isNotNull instead, will be removed with 1.0.0", ReplaceWith("TypeTransformationAssertions.isNotNull(plant, T::class, assertionCreator)"))
 inline fun <reified T : Any> _isNotNull(plant: AssertionPlantNullable<T?>, noinline assertionCreator: AssertionPlant<T>.() -> Unit) {
     TypeTransformationAssertions.isNotNull(plant, T::class, assertionCreator)
 }
 
+@Deprecated("use TypeTransformationAssertions.isA instead, will be removed with 1.0.0", ReplaceWith("TypeTransformationAssertions.isA(plant, TSub::class, assertionCreator)"))
 inline fun <reified TSub : Any> _isA(plant: AssertionPlant<Any>, noinline assertionCreator: AssertionPlant<TSub>.() -> Unit) {
     TypeTransformationAssertions.isA(plant, TSub::class, assertionCreator)
 }
 
+@Deprecated("use TypeTransformationAssertions.downCast instead, will be removed with 1.0.0", ReplaceWith("TypeTransformationAssertions.downCast(description, subType, subjectPlant, assertionCreator)"))
 fun <T : Any, TSub : T> _downCast(
     description: Translatable,
     subType: KClass<TSub>,
@@ -21,6 +24,7 @@ fun <T : Any, TSub : T> _downCast(
     TypeTransformationAssertions.downCast(description, subType, subjectPlant, assertionCreator)
 }
 
+@Deprecated("use TypeTransformationAssertions.typeTransformation instead, will be removed with 1.0.0", ReplaceWith("TypeTransformationAssertions.typeTransformation(description, representation, subjectPlant, assertionCreator, warningTransformationFailed, canBeTransformed, transform)"))
 fun <T : Any, TSub : Any> _typeTransformation(
     description: Translatable,
     representation: Any,
