bef4cd35e4de1c468c0c8a4da631e8b2fdbc911c
Robert Stoll <rstoll@tutteli.ch>
2018-02-05 18:18:28 +0100

    re-add deprecated BigDecimal assertions

---
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/creating/bigDecimalAssertions.kt
@@ -0,0 +1,21 @@
+@file:JvmName("DeprecatedBigDecimalAssertions")
+package ch.tutteli.atrium.creating
+
+import ch.tutteli.atrium.assertions.Assertion
+import java.math.BigDecimal
+
+@Deprecated("use BigDecimalAssertions.isNumericallyEqualTo instead, will be removed with 1.0.0", ReplaceWith("BigDecimalAssertions.isNumericallyEqualTo(plant, expected)"))
+fun <T : BigDecimal> _isNumericallyEqualTo(plant: AssertionPlant<T>, expected: T)
+   = BigDecimalAssertions.isNumericallyEqualTo(plant, expected)
+
+@Deprecated("use BigDecimalAssertions.isNotNumericallyEqualTo instead, will be removed with 1.0.0", ReplaceWith("BigDecimalAssertions.isNotNumericallyEqualTo(plant, expected)"))
+fun <T : BigDecimal> _isNotNumericallyEqualTo(plant: AssertionPlant<T>, expected: T)
+    = BigDecimalAssertions.isNotNumericallyEqualTo(plant, expected)
+
+@Deprecated("use BigDecimalAssertions.isEqualIncludingScale instead, will be removed with 1.0.0", ReplaceWith("BigDecimalAssertions.isEqualIncludingScale(plant, expected, nameOfIsNumericallyEqualTo)"))
+fun <T : BigDecimal> _isEqualIncludingScale(plant: AssertionPlant<T>, expected: T, nameOfIsNumericallyEqualTo: String): Assertion
+    = BigDecimalAssertions.isEqualIncludingScale(plant, expected, nameOfIsNumericallyEqualTo)
+
+@Deprecated("use BigDecimalAssertions.isNotEqualIncludingScale instead, will be removed with 1.0.0", ReplaceWith("BigDecimalAssertions.isNotEqualIncludingScale(plant, expected)"))
+fun <T : BigDecimal> _isNotEqualIncludingScale(plant: AssertionPlant<T>, expected: T)
+    = BigDecimalAssertions.isNotEqualIncludingScale(plant, expected)
