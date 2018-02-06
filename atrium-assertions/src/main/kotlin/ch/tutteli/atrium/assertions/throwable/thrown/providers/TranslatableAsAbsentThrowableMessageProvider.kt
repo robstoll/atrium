6ed92bcaf9bfc7c03131d58e971f1fd83e97d31d
Robert Stoll <rstoll@tutteli.ch>
2018-02-06 22:08:19 +0100

    re-add deprecated throwable thrown classes

---
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/throwable/thrown/providers/TranslatableAsAbsentThrowableMessageProvider.kt
@@ -0,0 +1,14 @@
+package ch.tutteli.atrium.assertions.throwable.thrown.providers
+
+import ch.tutteli.atrium.assertions.throwable.thrown.ThrowableThrown
+import ch.tutteli.atrium.reporting.RawString
+import ch.tutteli.atrium.reporting.translating.Translatable
+
+/**
+ * Represents a [ThrowableThrown.AbsentThrowableMessageProvider] which is using a given [Translatable] which in $
+ * turn explains an absent [Throwable].
+ */
+@Deprecated("use the provider from package creating, will be removed with 1.0.0", ReplaceWith("ch.tutteli.atrium.creating.throwable.thrown.providers.TranslatableAsAbsentThrowableMessageProvider"))
+class TranslatableAsAbsentThrowableMessageProvider(translatable: Translatable) : ThrowableThrown.AbsentThrowableMessageProvider {
+    override val message = RawString.create(translatable)
+}
