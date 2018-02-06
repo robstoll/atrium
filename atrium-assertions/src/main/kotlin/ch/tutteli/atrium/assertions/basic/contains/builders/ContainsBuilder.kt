810c8ec2f931372de23d8d2b2f71f9bc960e1ecb
Robert Stoll <rstoll@tutteli.ch>
2018-02-06 21:54:09 +0100

    re-add deprecated basic contains classes

---
+++ atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/basic/contains/builders/ContainsBuilder.kt
@@ -0,0 +1,25 @@
+package ch.tutteli.atrium.assertions.basic.contains.builders
+
+import ch.tutteli.atrium.assertions.basic.contains.Contains
+import ch.tutteli.atrium.creating.AssertionPlant
+
+/**
+ * Represents the base class for builders representing the entry point of the process of building a sophisticated
+ * `contains` assertion.
+ *
+ * @param T The type of the [AssertionPlant.subject].
+ * @param S The type of the current [Contains.SearchBehaviour].
+ *
+ * @property plant The [AssertionPlant] for which the sophisticated `contains` assertions shall be built.
+ * @property searchBehaviour The search behaviour which shall be applied to the input of the search.
+ *
+ * @constructor Represents the base class for builders representing the entry point of the process of building a
+ *   sophisticated `contains` assertion.
+ * @param plant The [AssertionPlant] for which the sophisticated `contains` assertions shall be built.
+ * @param searchBehaviour The search behaviour which shall be applied to the input of the search.
+ */
+@Deprecated("use the abstract class from package creating, will be removed with 1.0.0", ReplaceWith("ch.tutteli.atrium.creating.basic.contains.builders.ContainsBuilder"))
+abstract class ContainsBuilder<out T : Any, out S: Contains.SearchBehaviour>(
+    val plant: AssertionPlant<T>,
+    val searchBehaviour: S
+)
