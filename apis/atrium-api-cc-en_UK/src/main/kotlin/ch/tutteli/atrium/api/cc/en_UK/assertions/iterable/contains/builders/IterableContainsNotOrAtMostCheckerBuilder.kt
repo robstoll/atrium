93db611bc2d28b852ac6035fad0b99437aa80707
Robert Stoll <rstoll@tutteli.ch>
2018-02-08 22:19:22 +0100

    re-add deprecated API builders

---
+++ apis/atrium-api-cc-en_UK/src/main/kotlin/ch/tutteli/atrium/api/cc/en_UK/assertions/iterable/contains/builders/IterableContainsNotOrAtMostCheckerBuilder.kt
@@ -0,0 +1,25 @@
+package ch.tutteli.atrium.api.cc.en_UK.assertions.iterable.contains.builders
+
+import ch.tutteli.atrium.creating.iterable.contains.builders.IterableContainsBuilder
+import ch.tutteli.atrium.creating.iterable.contains.searchbehaviours.IterableContainsInAnyOrderSearchBehaviour
+
+/**
+ * Represents the builder of a `contains not or at most` check within the fluent API of a
+ * sophisticated `contains` assertion for [Iterable].
+ *
+ * @param T The input type of the search.
+ *
+ * @constructor Represents the builder of a `contains at least once but at most` check within the fluent API of a
+ *   sophisticated `contains` assertion for [Iterable].
+ * @param times The number which the check will compare against the actual number of times an expected entry is
+ *   found in the [Iterable].
+ * @param containsBuilder The previously used [IterableContainsBuilder].
+ */
+@Deprecated("use the builder from the package creating, will be removed with 1.0.0", ReplaceWith("ch.tutteli.atrium.api.cc.en_UK.creating.iterable.contains.builders.IterableContainsNotOrAtMostCheckerBuilder"))
+open class IterableContainsNotOrAtMostCheckerBuilder<out E, out T : Iterable<E>>(
+    times: Int,
+    containsBuilder: IterableContainsBuilder<E, T, IterableContainsInAnyOrderSearchBehaviour>
+) : ch.tutteli.atrium.api.cc.en_UK.creating.iterable.contains.builders.IterableContainsNotOrAtMostCheckerBuilder<E, T>(
+    times,
+    containsBuilder
+)
---
+++ apis/atrium-api-cc-infix-en_UK/src/main/kotlin/ch/tutteli/atrium/api/cc/infix/en_UK/assertions/iterable/contains/builders/IterableContainsButAtMostCheckerBuilder.kt
@@ -0,0 +1,27 @@
+package ch.tutteli.atrium.api.cc.infix.en_UK.assertions.iterable.contains.builders
+
+import ch.tutteli.atrium.creating.iterable.contains.builders.IterableContainsBuilder
+import ch.tutteli.atrium.creating.iterable.contains.searchbehaviours.IterableContainsInAnyOrderSearchBehaviour
+
+/**
+ * Represents the builder of the second step of a `contains at least but at most` check within the
+ * fluent API of a sophisticated `contains` assertion for [Iterable].
+ *
+ * @param T The input type of the search.
+ *
+ * @constructor Represents the builder of the second step of a `contains at least but at most` check within the
+ *   fluent API of a sophisticated `contains` assertion for [Iterable].
+ * @param times The number which the check will compare against the actual number of times an expected entry is
+ *   found in the [Iterable].
+ * @param containsBuilder The previously used [IterableContainsBuilder].
+ */
+@Deprecated("use the builder from the package creating, will be removed with 1.0.0", ReplaceWith("ch.tutteli.atrium.api.cc.infix.en_UK.creating.iterable.contains.builders.IterableContainsButAtMostCheckerBuilder"))
+open class IterableContainsButAtMostCheckerBuilder<out E, out T : Iterable<E>>(
+    times: Int,
+    atLeastBuilder: IterableContainsAtLeastCheckerBuilder<E, T>,
+    containsBuilder: IterableContainsBuilder<E, T, IterableContainsInAnyOrderSearchBehaviour>
+) : ch.tutteli.atrium.api.cc.infix.en_UK.creating.iterable.contains.builders.IterableContainsButAtMostCheckerBuilder<E, T>(
+    times,
+    atLeastBuilder,
+    containsBuilder
+)
