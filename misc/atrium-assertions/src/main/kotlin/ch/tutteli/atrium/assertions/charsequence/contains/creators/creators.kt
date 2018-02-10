9a4123dc384efcba0aed08ad7b1a27467ea161d1
Robert Stoll <rstoll@tutteli.ch>
2018-02-10 15:35:34 +0100

    improve error message for contains and object

--- misc/atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/charsequence/contains/creators/creators.kt
+++ misc/atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/charsequence/contains/creators/creators.kt
@@ -46,7 +46,7 @@ private fun <T : CharSequence, S : CharSequenceContains.SearchBehaviour> checkOn
 ): AssertionGroup {
     listOf(expected, *otherExpected).forEach {
         require(it is CharSequence || it is Number || it is Char) {
-            "Only CharSequence, Number and Char are allowed, `$it` given.\nWe provide an API with Any for convenience (so that you can mix String and Int for instance)."
+            "Only CharSequence, Number and Char are allowed, `$it` given.\nWe provide an API with Any for convenience (so that you can mix String and Int for instance).\nUse toString() if you really want to search for its toString()-representation."
         }
     }
     return createAssertionGroup(checker, searcher, expected, otherExpected)
