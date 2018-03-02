b87d7fcaee494945f01e4b51f99321b9d3f8a901
Robert Stoll <rstoll@tutteli.ch>
2018-03-02 14:35:39 +0100

    fail if bc test discovery failed
    The junit gradle plugin does not fail if test discovery fails. Yet, we
    need to know if there have been problems loading bc tests. Thus:
    
    - catch Throwables during discovery and add a fake descriptor with a
      test which always fails.

--- misc/atrium-bc-test/src/main/kotlin/ch/tutteli/atrium/spec/DeprecationTestEngine.kt
+++ misc/atrium-bc-test/src/main/kotlin/ch/tutteli/atrium/spec/DeprecationTestEngine.kt
@@ -2,8 +2,11 @@ package ch.tutteli.atrium.spec
 
 import org.jetbrains.spek.engine.Scope
 import org.jetbrains.spek.engine.SpekEngineDescriptor
+import org.jetbrains.spek.engine.SpekExecutionContext
 import org.jetbrains.spek.engine.SpekTestEngine
 import org.junit.platform.engine.*
+import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor
+import org.junit.platform.engine.support.hierarchical.Node
 
 class DeprecationTestEngine : TestEngine {
     private val spek = SpekTestEngine()
@@ -12,13 +15,21 @@ class DeprecationTestEngine : TestEngine {
     override fun execute(request: ExecutionRequest) = spek.execute(request)
 
     override fun discover(discoveryRequest: EngineDiscoveryRequest, uniqueId: UniqueId): TestDescriptor {
-        val descriptor = spek.discover(discoveryRequest, uniqueId) as SpekEngineDescriptor
-
-        val forgive = discoveryRequest.configurationParameters.get("forgive").orElse(null)
-        if (forgive != null) {
-            exchangeWithForgivingTests(descriptor, Regex(forgive))
+        return try {
+            val descriptor = spek.discover(discoveryRequest, uniqueId) as SpekEngineDescriptor
+            val forgive = discoveryRequest.configurationParameters.get("forgive").orElse(null)
+            if (forgive != null) {
+                exchangeWithForgivingTests(descriptor, Regex(forgive))
+            }
+            descriptor
+        } catch (t: Throwable) {
+            //since the junit gradle platform does not treat an error during discovery as failure, we have return a
+            // fake descriptor which fails
+            // TODO check if this changes with https://github.com/junit-team/junit5/issues/1298
+            val descriptor = SpekEngineDescriptor(uniqueId)
+            descriptor.addChild(FailingTest(uniqueId.append("discovery", "discovering tests"), t))
+            descriptor
         }
-        return descriptor
     }
 
     private fun exchangeWithForgivingTests(descriptor: TestDescriptor, testToForgive: Regex) {
@@ -35,6 +46,18 @@ class DeprecationTestEngine : TestEngine {
                 }
             }
             exchangeWithForgivingTests(child, testToForgive)
+        }
+    }
+
+    private class FailingTest(uniqueId: UniqueId, private val throwable: Throwable) :
+        AbstractTestDescriptor(uniqueId, "discovering tests"),
+        Node<SpekExecutionContext> {
+        override fun getType() = TestDescriptor.Type.TEST
+        override fun execute(
+            context: SpekExecutionContext?,
+            dynamicTestExecutor: Node.DynamicTestExecutor?
+        ): SpekExecutionContext {
+            throw AssertionError(throwable)
         }
     }
 }
