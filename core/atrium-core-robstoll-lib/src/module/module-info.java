80f5b6ad975f697b9eaa641f2538d37bf78db853
Robert Stoll <rstoll@tutteli.ch>
2018-03-15 21:22:32 +0100

    define module-info.java and include it into jars
    Unfortunately I cannot yet define module-info.java for all modules
    because some of them contain deprecated code which causes split
    packages.

---
+++ core/atrium-core-robstoll-lib/src/module/module-info.java
@@ -0,0 +1,9 @@
+module ch.tutteli.atrium.core.robstoll.lib {
+
+    requires ch.tutteli.atrium.core.api;
+
+    exports ch.tutteli.atrium.core.robstoll.lib.checking;
+    exports ch.tutteli.atrium.core.robstoll.lib.creating;
+    exports ch.tutteli.atrium.core.robstoll.lib.reporting;
+    exports ch.tutteli.atrium.core.robstoll.lib.reporting.translating;
+}
---
+++ translations/atrium-translations-de_CH/src/module/module-info.java
@@ -0,0 +1,6 @@
+module ch.tutteli.atrium.translations.de_CH {
+    requires kotlin.stdlib;
+    requires ch.tutteli.atrium.core.api;
+
+    exports ch.tutteli.atrium.translations;
+}
