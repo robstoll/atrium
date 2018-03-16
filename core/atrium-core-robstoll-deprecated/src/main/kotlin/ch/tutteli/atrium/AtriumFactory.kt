fa8a9ff50772efdc16195fbe26b6b73d397b38a6
Robert Stoll <rstoll@tutteli.ch>
2018-03-16 15:18:58 +0100

    move deprecated core to own module
    Moreover:
    - remove core-api-late-binding
    - turn core-robstoll into module

--- core/atrium-core-robstoll-deprecated/src/main/kotlin/ch/tutteli/atrium/AtriumFactory.kt
+++ core/atrium-core-robstoll-deprecated/src/main/kotlin/ch/tutteli/atrium/AtriumFactory.kt
@@ -1,8 +1,8 @@
-package ch.tutteli.atrium.core.robstoll
+package ch.tutteli.atrium
 
 import ch.tutteli.atrium.assertions.BulletPointIdentifier
 import ch.tutteli.atrium.checking.AssertionChecker
-import ch.tutteli.atrium.core.CoreFactory
+import ch.tutteli.atrium.core.coreFactory
 import ch.tutteli.atrium.core.robstoll.lib.checking.DelegatingAssertionChecker
 import ch.tutteli.atrium.core.robstoll.lib.checking.FeatureAssertionChecker
 import ch.tutteli.atrium.core.robstoll.lib.checking.ThrowingAssertionChecker
@@ -22,79 +22,72 @@ import ch.tutteli.atrium.reporting.translating.Translator
 import java.util.*
 
 /**
- * Robstoll's `abstract factory` for atrium-core.
- *
- * It provides factory methods to create:
- * - [AssertionPlant]
- * - [AssertionPlantNullable]
- * - [CheckingAssertionPlant]
- * - [CollectingAssertionPlant]
- * - [AssertionChecker]
- * - [MethodCallFormatter]
- * - [Translator]
- * - [TranslationSupplier]
- * - [CoroutineBasedLocaleOrderDecider]
- * - [ObjectFormatter]
- * - [AssertionFormatterFacade]
- * - [AssertionFormatterController]
- * - [AssertionFormatter]
- * - [AssertionPairFormatter]
- * - [Reporter]
+ * Deprecated, use [coreFactory] instead (uses [ServiceLoader]), will be removed with 1.0.0
  */
-class CoreFactoryImpl : CoreFactory {
+@Deprecated("use coreFactory instead, will be removed with 1.0.0", ReplaceWith("coreFactory", "ch.tutteli.atrium.core.coreFactory"))
+object AtriumFactory : IAtriumFactory {
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newReportingPlant(commonFields)"))
     override fun <T : Any> newReportingPlant(commonFields: AssertionPlantWithCommonFields.CommonFields<T>): ReportingAssertionPlant<T>
         = ReportingAssertionPlantImpl(commonFields)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newReportingPlantNullable(commonFields)"))
     override fun <T : Any?> newReportingPlantNullable(commonFields: AssertionPlantWithCommonFields.CommonFields<T>): ReportingAssertionPlantNullable<T>
         = ReportingAssertionPlantNullableImpl(commonFields)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newCheckingPlant(subject)"))
     override fun <T : Any> newCheckingPlant(subject: T): CheckingAssertionPlant<T>
         = CheckingAssertionPlantImpl(subject)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newCollectingPlant(subjectProvider)"))
     override fun <T : Any> newCollectingPlant(subjectProvider: () -> T): CollectingAssertionPlant<T>
         = CollectingAssertionPlantImpl(subjectProvider)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newThrowingAssertionChecker(reporter)"))
     override fun newThrowingAssertionChecker(reporter: Reporter): AssertionChecker
         = ThrowingAssertionChecker(reporter)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newFeatureAssertionChecker(subjectPlant)"))
     override fun <T : Any> newFeatureAssertionChecker(subjectPlant: AssertionPlant<T>): AssertionChecker
         = FeatureAssertionChecker(subjectPlant)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newDelegatingAssertionChecker(subjectPlant)"))
     override fun <T : Any?> newDelegatingAssertionChecker(subjectPlant: BaseAssertionPlant<T, *>): AssertionChecker
         = DelegatingAssertionChecker(subjectPlant)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newMethodCallFormatter()"))
     override fun newMethodCallFormatter(): MethodCallFormatter
         = TextMethodCallFormatter
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTranslator(translationSupplier, localeOrderDecider, primaryLocale, *fallbackLocales)"))
     override fun newTranslator(translationSupplier: TranslationSupplier, localeOrderDecider: LocaleOrderDecider, primaryLocale: Locale, vararg fallbackLocales: Locale): Translator
-        = TranslationSupplierBasedTranslator(
-        translationSupplier,
-        localeOrderDecider,
-        primaryLocale,
-        fallbackLocales
-    )
+        = TranslationSupplierBasedTranslator(translationSupplier, localeOrderDecider, primaryLocale, fallbackLocales)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newPropertiesBasedTranslationSupplier()"))
     override fun newPropertiesBasedTranslationSupplier(): TranslationSupplier
         = PropertiesPerEntityAndLocaleTranslationSupplier()
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newLocaleOrderDecider()"))
     override fun newLocaleOrderDecider(): LocaleOrderDecider
         = CoroutineBasedLocaleOrderDecider()
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newDetailedObjectFormatter(translator)"))
     override fun newDetailedObjectFormatter(translator: Translator): ObjectFormatter
         = DetailedObjectFormatter(translator)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newAssertionFormatterController()"))
     override fun newAssertionFormatterController(): AssertionFormatterController
         = AssertionFormatterControllerImpl()
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newAssertionFormatterFacade(assertionFormatterController)"))
     override fun newAssertionFormatterFacade(assertionFormatterController: AssertionFormatterController): AssertionFormatterFacade
-        = AssertionFormatterControllerBasedFacade(
-        assertionFormatterController
-    )
+        = AssertionFormatterControllerBasedFacade(assertionFormatterController)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextSameLineAssertionPairFormatter(objectFormatter, translator)"))
     override fun newTextSameLineAssertionPairFormatter(objectFormatter: ObjectFormatter, translator: Translator)
         = TextSameLineAssertionPairFormatter(objectFormatter, translator)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextFallbackAssertionFormatter(bulletPoints, assertionFormatterController, objectFormatter, translator)"))
     override fun newTextFallbackAssertionFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController, objectFormatter: ObjectFormatter, translator: Translator): AssertionFormatter
         = TextFallbackAssertionFormatter(
         bulletPoints,
@@ -103,6 +96,7 @@ class CoreFactoryImpl : CoreFactory {
         objectFormatter
     )
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextFeatureAssertionGroupFormatter(bulletPoints, assertionFormatterController, objectFormatter, translator)"))
     override fun newTextFeatureAssertionGroupFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController, objectFormatter: ObjectFormatter, translator: Translator): AssertionFormatter
         = TextFeatureAssertionGroupFormatter(
         bulletPoints,
@@ -110,6 +104,7 @@ class CoreFactoryImpl : CoreFactory {
         newTextSameLineAssertionPairFormatter(objectFormatter, translator)
     )
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextListAssertionGroupFormatter(bulletPoints, assertionFormatterController, objectFormatter, translator)"))
     override fun newTextListAssertionGroupFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController, objectFormatter: ObjectFormatter, translator: Translator): AssertionFormatter
         = TextListAssertionGroupFormatter(
         bulletPoints,
@@ -117,12 +112,11 @@ class CoreFactoryImpl : CoreFactory {
         newTextSameLineAssertionPairFormatter(objectFormatter, translator)
     )
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextExplanatoryAssertionGroupFormatter(bulletPoints, assertionFormatterController)"))
     override fun newTextExplanatoryAssertionGroupFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController): AssertionFormatter
-        = TextExplanatoryAssertionGroupFormatter(
-        bulletPoints,
-        assertionFormatterController
-    )
+        = TextExplanatoryAssertionGroupFormatter(bulletPoints, assertionFormatterController)
 
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.registerTextAssertionFormatterCapabilities(bulletPoints, assertionFormatterFacade, textAssertionPairFormatter, objectFormatter, translator)"))
     override fun registerTextAssertionFormatterCapabilities(
         bulletPoints: Map<Class<out BulletPointIdentifier>, String>,
         assertionFormatterFacade: AssertionFormatterFacade,
@@ -131,50 +125,28 @@ class CoreFactoryImpl : CoreFactory {
         translator: Translator
     ) {
         assertionFormatterFacade.register {
-            TextListAssertionGroupFormatter(
-                bulletPoints,
-                it,
-                textAssertionPairFormatter
-            )
+            TextListAssertionGroupFormatter(bulletPoints, it, textAssertionPairFormatter)
         }
         assertionFormatterFacade.register {
-            TextFeatureAssertionGroupFormatter(
-                bulletPoints,
-                it,
-                textAssertionPairFormatter
-            )
+            TextFeatureAssertionGroupFormatter(bulletPoints, it, textAssertionPairFormatter)
         }
         assertionFormatterFacade.register(::InvisibleAssertionGroupFormatter)
+
         assertionFormatterFacade.register {
-            TextExplanatoryAssertionGroupFormatter(
-                bulletPoints,
-                it
-            )
+            TextExplanatoryAssertionGroupFormatter(bulletPoints, it)
         }
         assertionFormatterFacade.register {
-            TextIndentAssertionGroupFormatter(
-                bulletPoints,
-                it
-            )
+            TextIndentAssertionGroupFormatter(bulletPoints, it)
         }
         assertionFormatterFacade.register {
-            TextSummaryAssertionGroupFormatter(
-                bulletPoints,
-                it,
-                textAssertionPairFormatter
-            )
+            TextSummaryAssertionGroupFormatter(bulletPoints, it, textAssertionPairFormatter)
         }
         assertionFormatterFacade.register {
-            TextFallbackAssertionFormatter(
-                bulletPoints,
-                it,
-                textAssertionPairFormatter,
-                objectFormatter
-            )
+            TextFallbackAssertionFormatter(bulletPoints, it, textAssertionPairFormatter, objectFormatter)
         }
     }
 
-
+    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newOnlyFailureReporter(assertionFormatterFacade)"))
     override fun newOnlyFailureReporter(assertionFormatterFacade: AssertionFormatterFacade): Reporter
         = OnlyFailureReporter(assertionFormatterFacade)
 }
--- core/atrium-core-robstoll/src/main/kotlin/ch/tutteli/atrium/robstoll/core/CoreFactoryImpl.kt
+++ core/atrium-core-robstoll/src/main/kotlin/ch/tutteli/atrium/robstoll/core/CoreFactoryImpl.kt
@@ -1,8 +1,8 @@
-package ch.tutteli.atrium
+package ch.tutteli.atrium.robstoll.core
 
 import ch.tutteli.atrium.assertions.BulletPointIdentifier
 import ch.tutteli.atrium.checking.AssertionChecker
-import ch.tutteli.atrium.core.coreFactory
+import ch.tutteli.atrium.core.CoreFactory
 import ch.tutteli.atrium.core.robstoll.lib.checking.DelegatingAssertionChecker
 import ch.tutteli.atrium.core.robstoll.lib.checking.FeatureAssertionChecker
 import ch.tutteli.atrium.core.robstoll.lib.checking.ThrowingAssertionChecker
@@ -22,44 +22,51 @@ import ch.tutteli.atrium.reporting.translating.Translator
 import java.util.*
 
 /**
- * Deprecated, use [coreFactory] instead (uses [ServiceLoader]), will be removed with 1.0.0
+ * Robstoll's `abstract factory` for atrium-core.
+ *
+ * It provides factory methods to create:
+ * - [AssertionPlant]
+ * - [AssertionPlantNullable]
+ * - [CheckingAssertionPlant]
+ * - [CollectingAssertionPlant]
+ * - [AssertionChecker]
+ * - [MethodCallFormatter]
+ * - [Translator]
+ * - [TranslationSupplier]
+ * - [CoroutineBasedLocaleOrderDecider]
+ * - [ObjectFormatter]
+ * - [AssertionFormatterFacade]
+ * - [AssertionFormatterController]
+ * - [AssertionFormatter]
+ * - [AssertionPairFormatter]
+ * - [Reporter]
  */
-@Deprecated("use coreFactory instead, will be removed with 1.0.0", ReplaceWith("coreFactory", "ch.tutteli.atrium.core.coreFactory"))
-object AtriumFactory : IAtriumFactory {
+class CoreFactoryImpl : CoreFactory {
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newReportingPlant(commonFields)"))
     override fun <T : Any> newReportingPlant(commonFields: AssertionPlantWithCommonFields.CommonFields<T>): ReportingAssertionPlant<T>
         = ReportingAssertionPlantImpl(commonFields)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newReportingPlantNullable(commonFields)"))
     override fun <T : Any?> newReportingPlantNullable(commonFields: AssertionPlantWithCommonFields.CommonFields<T>): ReportingAssertionPlantNullable<T>
         = ReportingAssertionPlantNullableImpl(commonFields)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newCheckingPlant(subject)"))
     override fun <T : Any> newCheckingPlant(subject: T): CheckingAssertionPlant<T>
         = CheckingAssertionPlantImpl(subject)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newCollectingPlant(subjectProvider)"))
     override fun <T : Any> newCollectingPlant(subjectProvider: () -> T): CollectingAssertionPlant<T>
         = CollectingAssertionPlantImpl(subjectProvider)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newThrowingAssertionChecker(reporter)"))
     override fun newThrowingAssertionChecker(reporter: Reporter): AssertionChecker
         = ThrowingAssertionChecker(reporter)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newFeatureAssertionChecker(subjectPlant)"))
     override fun <T : Any> newFeatureAssertionChecker(subjectPlant: AssertionPlant<T>): AssertionChecker
         = FeatureAssertionChecker(subjectPlant)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newDelegatingAssertionChecker(subjectPlant)"))
     override fun <T : Any?> newDelegatingAssertionChecker(subjectPlant: BaseAssertionPlant<T, *>): AssertionChecker
         = DelegatingAssertionChecker(subjectPlant)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newMethodCallFormatter()"))
     override fun newMethodCallFormatter(): MethodCallFormatter
         = TextMethodCallFormatter
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTranslator(translationSupplier, localeOrderDecider, primaryLocale, *fallbackLocales)"))
     override fun newTranslator(translationSupplier: TranslationSupplier, localeOrderDecider: LocaleOrderDecider, primaryLocale: Locale, vararg fallbackLocales: Locale): Translator
         = TranslationSupplierBasedTranslator(
         translationSupplier,
@@ -68,33 +75,26 @@ object AtriumFactory : IAtriumFactory {
         fallbackLocales
     )
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newPropertiesBasedTranslationSupplier()"))
     override fun newPropertiesBasedTranslationSupplier(): TranslationSupplier
         = PropertiesPerEntityAndLocaleTranslationSupplier()
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newLocaleOrderDecider()"))
     override fun newLocaleOrderDecider(): LocaleOrderDecider
         = CoroutineBasedLocaleOrderDecider()
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newDetailedObjectFormatter(translator)"))
     override fun newDetailedObjectFormatter(translator: Translator): ObjectFormatter
         = DetailedObjectFormatter(translator)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newAssertionFormatterController()"))
     override fun newAssertionFormatterController(): AssertionFormatterController
         = AssertionFormatterControllerImpl()
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newAssertionFormatterFacade(assertionFormatterController)"))
     override fun newAssertionFormatterFacade(assertionFormatterController: AssertionFormatterController): AssertionFormatterFacade
         = AssertionFormatterControllerBasedFacade(
         assertionFormatterController
     )
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextSameLineAssertionPairFormatter(objectFormatter, translator)"))
     override fun newTextSameLineAssertionPairFormatter(objectFormatter: ObjectFormatter, translator: Translator)
         = TextSameLineAssertionPairFormatter(objectFormatter, translator)
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextFallbackAssertionFormatter(bulletPoints, assertionFormatterController, objectFormatter, translator)"))
     override fun newTextFallbackAssertionFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController, objectFormatter: ObjectFormatter, translator: Translator): AssertionFormatter
         = TextFallbackAssertionFormatter(
         bulletPoints,
@@ -103,7 +103,6 @@ object AtriumFactory : IAtriumFactory {
         objectFormatter
     )
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextFeatureAssertionGroupFormatter(bulletPoints, assertionFormatterController, objectFormatter, translator)"))
     override fun newTextFeatureAssertionGroupFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController, objectFormatter: ObjectFormatter, translator: Translator): AssertionFormatter
         = TextFeatureAssertionGroupFormatter(
         bulletPoints,
@@ -111,7 +110,6 @@ object AtriumFactory : IAtriumFactory {
         newTextSameLineAssertionPairFormatter(objectFormatter, translator)
     )
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextListAssertionGroupFormatter(bulletPoints, assertionFormatterController, objectFormatter, translator)"))
     override fun newTextListAssertionGroupFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController, objectFormatter: ObjectFormatter, translator: Translator): AssertionFormatter
         = TextListAssertionGroupFormatter(
         bulletPoints,
@@ -119,14 +117,12 @@ object AtriumFactory : IAtriumFactory {
         newTextSameLineAssertionPairFormatter(objectFormatter, translator)
     )
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newTextExplanatoryAssertionGroupFormatter(bulletPoints, assertionFormatterController)"))
     override fun newTextExplanatoryAssertionGroupFormatter(bulletPoints: Map<Class<out BulletPointIdentifier>, String>, assertionFormatterController: AssertionFormatterController): AssertionFormatter
         = TextExplanatoryAssertionGroupFormatter(
         bulletPoints,
         assertionFormatterController
     )
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.registerTextAssertionFormatterCapabilities(bulletPoints, assertionFormatterFacade, textAssertionPairFormatter, objectFormatter, translator)"))
     override fun registerTextAssertionFormatterCapabilities(
         bulletPoints: Map<Class<out BulletPointIdentifier>, String>,
         assertionFormatterFacade: AssertionFormatterFacade,
@@ -178,7 +174,7 @@ object AtriumFactory : IAtriumFactory {
         }
     }
 
-    @Deprecated("use coreFactory, will be removed with 1.0.0", ReplaceWith("coreFactory.newOnlyFailureReporter(assertionFormatterFacade)"))
+
     override fun newOnlyFailureReporter(assertionFormatterFacade: AssertionFormatterFacade): Reporter
         = OnlyFailureReporter(assertionFormatterFacade)
 }
