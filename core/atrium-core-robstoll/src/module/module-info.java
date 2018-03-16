7f38c71d18b0985e18ce7c6077c568efbe665822
Robert Stoll <rstoll@tutteli.ch>
2018-03-16 15:18:58 +0100

    move deprecated core to own module
    Moreover:
    - remove core-api-late-binding
    - turn core-robstoll into module

---
+++ core/atrium-core-robstoll/src/module/module-info.java
@@ -0,0 +1,7 @@
+module ch.tutteli.atrium.core.robstoll {
+
+    requires transitive ch.tutteli.atrium.core.api;
+    requires ch.tutteli.atrium.core.robstoll.lib;
+
+    exports ch.tutteli.atrium.robstoll.core;
+}
