92b6a6fcb2b89a2f9d1f0ba238c47a2c2e4ce77b
Robert Stoll <rstoll@tutteli.ch>
2018-03-17 23:09:36 +0100

    spec for SingleServiceLoader

--- core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/core/SingleServiceLoader.kt
+++ core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/core/SingleServiceLoader.kt
@@ -6,7 +6,7 @@ object SingleServiceLoader {
     fun <T : Any> load(clazz: Class<T>): T {
         val itr = ServiceLoader.load(clazz).iterator()
         check(itr.hasNext()) {
-            "Could not find an implementation for ${clazz.name}"
+            "Could not find any implementation for ${clazz.name}"
         }
         val service = itr.next()
         check(!itr.hasNext()) {
@@ -15,7 +15,7 @@ object SingleServiceLoader {
                 sb.appendln()
                 sb.append(it::class.java.name)
             }
-            "Currently we do not support multiple implementations of ${clazz.name}, but found:\n${service::class.java.name}$sb"
+            "Found more than one implementation of ${clazz.name}, but found:\n${service::class.java.name}$sb"
         }
         return service
     }
