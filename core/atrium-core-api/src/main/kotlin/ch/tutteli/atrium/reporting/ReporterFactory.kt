6c35a5b483596a7eeb8277cc7e3d771c351baa05
Robert Stoll <rstoll@tutteli.ch>
2018-05-01 22:54:42 +0200

    use own ReporterFactory for cc-de-CH

--- core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/reporting/ReporterFactory.kt
+++ core/atrium-core-api/src/main/kotlin/ch/tutteli/atrium/reporting/ReporterFactory.kt
@@ -6,14 +6,17 @@ import java.util.*
  * The access point to an implementation of [Reporter].
  *
  * It loads implementations of [ReporterFactory] lazily via [ServiceLoader] and searches for the id specified via the
- * system property `ch.tutteli.atrium.reporting.reporterFactory` or uses `default` in case the system property is not specified.
+ * system property with key [ReporterFactory.SYSTEM_PROPERTY] (which is `ch.tutteli.atrium.reporting.reporterFactory`)
+ * or uses `default` in case the system property is not specified.
+ *
+ * Use [ReporterFactory.specifyFactory] or [ReporterFactory.specifyFactoryIfNotYetSet] to define the system property.
+ *
+ * Notice, that searching for a [ReporterFactory] is only done once and the result is cached afterwards.
+ * Please [open an issue](https://github.com/robstoll/atrium/issues/new) if you want to change reporter during a test-run.
  */
 val reporter by lazy {
-    val id = System.getProperty("ch.tutteli.atrium.reporting.reporterFactory") ?: "default"
+    val id = System.getProperty(ReporterFactory.SYSTEM_PROPERTY) ?: "default"
     val itr = ServiceLoader.load(ReporterFactory::class.java).iterator()
-    if (itr.hasNext()) {
-        val i = 0;
-    }
     val factory = itr
         .asSequence()
         .firstOrNull { it.id == id }
@@ -39,4 +42,37 @@ interface ReporterFactory {
      * Creates a new [Reporter].
      */
     fun create(): Reporter
+
+    /**
+     * Provides utility functions to specify a [ReporterFactory].
+     */
+    companion object {
+        /**
+         * The key of the system property which is used to define which [ReporterFactory] shall be used.
+         * You can use [specifyFactory] or [specifyFactoryIfNotYetSet]
+         */
+        const val SYSTEM_PROPERTY = "ch.tutteli.atrium.reporting.reporterFactory"
+
+        /**
+         * Sets the system property with key [SYSTEM_PROPERTY] (which is `ch.tutteli.atrium.reporting.reporterFactory`)
+         * to the given [reporterFactoryId] regardless if another id was specified before.
+         *
+         * Use [specifyFactoryIfNotYetSet] if you only want to set a default value but not overwrite an existing
+         * specification.
+         */
+        fun specifyFactory(reporterFactoryId: String) = System.setProperty(SYSTEM_PROPERTY, reporterFactoryId)
+
+        /**
+         * Sets the system property with key [SYSTEM_PROPERTY] (which is `ch.tutteli.atrium.reporting.reporterFactory`)
+         * to the given [reporterFactoryId] if another id was not *yet* specified before.
+         *
+         * Use [specifyFactory] if you do not care if another id was specified before or in other words, if you want to
+         * overwrite a previously defined id.
+         */
+        fun specifyFactoryIfNotYetSet(reporterFactoryId: String) {
+            if (System.getProperty(SYSTEM_PROPERTY) == null) {
+                specifyFactory(reporterFactoryId)
+            }
+        }
+    }
 }
