b80801c50b02ee01d9a006ffe9dca4c7d97a3dd5
Robert Stoll <rstoll@tutteli.ch>
2018-05-22 18:48:29 +0200

    move grouped nullable entries spec to non-nullable spec
    ...and remove IterableContainsInOrderOnlyGroupedNullableAssertionsSpec
    afterwards

--- apis/atrium-api-cc-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/en_GB/IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec.kt
+++ apis/atrium-api-cc-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/en_GB/IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec.kt
@@ -1,6 +1,7 @@
 package ch.tutteli.atrium.api.cc.en_GB
 
 import ch.tutteli.atrium.creating.Assert
+import ch.tutteli.atrium.domain.builders.utils.GroupWithNullableEntries
 import ch.tutteli.atrium.domain.builders.utils.GroupWithoutNullableEntries
 import ch.tutteli.atrium.verbs.internal.AssertionVerbFactory
 
@@ -8,6 +9,8 @@ class IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec : ch.tutteli.atriu
     AssertionVerbFactory,
     getContainsPair(),
     Companion::groupFactory,
+    getContainsNullablePair(),
+    Companion::nullableGroupFactory,
     "◆ ", "✔ ", "✘ ", "❗❗ ", "⚬ ", "» ", "▶ ", "◾ ",
     "[Atrium][Builder] "
 ) {
@@ -29,6 +32,26 @@ class IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec : ch.tutteli.atriu
                 0 -> object : GroupWithoutNullableEntries<Assert<Double>.() -> Unit> { override fun toList() = listOf<Assert<Double>.() -> Unit>() }
                 1 -> Entry(groups[0])
                 else -> Entries(groups[0], *groups.drop(1).toTypedArray())
+            }
+        }
+
+        fun getContainsNullablePair()
+            = "$contains.$inOrder.$only.$inOrderOnlyEntries.$within.$withinInAnyOrder" to Companion::containsInOrderOnlyGroupedNullableEntriesPair
+
+        private fun containsInOrderOnlyGroupedNullableEntriesPair(
+            plant: Assert<Iterable<Double?>>,
+            a1: GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>,
+            a2: GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>,
+            aX: Array<out GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>>
+        ): Assert<Iterable<Double?>> {
+            return plant.contains.inOrder.only.grouped.within.inAnyOrder(a1, a2, *aX)
+        }
+
+        private fun nullableGroupFactory(groups: Array<out (Assert<Double>.() -> Unit)?>): GroupWithNullableEntries<(Assert<Double>.() -> Unit)?> {
+            return when(groups.size){
+                0 -> object: GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>{ override fun toList() = listOf<Assert<Double>.() -> Unit>() }
+                1 -> NullableEntry(groups[0])
+                else -> NullableEntries(groups[0], *groups.drop(1).toTypedArray())
             }
         }
     }
--- apis/atrium-api-cc-infix-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec.kt
+++ apis/atrium-api-cc-infix-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec.kt
@@ -2,6 +2,7 @@ package ch.tutteli.atrium.api.cc.infix.en_GB
 
 import ch.tutteli.atrium.api.cc.infix.en_GB.keywords.*
 import ch.tutteli.atrium.creating.Assert
+import ch.tutteli.atrium.domain.builders.utils.GroupWithNullableEntries
 import ch.tutteli.atrium.domain.builders.utils.GroupWithoutNullableEntries
 import ch.tutteli.atrium.verbs.internal.AssertionVerbFactory
 
@@ -9,6 +10,8 @@ class IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec : ch.tutteli.atriu
     AssertionVerbFactory,
     getContainsPair(),
     Companion::groupFactory,
+    getContainsNullablePair(),
+    Companion::nullableGroupFactory,
     "◆ ", "✔ ", "✘ ", "❗❗ ", "⚬ ", "» ", "▶ ", "◾ ",
     "[Atrium][Builder] "
 ) {
@@ -30,6 +33,26 @@ class IterableContainsInOrderOnlyGroupedEntriesAssertionsSpec : ch.tutteli.atriu
                 0 -> object : GroupWithoutNullableEntries<Assert<Double>.() -> Unit>{ override fun toList() = listOf<Assert<Double>.() -> Unit>() }
                 1 -> Entry(groups[0])
                 else -> Entries(groups[0], *groups.drop(1).toTypedArray())
+            }
+        }
+
+        fun getContainsNullablePair()
+            = "$toContain $inOrder $butOnly $inOrderOnlyEntries $withinGroup $withinInAnyOrder" to Companion::containsInOrderOnlyGroupedNullableEntriesPair
+
+        private fun containsInOrderOnlyGroupedNullableEntriesPair(
+            plant: Assert<Iterable<Double?>>,
+            a1: GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>,
+            a2: GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>,
+            aX: Array<out GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>>
+        ): Assert<Iterable<Double?>> {
+            return plant to contain inGiven order and only grouped entries within group inAny Order(a1, a2, *aX)
+        }
+
+        private fun nullableGroupFactory(groups: Array<out (Assert<Double>.() -> Unit)?>): GroupWithNullableEntries<(Assert<Double>.() -> Unit)?> {
+            return when(groups.size){
+                0 -> object: GroupWithNullableEntries<(Assert<Double>.() -> Unit)?>{ override fun toList() = listOf<Assert<Double>.() -> Unit>() }
+                1 -> NullableEntry(groups[0])
+                else -> NullableEntries(groups[0], *groups.drop(1).toTypedArray())
             }
         }
     }
