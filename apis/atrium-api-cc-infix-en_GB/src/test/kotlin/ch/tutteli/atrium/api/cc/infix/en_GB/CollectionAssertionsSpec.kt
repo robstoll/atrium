be7b28a9258def1fb1bb755042238e0b4c27304a
Robert Stoll <rstoll@tutteli.ch>
2018-05-25 13:12:24 +0200

    use infix version in CollectionAssertionsSpec fo the infix API

--- apis/atrium-api-cc-infix-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/CollectionAssertionsSpec.kt
+++ apis/atrium-api-cc-infix-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/CollectionAssertionsSpec.kt
@@ -6,15 +6,18 @@ import ch.tutteli.atrium.creating.Assert
 
 class CollectionAssertionsSpec : ch.tutteli.atrium.spec.integration.CollectionAssertionsSpec(
     AssertionVerbFactory,
-    Assert<List<Int>>::hasSize.name to Assert<List<Int>>::hasSize,
+    Assert<List<Int>>::hasSize.name to Companion::hasSize,
     "${Assert<List<Int>>::toBe.name} ${Empty::class.simpleName}" to Companion::isEmpty,
     "${Assert<List<Int>>::notToBe.name} ${Empty::class.simpleName}" to Companion::isNotEmpty
 ) {
     companion object {
-        fun isEmpty(plant: Assert<List<Int>>)
+        private fun hasSize(plant: Assert<List<Int>>, size: Int): Assert<List<Int>>
+            = plant hasSize size
+
+        private fun isEmpty(plant: Assert<List<Int>>)
             = plant toBe Empty
 
-        fun isNotEmpty(plant: Assert<List<Int>>)
+        private fun isNotEmpty(plant: Assert<List<Int>>)
             = plant notToBe Empty
     }
 }
