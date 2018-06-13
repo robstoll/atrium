f16c40e836a91273e6259a26a4fb7a68e2926699
Robert Stoll <rstoll@tutteli.ch>
2018-06-14 00:38:42 +0200

    make subjectProvider a property of BaseAssertionPlant
    I think it is beneficial if we do not have to make a copy of the
    subjectProvider to often. Instead we should reuse the same one, thus
    make it a field of BaseAssertionPlant
    
    - turn subjectProvider to public in CommonFields
    - implement subjectProvider in MutableListBasedAssertionPlant
      - remove from CheckingAssertionPlantImpl and
        CollectingAssertionPlantImpl
    - remove extension function toSubjectProvider (one can now use the
      property)

--- core/atrium-core-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/core/robstoll/lib/creating/CollectingAssertionPlantImpl.kt
+++ core/atrium-core-robstoll-lib/src/main/kotlin/ch/tutteli/atrium/core/robstoll/lib/creating/CollectingAssertionPlantImpl.kt
@@ -4,12 +4,11 @@ import ch.tutteli.atrium.creating.AssertionPlant
 import ch.tutteli.atrium.creating.CollectingAssertionPlant
 
 class CollectingAssertionPlantImpl<out T : Any>(
-    private val subjectProvider: () -> T
-) : MutableListBasedAssertionPlant<T, AssertionPlant<T>>(),
+    subjectProvider: () -> T
+) : MutableListBasedAssertionPlant<T, AssertionPlant<T>>(subjectProvider),
     CollectingAssertionPlant<T> {
 
     override val self = this
-    override val subject by lazy { subjectProvider() }
 
     override fun addAssertionsCreatedBy(assertionCreator: AssertionPlant<T>.() -> Unit): CollectingAssertionPlant<T> {
         this.assertionCreator()
