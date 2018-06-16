ca21c41ba92320f0579ed42ecb0b03ee7522192c
Robert Stoll <rstoll@tutteli.ch>
2018-06-16 12:04:35 +0200

    introduce asIterable for array types (Array, ByteArray, IntArray...)
    - fixes #11

---
+++ apis/atrium-api-cc-infix-en_GB/src/test/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/ArrayAsIterableAssertionsSpec.kt
@@ -0,0 +1,18 @@
+package ch.tutteli.atrium.api.cc.infix.en_GB
+
+import ch.tutteli.atrium.creating.Assert
+import ch.tutteli.atrium.verbs.internal.AssertionVerbFactory
+
+object ArrayAsIterableAssertionsSpec : ch.tutteli.atrium.spec.integration.ArrayAsIterableAssertionsSpec(
+    AssertionVerbFactory,
+    Assert<Array<Int>>::asIterable.name,
+    Assert<Array<Int>>::asIterable,
+    Assert<ByteArray>::asIterable,
+    Assert<CharArray>::asIterable,
+    Assert<ShortArray>::asIterable,
+    Assert<IntArray>::asIterable,
+    Assert<LongArray>::asIterable,
+    Assert<FloatArray>::asIterable,
+    Assert<DoubleArray>::asIterable,
+    Assert<BooleanArray>::asIterable
+)
