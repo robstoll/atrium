7b53e596dd5211987dddc1ecca5c87f7d5ad3e41
Robert Stoll <rstoll@tutteli.ch>
2018-06-23 23:03:52 +0200

    fix getDefaultLocale() in JVM, should use toAtriumLocale
    Currently it just passes the value of java.util.Locale to Atrium's
    Locale which leads to errors since Atrium's Locale uses `null` instead
    of empty strings to represent an absent configuration.
    
    Moreover: so war we have used Class.name in all occasions in
    DetailedObjectFormatter. This due to the fact that Java's primitive
    `int` and the boxed type `Integer` are both represented as kotlin.Int
    I still think it is a good idea if one sees the difference but since we
    expect Any? for `format` we receive the boxed type in all cases. Hence
    we can also just show the kotlin type in such cases. Which leads to the
    following:
    - make fullName return String instead of String?
    - use qualifiedName if KClass is not a primitive type and use java.name
      as backup if qualifiedName returns null (which is the case for
      anonymous classes)
      - add this use case to ObjectFormatterSpec

--- core/api/atrium-core-api-js/src/main/kotlin/ch/tutteli/atrium/core/polyfills/fullName.kt
+++ core/api/atrium-core-api-js/src/main/kotlin/ch/tutteli/atrium/core/polyfills/fullName.kt
@@ -2,4 +2,4 @@ package ch.tutteli.atrium.core.polyfills
 
 import kotlin.reflect.KClass
 
-actual val KClass<*>.fullName get(): String? = simpleName
+actual val KClass<*>.fullName get(): String = simpleName ?: "<unknown class name>"
--- core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/core/polyfills/fullName.kt
+++ core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/core/polyfills/fullName.kt
@@ -2,4 +2,11 @@ package ch.tutteli.atrium.core.polyfills
 
 import kotlin.reflect.KClass
 
-actual val KClass<*>.fullName get(): String? = qualifiedName
+/**
+ * Returns [KClass.java].[name][Class.name].
+ *
+ * In contrast to [KClass.qualifiedName], this shows if a primitive type is used or a boxed type
+ * (e.g. `int` vs. `Integer`).
+ */
+actual val KClass<*>.fullName: String
+    get() = if (!java.isPrimitive) qualifiedName ?: java.name else java.name
