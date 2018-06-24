a84e06792c1e0011bc82b1a3666b7bd6a180de52
Robert Stoll <rstoll@tutteli.ch>
2018-06-24 20:18:48 +0200

    rename klass to kClass

--- core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/core/polyfills/loadServices.kt
+++ core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/core/polyfills/loadServices.kt
@@ -4,8 +4,8 @@ import ch.tutteli.atrium.core.SingleServiceLoader
 import java.util.*
 import kotlin.reflect.KClass
 
-actual fun <T: Any> loadSingleService(klass: KClass<T>): T
-    = SingleServiceLoader.load(klass.java)
+actual fun <T: Any> loadSingleService(kClass: KClass<T>): T
+    = SingleServiceLoader.load(kClass.java)
 
-actual fun <T : Any> loadServices(klass: KClass<T>): Sequence<T>
-    = ServiceLoader.load(klass.java).iterator().asSequence()
+actual fun <T : Any> loadServices(kClass: KClass<T>): Sequence<T>
+    = ServiceLoader.load(kClass.java).iterator().asSequence()
