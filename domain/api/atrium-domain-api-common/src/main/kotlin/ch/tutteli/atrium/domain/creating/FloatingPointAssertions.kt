e85cb08165dea6e833ea3078d921bbc6e84f4069
Robert Stoll <rstoll@tutteli.ch>
2018-06-24 20:45:22 +0200

    move domain-api jvm code to common
    with the exception of BigDecimalAssertions and the overload in
    FloatingPointAssertions defined for BigDecimal. Thus:
    - introduce a FloatingPointAssertionsCommon in the common module
      and an `expect` FloatingPointAssertions
      - add BigDecimal specific overload in `actual` FloatingPointAssertions
        in JVM module (nothing to add in JS)

---
+++ domain/api/atrium-domain-api-common/src/main/kotlin/ch/tutteli/atrium/domain/creating/FloatingPointAssertions.kt
@@ -0,0 +1,30 @@
+package ch.tutteli.atrium.domain.creating
+
+import ch.tutteli.atrium.assertions.Assertion
+import ch.tutteli.atrium.core.polyfills.loadSingleService
+import ch.tutteli.atrium.creating.AssertionPlant
+
+/**
+ * The access point to an implementation of [FloatingPointAssertions].
+ *
+ * It loads the implementation lazily via [loadSingleService].
+ */
+val floatingPointAssertions by lazy { loadSingleService(FloatingPointAssertions::class) }
+
+
+/**
+ * Defines the minimum set of assertion functions and builders applicable to floating points,
+ * which an implementation of the domain of Atrium has to provide.
+ *
+ * An `actual` or in other words platform specific interface might add further methods.
+ */
+expect interface FloatingPointAssertions: FloatingPointAssertionsCommon
+
+/**
+ * Defines the minimum set of assertion functions and builders applicable to floating points ([Float], [Double]),
+ * which an implementation of the domain of Atrium has to provide for any platform.
+ */
+interface FloatingPointAssertionsCommon {
+    fun toBeWithErrorTolerance(plant: AssertionPlant<Float>, expected: Float, tolerance: Float): Assertion
+    fun toBeWithErrorTolerance(plant: AssertionPlant<Double>, expected: Double, tolerance: Double): Assertion
+}
--- domain/api/atrium-domain-api-jvm/src/main/kotlin/ch/tutteli/atrium/domain/creating/FloatingPointAssertions.kt
+++ domain/api/atrium-domain-api-jvm/src/main/kotlin/ch/tutteli/atrium/domain/creating/FloatingPointAssertions.kt
@@ -6,19 +6,9 @@ import ch.tutteli.atrium.creating.AssertionPlant
 import java.math.BigDecimal
 
 /**
- * The access point to an implementation of [FloatingPointAssertions].
- *
- * It loads the implementation lazily via [loadSingleService].
- */
-val floatingPointAssertions by lazy { loadSingleService(FloatingPointAssertions::class) }
-
-
-/**
  * Defines the minimum set of assertion functions and builders applicable to floating points ([Float], [Double],
  * [BigDecimal]), which an implementation of the domain of Atrium has to provide.
  */
-interface FloatingPointAssertions {
-    fun toBeWithErrorTolerance(plant: AssertionPlant<Float>, expected: Float, tolerance: Float): Assertion
-    fun toBeWithErrorTolerance(plant: AssertionPlant<Double>, expected: Double, tolerance: Double): Assertion
+actual interface FloatingPointAssertions: FloatingPointAssertionsCommon {
     fun <T : BigDecimal> toBeWithErrorTolerance(plant: AssertionPlant<T>, expected: T, tolerance: T): Assertion
 }
