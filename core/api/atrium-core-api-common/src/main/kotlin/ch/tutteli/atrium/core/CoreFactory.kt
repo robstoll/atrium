a7174907822f9ec06f26ddc9396878d4e40852f0
Robert Stoll <rstoll@tutteli.ch>
2018-07-10 23:26:53 +0200

    introduce default methods of CoreFactory as extension fun in common
    this way we can still use the methods from other common packages (we
    cannot move the default method to common because this would break bbc).

--- core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/core/CoreFactory.kt
+++ core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/core/CoreFactory.kt
@@ -456,3 +456,65 @@ interface CoreFactoryCommon {
     fun newOnlyFailureReporter(assertionFormatterFacade: AssertionFormatterFacade): Reporter
 }
 
+
+/**
+ * Creates a [ReportingAssertionPlantNullable] which is the entry point for assertions about nullable types.
+ *
+ * It creates a [CoreFactory.newThrowingAssertionChecker] based on the given [reporter] for assertion checking,
+ * uses [subjectProvider] as [AssertionPlantWithCommonFields.CommonFields.subjectProvider] but also as
+ * [AssertionPlantWithCommonFields.CommonFields.representationProvider].
+ * Notice that [evalOnce] is applied to the given [subjectProvider] to avoid side effects
+ * (the provider is most likely called more than once).
+ *
+ * @param assertionVerb The assertion verb which will be used inter alia in reporting
+ *   (see [AssertionPlantWithCommonFields.CommonFields.assertionVerb]).
+ * @param subjectProvider Used as [AssertionPlantWithCommonFields.CommonFields.subjectProvider] but
+ *   also as [AssertionPlantWithCommonFields.CommonFields.representationProvider].
+ * @param reporter The reporter which will be use for a [CoreFactory.newThrowingAssertionChecker].
+ *
+ * @return The newly created assertion plant.
+ */
+fun <T : Any?> CoreFactoryCommon.newReportingPlantNullable(
+    assertionVerb: Translatable,
+    subjectProvider: () -> T,
+    reporter: Reporter,
+    nullRepresentation: Any = RawString.NULL
+): ReportingAssertionPlantNullable<T> = newReportingPlantNullable(
+    assertionVerb, subjectProvider, coreFactory.newThrowingAssertionChecker(reporter), nullRepresentation
+)
+
+/**
+ * Creates a [ReportingAssertionPlantNullable] which is the entry point for assertions about nullable types.
+ *
+ * It uses the given [assertionChecker] for assertion checking, uses [subjectProvider] as
+ * [AssertionPlantWithCommonFields.CommonFields.subjectProvider] but also as
+ * [AssertionPlantWithCommonFields.CommonFields.representationProvider].
+ * Notice that [evalOnce] is applied to the given [subjectProvider] to avoid side effects
+ * (the provider is most likely called more than once).
+ *
+ * @param assertionVerb The assertion verb which will be used inter alia in reporting
+ *   (see [AssertionPlantWithCommonFields.CommonFields.assertionVerb]).
+ * @param subjectProvider Used as [AssertionPlantWithCommonFields.CommonFields.subjectProvider] but
+ *   also as [AssertionPlantWithCommonFields.CommonFields.representationProvider].
+ * @param assertionChecker The checker which will be used to check [Assertion]s.
+ *   (see [AssertionPlantWithCommonFields.CommonFields.assertionChecker]).
+ *
+ * @return The newly created assertion plant.
+ */
+fun <T : Any?> CoreFactoryCommon.newReportingPlantNullable(
+    assertionVerb: Translatable,
+    subjectProvider: () -> T,
+    assertionChecker: AssertionChecker,
+    nullRepresentation: Any = RawString.NULL
+): ReportingAssertionPlantNullable<T> {
+    val evalOnceSubjectProvider = subjectProvider.evalOnce()
+    return newReportingPlantNullable(
+        AssertionPlantWithCommonFields.CommonFields(
+            assertionVerb,
+            evalOnceSubjectProvider,
+            evalOnceSubjectProvider,
+            assertionChecker,
+            nullRepresentation
+        )
+    )
+}
