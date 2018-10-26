a5de3913a9c21a7285487e5e669060c93c7a350e
Robert Stoll <rstoll@tutteli.ch>
2018-10-26 22:59:13 +0200

    write spec for String.format for JS platform

--- core/api/atrium-core-api-js/src/main/kotlin/ch/tutteli/atrium/core/polyfills/stringExtensions.kt
+++ core/api/atrium-core-api-js/src/main/kotlin/ch/tutteli/atrium/core/polyfills/stringExtensions.kt
@@ -1,14 +1,27 @@
 package ch.tutteli.atrium.core.polyfills
 
 import ch.tutteli.atrium.reporting.translating.Locale
-import kotlin.String
 
-actual fun String.format(locale: Locale, arg: Any, vararg otherArgs: Any): String {
-    //TODO we have to come up with a solution here
-    return this
-}
+/**
+ * Replaces the first %s inside this [String] with [arg] and all subsequent with [otherArgs].
+ *
+ * This implementation neglects the given [locale] for now.
+ */
+actual fun String.format(locale: Locale, arg: Any, vararg otherArgs: Any): String =
+    replacePlaceholdersWithArgs(this, arg, otherArgs)
 
-actual fun String.format(arg: Any, vararg otherArgs: Any): String {
-    //TODO we have to come up with a solution here
-    return this
+/**
+ * Replaces the first %s inside this [String] with [arg] and all subsequent with [otherArgs].
+ *
+ * This implementation does not take a default [Locale] into account.
+ */
+actual fun String.format(arg: Any, vararg otherArgs: Any): String =
+    replacePlaceholdersWithArgs(this, arg, otherArgs)
+
+private fun replacePlaceholdersWithArgs(string: String, arg: Any, otherArgs: Array<out Any>): String {
+    var tmp = string.replaceFirst("%s", arg.toString())
+    otherArgs.forEach {
+        tmp = tmp.replaceFirst("%s", it.toString())
+    }
+    return tmp
 }
