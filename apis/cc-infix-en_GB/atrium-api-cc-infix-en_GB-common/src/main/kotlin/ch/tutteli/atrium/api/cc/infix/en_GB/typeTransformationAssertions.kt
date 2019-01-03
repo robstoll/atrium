928e518abe2c4c2d2f858e4930bdd87b022279cd
Robert Stoll <rstoll@tutteli.ch>
2019-01-03 19:54:11 +0100

    document that isA is flawed in combination with generic types

--- apis/cc-infix-en_GB/atrium-api-cc-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/typeTransformationAssertions.kt
+++ apis/cc-infix-en_GB/atrium-api-cc-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/typeTransformationAssertions.kt
@@ -26,6 +26,18 @@ inline infix fun <reified T : Any> AssertionPlantNullable<T?>.notToBeNull(noinli
  * Makes the assertion that [AssertionPlant.subject] *is a* [TSub] (the same type or a sub-type) and if so,
  * uses [assertionCreator] which could create further assertions which are added as a group.
  *
+ * Notice, that asserting a function type is [flawed](https://youtrack.jetbrains.com/issue/KT-27846).
+ * The actual types are ignored as function types erase to Function0,
+ * Function1 etc. on byte code level, which means the assertion holds as long as the [AssertionPlant.subject] is a
+ * function and has the same amount of arguments regardless if the types differ. For instance
+ * `assert({x: Int -> "hello"}).isA<String -> Unit>{}` holds, even though `(Int) -> String` is clearly not
+ * a `(String) -> Unit`.
+ *
+ * More generally speaking, the [flaw](https://youtrack.jetbrains.com/issue/KT-27826) applies to all generic types.
+ * For instance `isA<List<String>>` would only check if the [AssertionPlant.subject] is a `List` without checking if
+ * the entry type is actually `String`. Or in other words
+ * `assert(listOf(1, 2)).isA<List<String>>{}` holds, even though `List<Int>` is clearly not a `List<String>`.
+ *
  * @return Notice, that this assertion function cannot provide a fluent API because it depends on whether the first
  *   assertion ([AssertionPlant.subject] *is a* [TSub]) holds or not. Define subsequent assertions
  *   via the [assertionCreator] lambda.
