a0a885031c24dc714b269068a46e406fb5bee673
Robert Stoll <rstoll@tutteli.ch>
2019-01-20 00:49:18 +0100

    make Representation/ParameterObjectOptionImpl internal

--- domain/api/atrium-domain-api-common/src/main/kotlin/ch/tutteli/atrium/domain/creating/feature/extract/impl/defaultImpl.kt
+++ domain/api/atrium-domain-api-common/src/main/kotlin/ch/tutteli/atrium/domain/creating/feature/extract/impl/defaultImpl.kt
@@ -3,11 +3,11 @@ package ch.tutteli.atrium.domain.creating.feature.extract.impl
 import ch.tutteli.atrium.domain.creating.feature.extract.FeatureExtractor
 import ch.tutteli.atrium.reporting.translating.Translatable
 
-class RepresentationOptionImpl : FeatureExtractor.RepresentationOption {
+internal class RepresentationOptionImpl : FeatureExtractor.RepresentationOption {
     override fun withDescription(translatable: Translatable): FeatureExtractor.ParameterObjectOption
         = ParameterObjectOptionImpl(translatable)
 }
 
-class ParameterObjectOptionImpl(
+internal class ParameterObjectOptionImpl(
     override val featureRepresentation: Translatable
 ) : FeatureExtractor.ParameterObjectOption
