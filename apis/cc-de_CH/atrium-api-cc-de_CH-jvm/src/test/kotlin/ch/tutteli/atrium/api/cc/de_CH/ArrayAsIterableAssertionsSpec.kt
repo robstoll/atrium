14183c91413207c790b15495f50596345a746901
Robert Stoll <rstoll@tutteli.ch>
2019-01-19 18:55:46 +0100

    add overload with assertion creator to asIterable

--- apis/cc-de_CH/atrium-api-cc-de_CH-jvm/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/ArrayAsIterableAssertionsSpec.kt
+++ apis/cc-de_CH/atrium-api-cc-de_CH-jvm/src/test/kotlin/ch/tutteli/atrium/api/cc/de_CH/ArrayAsIterableAssertionsSpec.kt
@@ -5,7 +5,16 @@ import ch.tutteli.atrium.creating.Assert
 
 object ArrayAsIterableAssertionsSpec : ch.tutteli.atrium.spec.integration.ArrayAsIterableAssertionsSpec(
     AssertionVerbFactory,
-    Assert<Array<Int>>::asIterable.name,
+    "asIterable",
+    Assert<Array<Int>>::asIterable,
+    Assert<ByteArray>::asIterable,
+    Assert<CharArray>::asIterable,
+    Assert<ShortArray>::asIterable,
+    Assert<IntArray>::asIterable,
+    Assert<LongArray>::asIterable,
+    Assert<FloatArray>::asIterable,
+    Assert<DoubleArray>::asIterable,
+    Assert<BooleanArray>::asIterable,
     Assert<Array<Int>>::asIterable,
     Assert<ByteArray>::asIterable,
     Assert<CharArray>::asIterable,
--- misc/atrium-spec/src/main/kotlin/ch/tutteli/atrium/spec/integration/ArrayAsIterableAssertionsSpec.kt
+++ misc/atrium-spec/src/main/kotlin/ch/tutteli/atrium/spec/integration/ArrayAsIterableAssertionsSpec.kt
@@ -19,35 +19,54 @@ abstract class ArrayAsIterableAssertionsSpec(
     arrFloat: Assert<FloatArray>.() -> Assert<Iterable<Float>>,
     arrDouble: Assert<DoubleArray>.() -> Assert<Iterable<Double>>,
     arrBoolean: Assert<BooleanArray>.() -> Assert<Iterable<Boolean>>,
+    arrWithCreator: Assert<Array<Int>>.(Assert<Iterable<Int>>.() -> Unit) -> Assert<Iterable<Int>>,
+    arrByteWithCreator: Assert<ByteArray>.(Assert<Iterable<Byte>>.() -> Unit) -> Assert<Iterable<Byte>>,
+    arrCharWithCreator: Assert<CharArray>.(Assert<Iterable<Char>>.() -> Unit) -> Assert<Iterable<Char>>,
+    arrShortWithCreator: Assert<ShortArray>.(Assert<Iterable<Short>>.() -> Unit) -> Assert<Iterable<Short>>,
+    arrIntWithCreator: Assert<IntArray>.(Assert<Iterable<Int>>.() -> Unit) -> Assert<Iterable<Int>>,
+    arrLongWithCreator: Assert<LongArray>.(Assert<Iterable<Long>>.() -> Unit) -> Assert<Iterable<Long>>,
+    arrFloatWithCreator: Assert<FloatArray>.(Assert<Iterable<Float>>.() -> Unit) -> Assert<Iterable<Float>>,
+    arrDoubleWithCreator: Assert<DoubleArray>.(Assert<Iterable<Double>>.() -> Unit) -> Assert<Iterable<Double>>,
+    arrBooleanWithCreator: Assert<BooleanArray>.(Assert<Iterable<Boolean>>.() -> Unit) -> Assert<Iterable<Boolean>>,
     describePrefix: String = "[Atrium] "
 ) : IterablePredicateSpecBase(verbs, {
 
+    val asIterableWithCreator = "$asIterableFunName with Creator"
     include(object : SubjectLessAssertionSpec<Array<Int>>("$describePrefix[arr] ",
-        asIterableFunName to mapToCreateAssertion { arr(this) }
+        asIterableFunName to mapToCreateAssertion { arr(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrWithCreator(this) { contains(1) } }
     ) {})
     include(object : SubjectLessAssertionSpec<ByteArray>("$describePrefix[arrByte] ",
-        asIterableFunName to mapToCreateAssertion { arrByte(this) }
+        asIterableFunName to mapToCreateAssertion { arrByte(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrByteWithCreator(this) { contains(1) } }
     ) {})
     include(object : SubjectLessAssertionSpec<CharArray>("$describePrefix[arrChar] ",
-        asIterableFunName to mapToCreateAssertion { arrChar(this) }
+        asIterableFunName to mapToCreateAssertion { arrChar(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrCharWithCreator(this) { contains('a') } }
     ) {})
     include(object : SubjectLessAssertionSpec<ShortArray>("$describePrefix[arrShort] ",
-        asIterableFunName to mapToCreateAssertion { arrShort(this) }
+        asIterableFunName to mapToCreateAssertion { arrShort(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrShortWithCreator(this) { contains(1.toShort()) } }
     ) {})
     include(object : SubjectLessAssertionSpec<IntArray>("$describePrefix[arrInt] ",
-        asIterableFunName to mapToCreateAssertion { arrInt(this) }
+        asIterableFunName to mapToCreateAssertion { arrInt(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrIntWithCreator(this) { contains(1) } }
     ) {})
     include(object : SubjectLessAssertionSpec<LongArray>("$describePrefix[arrLong] ",
-        asIterableFunName to mapToCreateAssertion { arrLong(this) }
+        asIterableFunName to mapToCreateAssertion { arrLong(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrLongWithCreator(this) { contains(1L) } }
     ) {})
     include(object : SubjectLessAssertionSpec<FloatArray>("$describePrefix[arrFloat] ",
-        asIterableFunName to mapToCreateAssertion { arrFloat(this) }
+        asIterableFunName to mapToCreateAssertion { arrFloat(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrFloatWithCreator(this) { contains(1f) } }
     ) {})
     include(object : SubjectLessAssertionSpec<DoubleArray>("$describePrefix[arrDouble] ",
-        asIterableFunName to mapToCreateAssertion { arrDouble(this) }
+        asIterableFunName to mapToCreateAssertion { arrDouble(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrDoubleWithCreator(this) { contains(1.0) } }
     ) {})
     include(object : SubjectLessAssertionSpec<BooleanArray>("$describePrefix[arrBoolean] ",
-        asIterableFunName to mapToCreateAssertion { arrBoolean(this) }
+        asIterableFunName to mapToCreateAssertion { arrBoolean(this) },
+        asIterableWithCreator to mapToCreateAssertion { arrBooleanWithCreator(this) { contains(true) } }
     ) {})
 
     fun bytes(vararg bytes : Byte) = bytes
@@ -60,81 +79,110 @@ abstract class ArrayAsIterableAssertionsSpec(
     fun booleans(vararg booleans: Boolean) = booleans
 
     include(object : CheckingAssertionSpec<Array<Int>>(verbs, "$describePrefix[arr] ",
-        checkingTriple("$asIterableFunName arr", { arr(this).contains(5) }, arrayOf(1, 3, 5, 6), arrayOf(1, 2, 3))
+        checkingTriple(asIterableFunName, { arr(this).contains(5) }, arrayOf(1, 3, 5, 6), arrayOf(1, 2, 3)),
+        checkingTriple(asIterableWithCreator, { arrWithCreator(this) { contains(5) }}, arrayOf(1, 3, 5, 6), arrayOf(1, 2, 3))
     ) {})
     include(object : CheckingAssertionSpec<ByteArray>(verbs, "$describePrefix[arrByte] ",
-        checkingTriple("$asIterableFunName arr", { arrByte(this).contains(5.toByte()) }, bytes(1.toByte(), 5.toByte()), bytes())
+        checkingTriple(asIterableFunName, { arrByte(this).contains(5.toByte()) }, bytes(1.toByte(), 5.toByte()), bytes()),
+        checkingTriple(asIterableWithCreator, { arrByteWithCreator(this) { contains(5.toByte()) }}, bytes(1.toByte(), 5.toByte()), bytes())
     ) {})
     include(object : CheckingAssertionSpec<CharArray>(verbs, "$describePrefix[arrChar] ",
-        checkingTriple("$asIterableFunName arr", { arrChar(this).contains(5.toChar()) }, chars(1.toChar(), 5.toChar()), chars())
+        checkingTriple(asIterableFunName, { arrChar(this).contains(5.toChar()) }, chars(1.toChar(), 5.toChar()), chars()),
+        checkingTriple(asIterableWithCreator, { arrCharWithCreator(this) { contains(5.toChar()) }}, chars(1.toChar(), 5.toChar()), chars())
     ) {})
     include(object : CheckingAssertionSpec<ShortArray>(verbs, "$describePrefix[arrShort] ",
-        checkingTriple("$asIterableFunName arr", { arrShort(this).contains(5.toShort()) }, shorts(1.toShort(), 5.toShort()), shorts())
+        checkingTriple(asIterableFunName, { arrShort(this).contains(5.toShort()) }, shorts(1.toShort(), 5.toShort()), shorts()),
+        checkingTriple(asIterableWithCreator, { arrShortWithCreator(this) { contains(5.toShort()) }}, shorts(1.toShort(), 5.toShort()), shorts())
     ) {})
     include(object : CheckingAssertionSpec<IntArray>(verbs, "$describePrefix[arrInt] ",
-        checkingTriple("$asIterableFunName arr", { arrInt(this).contains(5) }, ints(1, 5), ints())
+        checkingTriple(asIterableFunName, { arrInt(this).contains(5) }, ints(1, 5), ints()),
+        checkingTriple(asIterableWithCreator, { arrIntWithCreator(this) { contains(5) }}, ints(1, 5), ints())
     ) {})
     include(object : CheckingAssertionSpec<LongArray>(verbs, "$describePrefix[arrLong] ",
-        checkingTriple("$asIterableFunName arr", { arrLong(this).contains(5) }, longs(1, 5), longs())
+        checkingTriple(asIterableFunName, { arrLong(this).contains(5) }, longs(1, 5), longs()),
+        checkingTriple(asIterableWithCreator, { arrLongWithCreator(this) { contains(5) }}, longs(1, 5), longs())
     ) {})
     include(object : CheckingAssertionSpec<FloatArray>(verbs, "$describePrefix[arrFloat] ",
-        checkingTriple("$asIterableFunName arr", { arrFloat(this).contains(5f) }, floats(1f, 5f), floats())
+        checkingTriple(asIterableFunName, { arrFloat(this).contains(5f) }, floats(1f, 5f), floats()),
+        checkingTriple(asIterableWithCreator, { arrFloatWithCreator(this) { contains(5f) }}, floats(1f, 5f), floats())
     ) {})
     include(object : CheckingAssertionSpec<DoubleArray>(verbs, "$describePrefix[arrDouble] ",
-        checkingTriple("$asIterableFunName arr", { arrDouble(this).contains(5.0) }, doubles(1.0, 5.0), doubles())
+        checkingTriple(asIterableFunName, { arrDouble(this).contains(5.0) }, doubles(1.0, 5.0), doubles()),
+        checkingTriple(asIterableWithCreator, { arrDoubleWithCreator(this) { contains(5.0) }}, doubles(1.0, 5.0), doubles())
     ) {})
 
 
     describe("$asIterableFunName arr") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(arrayOf(1, 2)).arr().containsExactly(1, 2)
+        }
+        test("transformation can be applied and a sub-assertion made") {
+            verbs.checkImmediately(arrayOf(1, 2)).arrWithCreator { containsExactly(1, 2) }
         }
     }
 
     describe("$asIterableFunName arrByte") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(bytes(1.toByte(), 2.toByte())).arrByte().containsExactly(1.toByte(), 2.toByte())
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(bytes(1.toByte(), 2.toByte())).arrByteWithCreator{ containsExactly(1.toByte(), 2.toByte()) }
         }
     }
     describe("$asIterableFunName arrChar") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(chars(1.toChar(), 2.toChar())).arrChar().containsExactly(1.toChar(), 2.toChar())
         }
-    }
-    describe("$asIterableFunName arrShort") {
-        test("transformation can be applied and an assertion made") {
-            verbs.checkImmediately(shorts(1, 2)).arrShort().containsExactly(1.toShort(), 2.toShort())
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(chars(1.toChar(), 2.toChar())).arrCharWithCreator{ containsExactly(1.toChar(), 2.toChar()) }
         }
     }
     describe("$asIterableFunName arrShort") {
-        test("transformation can be applied and an assertion made") {
-            verbs.checkImmediately(shorts(1.toShort(), 2.toShort())).arrShort()
-                .containsExactly(1.toShort(), 2.toShort())
+        test("transformation can be applied and an subsequent assertion made") {
+            verbs.checkImmediately(shorts(1, 2)).arrShort().containsExactly(1.toShort(), 2.toShort())
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(shorts(1, 2)).arrShortWithCreator{ containsExactly(1.toShort(), 2.toShort()) }
         }
     }
     describe("$asIterableFunName arrInt") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(ints(1, 2)).arrInt().containsExactly(1, 2)
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(ints(1, 2)).arrIntWithCreator{ containsExactly(1, 2) }
         }
     }
     describe("$asIterableFunName arrLong") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(longs(1, 2)).arrLong().containsExactly(1L, 2L)
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(longs(1, 2)).arrLongWithCreator{ containsExactly(1L, 2L) }
         }
     }
     describe("$asIterableFunName arrFloat") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(floats(1f, 2f)).arrFloat().containsExactly(1f, 2f)
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(floats(1f, 2f)).arrFloatWithCreator{ containsExactly(1f, 2f) }
         }
     }
     describe("$asIterableFunName arrDouble") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(doubles(1.0, 2.0)).arrDouble().containsExactly(1.0, 2.0)
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(doubles(1.0, 2.0)).arrDoubleWithCreator{ containsExactly(1.0, 2.0) }
         }
     }
     describe("$asIterableFunName arrBoolean") {
-        test("transformation can be applied and an assertion made") {
+        test("transformation can be applied and an subsequent assertion made") {
             verbs.checkImmediately(booleans(true, false)).arrBoolean().containsExactly(true, false)
+        }
+        test("transformation can be applied and a sub assertion made") {
+            verbs.checkImmediately(booleans(true, false)).arrBooleanWithCreator{ containsExactly(true, false) }
         }
     }
 })
