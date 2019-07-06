4b2fdf54783b63945a81e33138862f5e13ef322b
Robert Stoll <rstoll@tutteli.ch>
2019-07-06 23:27:24 +0200

    add bundle fluent-en_GB, do no longer include -robstoll
    I currently doubt we are going to provide another implementation of
    core and domain in the Atrium repository. Or in other words, the
    -robstoll implementation is the default one and does not need to be
    mentioned.

--- bundles/fluent-en_GB/atrium-fluent-en_GB-js/src/main/kotlin/ch/tutteli/atrium/cc/en_GB/robstoll/dependOnMe.kt
+++ bundles/fluent-en_GB/atrium-fluent-en_GB-js/src/main/kotlin/ch/tutteli/atrium/cc/en_GB/robstoll/dependOnMe.kt
@@ -1,10 +1,10 @@
-package ch.tutteli.atrium.cc.de_CH.robstoll
+package ch.tutteli.atrium.cc.en_GB.robstoll
 
 import ch.tutteli.atrium.domain.robstoll.dependOn_atrium_domain_robstoll
 import ch.tutteli.atrium.core.robstoll.dependOn_atrium_core_robstoll
 
 /**
- * Dummy function in order that other modules can define a dependency on atrium-cc-de_CH-robstoll-js
+ * Dummy function in order that other modules can define a dependency on atrium-fluent-en_GB-js
  *
  * Moreover it has the side effect that a dependency on core-robstoll and domain-robstoll is established.
  * This is necessary, as it has only a loosely coupled dependency (via serviceLoader).
