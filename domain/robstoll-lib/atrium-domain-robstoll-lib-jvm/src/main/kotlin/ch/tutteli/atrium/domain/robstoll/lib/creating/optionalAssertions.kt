59f390abff3efc493856acf0130c67dca3796d7e
Robert Stoll <rstoll@tutteli.ch>
2019-07-23 16:28:43 +0200

    add niok as dependency, give hint about new-type-inference bug

--- domain/robstoll-lib/atrium-domain-robstoll-lib-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/optionalAssertions.kt
+++ domain/robstoll-lib/atrium-domain-robstoll-lib-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/optionalAssertions.kt
@@ -7,4 +7,10 @@ import java.util.*
 //TODO #47 remove annotation
 @Suppress("UNUSED_PARAMETER")
 fun <T> _isEmpty(assertionContainer: Expect<Optional<T>>): Assertion =
-    TODO("#47 create a feature assertion for Optional<T>::isEmpty and assert it to be true")
+    TODO(
+        """
+        #47 create a feature assertion for Optional<T>::isEmpty and assert it to be true
+        Notice, there is a bug in the new-type-inference https://youtrack.jetbrains.com/issue/KT-32851 
+        the bug is only within IDEA, check gradle if it works
+        """
+    )
--- domain/robstoll-lib/atrium-domain-robstoll-lib-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/pathAssertions.kt
+++ domain/robstoll-lib/atrium-domain-robstoll-lib-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/pathAssertions.kt
@@ -4,7 +4,13 @@ import ch.tutteli.atrium.assertions.Assertion
 import ch.tutteli.atrium.creating.Expect
 import java.nio.file.Path
 
-//TODO #47 remove annotation
+//TODO #104 remove annotation
 @Suppress("UNUSED_PARAMETER")
-fun <T: Path> _exists(assertionContainer: Expect<T>): Assertion =
-    TODO ("#104 create a feature assertion for Path::exists and assert it is true")
+fun <T : Path> _exists(assertionContainer: Expect<T>): Assertion =
+    TODO(
+        """
+        #104 create a feature assertion for Path::exists and assert it is true.
+        Notice, there is a bug in the new-type-inference https://youtrack.jetbrains.com/issue/KT-32851 
+        the bug is only within IDEA, check gradle if it works
+        """
+    )
