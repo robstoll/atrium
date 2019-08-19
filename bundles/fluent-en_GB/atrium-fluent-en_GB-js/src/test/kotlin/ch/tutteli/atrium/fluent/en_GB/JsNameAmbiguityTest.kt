eadfccaafcc9337eab319f042c2e0b4f69611369
Robert Stoll <rstoll@tutteli.ch>
2019-08-19 22:11:34 +0200

    activate smoke test for fluent-en_GB and add JsName regression tests

---
+++ bundles/fluent-en_GB/atrium-fluent-en_GB-js/src/test/kotlin/ch/tutteli/atrium/fluent/en_GB/JsNameAmbiguityTest.kt
@@ -0,0 +1,27 @@
+package ch.tutteli.atrium.fluent.en_GB
+
+import ch.tutteli.atrium.api.fluent.en_GB.asEntries
+import ch.tutteli.atrium.api.fluent.en_GB.containsExactly
+import ch.tutteli.atrium.api.fluent.en_GB.isKeyValue
+import ch.tutteli.atrium.api.fluent.en_GB.toBe
+import ch.tutteli.atrium.api.verbs.expect
+import kotlin.test.Test
+
+/**
+ * As there are bugs related to JsName in Kotlin we should test each to be sure that it works also during runtime.
+ */
+class JsNameTest {
+
+    @Test
+    fun toBeNullable() {
+        expect(null as Int?).toBe(null)
+        expect(1 as Int?).toBe(1)
+    }
+
+    @Test
+    fun isKeyValueNullable() {
+        expect(mapOf(1 to null as Int?)).asEntries().containsExactly {
+            isKeyValue(1, null)
+        }
+    }
+}
