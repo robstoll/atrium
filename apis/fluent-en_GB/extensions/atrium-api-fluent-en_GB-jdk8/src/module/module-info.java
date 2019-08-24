43140a444247223ccff621ba0ac23f4d1c7bc6fe
Robert Stoll <rstoll@tutteli.ch>
2019-08-24 16:46:34 +0200

    update to niok 0.3.1, resolves issues with jdk9

--- apis/fluent-en_GB/extensions/atrium-api-fluent-en_GB-jdk8/src/module/module-info.java
+++ apis/fluent-en_GB/extensions/atrium-api-fluent-en_GB-jdk8/src/module/module-info.java
@@ -1,6 +1,7 @@
 module ch.tutteli.atrium.api.fluent.en_GB.jdk8 {
     requires        ch.tutteli.atrium.domain.builders;
     requires        ch.tutteli.kbox;
+    requires        ch.tutteli.niok;
     requires        kotlin.stdlib;
 
     exports ch.tutteli.atrium.api.fluent.en_GB.jdk8;
