62db0fdb57453319261bbd81eabb6024a3f6db78
Joshua Gleitze <dev@joshuagleitze.de>
2019-08-26 23:48:08 +0200

    Re-add (NOT_)TO_BE constants to DescriptionAnyAssertion and deprecate them
    … in order not to break backwards compatibility.

--- translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionAnyAssertion.kt
+++ translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionAnyAssertion.kt
@@ -7,6 +7,10 @@ import ch.tutteli.atrium.reporting.translating.StringBasedTranslatable
  * Contains the [DescriptiveAssertion.description]s of the assertion functions which are applicable to [Any].
  */
 enum class DescriptionAnyAssertion(override val value: String) : StringBasedTranslatable {
+    @Deprecated("Will be removed in version 1.0.0.", ReplaceWith("DescriptionBasic.TO_BE", imports = ["ch.tutteli.atrium.translations.DescriptionBasic"]))
+    TO_BE(DescriptionBasic.TO_BE.value),
+    @Deprecated("Will be removed in version 1.0.0.", ReplaceWith("DescriptionBasic.NOT_TO_BE", imports = ["ch.tutteli.atrium.translations.DescriptionBasic"]))
+    NOT_TO_BE(DescriptionBasic.NOT_TO_BE.value),
     IS_A("ist eine Instanz vom Typ"),
     IS_SAME("ist dieselbe Instanz wie"),
     IS_NOT_SAME("ist nicht dieselbe Instanz wie"),
