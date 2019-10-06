d75a5c91f66620d47b4b48935d45c834a998959f
Luiz Picanço <lpicanco@gmail.com>
2019-10-06 15:37:12 -0300

    fix package import on pathAssertions.

--- domain/robstoll-lib/atrium-domain-robstoll-lib-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/pathAssertions.kt
+++ domain/robstoll-lib/atrium-domain-robstoll-lib-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/pathAssertions.kt
@@ -7,8 +7,8 @@ import ch.tutteli.atrium.domain.creating.changers.ExtractedFeaturePostStep
 import ch.tutteli.atrium.reporting.RawString
 import ch.tutteli.atrium.translations.DescriptionBasic.NOT_TO
 import ch.tutteli.atrium.translations.DescriptionBasic.TO
-import ch.tutteli.atrium.translations.DescriptionPathAssertion
 import ch.tutteli.atrium.translations.DescriptionPathAssertion.EXIST
+import ch.tutteli.atrium.translations.DescriptionPathAssertion.PARENT
 import ch.tutteli.niok.exists
 import ch.tutteli.niok.notExists
 import java.nio.file.Path
@@ -20,4 +20,4 @@ fun <T : Path> _existsNot(assertionContainer: Expect<T>): Assertion =
     ExpectImpl.builder.createDescriptive(assertionContainer, NOT_TO, RawString.create(EXIST)) { it.notExists }
 
 fun <T : Path> _parent(assertionContainer: Expect<T>): ExtractedFeaturePostStep<T, Path> =
-    ExpectImpl.feature.manualFeature(assertionContainer, DescriptionPathAssertion.PARENT) { parent }
+    ExpectImpl.feature.manualFeature(assertionContainer, PARENT) { parent }
