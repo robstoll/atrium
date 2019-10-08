abb451ac2d6b719acbaeba1ca77e1b8801a31192
gaconkzk <gaconkzk@gmail.com>
2019-10-08 17:49:58 +0700

    no wildcard import

--- domain/robstoll/atrium-domain-robstoll-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/creating/PathAssertionsImpl.kt
+++ domain/robstoll/atrium-domain-robstoll-jvm/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/creating/PathAssertionsImpl.kt
@@ -2,7 +2,10 @@ package ch.tutteli.atrium.domain.robstoll.creating
 
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.domain.creating.PathAssertions
-import ch.tutteli.atrium.domain.robstoll.lib.creating.*
+import ch.tutteli.atrium.domain.robstoll.lib.creating._endsWith
+import ch.tutteli.atrium.domain.robstoll.lib.creating._exists
+import ch.tutteli.atrium.domain.robstoll.lib.creating._existsNot
+import ch.tutteli.atrium.domain.robstoll.lib.creating._parent
 import java.nio.file.Path
 
 class PathAssertionsImpl : PathAssertions {
