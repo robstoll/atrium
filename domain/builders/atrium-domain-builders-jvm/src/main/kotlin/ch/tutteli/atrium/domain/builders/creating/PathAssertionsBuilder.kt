813a34b3d7c3577185d879f1eef27691292759c3
Segun Famisa <segunfamisa@gmail.com>
2019-10-10 11:06:09 +0200

    Inline `endsNotWith` function

--- domain/builders/atrium-domain-builders-jvm/src/main/kotlin/ch/tutteli/atrium/domain/builders/creating/PathAssertionsBuilder.kt
+++ domain/builders/atrium-domain-builders-jvm/src/main/kotlin/ch/tutteli/atrium/domain/builders/creating/PathAssertionsBuilder.kt
@@ -23,7 +23,7 @@ object PathAssertionsBuilder : PathAssertions {
     override fun <T : Path> endsWith(assertionContainer: Expect<T>, expected: Path) =
         pathAssertions.endsWith(assertionContainer, expected)
 
-    override fun <T : Path> endsNotWith(assertionContainer: Expect<T>, expected: Path) =
+    override inline fun <T : Path> endsNotWith(assertionContainer: Expect<T>, expected: Path) =
         pathAssertions.endsNotWith(assertionContainer, expected)
 
     override inline fun <T : Path> exists(assertionContainer: Expect<T>) =
