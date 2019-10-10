e15daa90f593cd24fc79f02d897ddd51c3eef522
Robert Stoll <robert.stoll@tegonal.com>
2019-10-10 13:38:15 +0200

    prepare Result assertions

---
+++ apis/fluent-en_GB/extensions/kotlin_1_3/atrium-api-fluent-en_GB-kotlin_1_3-common/src/main/kotlin/ch/tutteli/atrium/api/fluent/en_GB/kotlin_1_3/resultAssertions.kt
@@ -0,0 +1,6 @@
+package ch.tutteli.atrium.api.fluent.en_GB.kotlin_1_3
+
+import ch.tutteli.atrium.creating.Expect
+
+//TODO copy KDoc from another assertion which includes a check, e.g. listAssertions.kt -> get
+fun <E, T : Result<E>> Expect<T>.isSuccess(): Expect<E> = TODO("implement")
---
+++ domain/robstoll/extensions/kotlin_1_3/atrium-domain-robstoll-kotlin_1_3-common/src/main/kotlin/ch/tutteli/atrium/domain/robstoll.kotlin_1_3/creating/ResultAssertionsImpl.kt
@@ -0,0 +1,6 @@
+package ch.tutteli.atrium.domain.robstoll.kotlin_1_3.creating
+
+import ch.tutteli.atrium.domain.kotlin_1_3.creating.ResultAssertions
+
+class ResultAssertionsImpl : ResultAssertions {
+}
