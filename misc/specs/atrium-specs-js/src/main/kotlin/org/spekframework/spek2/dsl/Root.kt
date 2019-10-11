87eb66e73c0cd713d973b1a2e1582a6f0c58d20f
Anubhav007 <anubhav.sadana@gmail.com>
2019-10-11 20:44:19 +0530

    Update Root.kt

--- misc/specs/atrium-specs-js/src/main/kotlin/org/spekframework/spek2/dsl/Root.kt
+++ misc/specs/atrium-specs-js/src/main/kotlin/org/spekframework/spek2/dsl/Root.kt
@@ -1,5 +1,6 @@
 package org.spekframework.spek2.dsl
 
+import org.spekframework.spek2.Spek
 import org.spekframework.spek2.lifecycle.CachingMode
 import org.spekframework.spek2.style.specification.MemoizedValue
 
