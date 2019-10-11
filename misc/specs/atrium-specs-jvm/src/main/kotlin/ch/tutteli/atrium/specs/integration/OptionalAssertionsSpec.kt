983663b1a357db82a4f16037bbd21895eb736476
Arjan Kleene <arjankleene@gmail.com>
2019-10-11 12:11:47 +0200

    ATRIUM-47 - Include a SubjectLessSpec in OptionalAssertionsSpec

--- misc/specs/atrium-specs-jvm/src/main/kotlin/ch/tutteli/atrium/specs/integration/OptionalAssertionsSpec.kt
+++ misc/specs/atrium-specs-jvm/src/main/kotlin/ch/tutteli/atrium/specs/integration/OptionalAssertionsSpec.kt
@@ -3,11 +3,7 @@ package ch.tutteli.atrium.specs.integration
 import ch.tutteli.atrium.api.fluent.en_GB.messageContains
 import ch.tutteli.atrium.api.fluent.en_GB.toThrow
 import ch.tutteli.atrium.api.verbs.internal.expect
-import ch.tutteli.atrium.specs.Fun0
-import ch.tutteli.atrium.specs.describeFunTemplate
-import ch.tutteli.atrium.specs.isDescr
-import ch.tutteli.atrium.specs.lambda
-import ch.tutteli.atrium.specs.name
+import ch.tutteli.atrium.specs.*
 import ch.tutteli.atrium.translations.DescriptionOptionalAssertion.EMPTY
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.Suite
@@ -18,6 +14,11 @@ abstract class OptionalAssertionsSpec (
     isEmpty: Fun0<Optional<Any>>,
     describePrefix: String = "[Atrium] "
 ) : Spek({
+
+    include(object : SubjectLessSpec<Optional<Any>>(
+        "$describePrefix[Path] ",
+        isEmpty.forSubjectLess()
+    ) {})
 
     fun describeFun(vararg funName: String, body: Suite.() -> Unit) =
         describeFunTemplate(describePrefix, funName, body = body)
