2ca1a3638126a5870a6588ee0734ae002ae72c70
Shardulsonar <shardulsonar@gmail.com>
2019-10-18 00:00:48 +0200

    #203 : Adding code for ResultAssertions-isSuccess

---
+++ translations/en_GB/atrium-translations-en_GB-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionResultAssertion.kt
@@ -0,0 +1,11 @@
+package ch.tutteli.atrium.translations
+
+import ch.tutteli.atrium.assertions.DescriptiveAssertion
+import ch.tutteli.atrium.reporting.translating.StringBasedTranslatable
+
+/**
+ * Contains the [DescriptiveAssertion.description]s of the assertion functions which are applicable to [Map].
+ */
+enum class DescriptionResultAssertion(override val value: String) : StringBasedTranslatable {
+    IS_NOT_SUCCESS("!! is not a Success"), UNBOXED("unboxed")
+}
