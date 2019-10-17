5c73dd9e81e9d95ad4a123d3cbac123c7937f322
neelkamath <neelkamath@icloud.com>
2019-10-17 18:48:01 +0530

    Patch api tests

--- apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/CharSequenceContainsRegexAssertionsSpec.kt
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/CharSequenceContainsRegexAssertionsSpec.kt
@@ -4,11 +4,16 @@ import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.include
 import org.spekframework.spek2.Spek
 
+private typealias StringFun =
+    Pair<String, (Expect<CharSequence>, Int, String, Array<out String>) -> Expect<CharSequence>>
+
+private typealias RegexFun = Pair<String, (Expect<CharSequence>, Int, Regex, Array<out Regex>) -> Expect<CharSequence>>
+
 class CharSequenceContainsRegexAssertionsSpec : Spek({
     include(StringSpec)
     include(RegexSpec)
 }) {
-    object StringSpec : ch.tutteli.atrium.specs.integration.CharSequenceContainsRegexAssertionsSpecString(
+    object StringSpec : ch.tutteli.atrium.specs.integration.CharSequenceContainsRegexAssertionsSpec(
         getNameContainsRegex(),
         getAtLeastTripleString(),
         getAtLeastIgnoringCaseTripleString(),
@@ -18,7 +23,7 @@ class CharSequenceContainsRegexAssertionsSpec : Spek({
         "◆ ", "⚬ "
     )
 
-    object RegexSpec : ch.tutteli.atrium.specs.integration.CharSequenceContainsRegexAssertionsSpecRegex(
+    object RegexSpec : ch.tutteli.atrium.specs.integration.CharSequenceContainsRegexAssertionsSpec(
         getNameContainsRegex(),
         getAtLeastTripleRegex(),
         getAtLeastIgnoringCaseTripleRegex(),
@@ -32,15 +37,18 @@ class CharSequenceContainsRegexAssertionsSpec : Spek({
 
         private fun getNameContainsRegex() = "$contains with search mode $regex"
 
-        private fun getAtLeastTripleString() =
+        private fun getAtLeastTripleString(): Pair<(String, String) -> String, StringFun> =
             { what: String, times: String -> "$contains $what $atLeast $times" } to
                 ("$contains.$atLeast.$regex" to Companion::containsAtLeast)
 
-        private fun getAtLeastTripleRegex() =
+        private fun getAtLeastTripleRegex(): Pair<(Regex, String) -> String, RegexFun> =
             { what: Regex, times: String -> "$contains $what $atLeast $times" } to
                 ("$contains.$atLeast.$regex" to Companion::containsAtLeast)
 
         private fun containsAtLeast(expect: Expect<CharSequence>, atLeast: Int, a: String, aX: Array<out String>) =
+            expect.contains.atLeast(atLeast).regex(a, *aX)
+
+        private fun containsAtLeast(expect: Expect<CharSequence>, atLeast: Int, a: Regex, aX: Array<out Regex>) =
             expect.contains.atLeast(atLeast).regex(a, *aX)
 
         private fun getAtLeastIgnoringCaseTripleString() =
