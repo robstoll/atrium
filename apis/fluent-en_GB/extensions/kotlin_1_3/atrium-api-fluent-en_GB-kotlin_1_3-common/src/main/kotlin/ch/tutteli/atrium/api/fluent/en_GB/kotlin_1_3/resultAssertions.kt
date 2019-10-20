5f47715a61408d9435e0450b92b57aaa71bd44de
Shardulsonar <shardulsonar@gmail.com>
2019-10-20 23:12:51 +0200

    #203 : Adding result assertions in api

--- apis/fluent-en_GB/extensions/kotlin_1_3/atrium-api-fluent-en_GB-kotlin_1_3-common/src/main/kotlin/ch/tutteli/atrium/api/fluent/en_GB/kotlin_1_3/resultAssertions.kt
+++ apis/fluent-en_GB/extensions/kotlin_1_3/atrium-api-fluent-en_GB-kotlin_1_3-common/src/main/kotlin/ch/tutteli/atrium/api/fluent/en_GB/kotlin_1_3/resultAssertions.kt
@@ -1,6 +1,24 @@
 package ch.tutteli.atrium.api.fluent.en_GB.kotlin_1_3
 
 import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.domain.builders.ExpectImpl
+import ch.tutteli.atrium.domain.builders.kotlin_1_3.result
 
-//TODO copy KDoc from another assertion which includes a check, e.g. listAssertions.kt -> get
-fun <E, T : Result<E>> Expect<T>.isSuccess(): Expect<E> = TODO("implement")
+/**
+ * Expects that the given assertion is a success
+ * returns an [Expect]
+ *
+ * @return The newly created [Expect] if the given assertion is success
+ * @throws AssertionError Might throw an [AssertionError] if the given assertion is not a success.
+ */
+fun <E, T : Result<E>> Expect<T>.isSuccess(): Expect<E> = ExpectImpl.result.isSuccess(this).getExpectOfFeature()
+
+/**
+ * Expects that the given assertions are success
+ * returns [Expect]
+ *
+ * @return The newly created [Expect] if the given assertions are success
+ * @throws AssertionError Might throw an [AssertionError]  if the given assertions are not success.
+ */
+fun <E, T : Result<E>> Expect<T>.isSuccess(assertionCreator: Expect<E>.() -> Unit): Expect<T> =
+    ExpectImpl.result.isSuccess(this).addToInitial(assertionCreator)
--- domain/robstoll-lib/extensions/kotlin_1_3/atrium-domain-robstoll-lib-kotlin_1_3-common/src/main/kotlin/ch/tutteli/atrium/domain.robstoll.lib.kotlin_1_3/creating/resultAssertions.kt
+++ domain/robstoll-lib/extensions/kotlin_1_3/atrium-domain-robstoll-lib-kotlin_1_3-common/src/main/kotlin/ch/tutteli/atrium/domain.robstoll.lib.kotlin_1_3/creating/resultAssertions.kt
@@ -5,11 +5,11 @@ import ch.tutteli.atrium.domain.builders.ExpectImpl
 import ch.tutteli.atrium.domain.creating.changers.ExtractedFeaturePostStep
 import ch.tutteli.atrium.translations.DescriptionResultAssertion
 
-fun  <E, T : Result<E>>_isSuccess( assertionContainer: Expect<T>) : ExtractedFeaturePostStep<T, E> =
+fun  <E, T : Result<E>>_isSuccess( assertionContainer: Expect<T>) : ch.tutteli.atrium.domain.creating.changers.ExtractedFeaturePostStep<T, E> =
     ExpectImpl.feature.extractor(assertionContainer)
         .withDescription(DescriptionResultAssertion.UNBOXED)
         .withRepresentationForFailure(DescriptionResultAssertion.IS_NOT_SUCCESS)
         .withCheck { it.isSuccess }
         .withFeatureExtraction { it.getOrNull() ?: IllegalStateException("checked it.isSuccess and suddenly it isn't any more: $it")}
         .build()
-}
+
