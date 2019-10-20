5e1c27cb6993fd3227b94d8168fb115a9803d497
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2019-10-20 11:42:56 +0200

    Add infix assertion functions

---
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/PairFeatureAssertionsSpec.kt
@@ -0,0 +1,40 @@
+package ch.tutteli.atrium.api.infix.en_GB
+
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.specs.fun1
+import ch.tutteli.atrium.specs.notImplemented
+import ch.tutteli.atrium.specs.property
+import ch.tutteli.atrium.api.infix.en_GB.*
+
+class PairFeatureAssertionsSpec : ch.tutteli.atrium.specs.integration.PairFeatureAssertionsSpec(
+    property<Pair<String, Int>, String>(Expect<Pair<String, Int>>::first),
+    fun1<Pair<String, Int>, Expect<String>.() -> Unit>(Expect<Pair<String, Int>>::first),
+    property<Pair<String, Int>, Int>(Expect<Pair<String, Int>>::second),
+    fun1<Pair<String, Int>, Expect<Int>.() -> Unit>(Expect<Pair<String, Int>>::second),
+    property<Pair<String?, Int?>, String?>(Expect<Pair<String?, Int?>>::first),
+    fun1<Pair<String?, Int?>, Expect<String?>.() -> Unit>(Expect<Pair<String?, Int?>>::first),
+    property<Pair<String?, Int?>, Int?>(Expect<Pair<String?, Int?>>::second),
+    fun1<Pair<String?, Int?>, Expect<Int?>.() -> Unit>(Expect<Pair<String?, Int?>>::second)
+) {
+
+    @Suppress("unused", "UNUSED_VALUE")
+    private fun ambiguityTest() {
+        var a1: Expect<out Pair<String, Int>> = notImplemented()
+        var a2: Expect<Pair<String?, Int>> = notImplemented()
+        var a3: Expect<Pair<String?, Int?>> = notImplemented()
+
+        a1.first
+        a2.first
+        a3.first
+        a1 = a1 first { }
+        a2 = a2 first { }
+        a3 = a3 first { }
+
+        a1.second
+        a2.second
+        a3.second
+        a1 = a1 second { }
+        a2 = a2 second { }
+        a3 = a3 second { }
+    }
+}
