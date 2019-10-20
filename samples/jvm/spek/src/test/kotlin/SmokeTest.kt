c47f85ce2d51c12b53f39f93593a6598d73a1fb7
Shubham Kumar <shubham.iitp@gmail.com>
2019-10-21 05:03:02 +0530

    #219, add a project under samples for atrium + spek

---
+++ samples/jvm/spek/src/test/kotlin/SmokeTest.kt
@@ -0,0 +1,30 @@
+package org.spekframework.spek2
+
+import ch.tutteli.atrium.api.fluent.en_GB.toBe
+import ch.tutteli.atrium.api.fluent.en_GB.toThrow
+import ch.tutteli.atrium.api.verbs.expect
+import org.spekframework.spek2.style.specification.describe
+import java.lang.IllegalArgumentException
+
+object CalculatorTestBySpek : Spek({
+    describe("Calculator") {
+        val calculator by memoized { Calculator() }
+
+        it("1 + 2 = 3") {
+            val sum = 3
+            expect(sum).toBe(calculator.add(1, 2))
+        }
+
+        it("division sanity") {
+            expect {
+                // the below statement throws exception
+                calculator.divide(12, 0)
+            }.toThrow<IllegalArgumentException>()
+        }
+
+        it("defaults to fancy"){
+            expect(calculator.isFancy)
+        }
+    }
+})
+
