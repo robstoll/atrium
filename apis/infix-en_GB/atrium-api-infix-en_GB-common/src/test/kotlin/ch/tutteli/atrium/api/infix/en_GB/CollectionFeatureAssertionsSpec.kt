e226abc822fb4dc80bb4e8e05cdadc48f35220a8
Rátkay András <ratkay.andras@gmail.com>
2019-10-23 09:58:51 +0200

    Update apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/CollectionFeatureAssertionsSpec.kt

--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/CollectionFeatureAssertionsSpec.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/CollectionFeatureAssertionsSpec.kt
@@ -7,7 +7,7 @@ import ch.tutteli.atrium.specs.notImplemented
 import ch.tutteli.atrium.specs.property
 
 class CollectionFeatureAssertionsSpec : ch.tutteli.atrium.specs.integration.CollectionFeatureAssertionsSpec(
-    property<Collection<String>, Int>(Expect<Collection<String>>::size).name to Expect<Collection<String>>::size,
+    property<Collection<String>, Int>(Expect<Collection<String>>::size),
     fun1<Collection<String>, Expect<Int>.() -> Unit>(Expect<Collection<String>>::size).name to Companion::size
 ) {
     @Suppress("unused", "UNUSED_VALUE")
