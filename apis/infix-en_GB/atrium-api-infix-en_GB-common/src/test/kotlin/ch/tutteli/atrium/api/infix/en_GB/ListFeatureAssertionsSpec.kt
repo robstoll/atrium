6057d782ba0c16f6bcbdea71e7954efe49aa607e
sanatik <serikulys@gmail.com>
2019-10-27 15:00:08 +0100

    Rearranged ListFeatureAssertionsSpec a bit(order and more testing)

--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/ListFeatureAssertionsSpec.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/ListFeatureAssertionsSpec.kt
@@ -17,28 +17,30 @@ class ListFeatureAssertionsSpec : ch.tutteli.atrium.specs.integration.ListFeatur
         @Suppress("unused", "UNUSED_VALUE")
         private fun ambiguityTest() {
 
-            val a1: Expect<List<Int>> = notImplemented()
-            val a2: Expect<out List<Int>> = notImplemented()
-            val a1b: Expect<List<Int?>> = notImplemented()
-            val a2b: Expect<out List<Int?>> = notImplemented()
+            var a1: Expect<List<Int>> = notImplemented()
+            var a2: Expect<out List<Int>> = notImplemented()
+            var a1b: Expect<List<Int?>> = notImplemented()
+            var a2b: Expect<out List<Int?>> = notImplemented()
 
-            val a3: Expect<out List<*>> = notImplemented()
+            var a3: Expect<out List<*>> = notImplemented()
 
             a1 get 1
             a2 get 1
+            a1 get Index(1)
+            a2 get Index(1)
+            a1 = a1 get Index(1) assertIt { }
+            a2 = a2 get Index(1) assertIt { }
 
             a1b get 1
             a2b get 1
-
-            a3 get 1
-
-            a1 get Index(1)
-            a2 get Index(1)
-
             a1b get Index(1)
             a2b get Index(1)
+            a1b = a1b get Index(1) assertIt { }
+            a2b = a2b get Index(1) assertIt { }
 
+            a3 get 1
             a3 get Index(1)
+            a3 = a3 get Index(1) assertIt { }
         }
     }
 }
