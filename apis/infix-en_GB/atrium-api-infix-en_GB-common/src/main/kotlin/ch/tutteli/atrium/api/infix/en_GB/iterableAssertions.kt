d8a751335f57ed34d7b62babfdf6445a82ab01c6
Robert Stoll <rstoll@tutteli.ch>
2019-11-27 23:12:00 +0100

    use the AsciiBulletPointReporterFactory for the new infix API
    - add Iterable.all including spec as PoC

---
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/iterableAssertions.kt
@@ -0,0 +1,15 @@
+package ch.tutteli.atrium.api.infix.en_GB
+
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.domain.builders.ExpectImpl
+
+/**
+ * Expects that the subject of the assertion (an [Iterable]) has at least one element and
+ * that every element holds all assertions created by the [assertionCreatorOrNull] or that all elements are `null`
+ * in case [assertionCreatorOrNull] is defined as `null`.
+ *
+ * @return This assertion container to support a fluent API.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ */
+infix fun <E : Any, T : Iterable<E?>> Expect<T>.all(assertionCreatorOrNull: (Expect<E>.() -> Unit)?) =
+    addAssertion(ExpectImpl.iterable.all(this, assertionCreatorOrNull))
