cf3007916e9f783a32e3f903edf7a4400fa5b4b9
Robert Stoll <rstoll@tutteli.ch>
2020-01-04 22:20:33 +0100

    suppress deprecation in atrium-assertions -> throwableAssertions.kt

--- misc/atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/throwableAssertions.kt
+++ misc/atrium-assertions/src/main/kotlin/ch/tutteli/atrium/assertions/throwableAssertions.kt
@@ -1,7 +1,10 @@
+@file:Suppress("DEPRECATION")
+
 package ch.tutteli.atrium.assertions
 
 import ch.tutteli.atrium.creating.AssertionPlant
 import ch.tutteli.atrium.domain.builders.AssertImpl
+import ch.tutteli.atrium.domain.robstoll.lib.creating.throwable.thrown.builders.ThrowableThrownBuilder
 import kotlin.reflect.KClass
 
 @Deprecated("Use AssertImpl.throwable.thrown.toBe; will be removed with 1.0.0",
@@ -10,7 +13,7 @@ import kotlin.reflect.KClass
         "ch.tutteli.atrium.creating.AssertImpl"
     )
 )
-fun <TExpected : Throwable> _toThrow(throwableThrownBuilder: ch.tutteli.atrium.domain.robstoll.lib.creating.throwable.thrown.builders.ThrowableThrownBuilder, expectedType: KClass<TExpected>, assertionCreator: AssertionPlant<TExpected>.() -> Unit) {
+fun <TExpected : Throwable> _toThrow(throwableThrownBuilder: ThrowableThrownBuilder, expectedType: KClass<TExpected>, assertionCreator: AssertionPlant<TExpected>.() -> Unit) {
     @Suppress("DEPRECATION")
     AssertImpl.throwable.thrown.toBe(throwableThrownBuilder, expectedType, assertionCreator)
 }
