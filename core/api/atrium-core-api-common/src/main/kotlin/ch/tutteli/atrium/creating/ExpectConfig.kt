fb447bca3621699d643c99f4d51472795d9b0c7f
Robert Stoll <rstoll@tutteli.ch>
2020-01-06 22:39:40 +0100

    cache node_modules and format .travis.yml and java-windows.yml

--- core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/creating/ExpectConfig.kt
+++ core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/creating/ExpectConfig.kt
@@ -1,31 +1,34 @@
 package ch.tutteli.atrium.creating
 
+import ch.tutteli.atrium.reporting.Reporter
 import ch.tutteli.atrium.reporting.translating.Translatable
 
 //TODO #280 add doc
 interface BaseExpectConfig {
     val description: Translatable
     val representation: Any?
+    val reporter: Reporter
 }
 
 //TODO #280 add doc
 interface RootExpectConfig : BaseExpectConfig {
     companion object {
-        fun create(description: Translatable, representation: Any?): RootExpectConfig =
-            ExpectConfig(description, representation)
+        fun create(description: Translatable, representation: Any?, reporter: Reporter): RootExpectConfig =
+            ExpectConfig(description, representation, reporter)
     }
 }
 
 //TODO #280 add doc
 interface FeatureExpectConfig : BaseExpectConfig {
     companion object {
-        fun create(description: Translatable, representation: Any?): FeatureExpectConfig =
-            ExpectConfig(description, representation)
+        fun create(description: Translatable, representation: Any?, reporter: Reporter): FeatureExpectConfig =
+            ExpectConfig(description, representation, reporter)
     }
 }
 
 private data class ExpectConfig(
     override val description: Translatable,
-    override val representation: Any?
+    override val representation: Any?,
+    override val reporter: Reporter
 ) : FeatureExpectConfig,
     RootExpectConfig
