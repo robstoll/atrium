d7e07713248be5e516bfecf04b918a942ed0d800
Luke Biddell <luke.a.biddell@gmail.com>
2020-01-09 13:42:27 +0000

    fix conflict with assertions

--- translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionDateTimeLikeAssertion.kt
+++ translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionDateTimeLikeAssertion.kt
@@ -11,9 +11,14 @@ enum class DescriptionDateTimeLikeAssertion(override val value: String) : String
     YEAR("Jahr"),
     MONTH("Monat"),
     DAY_OF_WEEK("Wochentag"),
+<<<<<<< HEAD
     IS_AFTER_OR_EQUALS("ist nach oder gleichzeitig"),
     IS_AFTER("ist nach"),
     IS_BEFORE_OR_EQUALS("ist vor oder gleichzeitig"),
     IS_BEFORE("ist vor"),
     DAY("Tag")
+=======
+    DAY("Tag"),
+    IS_AFTER_OR_EQUALS("ist nach dem oder gleich"),
+>>>>>>> rename mentions of equal to equals
 }
