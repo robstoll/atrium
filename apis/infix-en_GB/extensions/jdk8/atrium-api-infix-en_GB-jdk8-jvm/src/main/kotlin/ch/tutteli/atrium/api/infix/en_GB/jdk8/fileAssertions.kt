3e41c14b9cb4630cf648e022599b6836da989794
Joshua Gleitze <dev@joshuagleitze.de>
2020-02-03 21:53:05 +0200

    feat(infix-api): file.asPath() assertion

---
+++ apis/infix-en_GB/extensions/jdk8/atrium-api-infix-en_GB-jdk8-jvm/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/jdk8/fileAssertions.kt
@@ -0,0 +1,35 @@
+@file:Suppress("JAVA_MODULE_DOES_NOT_READ_UNNAMED_MODULE" /* TODO remove once https://youtrack.jetbrains.com/issue/KT-35343 is fixed */)
+
+package ch.tutteli.atrium.api.infix.en_GB.jdk8
+
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.domain.builders.ExpectImpl
+import java.io.File
+import java.nio.file.Path
+
+/**
+ * Turns `Expect<File>` into `Expect<Path>`.
+ *
+ * The transformation as such is not reflected in reporting.
+ * Use `feature(File::toPath)` if you want to show the transformation in reporting.
+ *
+ * @return The newly created [Expect] for the transformed subject.
+ *
+ * @since 0.10.0
+ */
+fun <T : File> Expect<T>.asPath(): Expect<Path> =
+    ExpectImpl.changeSubject(this).unreported { it.toPath() }
+
+/**
+ * Expects that the subject of the assertion holds all assertions the given [assertionCreator] creates for
+ * the subject as [Path].
+ *
+ * The transformation as such is not reflected in reporting.
+ * Use `feature(File::toPath, assertionCreator)` if you want to show the transformation in reporting.
+ *
+ * @return This assertion container to support a fluent API.
+ *
+ * @since 0.10.0
+ */
+infix fun <T : File> Expect<T>.asPath(assertionCreator: Expect<Path>.() -> Unit): Expect<T> =
+    apply { asPath().addAssertionsCreatedBy(assertionCreator) }
