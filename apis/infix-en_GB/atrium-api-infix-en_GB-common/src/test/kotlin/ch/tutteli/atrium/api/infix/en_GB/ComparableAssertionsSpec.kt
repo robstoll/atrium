038a8db947224feae6f32fb6b33f4e315529853b
Robert Stoll <rstoll@tutteli.ch>
2020-02-06 00:02:40 +0100

    add Comparable assertions to the new infix api

---
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/ComparableAssertionsSpec.kt
@@ -0,0 +1,22 @@
+package ch.tutteli.atrium.api.infix.en_GB
+
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.specs.fun1
+import ch.tutteli.atrium.specs.notImplemented
+
+class ComparableAssertionsSpec : ch.tutteli.atrium.specs.integration.ComparableAssertionsSpec(
+    fun1(Expect<Int>::isLessThan),
+    fun1(Expect<Int>::isLessThanOrEqual),
+    fun1(Expect<Int>::isGreaterThan),
+    fun1(Expect<Int>::isGreaterThanOrEqual)
+) {
+
+    @Suppress("unused")
+    fun ambiguityTest() {
+        val a1: Expect<Int> = notImplemented()
+        a1 isLessThan 1
+        a1 isLessThanOrEqual 1
+        a1 isGreaterThan 1
+        a1 isGreaterThanOrEqual 1
+    }
+}
