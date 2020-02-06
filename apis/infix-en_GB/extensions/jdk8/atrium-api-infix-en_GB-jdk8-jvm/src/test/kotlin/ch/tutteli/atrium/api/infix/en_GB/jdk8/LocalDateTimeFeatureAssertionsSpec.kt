e4794deffca6b7375126eca15387aaea3ae5de55
Joshua Gleitze <dev@joshuagleitze.de>
2020-02-06 13:07:39 +0200

    feat(infix-api): LocalDateTime assertions

---
+++ apis/infix-en_GB/extensions/jdk8/atrium-api-infix-en_GB-jdk8-jvm/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/jdk8/LocalDateTimeFeatureAssertionsSpec.kt
@@ -0,0 +1,36 @@
+package ch.tutteli.atrium.api.infix.en_GB.jdk8
+
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.specs.fun1
+import ch.tutteli.atrium.specs.notImplemented
+import ch.tutteli.atrium.specs.property
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+
+class LocalDateTimeFeatureAssertionsSpec : ch.tutteli.atrium.specs.integration.LocalDateTimeFeatureAssertionsSpec(
+    property<LocalDateTime, Int>(Expect<LocalDateTime>::year),
+    fun1<LocalDateTime, Expect<Int>.() -> Unit>(Expect<LocalDateTime>::year),
+    property<LocalDateTime, Int>(Expect<LocalDateTime>::month),
+    fun1<LocalDateTime, Expect<Int>.() -> Unit>(Expect<LocalDateTime>::month),
+    property<LocalDateTime, Int>(Expect<LocalDateTime>::day),
+    fun1<LocalDateTime, Expect<Int>.() -> Unit>(Expect<LocalDateTime>::day),
+    property<LocalDateTime, DayOfWeek>(Expect<LocalDateTime>::dayOfWeek),
+    fun1<LocalDateTime, Expect<DayOfWeek>.() -> Unit>(Expect<LocalDateTime>::dayOfWeek)
+) {
+    @Suppress("unused", "UNUSED_VALUE")
+    private fun ambiguityTest() {
+        var a1: Expect<LocalDateTime> = notImplemented()
+
+        a1.year
+        a1 = a1 year { }
+
+        a1.month
+        a1 = a1 month { }
+
+        a1.day
+        a1 = a1 day { }
+
+        a1.dayOfWeek
+        a1 = a1 dayOfWeek { }
+    }
+}
