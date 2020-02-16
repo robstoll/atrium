b13a562d57dc9bc93a19ab7448284b95824b808c
Robert Stoll <rstoll@tutteli.ch>
2020-02-16 13:20:26 +0100

    fix duplicate file problem, add missing android dependency

--- misc/verbs/atrium-verbs-jvm/src/main/kotlin/ch/tutteli/atrium/api/verbs/expectJvm.kt
+++ misc/verbs/atrium-verbs-jvm/src/main/kotlin/ch/tutteli/atrium/api/verbs/expectJvm.kt
@@ -1,12 +1,15 @@
 @file:Suppress("JAVA_MODULE_DOES_NOT_READ_UNNAMED_MODULE" /* TODO remove once https://youtrack.jetbrains.com/issue/KT-35343 is fixed */)
 @file:JvmMultifileClass
-@file:JvmName("ExpectKt")
+
 package ch.tutteli.atrium.api.verbs
 
 import ch.tutteli.atrium.creating.RootExpect
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
-@Suppress("UNCHECKED_CAST")
+@Suppress(
+    "UNCHECKED_CAST",
+    /* TODO remove once https://youtrack.jetbrains.com/issue/KT-36752 is fixed */ "USELESS_CAST"
+)
 fun <T> expect(subject: suspend CoroutineScope.() -> T): RootExpect<() -> T> =
     expect({ runBlocking(block = subject) } as () -> T)
