8223b1fb7684ad8cbeaf9f48ff641a64d6e9efa4
Robert Stoll <rstoll@tutteli.ch>
2020-02-16 13:33:17 +0100

    remove kotlinx from module-info.java in api-fluent-en_GB-kotlin_1_3

--- apis/fluent-en_GB/extensions/kotlin_1_3/atrium-api-fluent-en_GB-kotlin_1_3-jvm/src/module/module-info.java
+++ apis/fluent-en_GB/extensions/kotlin_1_3/atrium-api-fluent-en_GB-kotlin_1_3-jvm/src/module/module-info.java
@@ -2,7 +2,6 @@ module ch.tutteli.atrium.api.fluent.en_GB.kotlin_1_3 {
     requires ch.tutteli.atrium.domain.builders.kotlin_1_3;
     requires ch.tutteli.kbox;
     requires kotlin.stdlib;
-    requires kotlinx.coroutines.core;
 
     exports ch.tutteli.atrium.api.fluent.en_GB.kotlin_1_3;
 }
