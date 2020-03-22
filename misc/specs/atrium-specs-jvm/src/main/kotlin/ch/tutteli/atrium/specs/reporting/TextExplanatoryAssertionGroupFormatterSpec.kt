f90c1d4f0db6148e468817335030bc35db356880
assaflei <genemails2011@gmail.com>
2020-03-22 22:09:22 +0200

    removed unnecessary changes and comments from TextExplanatoryAssertionGroupFormatterSpec

--- misc/specs/atrium-specs-jvm/src/main/kotlin/ch/tutteli/atrium/specs/reporting/TextExplanatoryAssertionGroupFormatterSpec.kt
+++ misc/specs/atrium-specs-jvm/src/main/kotlin/ch/tutteli/atrium/specs/reporting/TextExplanatoryAssertionGroupFormatterSpec.kt
@@ -5,18 +5,14 @@ import ch.tutteli.atrium.assertions.ExplanatoryAssertionGroupType
 import ch.tutteli.atrium.domain.builders.ExpectImpl
 import ch.tutteli.atrium.reporting.AssertionFormatter
 import ch.tutteli.atrium.reporting.AssertionFormatterController
-import org.spekframework.spek2.Spek
 import kotlin.reflect.KClass
 
-//TODO #116 migrate spek1 to spek2 - move to specs-common
 abstract class TextExplanatoryAssertionGroupFormatterSpec(
     testeeFactory: (Map<KClass<out BulletPointIdentifier>, String>, AssertionFormatterController) -> AssertionFormatter,
     describePrefix: String = "[Atrium] "
-) : Spek({
-    include(object : TextExplanatoryBasedAssertionGroupFormatterSpec<ExplanatoryAssertionGroupType>(
+) : TextExplanatoryBasedAssertionGroupFormatterSpec<ExplanatoryAssertionGroupType>(
     testeeFactory,
     ExplanatoryAssertionGroupType::class,
     object : ExplanatoryAssertionGroupType {},
     { ExpectImpl.builder.explanatoryGroup.withDefaultType.withAssertions(it).build() },
-    describePrefix) {})
-})
+    describePrefix)
