5c1e99709d87f7e988ac4dcecf7b560e6496c490
Tornike Kechakmadze <44344828+tkech17@users.noreply.github.com>
2020-04-15 09:47:13 +0300

    use _ for unused parameter (#446)

--- misc/specs/atrium-specs-jvm/src/main/kotlin/ch/tutteli/atrium/specs/integration/OptionalAssertionsSpec.kt
+++ misc/specs/atrium-specs-jvm/src/main/kotlin/ch/tutteli/atrium/specs/integration/OptionalAssertionsSpec.kt
@@ -64,7 +64,7 @@ abstract class OptionalAssertionsSpec(
                 }
             }
 
-            isPresentFunctions.forEach { (name, isPresentFun, hasExtraHint) ->
+            isPresentFunctions.forEach { (name, isPresentFun, _) ->
                 it("$name - can perform sub-assertion which holds") {
                     expect(presentValue).isPresentFun { toBe(2) }
                 }
