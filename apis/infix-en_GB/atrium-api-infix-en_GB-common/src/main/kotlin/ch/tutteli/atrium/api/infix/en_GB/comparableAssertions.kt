5a4ef56462fd2eaff427b6eb9f9d73e7eca76ac2
Jacob Lawson <jaibhavaya@gmail.com>
2020-05-18 18:43:26 -0400

    Update apis/infix-en_GB/atrium-api-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/comparableAssertions.kt
    Co-authored-by: Robert Stoll <rstoll@tutteli.ch>

--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/comparableAssertions.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/comparableAssertions.kt
@@ -44,8 +44,8 @@ infix fun <T : Comparable<T>> Expect<T>.isGreaterThanOrEqual(expected: T) =
     addAssertion(ExpectImpl.comparable.isGreaterOrEquals(this, expected))
 
 /**
- * Expects that the subject of the assertion is equal to [expected].
- * The comparison is carried out with [Comparable.compareTo].
+ * Expects that the subject of the assertion is equal to [expected]
+ * where the comparison is carried out with [Comparable.compareTo].
  *
  * @return An [Expect] for the current subject of the assertion.
  * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
