34ec1d4b09110f5e79a8e49766f99f23a9df480e
Robert Stoll <rstoll@tutteli.ch>
2020-05-20 21:46:03 +0200

    deprecate parameterObjects in cc-infix

--- apis/cc-infix-en_GB/atrium-api-cc-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/parameterObjects.kt
+++ apis/cc-infix-en_GB/atrium-api-cc-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/cc/infix/en_GB/parameterObjects.kt
@@ -16,6 +16,10 @@ import kotlin.jvm.JvmName
 /**
  * Parameter object to express `T, vararg T` in the infix-api.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.all(expected, *otherExpected)")
+)
 class All<out T>(override val expected: T, override vararg val otherExpected: T) : VarArgHelper<T>
 
 
@@ -26,6 +30,10 @@ class All<out T>(override val expected: T, override vararg val otherExpected: T)
  *   to be identified if it holds all [Assertion]s the lambda might create.
  *   In case it is defined as `null`, then an entry is identified if it is `null` as well.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.entry(assertionCreatorOrNull)")
+)
 class Entry<in T: Any>(
     val assertionCreatorOrNull: (Assert<T>.() -> Unit)?
 ): GroupWithoutNullableEntries<(Assert<T>.() -> Unit)?>, GroupWithNullableEntries<(Assert<T>.() -> Unit)?> {
@@ -45,6 +53,10 @@ class Entry<in T: Any>(
  *   In case it is defined as `null`, then an entry is identified if it is `null` as well.
  * @param otherAssertionCreatorsOrNulls A variable amount of additional identification lambdas or `null`s.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.entries(assertionCreatorOrNull, *otherAssertionCreatorsOrNulls)")
+)
 class Entries<in T : Any>(
     val assertionCreatorOrNull: (Assert<T>.() -> Unit)?,
     vararg val otherAssertionCreatorsOrNulls: (Assert<T>.() -> Unit)?
@@ -58,17 +70,29 @@ class Entries<in T : Any>(
 /**
  * Wrapper for a single index -- can be used as distinguishable type for an overload where Int is already in use.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.index(index)")
+)
 data class Index(val index: Int)
 
 /**
  * Wrapper for a single key -- can be used as distinguishable type for an overload where [K] is already in use.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.key(key)")
+)
 data class Key<out K>(val key: K)
 
 /**
  * Parameter object to express a key/value [Pair] whose value type is a lambda with an
  * [Assert][AssertionPlant] receiver, which means one can either pass a lambda or `null`.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.keyValue(key, valueAssertionCreatorOrNull)")
+)
 data class KeyValue<out K, V : Any>(val key: K, val valueAssertionCreatorOrNull: (Assert<V>.() -> Unit)?) {
     fun toPair(): Pair<K, (Assert<V>.() -> Unit)?> = key to valueAssertionCreatorOrNull
     override fun toString(): String
@@ -81,6 +105,10 @@ data class KeyValue<out K, V : Any>(val key: K, val valueAssertionCreatorOrNull:
  *
  * Notice, most probably the type parameter G will be removed in the future, will be fixed to [Group].
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.order(firstGroup, secondGroup, *otherExpectedGroups)")
+)
 class Order<out T, out G : Group<T>>(
     val firstGroup: G,
     val secondGroup: G,
@@ -90,6 +118,10 @@ class Order<out T, out G : Group<T>>(
 /**
  * Parameter object to express `Pair<K, V>, vararg Pair<K, V>` in the infix-api.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.pairs(expected, *otherExpected)")
+)
 class Pairs<out K, out V>(
     override val expected: Pair<K, V>,
     override vararg val otherExpected: Pair<K, V>
@@ -98,6 +130,10 @@ class Pairs<out K, out V>(
 /**
  * Parameter object to express `String, vararg String` in the infix-api.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.regexPatterns(pattern, *otherPatterns)")
+)
 class RegexPatterns(pattern: String, vararg otherPatterns: String) : VarArgHelper<String> {
     override val expected = pattern
     override val otherExpected = otherPatterns
@@ -107,6 +143,10 @@ class RegexPatterns(pattern: String, vararg otherPatterns: String) : VarArgHelpe
 /**
  * Represents a [Group] with a single value.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.value(expected)")
+)
 data class Value<out T>(val expected: T) : GroupWithNullableEntries<T>, GroupWithoutNullableEntries<T> {
     override fun toList() = listOf(expected)
 }
@@ -114,6 +154,10 @@ data class Value<out T>(val expected: T) : GroupWithNullableEntries<T>, GroupWit
 /**
  * Represents a [Group] of multiple values.
  */
+@Deprecated(
+    "Switch from Assert to Expect; will be removed with 1.0.0 -- see https://github.com/robstoll/atrium/releases/tag/v0.9.0#migration for migration hints and scripts.",
+    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.values(expected, *otherExpected)")
+)
 class Values<out T>(
     override val expected: T,
     override vararg val otherExpected: T
