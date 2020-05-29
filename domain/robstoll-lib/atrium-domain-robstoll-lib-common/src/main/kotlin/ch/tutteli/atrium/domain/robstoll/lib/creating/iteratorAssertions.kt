81fd17541e19ef4199e47d587e364636d96e9f01
Robert Stoll <rstoll@tutteli.ch>
2020-05-29 21:30:17 +0200

    fix iteratorAssertion, do no longer use RawString

--- domain/robstoll-lib/atrium-domain-robstoll-lib-common/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/iteratorAssertions.kt
+++ domain/robstoll-lib/atrium-domain-robstoll-lib-common/src/main/kotlin/ch/tutteli/atrium/domain/robstoll/lib/creating/iteratorAssertions.kt
@@ -3,18 +3,12 @@ package ch.tutteli.atrium.domain.robstoll.lib.creating
 import ch.tutteli.atrium.assertions.Assertion
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.domain.builders.ExpectImpl
-import ch.tutteli.atrium.reporting.RawString
 import ch.tutteli.atrium.translations.DescriptionBasic
 import ch.tutteli.atrium.translations.DescriptionIterableAssertion.NEXT_ELEMENT
 
-
 fun <E, T : Iterator<E>> _hasNext(expect: Expect<T>): Assertion =
-    ExpectImpl.builder.createDescriptive(expect, DescriptionBasic.HAS, RawString.create(NEXT_ELEMENT)) {
-        it.hasNext()
-    }
+    ExpectImpl.builder.createDescriptive(expect, DescriptionBasic.HAS, NEXT_ELEMENT) { it.hasNext() }
 
 fun <E, T : Iterator<E>> _hasNotNext(expect: Expect<T>): Assertion =
-    ExpectImpl.builder.createDescriptive(expect, DescriptionBasic.HAS_NOT, RawString.create(NEXT_ELEMENT)) {
-        !it.hasNext()
-    }
+    ExpectImpl.builder.createDescriptive(expect, DescriptionBasic.HAS_NOT, NEXT_ELEMENT) { !it.hasNext() }
 
