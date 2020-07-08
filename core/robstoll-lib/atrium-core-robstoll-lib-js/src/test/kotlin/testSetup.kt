3e832d5807c13e10692f589da829146622c3644c
Robert Stoll <rstoll@tutteli.ch>
2020-07-08 23:09:31 +0200

    deprecate dependOn_atrium_domain_robstoll

--- core/robstoll-lib/atrium-core-robstoll-lib-js/src/test/kotlin/testSetup.kt
+++ core/robstoll-lib/atrium-core-robstoll-lib-js/src/test/kotlin/testSetup.kt
@@ -1,3 +1,5 @@
+//TODO remove file with 1.0.0
+@file:Suppress("DEPRECATION")
 import ch.tutteli.atrium.core.robstoll.dependOn_atrium_core_robstoll
 import ch.tutteli.atrium.domain.robstoll.dependOn_atrium_domain_robstoll
 
