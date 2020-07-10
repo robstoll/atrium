b683013e3287e86d6b1003c165c62a45bf19834a
Robert Stoll <rstoll@tutteli.ch>
2020-07-10 22:57:14 +0200

    mark all new Expect types as experimental including getImpl

--- core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/creating/DelegatingExpect.kt
+++ core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/creating/DelegatingExpect.kt
@@ -1,10 +1,13 @@
 package ch.tutteli.atrium.creating
 
+import ch.tutteli.atrium.core.ExperimentalNewExpectTypes
 import ch.tutteli.atrium.core.Option
 import ch.tutteli.atrium.creating.impl.DelegatingExpectImpl
 
-interface DelegatingExpect<T>: Expect<T> {
-    companion object{
+interface DelegatingExpect<T> : Expect<T> {
+    companion object {
+        @Suppress("DEPRECATION" /* OptIn is only available since 1.3.70 which we cannot use if we want to support 1.2 */)
+        @UseExperimental(ExperimentalNewExpectTypes::class)
         operator fun <T> invoke(assertionHolder: AssertionHolder, maybeSubject: Option<T>): Expect<T> =
             DelegatingExpectImpl(assertionHolder, maybeSubject)
     }
--- logic/extensions/kotlin_1_3/atrium-logic-kotlin_1_3-common/src/generated/kotlin/ch/tutteli/atrium/logic/kotlin_1_3/impls.kt
+++ logic/extensions/kotlin_1_3/atrium-logic-kotlin_1_3-common/src/generated/kotlin/ch/tutteli/atrium/logic/kotlin_1_3/impls.kt
@@ -8,6 +8,7 @@
 @file:JvmName("ImplsKt")
 package ch.tutteli.atrium.logic.kotlin_1_3
 
+import ch.tutteli.atrium.core.ExperimentalNewExpectTypes
 import kotlin.jvm.JvmMultifileClass
 import kotlin.jvm.JvmName
 
@@ -15,6 +16,8 @@ import ch.tutteli.atrium.creating.AssertionContainer
 import ch.tutteli.atrium.logic.kotlin_1_3.impl.DefaultResultAssertions
 
 @PublishedApi
+@Suppress("DEPRECATION" /* OptIn is only available since 1.3.70 which we cannot use if we want to support 1.2 */)
+@UseExperimental(ExperimentalNewExpectTypes::class)
 internal inline val <T> AssertionContainer<T>._resultImpl
     get() = getImpl(ResultAssertions::class) { DefaultResultAssertions() }
 
