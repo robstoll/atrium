a785fd8f86e7d83bd1c7fec4ea04a8f7c5ef5985
Robert Stoll <rstoll@tutteli.ch>
2020-07-11 23:14:58 +0200

    suppress symbol-from-unnamed-modul-bug in atrium-logic-jvm

--- logic/atrium-logic-jvm/src/main/kotlin/ch/tutteli/atrium/logic/ChronoLocalDateAssertions.kt
+++ logic/atrium-logic-jvm/src/main/kotlin/ch/tutteli/atrium/logic/ChronoLocalDateAssertions.kt
@@ -1,8 +1,9 @@
+@file:Suppress("JAVA_MODULE_DOES_NOT_READ_UNNAMED_MODULE" /* TODO remove once https://youtrack.jetbrains.com/issue/KT-35343 is fixed */)
+
 package ch.tutteli.atrium.logic
 
 import ch.tutteli.atrium.assertions.Assertion
 import ch.tutteli.atrium.creating.AssertionContainer
-import java.math.BigDecimal
 import java.time.chrono.ChronoLocalDate
 
 /**
