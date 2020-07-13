8ba1bb54670ff5ca234cc2284ab1fc3fc2aae72d
Guga Tkesheladze <guram.tkesheladze@azry.com>
2020-07-13 21:37:33 +0400

    removed redundant class full path use

--- misc/verbs-internal/atrium-verbs-internal-js/src/main/kotlin/ch/tutteli/atrium/verbs/internal/registerServices.kt
+++ misc/verbs-internal/atrium-verbs-internal-js/src/main/kotlin/ch/tutteli/atrium/verbs/internal/registerServices.kt
@@ -7,5 +7,5 @@ import ch.tutteli.atrium.core.polyfills.registerService
 @Suppress("unused" /* here in order that the code is executed when module is loaded */)
 private val register = run {
 
-    registerService<ch.tutteli.atrium.reporting.ReporterFactory> { ch.tutteli.atrium.verbs.internal.NoAdjustingReporterFactory() }
+    registerService<ch.tutteli.atrium.reporting.ReporterFactory> { NoAdjustingReporterFactory() }
 }
