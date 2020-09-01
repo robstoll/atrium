92c354a4621c34ed71f3a5c57fc77326f443ae93
Valefant <valentino.bernardo_ciddio@smail.th-koeln.de>
2020-09-01 21:48:00 +0200

    Add a more sophisticated textual limitation function, which shows visually if a string is too long. Furthermore show the type information

--- core/robstoll-lib/atrium-core-robstoll-lib-common/src/main/kotlin/ch/tutteli/atrium/core/robstoll/lib/reporting/DetailedObjectFormatter.kt
+++ core/robstoll-lib/atrium-core-robstoll-lib-common/src/main/kotlin/ch/tutteli/atrium/core/robstoll/lib/reporting/DetailedObjectFormatter.kt
@@ -41,24 +41,24 @@ abstract class DetailedObjectFormatterCommon(
      * @return The formatted [value].
      */
     @Suppress( /* TODO remove with 1.0.0 */ "DEPRECATION")
-    override fun format(value: Any?): String = limitRepresentation(when (value) {
+    override fun format(value: Any?): String = when (value) {
         null -> Text.NULL.string
         is LazyRepresentation -> format(safeEval(value))
         is Char -> "'$value'"
         is Boolean -> value.toString()
         is String -> format(value)
         is CharSequence -> format(value)
-        is Text -> value.string
-        is Translatable -> translator.translate(value)
+        is Text -> limitRepresentation(value.string)
+        is Translatable -> limitRepresentation(translator.translate(value))
         is KClass<*> -> format(value)
         is Enum<*> -> format(value)
         is Throwable -> format(value)
         //TODO remove with 1.0.0
-        is StringBasedRawString -> value.string
-        is ch.tutteli.atrium.reporting.translating.TranslatableBasedRawString -> translator.translate(value.translatable)
+        is StringBasedRawString -> limitRepresentation(value.string)
+        is ch.tutteli.atrium.reporting.translating.TranslatableBasedRawString -> limitRepresentation(translator.translate(value.translatable))
 
-        else -> value.toString() + classNameAndIdentity(value)
-    })
+        else -> limitRepresentation(value.toString()) + classNameAndIdentity(value)
+    }
 
     private fun safeEval(lazyRepresentation: LazyRepresentation) =
         //TODO remove try-catch with 1.0.0 should no longer be necessary
@@ -68,15 +68,17 @@ abstract class DetailedObjectFormatterCommon(
             ErrorMessages.REPRESENTATION_BASED_ON_SUBJECT_NOT_DEFINED
         }
 
-    private fun format(string: String) = "\"$string\"" + identityHash(INDENT, string)
-    private fun format(charSequence: CharSequence) = "\"$charSequence\"" + classNameAndIdentity(charSequence)
+    private fun format(string: String) = "\"${limitRepresentation(string)}\"" + identityHash(INDENT, string)
+    private fun format(charSequence: CharSequence) = "\"${limitRepresentation(charSequence.toString())}\"" + classNameAndIdentity(charSequence)
     private fun format(enum: Enum<*>) =
-        enum.toString() + INDENT + "(" + (enum::class.fullName) + ")"
+        limitRepresentation(enum.toString()) + INDENT + "(" + (enum::class.fullName) + ")"
 
     private fun format(throwable: Throwable) = throwable::class.fullName
 
     private fun classNameAndIdentity(any: Any): String = INDENT + "(${any::class.fullName}${identityHash(" ", any)})"
-    private fun limitRepresentation(value: String) = if (value.length > 10000) value.substring(0, 10000) else value
+    private fun limitRepresentation(value: String): String {
+        return if (value.length > 10000) "${value.substring(0, 10000)}..." else value
+    }
 
     protected abstract fun format(kClass: KClass<*>): String
     protected abstract fun identityHash(indent: String, any: Any): String
