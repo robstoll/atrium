b3f5580fdd641dad49a09f3921926bf6a055e448
Valefant <valentino.bernardo_ciddio@smail.th-koeln.de>
2020-09-07 17:48:49 +0200

    Implement infix api

--- apis/infix-en_GB/atrium-api-infix-en_GB-jvm/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/chronoLocalDateAssertions.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-jvm/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/chronoLocalDateAssertions.kt
@@ -68,3 +68,68 @@ infix fun <T : ChronoLocalDate> Expect<T>.isAfterOrEqual(expected: ChronoLocalDa
  */
 infix fun <T : ChronoLocalDate> Expect<T>.isEqual(expected: ChronoLocalDate): Expect<T> =
     _logicAppend { isEqual(expected) }
+
+/**
+ * Expects that the subject of the assertion (a [ChronoLocalDate])
+ * is before the [expected] [java.time.LocalDate] given as [String].
+ * The [expected] parameter needs to be in the form of **yyyy-mm-dd** or else a [java.time.DateTimeException] will be thrown.
+ *
+ * @return An [Expect] for the current subject of the assertion.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ *
+ * @since 0.14.0
+ */
+infix fun <T : ChronoLocalDate> Expect<T>.isBefore(expected: String): Expect<T> =
+    _logicAppend { isBefore(expected) }
+
+/**
+ * Expects that the subject of the assertion (a [ChronoLocalDate])
+ * is before or equal the [expected] [java.time.LocalDate] given as [String].
+ * The [expected] parameter needs to be in the form of **yyyy-mm-dd** or else a [java.time.DateTimeException] will be thrown.
+ *
+ * @return An [Expect] for the current subject of the assertion.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ *
+ * @since 0.14.0
+ */
+infix fun <T : ChronoLocalDate> Expect<T>.isBeforeOrEqual(expected: String): Expect<T> =
+    _logicAppend { isBeforeOrEqual(expected) }
+
+/**
+ * Expects that the subject of the assertion (a [ChronoLocalDate])
+ * is after the [expected] [java.time.LocalDate] given as [String].
+ * The [expected] parameter needs to be in the form of **yyyy-mm-dd** or else a [java.time.DateTimeException] will be thrown.
+ *
+ * @return An [Expect] for the current subject of the assertion.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ *
+ * @since 0.14.0
+ */
+infix fun <T : ChronoLocalDate> Expect<T>.isAfter(expected: String): Expect<T> =
+    _logicAppend { isAfter(expected) }
+
+/**
+ * Expects that the subject of the assertion (a [ChronoLocalDate])
+ * is after or equal the [expected] [java.time.LocalDate] given as [String].
+ * The [expected] parameter needs to be in the form of **yyyy-mm-dd** or else a [java.time.DateTimeException] will be thrown.
+ *
+ * @return An [Expect] for the current subject of the assertion.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ *
+ * @since 0.14.0
+ */
+infix fun <T : ChronoLocalDate> Expect<T>.isAfterOrEqual(expected: String): Expect<T> =
+    _logicAppend { isAfterOrEqual(expected) }
+
+/**
+ * Expects that the subject of the assertion (a [ChronoLocalDate])
+ * is equal to the [expected] [java.time.LocalDate] given as [String].
+ * The [expected] parameter needs to be in the form of **yyyy-mm-dd** or else a [java.time.DateTimeException] will be thrown.
+ *
+ * @return An [Expect] for the current subject of the assertion.
+ * @throws AssertionError Might throw an [AssertionError] if the assertion made is not correct.
+ *
+ * @since 0.14.0
+ */
+infix fun <T : ChronoLocalDate> Expect<T>.isEqual(expected: String): Expect<T> =
+    _logicAppend { isEqual(expected) }
