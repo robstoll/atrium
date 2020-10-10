176b02fdf01a11fcb72785be51b56cdfc8ab6d03
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2020-10-10 14:37:30 +0200

    Fuse collection and iterable specs (#602)

--- apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/CollectionAssertionsSpec.kt
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/CollectionAssertionsSpec.kt
@@ -1,19 +1,20 @@
 package ch.tutteli.atrium.api.fluent.en_GB
 
 import ch.tutteli.atrium.creating.Expect
-import ch.tutteli.atrium.specs.fun0
-import ch.tutteli.atrium.specs.notImplemented
+import ch.tutteli.atrium.specs.*
 
 object CollectionAssertionsSpec : ch.tutteli.atrium.specs.integration.CollectionAssertionsSpec(
     fun0(Expect<Collection<Int>>::isEmpty),
-    fun0(Expect<Collection<Int>>::isNotEmpty)
+    fun0(Expect<Collection<Int>>::isNotEmpty),
+    property<Collection<Int>, Int>(Expect<Collection<Int>>::size),
+    fun1<Collection<Int>, Expect<Int>.() -> Unit>(Expect<Collection<Int>>::size)
 ) {
     @Suppress("unused", "UNUSED_VALUE")
     private fun ambiguityTest() {
-        val a1: Expect<List<Int>> = notImplemented()
-        val a1b: Expect<Set<Int?>> = notImplemented()
+        var a1: Expect<List<Int>> = notImplemented()
+        var a1b: Expect<Set<Int?>> = notImplemented()
 
-        val star: Expect<Collection<*>> = notImplemented()
+        var star: Expect<Collection<*>> = notImplemented()
 
         a1.isEmpty()
         a1.isNotEmpty()
@@ -23,5 +24,14 @@ object CollectionAssertionsSpec : ch.tutteli.atrium.specs.integration.Collection
 
         star.isEmpty()
         star.isNotEmpty()
+
+        a1.size
+        a1 = a1.size { }
+
+        a1b.size
+        a1b = a1b.size { }
+
+        star.size
+        star = star.size { }
     }
 }
--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/CollectionAssertionsSpec.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/CollectionAssertionsSpec.kt
@@ -1,12 +1,14 @@
 package ch.tutteli.atrium.api.infix.en_GB
 
 import ch.tutteli.atrium.creating.Expect
-import ch.tutteli.atrium.specs.notImplemented
+import ch.tutteli.atrium.specs.*
 import ch.tutteli.atrium.specs.testutils.WithAsciiReporter
 
 class CollectionAssertionsSpec : ch.tutteli.atrium.specs.integration.CollectionAssertionsSpec(
     "toBe ${empty::class.simpleName}" to Companion::isEmpty,
-    "notToBe ${empty::class.simpleName}" to Companion::isNotEmpty
+    "notToBe ${empty::class.simpleName}" to Companion::isNotEmpty,
+    property<Collection<Int>, Int>(Expect<Collection<Int>>::size),
+    fun1<Collection<Int>, Expect<Int>.() -> Unit>(Expect<Collection<Int>>::size)
 ) {
     companion object : WithAsciiReporter() {
         private fun isEmpty(expect: Expect<Collection<Int>>) = expect toBe empty
@@ -16,10 +18,10 @@ class CollectionAssertionsSpec : ch.tutteli.atrium.specs.integration.CollectionA
 
     @Suppress("unused", "UNUSED_VALUE")
     private fun ambiguityTest() {
-        val a1: Expect<List<Int>> = notImplemented()
-        val a1b: Expect<Set<Int?>> = notImplemented()
+        var a1: Expect<List<Int>> = notImplemented()
+        var a1b: Expect<Set<Int?>> = notImplemented()
 
-        val star: Expect<Collection<*>> = notImplemented()
+        var star: Expect<Collection<*>> = notImplemented()
 
         a1 toBe empty
         a1 notToBe empty
@@ -29,6 +31,15 @@ class CollectionAssertionsSpec : ch.tutteli.atrium.specs.integration.CollectionA
 
         star toBe empty
         star notToBe empty
+
+        a1.size
+        a1 = a1 size { }
+
+        a1b.size
+        a1b = a1b size { }
+
+        star.size
+        star = star size { }
     }
 }
 
