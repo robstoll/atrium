b911ec75caf537e098dbf27bf87a062aacc8d92f
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2020-10-12 21:21:39 +0200

    Use anonymous object instead of duplicating code

--- misc/specs/atrium-specs-common/src/main/kotlin/ch/tutteli/atrium/specs/integration/MapEntryAssertionsSpec.kt
+++ misc/specs/atrium-specs-common/src/main/kotlin/ch/tutteli/atrium/specs/integration/MapEntryAssertionsSpec.kt
@@ -4,8 +4,6 @@ import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.api.verbs.internal.expect
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.*
-import ch.tutteli.atrium.translations.DescriptionCharSequenceAssertion
-import ch.tutteli.atrium.translations.DescriptionComparableAssertion
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.Suite
 
@@ -27,9 +25,26 @@ abstract class MapEntryAssertionsSpec(
         describePrefix,
         isKeyValue.forSubjectLess("key", 1)
     ) {})
+
     include(object : SubjectLessSpec<Map.Entry<String?, Int?>>(
         "$describePrefix[nullable] ",
         isKeyValueNullable.forSubjectLess("key", 1)
+    ) {})
+
+    include(object : KeyValueLikeAssertionsSpec<Map.Entry<String, Int>, Map.Entry<String?, Int?>>(
+        ::mapEntry,
+        ::mapEntry,
+        "key",
+        "value",
+        keyFeature,
+        key,
+        valueFeature,
+        value,
+        nullableKeyFeature,
+        nullableKey,
+        nullableValueFeature,
+        nullableValue,
+        describePrefix
     ) {})
 
     fun describeFun(vararg pairs: SpecPair<*>, body: Suite.() -> Unit) =
@@ -104,137 +119,4 @@ abstract class MapEntryAssertionsSpec(
         }
     }
 
-    val keyName = "key"
-    val valueName = "value"
-    val nullMapEntry = mapEntry(null, null)
-    val nullableMapEntry = mapEntry("hello", 1)
-    val nullableFluent: Expect<Map.Entry<String?, Int?>> = expect(nullableMapEntry)
-    val nullFluent: Expect<Map.Entry<String?, Int?>> = expect(nullMapEntry)
-
-    include(object : SubjectLessSpec<Map.Entry<String, Int>>(describePrefix,
-        keyFeature.forSubjectLess(),
-        key.forSubjectLess { endsWith("a") },
-        valueFeature.forSubjectLess(),
-        value.forSubjectLess { isGreaterThan(2) }
-    ) {})
-
-    include(object : SubjectLessSpec<Map.Entry<String?, Int?>>(
-        "$describePrefix[nullable] ",
-        nullableKeyFeature.forSubjectLess(),
-        nullableKey.forSubjectLess { toBe(null) },
-        nullableValueFeature.forSubjectLess(),
-        nullableValue.forSubjectLess { toBe(null) }
-    ) {})
-
-    include(object : AssertionCreatorSpec<Map.Entry<String, Int>>(
-        describePrefix, mapEntry,
-        key.forAssertionCreatorSpec("$toBeDescr: hello") { toBe("hello") },
-        value.forAssertionCreatorSpec("$toBeDescr: 1") { toBe(1) }
-    ) {})
-
-    include(object : AssertionCreatorSpec<Map.Entry<String?, Int?>>(
-        "$describePrefix[nullable]", nullMapEntry,
-        nullableKey.forAssertionCreatorSpec("$toBeDescr: null") { toBe(null) },
-        nullableValue.forAssertionCreatorSpec("$toBeDescr: null") { toBe(null) }
-    ) {})
-
-    describeFun(keyFeature, key, nullableKeyFeature, nullableKey, valueFeature, value, valueFeature, nullableValue) {
-        val keyFunctions = uncheckedToNonNullable(
-            unifySignatures(keyFeature, key),
-            unifySignatures(nullableKeyFeature, nullableKey)
-        )
-
-        val valueFunctions = unifySignatures(valueFeature, value)
-        context("$mapEntry") {
-            keyFunctions.forEach { (name, keyFun, _) ->
-                it("$name - startsWith(h) holds") {
-                    fluent.keyFun { startsWith("h") }
-                }
-                it("$name - endsWith(h) fails") {
-                    expect {
-                        fluent.keyFun { endsWith('h') }
-                    }.toThrow<AssertionError> {
-                        messageContains(
-                            "$keyName: \"hello\"",
-                            DescriptionCharSequenceAssertion.ENDS_WITH.getDefault() + ": \"h\""
-                        )
-                    }
-                }
-            }
-
-            valueFunctions.forEach { (name, valueFun, _) ->
-                it("$name - isGreaterThan(0) holds") {
-                    fluent.valueFun { isGreaterThan(0) }
-                }
-                it("$name - isGreaterThan(1) fails") {
-                    expect {
-                        fluent.valueFun { isGreaterThan(1) }
-                    }.toThrow<AssertionError> {
-                        messageContains(
-                            "$valueName: 1",
-                            DescriptionComparableAssertion.IS_GREATER_THAN.getDefault() + ": 1"
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    describeFun(nullableKeyFeature, nullableKey, nullableValueFeature, nullableValue) {
-        val keyFunctions = unifySignatures(nullableKeyFeature, nullableKey)
-        val valueFunctions = unifySignatures(nullableValueFeature, nullableValue)
-
-        context("$nullableMapEntry") {
-            keyFunctions.forEach { (name, nullableKeyFun, _) ->
-                it("$name - toBe(hello) holds") {
-                    nullableFluent.nullableKeyFun { toBe("hello") }
-                }
-                it("$name - toBe(null) throws AssertionError") {
-                    expect {
-                        nullableFluent.nullableKeyFun { toBe(null) }
-                    }.toThrow<AssertionError> {
-                        messageContains("$keyName: \"hello\"")
-                    }
-                }
-            }
-            valueFunctions.forEach { (name, nullableValueFun, _) ->
-                it("$name - isGreaterThan(0) holds") {
-                    nullableFluent.nullableValueFun { notToBeNull { isGreaterThan(0) } }
-                }
-                it("$name - toBe(null) throws AssertionError") {
-                    expect {
-                        nullableFluent.nullableValueFun { toBe(null) }
-                    }.toThrow<AssertionError> {
-                        messageContains("$valueName: 1")
-                    }
-                }
-            }
-        }
-        context("$nullMapEntry") {
-            keyFunctions.forEach { (name, nullableKeyFun, _) ->
-                it("$name - toBe(null) holds") {
-                    nullFluent.nullableKeyFun { toBe(null) }
-                }
-                it("$name - toBe(hello) throws AssertionError") {
-                    expect {
-                        nullFluent.nullableKeyFun { toBe("hello") }
-                    }.toThrow<AssertionError> {
-                        messageContains("$keyName: null", "$toBeDescr: \"hello\"")
-                    }
-                }
-            }
-            valueFunctions.forEach { (name, nullableValueFun, _) ->
-                it("$name - toBe(null) holds") {
-                    nullFluent.nullableValueFun { toBe(null) }
-                }
-                it("$name - toBe(1) throws AssertionError") {
-                    expect {
-                        nullFluent.nullableValueFun { toBe(1) }
-                    }.toThrow<AssertionError> {
-                        messageContains("$valueName: null")
-                    }
-                }
-            }
-        }
-    }
 })
