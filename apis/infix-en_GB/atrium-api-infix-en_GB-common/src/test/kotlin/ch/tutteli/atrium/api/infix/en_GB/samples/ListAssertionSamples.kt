c11e5125098296ccab8fdbebe3d21b7cc65a9db4
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2020-12-23 18:45:45 +0530

    add comments for sample of listAssertions in api-infix

--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/ListAssertionSamples.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/ListAssertionSamples.kt
@@ -14,18 +14,28 @@ class ListAssertionSamples {
         val list = listOf(1, 2, 3)
 
         expect(list) get 0 isLessThan 2
+        //            |        | subject is still of type Int (still 1)
+        //            | subject is now of type Int (actually 1)
+
         expect(list) get 0 isGreaterThan 0
+        //            |         | subject is still of type Int (still 1)
+        //            | subject is now of type Int (actually 1)
 
         fails {
             expect(list) get 3 isLessThan 0
+        //                |        | not reported
+        //                | fails because index 3 is out of bound
+        // use `get index(elementIndex) { ... }` if you want that all assertions are evaluated
         } message {
             contains("index out of bounds")
             containsNot("is less than: 2")
         }
 
         fails {
-            expect(list) get 0 isGreaterThan 2
-            expect(list) get 0 isLessThan 0
+            expect(list) get 0 isGreaterThan 2     //fails
+            expect(list) get 0 isLessThan 0        // not reported because `isGreaterThan 2` already fails
+        // use `get index(elementIndex) { ... }` if you want that all assertions are evaluated
+
         } message {
             contains("${isGreaterThanDescr}: 2")
             containsNot("${isLessThanDescr}: 0")
@@ -35,19 +45,25 @@ class ListAssertionSamples {
     @Test
     fun get() {
         val list = listOf(1, 2, 3)
-        expect(list) get index(0) {
+
+        expect(list) get index(0) {           // subject inside this block is of type Int (actually 1)
             isLessThan(2)
             isGreaterThan(0)
         }
-        expect(list) get index(1) {
+
+        expect(list) get index(1) {           // subject inside this block is of type Int (actually 2)
             isLessThan(3)
             isGreaterThan(1)
         }
 
         fails {
+            // all assertions are evaluated inside an assertion group block; for more details:
+            // https://github.com/robstoll/atrium#define-single-assertions-or-assertion-groups
+
             expect(list) get index(0) {
-                isGreaterThan(2)
-                isLessThan(0)
+                isGreaterThan(2)           // fails
+                isLessThan(0)              // still evaluated even though `isGreaterThan(2)` already fails,
+            // use the `.get elementIndex ` if you want a fail fast behaviour
             }
         } messageContains values(
             "${isGreaterThanDescr}: 2",
