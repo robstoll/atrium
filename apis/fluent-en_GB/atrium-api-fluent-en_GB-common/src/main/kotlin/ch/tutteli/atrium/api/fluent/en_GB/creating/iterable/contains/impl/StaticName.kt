63155eed451df19b876e3b1794349fbfca46b4d9
Robert Stoll <rstoll@tutteli.ch>
2021-01-12 23:03:18 +0100

    re-establish bbcTests, re-add StaticName (due to setup)
    forgive more specs due to bbc

---
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/fluent/en_GB/creating/iterable/contains/impl/StaticName.kt
@@ -0,0 +1,23 @@
+package ch.tutteli.atrium.api.fluent.en_GB.creating.iterable.contains.impl
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.logic.creating.iterable.contains.IterableLikeContains
+import ch.tutteli.atrium.logic.creating.iterable.contains.searchbehaviours.InAnyOrderSearchBehaviour
+import ch.tutteli.atrium.logic.creating.iterable.contains.steps.AtLeastCheckerStep
+import kotlin.reflect.KFunction3
+
+@Deprecated("Use StaticNames; will be removed latest with 1.0.0 (maybe earlier)", ReplaceWith("StaticNames"))
+internal object StaticName {
+    val containsNotValuesFun = run {
+        val f: KFunction3<Expect<Iterable<Double>>, Double, Array<out Double>, Expect<Iterable<Double>>> =
+            Expect<Iterable<Double>>::containsNot
+        f.name
+    }
+
+    val atLeast = IterableLikeContains.EntryPointStep<*, *, InAnyOrderSearchBehaviour>::atLeast.name
+    val butAtMost = AtLeastCheckerStep<*, *, InAnyOrderSearchBehaviour>::butAtMost.name
+    val atMost = IterableLikeContains.EntryPointStep<*, *, InAnyOrderSearchBehaviour>::atMost.name
+    val exactly = IterableLikeContains.EntryPointStep<*, *, InAnyOrderSearchBehaviour>::exactly.name
+    val notOrAtMost = IterableLikeContains.EntryPointStep<*, *, InAnyOrderSearchBehaviour>::notOrAtMost.name
+}
