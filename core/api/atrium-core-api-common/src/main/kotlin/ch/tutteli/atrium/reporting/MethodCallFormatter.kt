54aaefa846506675ebc7adec5b102ef6aa6168a3
Robert Stoll <rstoll@tutteli.ch>
2021-04-05 21:03:55 +0200

    remove MethodCallFormatter.format

--- core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/reporting/MethodCallFormatter.kt
+++ core/api/atrium-core-api-common/src/main/kotlin/ch/tutteli/atrium/reporting/MethodCallFormatter.kt
@@ -4,18 +4,6 @@ package ch.tutteli.atrium.reporting
  * Responsible to format a method call in reporting.
  */
 interface MethodCallFormatter {
-
-    /**
-     * Returns a lazy representation of the method call to a method named [methodName] with the given [arguments].
-     *
-     * @param methodName The name of the method for which a call with the given [arguments] should be formatted.
-     * @param arguments The arguments of the method call.
-     *
-     * @return An lambda containing the logic to build the representation.
-     */
-    @Deprecated("Use the overload which returns a string right away, wrap it into a lambda on your own if you need this functionality; will be removed with 0.17.0")
-    fun format(methodName: String, arguments: Array<out Any?>): () -> String
-
     /**
      * Returns a representation of a method call to a method named [methodName] with the given [arguments].
      *
@@ -25,7 +13,6 @@ interface MethodCallFormatter {
      * @return An lambda containing the logic to build the representation.
      */
     fun formatCall(methodName: String, arguments: Array<out Any?>): String
-
 
     /**
      * Formats the given [argument].
