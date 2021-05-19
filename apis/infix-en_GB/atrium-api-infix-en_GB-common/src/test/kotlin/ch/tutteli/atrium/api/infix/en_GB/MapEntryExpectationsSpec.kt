d720ad5d5bca066cdbf63f240b15e72ab89ea675
Robert Stoll <rstoll@tutteli.ch>
2021-05-19 12:48:27 +0200

    rename mapEntryAssertion functions to new schema in api-infix

--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/MapEntryExpectationsSpec.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/infix/en_GB/MapEntryExpectationsSpec.kt
@@ -4,8 +4,8 @@ import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.*
 
 class MapEntryExpectationsSpec : ch.tutteli.atrium.specs.integration.MapEntryExpectationsSpec(
-    fun1(Expect<Map.Entry<String, Int>>::isKeyValue).name to Companion::isKeyValue,
-    (fun1(Expect<Map.Entry<String?, Int?>>::isKeyValue).name to Companion::isKeyValueNullable).withNullableSuffix(),
+    fun1(Expect<Map.Entry<String, Int>>::toEqualKeyValue).name to Companion::toEqualKeyValue,
+    (fun1(Expect<Map.Entry<String?, Int?>>::toEqualKeyValue).name to Companion::toEqualKeyValueNullable).withNullableSuffix(),
     property<Map.Entry<String, Int>, String>(Expect<Map.Entry<String, Int>>::key),
     fun1<Map.Entry<String, Int>, Expect<String>.() -> Unit>(Expect<Map.Entry<String, Int>>::key),
     property<Map.Entry<String, Int>, Int>(Expect<Map.Entry<String, Int>>::value),
@@ -17,11 +17,11 @@ class MapEntryExpectationsSpec : ch.tutteli.atrium.specs.integration.MapEntryExp
 ) {
     companion object {
 
-        private fun isKeyValue(expect: Expect<Map.Entry<String, Int>>, key: String, value: Int) =
-            expect isKeyValue (key to value)
+        private fun toEqualKeyValue(expect: Expect<Map.Entry<String, Int>>, key: String, value: Int) =
+            expect toEqualKeyValue (key to value)
 
-        private fun isKeyValueNullable(expect: Expect<Map.Entry<String?, Int?>>, key: String?, value: Int?) =
-            expect isKeyValue (key to value)
+        private fun toEqualKeyValueNullable(expect: Expect<Map.Entry<String?, Int?>>, key: String?, value: Int?) =
+            expect toEqualKeyValue (key to value)
     }
 
     @Suppress("unused", "UNUSED_VALUE")
@@ -33,16 +33,16 @@ class MapEntryExpectationsSpec : ch.tutteli.atrium.specs.integration.MapEntryExp
 
         var star: Expect<Map.Entry<*, *>> = notImplemented()
 
-        a1 isKeyValue ("a" to 1)
-        a2 isKeyValue ("a" to 1)
-        a3 isKeyValue ("a" to 1)
-        a4 isKeyValue ("a" to 1)
-        star isKeyValue ("a" to 1)
+        a1 toEqualKeyValue ("a" to 1)
+        a2 toEqualKeyValue ("a" to 1)
+        a3 toEqualKeyValue ("a" to 1)
+        a4 toEqualKeyValue ("a" to 1)
+        star toEqualKeyValue ("a" to 1)
 
-        a2 isKeyValue (null to 1)
-        a3 isKeyValue ("a" to null)
-        a4 isKeyValue (null to null)
-        star isKeyValue (null to null)
+        a2 toEqualKeyValue (null to 1)
+        a3 toEqualKeyValue ("a" to null)
+        a4 toEqualKeyValue (null to null)
+        star toEqualKeyValue (null to null)
 
         a1.key
         a2.key
