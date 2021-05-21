4d98afab2d12ef3708b3e7d500d1d38f6c7d3ad4
Robert Stoll <rstoll@tutteli.ch>
2021-05-21 21:18:33 +0200

    rename chronoLocalDateAssertion functions to new schema in api-infix

--- apis/infix-en_GB/atrium-api-infix-en_GB-jvm/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/chronoLocalDateExpectations.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-jvm/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/chronoLocalDateExpectations.kt
@@ -3,7 +3,7 @@
     "JAVA_MODULE_DOES_NOT_READ_UNNAMED_MODULE"
 )
 
-package ch.tutteli.atrium.api.fluent.en_GB
+package ch.tutteli.atrium.api.infix.en_GB
 
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.logic.*
@@ -17,7 +17,7 @@ import java.time.chrono.ChronoLocalDate
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeBefore(expected: ChronoLocalDate): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeBefore(expected: ChronoLocalDate): Expect<T> =
     _logicAppend { isBefore(expected) }
 
 /**
@@ -29,7 +29,7 @@ fun <T : ChronoLocalDate> Expect<T>.toBeBefore(expected: ChronoLocalDate): Expec
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeBefore(expected: String): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeBefore(expected: String): Expect<T> =
     _logicAppend { isBefore(expected) }
 
 
@@ -41,19 +41,20 @@ fun <T : ChronoLocalDate> Expect<T>.toBeBefore(expected: String): Expect<T> =
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeBeforeOrTheSamePointInTimeAs(expected: ChronoLocalDate): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeBeforeOrTheSamePointInTimeAs(expected: ChronoLocalDate): Expect<T> =
     _logicAppend { isBeforeOrEqual(expected) }
 
 /**
  * Expects that the subject of `this` expectation (a [ChronoLocalDate])
  * is before or equal the [expected] [java.time.LocalDate] given as [String].
+ * The [expected] parameter needs to be in the form of **yyyy-mm-dd** or else an exception will be thrown.
  *
  * @return an [Expect] for the subject of `this` expectation.
  * @throws [IllegalArgumentException] in case [expected] is not in the form of **yyyy-mm-dd**
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeBeforeOrTheSamePointInTimeAs(expected: String): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeBeforeOrTheSamePointInTimeAs(expected: String): Expect<T> =
     _logicAppend { isBeforeOrEqual(expected) }
 
 
@@ -65,7 +66,7 @@ fun <T : ChronoLocalDate> Expect<T>.toBeBeforeOrTheSamePointInTimeAs(expected: S
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeTheSamePointInTimeAs(expected: ChronoLocalDate): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeTheSamePointInTimeAs(expected: ChronoLocalDate): Expect<T> =
     _logicAppend { isEqual(expected) }
 
 /**
@@ -77,8 +78,9 @@ fun <T : ChronoLocalDate> Expect<T>.toBeTheSamePointInTimeAs(expected: ChronoLoc
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeTheSamePointInTimeAs(expected: String): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeTheSamePointInTimeAs(expected: String): Expect<T> =
     _logicAppend { isEqual(expected) }
+
 
 /**
  * Expects that the subject of `this` expectation (a [ChronoLocalDate])
@@ -88,7 +90,7 @@ fun <T : ChronoLocalDate> Expect<T>.toBeTheSamePointInTimeAs(expected: String): 
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeAfterOrTheSamePointInTimeAs(expected: ChronoLocalDate): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeAfterOrTheSamePointInTimeAs(expected: ChronoLocalDate): Expect<T> =
     _logicAppend { isAfterOrEqual(expected) }
 
 /**
@@ -100,7 +102,7 @@ fun <T : ChronoLocalDate> Expect<T>.toBeAfterOrTheSamePointInTimeAs(expected: Ch
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeAfterOrTheSamePointInTimeAs(expected: String): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeAfterOrTheSamePointInTimeAs(expected: String): Expect<T> =
     _logicAppend { isAfterOrEqual(expected) }
 
 
@@ -112,7 +114,7 @@ fun <T : ChronoLocalDate> Expect<T>.toBeAfterOrTheSamePointInTimeAs(expected: St
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeAfter(expected: ChronoLocalDate): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeAfter(expected: ChronoLocalDate): Expect<T> =
     _logicAppend { isAfter(expected) }
 
 /**
@@ -124,5 +126,6 @@ fun <T : ChronoLocalDate> Expect<T>.toBeAfter(expected: ChronoLocalDate): Expect
  *
  * @since 0.17.0
  */
-fun <T : ChronoLocalDate> Expect<T>.toBeAfter(expected: String): Expect<T> =
+infix fun <T : ChronoLocalDate> Expect<T>.toBeAfter(expected: String): Expect<T> =
     _logicAppend { isAfter(expected) }
+
