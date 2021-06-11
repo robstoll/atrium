6905d1030974c6dfb40db4dd990a77187412708b
Robert Stoll <rstoll@tutteli.ch>
2021-06-11 07:36:53 +0200

    Update translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionIterableAssertion.kt

--- translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionIterableAssertion.kt
+++ translations/de_CH/atrium-translations-de_CH-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionIterableAssertion.kt
@@ -48,7 +48,7 @@ enum class DescriptionIterableAssertion(override val value: String) : StringBase
     DUPLICATE_ELEMENTS("doppelte Elemente"),
     DUPLICATED_BY("(dupliziert von Index: %s"),
     ELEMENT_NOT_FOUND("aber es konnte kein solches Element gefunden werden"),
-    NUMBER_OF_ELEMENTS_FOUND(/* TODO translate */ "and %s such elements were found")
+    NUMBER_OF_ELEMENTS_FOUND("und % Elemente wurden gefunden")
 }
 
 internal const val COULD_NOT_EVALUATE_DEFINED_ASSERTIONS =
