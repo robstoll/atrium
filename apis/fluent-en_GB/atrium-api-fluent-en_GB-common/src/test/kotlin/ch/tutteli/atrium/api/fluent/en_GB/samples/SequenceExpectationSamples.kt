dba6d75a1122d7869cbac09ef19ad076b193d37c
loge1998 <logeshbalu1998@gmail.com>
2021-10-07 23:03:59 +0530

    Add comments to explain changing type

--- apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/SequenceExpectationSamples.kt
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-common/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/SequenceExpectationSamples.kt
@@ -9,7 +9,7 @@ class SequenceExpectationSamples {
     @Test
     fun asIterableFeature() {
         expect(sequenceOf(1, 2, 3))
-            .asIterable()
+            .asIterable() // subject is now of type Iterable<Int>
             .toContain
             .inOrder // order specifier
             .only
@@ -17,40 +17,41 @@ class SequenceExpectationSamples {
 
         fails {
             expect(sequenceOf(1, 2, 3))
-                .asIterable()
+                .asIterable() // subject is now of type Iterable<Int>
                 .toContain(4)
         }
+
     }
 
     @Test
     fun asIterable() {
         expect(sequenceOf(1, 2, 3))
-            .asIterable {
+            .asIterable { // subject within this block is of type List<Int>
                 toContain(1)
                 toContain(2)
                 toContain(3)
-            }
+            } // subject here is back to type Sequence<Int>
 
         fails {
             expect(sequenceOf(1, 2, 3))
-                .asIterable {
+                .asIterable { // subject within this block is of type List<Int>
                     toContain(4)
-                }
+                } // subject here is back to type Sequence<Int>
         }
     }
 
     @Test
     fun asListFeature() {
         expect(sequenceOf(1, 2, 3))
-            .asList()
+            .asList() // subject is now of type List<Int>
             .toEqual(listOf(1, 2, 3))
     }
 
     @Test
     fun asList() {
         expect(sequenceOf(1, 2, 3))
-            .asList {
+            .asList { // subject within this block is of type List<Int>
                 toEqual(listOf(1, 2, 3))
-            }
+            } // subject here is back to type Sequence<Int>
     }
 }
