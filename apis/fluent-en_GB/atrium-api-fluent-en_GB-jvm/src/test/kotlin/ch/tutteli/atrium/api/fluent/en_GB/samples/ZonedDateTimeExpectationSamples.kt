1711448ec252eac5cee13ef2264e429934d98c15
Vinayak Kadam <kadamvinayak03@gmail.com>
2021-10-13 11:51:06 +0530

    Add samples for ZonedDateTimeAssertions
    Signed-off-by: Vinayak Kadam <kadamvinayak03@gmail.com>

---
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-jvm/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/ZonedDateTimeExpectationSamples.kt
@@ -0,0 +1,145 @@
+package ch.tutteli.atrium.api.fluent.en_GB.samples
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
+import ch.tutteli.atrium.api.verbs.internal.expect
+import java.time.*
+import kotlin.test.Test
+
+class ZonedDateTimeExpectationSamples {
+
+    private val zonedDateTime = ZonedDateTime.of(LocalDateTime.of(2021, 10, 9, 11, 56), ZoneId.systemDefault())
+
+    @Test
+    fun yearFeature() {
+        expect(zonedDateTime)
+            .year
+            .toEqual(2021)
+
+        fails {
+            expect(zonedDateTime)
+                .year // subject is now of type Int (actually 2021)
+                .toBeGreaterThan(2030)  // fails
+                .toBeLessThan(2000)     // not reported because toBeLessThan already fails
+            //                             use `.year { ... }` if you want that all expectations are evaluated
+        }
+    }
+
+    @Test
+    fun year() {
+        expect(zonedDateTime)
+            .year { // subject inside this block is of type Int (actually 2021)
+                toEqual(2021)
+                toBeGreaterThan(2020)
+            } // subject here is back to type ZonedDateTime
+
+        fails {
+            expect(zonedDateTime)
+                .year { // subject inside this block is of type Int (actually 2021)
+                    notToEqual(1980)    // fails
+                    toBeLessThan(2000)  // not reported because notToEqual already fails
+                    //                     use `.year.` if you want a fail fast behaviour
+                } // subject here is back to type ZonedDateTime
+        }
+    }
+
+    @Test
+    fun monthFeature() {
+        expect(zonedDateTime)
+            .month // subject is now of type Int (actually Month.OCTOBER.value i.e. 10)
+            .toEqual(10)
+
+        fails {
+            expect(zonedDateTime)
+                .month               // subject is now of type Int (actually Month.OCTOBER.value i.e. 10)
+                .toBeLessThan(9)     // fails
+                .toBeGreaterThan(11) // not reported because toBeLessThan already fails
+            //                          use `.month { ... }` if you want that all expectations are evaluated
+        }
+    }
+
+    @Test
+    fun month() {
+        expect(zonedDateTime)
+            .month { // subject inside this block is of type Int (actually Month.OCTOBER.value i.e. 10)
+                toBeGreaterThan(5)
+                notToEqual(8)
+            } // subject here is back to type ZonedDateTime
+
+        fails {
+            expect(zonedDateTime)
+                .month { // subject inside this block is of type Int (actually Month.OCTOBER.value i.e. 10)
+                    toBeLessThan(9)      // fails
+                    toBeGreaterThan(11)  // still evaluated even though toBeLessThan already fails
+                    //                      use `.month.` if you want a fail fast behaviour
+                } // subject here is back to type ZonedDateTime
+        }
+    }
+
+    @Test
+    fun dayOfWeekFeature() {
+        expect(zonedDateTime)
+            .dayOfWeek // subject is now of type DayOfWeek (actually SATURDAY)
+            .toEqual(DayOfWeek.SATURDAY)
+
+
+        fails {
+            expect(zonedDateTime)
+                .dayOfWeek // subject is now of type DayOfWeek (actually SATURDAY)
+                .toEqual(DayOfWeek.MONDAY)       // fails
+                .notToEqual(DayOfWeek.SATURDAY)  // not reported because toEqual already fails
+            //                                      use `.dayOfWeek { ... }` if you want that all expectations are evaluated
+
+        }
+    }
+
+    @Test
+    fun dayOfWeek() {
+        expect(zonedDateTime)
+            .dayOfWeek { // subject inside this block is of type DayOfWeek (actually SATURDAY)
+                toEqual(DayOfWeek.SATURDAY)
+                notToEqual(DayOfWeek.SUNDAY)
+            } // subject here is back to type ZonedDateTime
+
+        fails {
+            expect(zonedDateTime)
+                .dayOfWeek { // subject inside this block is of type DayOfWeek (actually SATURDAY)
+                    toEqual(DayOfWeek.MONDAY)       // fails
+                    notToEqual(DayOfWeek.SATURDAY)  // still evaluated even though toEqual already fails
+                    //                                 use `.dayOfWeek.` if you want a fail fast behaviour
+                } // subject here is back to type ZonedDateTime
+        }
+    }
+
+    @Test
+    fun dayFeature() {
+        expect(zonedDateTime)
+            .day // subject is now of type Int (actually 9)
+            .toEqual(9)
+
+        fails {
+            expect(zonedDateTime)
+                .day // subject is now of type Int (actually 9)
+                .toEqual(5)          // fails
+                .toBeGreaterThan(10) // not reported because toEqual already fails
+            //                          use `.day { ... }` if you want that all expectations are evaluated
+        }
+    }
+
+    @Test
+    fun day() {
+        expect(zonedDateTime)
+            .day { // subject inside this block is of type Int (actually 9)
+                toEqual(9)
+                toBeGreaterThan(5)
+            } // subject here is back to type ZonedDateTime
+
+        fails {
+            expect(zonedDateTime)
+                .day { // subject inside this block is of type Int (actually 9)
+                    toEqual(5)       // fails
+                    toBeLessThan(7)  // still evaluated even though toEqual already fails
+                    //                  use `.day.` if you want a fail fast behaviour
+                } // subject here is back to type ZonedDateTime
+        }
+    }
+}
