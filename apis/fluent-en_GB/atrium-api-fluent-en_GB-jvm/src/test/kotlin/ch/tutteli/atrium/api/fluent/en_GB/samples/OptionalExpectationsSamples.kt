de0645b42b4ee76e40172c17439b19d1d1aae0a4
Tim Acosta <timacosta06@gmail.com>
2021-10-28 00:53:08 +0100

    added samples to OptionalExpectationsSamples.kt

---
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-jvm/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/OptionalExpectationsSamples.kt
@@ -0,0 +1,48 @@
+package ch.tutteli.atrium.api.fluent.en_GB.samples
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
+import ch.tutteli.atrium.api.verbs.internal.expect
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class OptionalExpectationsSamples {
+
+    @Test
+    fun toBeEmpty() {
+        val empty: String? = null
+        val opt = Optional.ofNullable(empty)
+
+        expect(opt).toBeEmpty()
+    }
+
+    @Test
+    fun toBePresent() {
+        val notEmpty: String? = "toBePresent"
+        val opt = Optional.ofNullable(notEmpty)
+
+        expect(opt).toBePresent()
+    }
+
+    @Test
+    fun toBePresentAssertionCreator() {
+        val x  = 10
+        val y  = 5
+
+        val optX = Optional.ofNullable(x)
+        val optY = Optional.ofNullable(y)
+
+        expect(optX) {
+
+            val sum = optX.get() + optY.get()
+
+            feature("toBeLessThan") { sum }.toBeLessThan(20)
+            feature("toBeGreaterThan") { sum }.toBeGreaterThan(5)
+            feature("toBeGreaterThanOrEqualTo") { sum }.toBeGreaterThanOrEqualTo(15)
+
+        }.toBePresent()
+
+
+    }
+
+
+}
