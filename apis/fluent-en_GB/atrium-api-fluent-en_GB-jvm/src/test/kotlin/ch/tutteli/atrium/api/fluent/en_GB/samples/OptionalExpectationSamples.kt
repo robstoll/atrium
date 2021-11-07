157b12f3fbbf45354f1e6812117b130f895bc309
Tim Acosta <timacosta06@gmail.com>
2021-11-07 16:14:57 +0000

    renamed file to OptionalExpectationSamples.kt + fixed toBeEmpty() method

--- apis/fluent-en_GB/atrium-api-fluent-en_GB-jvm/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/OptionalExpectationSamples.kt
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB-jvm/src/test/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/OptionalExpectationSamples.kt
@@ -5,34 +5,43 @@ import ch.tutteli.atrium.api.verbs.internal.expect
 import org.junit.jupiter.api.Test
 import java.util.*
 
-class OptionalExpectationsSamples {
+class OptionalExpectationSamples {
 
     @Test
     fun toBeEmpty() {
-        val empty: String? = null
-        val opt = Optional.ofNullable(empty)
+
+        val opt: Optional<String> = Optional.empty()
 
         expect(opt).toBeEmpty()
-    }
 
-    @Test
-    fun toBePresent() {
-        val notEmpty: String? = "toBePresent"
-        val opt = Optional.ofNullable(notEmpty)
-
-        expect(opt).toBePresent()
+        fails {
+            expect(opt).toBePresent() // fails
+        }
     }
 
     @Test
     fun toBePresentFeature() {
-        val x  = 10
-        val y  = 5
+        val notEmpty: String? = "toBePresentFeature"
+        val opt = Optional.ofNullable(notEmpty)
+
+
+        expect(opt).toBePresent()
+
+        fails {
+            expect(opt).toBeEmpty()
+        }
+    }
+
+
+    @Test
+    fun toBePresent() {
+        val x = 10
+        val y = 5
 
         val optX = Optional.ofNullable(x)
         val optY = Optional.ofNullable(y)
 
         expect(optX) {
-
             val sum = optX.get() + optY.get()
 
             feature("toBeLessThan") { sum }.toBeLessThan(20)
