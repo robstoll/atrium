0427d170c0abe87c54de9ad236f3cd532a6a1f38
Robert Stoll <rstoll@tutteli.ch>
2021-12-27 21:55:40 +0100

    rewrite DescriptionFloatingPointAssertion to ...Expectation

--- README.md
+++ README.md
@@ -1872,7 +1872,7 @@ But Atrium shows where it goes wrong and even gives a possible hint:
 expected subject: 9.99        (kotlin.Float <1234789>)
 ◆ to equal (error ± 0.01): 10.0        (kotlin.Float <1234789>)
     » failure might be due to using kotlin.Float, see exact check on the next line
-    » exact check is |9.989999771118164 - 10.0| = 0.010000228881835938 ≤ 0.009999999776482582
+    » exact check was |9.989999771118164 - 10.0| = 0.010000228881835938 ≤ 0.009999999776482582
 ```
 </exs-add-info-2-output>
 
--- translations/en_GB/atrium-translations-en_GB-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionFloatingPointException.kt
+++ translations/en_GB/atrium-translations-en_GB-common/src/main/kotlin/ch/tutteli/atrium/translations/DescriptionFloatingPointException.kt
@@ -7,11 +7,13 @@ import ch.tutteli.atrium.reporting.translating.StringBasedTranslatable
  * Contains the [DescriptiveAssertion.description]s of the assertion functions which are applicable to [Float], [Double]
  * and maybe other platform specific floating point types (such as `BigDecimal` in JVM).
  */
-enum class DescriptionFloatingPointAssertion(override val value: String) : StringBasedTranslatable {
+enum class DescriptionFloatingPointException(override val value: String) : StringBasedTranslatable {
+    /** @since 0.18.0 */
     TO_EQUAL_WITH_ERROR_TOLERANCE("to equal (error ± %s)"),
-    FAILURE_DUE_TO_FLOATING_POINT_NUMBER("failure might be due to using %s, see exact check on the next line"),
-    TO_BE_WITH_ERROR_TOLERANCE_EXPLAINED("exact check is |%s - %s| = %s ≤ %s"),
 
-    @Deprecated("Use TO_EQUAL_WITH_ERROR_TOLERANCE; will be removed with 0.18.0", ReplaceWith("TO_EQUAL_WITH_ERROR_TOLERANCE"))
-    TO_BE_WITH_ERROR_TOLERANCE(TO_EQUAL_WITH_ERROR_TOLERANCE.getDefault()),
+    /** @since 0.18.0 */
+    FAILURE_DUE_TO_FLOATING_POINT_NUMBER("failure might be due to using %s, see exact check on the next line"),
+
+    /** @since 0.18.0 */
+    TO_EQUAL_WITH_ERROR_TOLERANCE_EXPLAINED("exact check was |%s - %s| = %s ≤ %s")
 }
