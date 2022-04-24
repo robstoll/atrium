511c4690eeafaeb88c357297cebb5b8b4c8ce356
Robert Stoll <rstoll@tutteli.ch>
2022-04-24 22:09:29 +0200

    prepare specs for reportOptions in MapLikeToContain...

--- misc/specs/atrium-specs-common/src/main/kotlin/ch/tutteli/atrium/specs/integration/MapLikeToContainSpecBase.kt
+++ misc/specs/atrium-specs-common/src/main/kotlin/ch/tutteli/atrium/specs/integration/MapLikeToContainSpecBase.kt
@@ -3,19 +3,28 @@ package ch.tutteli.atrium.specs.integration
 import ch.tutteli.atrium.core.polyfills.format
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.Fun2
+import ch.tutteli.atrium.specs.Fun3
 import ch.tutteli.atrium.specs.fun2
+import ch.tutteli.atrium.specs.fun3
 import ch.tutteli.atrium.specs.lineSeparator
 import ch.tutteli.atrium.translations.DescriptionComparableExpectation
 import ch.tutteli.atrium.translations.DescriptionMapLikeExpectation
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.dsl.Root
 import kotlin.reflect.KFunction3
+import kotlin.reflect.KFunction4
 
 typealias MFun2<K, V, T> = Fun2<Map<out K, V>, Pair<K, T>, Array<out Pair<K, T>>>
+typealias MFun3<K, V, T, A3> = Fun3<Map<out K, V>, Pair<K, T>, Array<out Pair<K, T>>, A3>
 
 fun <K, V, T> mfun2(
     f: KFunction3<Expect<Map<out K, V>>, Pair<K, T>, Array<out Pair<K, T>>, Expect<Map<out K, V>>>
 ) = fun2(f)
+
+fun <K, V, T, A3> mfun3(
+    f: KFunction4<Expect<Map<out K, V>>, Pair<K, T>, Array<out Pair<K, T>>, A3, Expect<Map<out K, V>>>
+) = fun3(f)
+
 
 abstract class MapLikeToContainSpecBase(spec: Root.() -> Unit) : Spek(spec) {
 
