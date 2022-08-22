39d12e17660c856aa734ab87efaa572ef51bb3ab
Jordan Harper <fudgeismydog4@gmail.com>
2022-08-22 11:35:34 +0100

    Added SequenceSubjectChangerSamples.kt with samples and comments in format

---
+++ apis/infix-en_GB/atrium-api-infix-en_GB/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/SequenceSubjectChangerSamples.kt
@@ -0,0 +1,57 @@
+package ch.tutteli.atrium.api.infix.en_GB.samples
+
+import ch.tutteli.atrium.api.infix.en_GB.*
+import ch.tutteli.atrium.api.verbs.internal.expect
+import kotlin.test.Test
+
+class SequenceSubjectChangerSamples {
+
+    @Test
+    fun asIterableFeature() {
+        val sequence = sequenceOf(1, 2, 3)
+        expect(sequence) asIterable o toContain o inGiven order and only the values(1, 2, 3)
+        //                  |                       | order specifier
+        //                  |  subject is now type Iterable<Int>
+
+        fails {
+            expect(sequenceOf(1, 2, 3)) asIterable o toContain 4
+            //                                      | subject is now of Iterator<Int>
+        }
+    }
+
+    @Test
+    fun asIterable() {
+        expect(sequenceOf(1, 2, 3))
+            .asIterable { //subject within this expectation-group is of type Iterable<Int>
+                it toContain 1
+                it toContain 2
+                it toContain 3
+            } // subject is back to Sequence<Int>
+
+        fails {
+            expect(sequenceOf(1, 2, 3))
+                .asIterable {//subject within this expectation-group is of type Iterable<Int>
+
+                    it toContain 4
+                } //subject here is back to type Sequence<Int>
+        }
+    }
+
+    @Test
+    fun asListFeature() {
+        expect(sequenceOf(1, 2, 3)) asList o toEqual listOf(1, 2, 3)
+        //                                      |subject is now of type List<Int>
+    }
+
+    @Test
+    fun asList() {
+        expect(sequenceOf(1, 2, 3))
+
+            .asList {//subject within this expectation group is of type List<Int>
+                toEqual(listOf(1, 2, 3))
+            } //subject here is back to type to Sequence<Int>
+    }
+}
+
+
+
