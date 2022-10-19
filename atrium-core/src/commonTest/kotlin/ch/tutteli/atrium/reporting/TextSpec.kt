a80ab67a9a56ddfd33d6bbfddf6478ca11bf03e8
Vedant Bhamare <55763604+Dark-Knight11@users.noreply.github.com>
2022-10-20 01:39:34 +0530

    Migrate TextSpec from spek to kotest (#1230)

--- atrium-core/src/commonTest/kotlin/ch/tutteli/atrium/reporting/TextSpec.kt
+++ atrium-core/src/commonTest/kotlin/ch/tutteli/atrium/reporting/TextSpec.kt
@@ -3,10 +3,9 @@ package ch.tutteli.atrium.reporting
 import ch.tutteli.atrium.api.infix.en_GB.messageToContain
 import ch.tutteli.atrium.api.infix.en_GB.toThrow
 import ch.tutteli.atrium.api.verbs.internal.expect
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import io.kotest.core.spec.style.DescribeSpec
 
-class TextSpec : Spek({
+class TextSpec : DescribeSpec({
     describe("creating a Text") {
         it("empty string; throws IllegalArgumentException") {
             expect{
