acc673b640e733216c2bd637b8f7869eaaf9834f
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2022-12-07 11:22:13 +0000

    Comparisons draft 1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     // keep in sync with /build.gradle
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
     api("ch.tutteli.niok:niok:1.4.7")
 }
