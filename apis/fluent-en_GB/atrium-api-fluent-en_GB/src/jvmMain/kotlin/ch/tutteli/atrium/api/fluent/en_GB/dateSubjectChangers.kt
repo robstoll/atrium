259627d5b35e9363de17745f5735832b9b96fa3a
sajalverma17 <sajalv128@gmail.com>
2023-01-15 23:49:02 +0100

    Added dateSubjectChangers with two extension functions to convert Date to LocalDate

---
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB/src/jvmMain/kotlin/ch/tutteli/atrium/api/fluent/en_GB/dateSubjectChangers.kt
@@ -0,0 +1,32 @@
+package ch.tutteli.atrium.api.fluent.en_GB
+
+import ch.tutteli.atrium.creating.Expect
+import ch.tutteli.atrium.logic._logic
+import ch.tutteli.atrium.logic.changeSubject
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Date
+
+/**
+ * Turns `Expect<Date>` into `Expect<LocalDate>`.
+ *
+ * The transformation as such is not reflected in reporting.
+ *
+ * @return The newly created [Expect] for the transformed subject.
+ *
+ * @since 0.20.0
+ */
+fun <T : Date> Expect<T>.asLocalDate(): Expect<LocalDate> =
+    _logic.changeSubject.unreported { it.toInstant().atZone(ZoneId.systemDefault()).toLocalDate() }
+
+/**
+ * Expects that the subject of `this` expectation holds all assertions the given [assertionCreator] creates for the subject as[Date].
+ *
+ * The transformation as such is not reflected in reporting.
+ *
+ * @return an [Expect] for the subject of `this` expectation.
+ *
+ * @since 0.20.0
+ */
+fun <T:Date> Expect<T>.asLocalDate(assertionCreator:Expect<LocalDate>.()->Unit): Expect<T> =
+    apply{ asLocalDate()._logic.appendAsGroup(assertionCreator) }
