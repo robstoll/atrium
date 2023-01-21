2472658e16ceeed6974099678c74323b7ffda1a5
sajalverma17 <sajalv128@gmail.com>
2023-01-21 21:57:02 +0100

    Change spec to use a constant date val instead of date.now

--- misc/atrium-specs/src/jvmMain/kotlin/ch/tutteli/atrium/specs/integration/DateAsLocalDateExpectationsSpec.kt
+++ misc/atrium-specs/src/jvmMain/kotlin/ch/tutteli/atrium/specs/integration/DateAsLocalDateExpectationsSpec.kt
@@ -5,16 +5,20 @@ import ch.tutteli.atrium.api.verbs.internal.expect
 import ch.tutteli.atrium.creating.Expect
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.util.Date
+import java.util.*
+
 
 abstract class DateAsLocalDateExpectationsSpec(
     asLocalDate: Expect<Date>.(Expect<LocalDate>.() -> Unit) -> Expect<Date>
 ) : Spek({
     describe("asLocalDate") {
         it("transformation can be applied and a subsequent assertion made") {
-            val date = Date()
-            expect(date).asLocalDate { toEqual(LocalDate.now()) }
+            val formatter: DateFormat = SimpleDateFormat("yyyy-MM-dd")
+            val date = formatter.parse("1995-07-17")
+            expect(date).asLocalDate { toEqual(LocalDate.parse("1995-07-17")) }
         }
     }
 })
