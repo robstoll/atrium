9a8d1a7e6aba8902bf3fd96c17d8c3f0322a9136
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-05-06 21:38:46 +0100

    Resolve conflicts

--- logic/extensions/atrium-logic-kotlin_1_3/src/commonMain/kotlin/ch/tutteli/atrium/logic/kotlin_1_3/impl/DefaultResultAssertions.kt
+++ logic/extensions/atrium-logic-kotlin_1_3/src/commonMain/kotlin/ch/tutteli/atrium/logic/kotlin_1_3/impl/DefaultResultAssertions.kt
@@ -2,6 +2,7 @@ package ch.tutteli.atrium.logic.kotlin_1_3.impl
 
 import ch.tutteli.atrium.core.ExperimentalNewExpectTypes
 import ch.tutteli.atrium.core.Option
+import ch.tutteli.atrium.core.getOrElse
 import ch.tutteli.atrium.creating.AssertionContainer
 import ch.tutteli.atrium.creating.ExperimentalComponentFactoryContainer
 import ch.tutteli.atrium.creating.FeatureExpect
@@ -36,13 +37,35 @@ class DefaultResultAssertions : ResultAssertions {
     ):  SubjectChangerBuilder.ExecutionStep<Throwable?, TExpected> =
         container.manualFeature(EXCEPTION) {
 
-
-            if(exceptionOrNull() == null && container.maybeSubject.map { exceptionOrNull() } != null){
-                //unwrap
-                exceptionOrNull()
-            } else {
-                exceptionOrNull()
+//fix is here for bug in kotlin 1.3, 1.4, 1.5 (fixed in 1.6) => https://youtrack.jetbrains.com/issue/KT-50974
+            //Comments below explain the fix
+            val badResult = exceptionOrNull()
+            //mapping manually to get internal value throwable - exception or null just produced null + we lost the exception
+            val maybeSubjectResult = container.maybeSubject.map {
+                onSuccess { null }
+                onFailure { it.cause }
             }
+
+
+            if (maybeSubjectResult.getOrElse { null } != null && badResult == null) {
+                //there is something inside first option type so unwrap
+                val successError = maybeSubjectResult.getOrElse { null }
+
+                if (successError == null) {
+
+                    return@manualFeature exceptionOrNull()
+                }
+                //if the result is a success we have another value to check
+                if (successError.isSuccess) {
+
+                    val valueInsideSuccess = getOrNull() as Result<*>
+                    if (valueInsideSuccess.isFailure) {
+                        return@manualFeature valueInsideSuccess.exceptionOrNull()
+                    }
+                }
+
+            }
+            exceptionOrNull()
 
 
 
