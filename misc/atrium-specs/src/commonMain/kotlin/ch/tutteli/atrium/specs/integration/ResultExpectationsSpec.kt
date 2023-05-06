4b6cec8e3be61184c638435b7729e3b0024c343e
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-05-06 21:42:42 +0100

    Uncomment spec

--- misc/atrium-specs/src/commonMain/kotlin/ch/tutteli/atrium/specs/integration/ResultExpectationsSpec.kt
+++ misc/atrium-specs/src/commonMain/kotlin/ch/tutteli/atrium/specs/integration/ResultExpectationsSpec.kt
@@ -44,128 +44,128 @@ abstract class ResultExpectationsSpec(
         toBeASuccessNullable.forAssertionCreatorSpec("$toEqualDescr: 2") { toEqual(2) }
     ) {})
 
-//    //TODO 0.20.0 activate once we have the workaround for #1234 implemented
-//    include(object : AssertionCreatorSpec<Result<Int>>(
-//        "$describePrefix[failure] ", Result.failure(IllegalArgumentException("oh no...")),
-//        assertionCreatorSpecTriple(
-//            toBeAFailure.name,
-//            "${DescriptionCharSequenceExpectation.VALUE.getDefault()}: \"oh no...\"",
-//            { apply { toBeAFailure.invoke(this) { messageToContain("oh no...") } } },
-//            { apply { toBeAFailure.invoke(this) {} } }
-//        )
-//    ) {})
-//
-//    fun describeFun(vararg pairs: SpecPair<*>, body: Suite.() -> Unit) =
-//        describeFunTemplate(describePrefix, pairs.map { it.name }.toTypedArray(), body = body)
-//
-//    val resultSuccess = Result.success(1)
-//    val resultFailure = Result.failure<Int>(IllegalArgumentException("oh no..."))
-//    val resultNullSuccess = Result.success(null as Int?)
-//    val resultNullableFailure = Result.failure<Int?>(IllegalArgumentException("oh no nullable..."))
-//
-//    val isNotSuccessDescr = DescriptionResultExpectation.IS_NOT_SUCCESS.getDefault()
-//    val isNotFailureDescr = DescriptionResultExpectation.IS_NOT_FAILURE.getDefault()
-//    val exceptionDescr = DescriptionResultExpectation.EXCEPTION.getDefault()
-//
-//    describeFun(toBeASuccessFeature, toBeASuccess, toBeASuccessFeatureNullable, toBeASuccessNullable, toBeAFailureFeature, toBeAFailure) {
-//        val successFunctions = uncheckedToNonNullable(
-//            unifySignatures(toBeASuccessFeature, toBeASuccess),
-//            unifySignatures(toBeASuccessFeatureNullable, toBeASuccessNullable)
-//        )
-//        val failureFunctions = unifySignatures(toBeAFailureFeature, toBeAFailure)
-//
-//        context("subject is $resultSuccess") {
-//            successFunctions.forEach { (name, toBeASuccessFun, _) ->
-//                it("$name - can perform sub-assertion which holds") {
-//                    expect(resultSuccess).toBeASuccessFun { toEqual(1) }
-//                }
-//                it("$name - can perform sub-assertion which fails, throws AssertionError") {
-//                    expect {
-//                        expect(resultSuccess).toBeASuccessFun { toEqual(2) }
-//                    }.toThrow<AssertionError> {
-//                        messageToContain("value: 1", "$toEqualDescr: 2")
-//                    }
-//                }
-//            }
-//
-//            failureFunctions.forEach { (name, toBeAFailureFun, hasExtraHint) ->
-//                it("$name - throws AssertionError showing the expected type" + if (hasExtraHint) " and the expected message" else "") {
-//                    expect {
-//                        expect(resultSuccess).toBeAFailureFun { messageToContain("oh yes...") }
-//                    }.toThrow<AssertionError> {
-//                        messageToContain(
-//                            "exception: $isNotFailureDescr",
-//                            "$toBeAnInstanceOfDescr: ${IllegalArgumentException::class.simpleName}"
-//                        )
-//                        if (hasExtraHint) {
-//                            messageToContain(
-//                                DescriptionCharSequenceExpectation.TO_CONTAIN.getDefault(),
-//                                "${DescriptionCharSequenceExpectation.VALUE.getDefault()}: \"oh yes...\""
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        context("subject is $resultFailure") {
-//            successFunctions.forEach { (name, toBeASuccessFun, hasExtraHint) ->
-//                it("$name throws AssertionError" + showsSubAssertionIf(hasExtraHint)) {
-//                    expect {
-//                        expect(resultFailure).toBeASuccessFun { toEqual(1) }
-//                    }.toThrow<AssertionError> {
-//                        messageToContain("value: $isNotSuccessDescr")
-//                        if (hasExtraHint) messageToContain("$toEqualDescr: 1")
-//                    }
-//                }
-//            }
-//
-//            //TODO 0.20.0 activate once we have the workaround for #1234 implemented
-//            failureFunctions.forEach { (name, toBeAFailureFun, _) ->
-//                it("$name - can perform sub-assertion which holds") {
-//                    expect(resultFailure).toBeAFailureFun { messageToContain("oh no...") }
-//                }
-//                it("$name - can perform sub-assertion which fails, throws AssertionError") {
-//                    expect {
-//                        expect(resultFailure).toBeAFailureFun { messageToContain("oh yes...") }
-//                    }.toThrow<AssertionError> {
-//                        messageToContain(
-//                            "$exceptionDescr: ${IllegalArgumentException::class.fullName}",
-//                            DescriptionCharSequenceExpectation.TO_CONTAIN.getDefault(), "${DescriptionCharSequenceExpectation.VALUE.getDefault()}: \"oh yes...\""
-//                        )
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    describeFun(toBeASuccessFeatureNullable, toBeASuccessNullable) {
-//        val successFunctions = unifySignatures(toBeASuccessFeatureNullable, toBeASuccessNullable)
-//
-//        successFunctions.forEach { (name, toBeASuccessFun, hasExtraHint) ->
-//            context("subject is $resultNullSuccess") {
-//                it("$name - can perform sub-assertion which holds") {
-//                    expect(resultNullSuccess).toBeASuccessFun { toEqual(null) }
-//                }
-//                it("$name - can perform sub-assertion which fails, throws AssertionError") {
-//                    expect {
-//                        expect(resultNullSuccess).toBeASuccessFun { toEqual(2) }
-//                    }.toThrow<AssertionError> {
-//                        messageToContain("value: null", "$toEqualDescr: 2")
-//                    }
-//                }
-//            }
-//
-//            context("subject is $resultNullableFailure") {
-//                it("${toBeASuccessFeature.name} throws AssertionError" + showsSubAssertionIf(hasExtraHint)) {
-//                    expect {
-//                        expect(resultNullableFailure).toBeASuccessFun { toEqual(1) }
-//                    }.toThrow<AssertionError> {
-//                        messageToContain("value: $isNotSuccessDescr")
-//                        if (hasExtraHint) messageToContain("$toEqualDescr: 1")
-//                    }
-//                }
-//            }
-//        }
-//    }
+    //TODO 0.20.0 activate once we have the workaround for #1234 implemented
+    include(object : AssertionCreatorSpec<Result<Int>>(
+        "$describePrefix[failure] ", Result.failure(IllegalArgumentException("oh no...")),
+        assertionCreatorSpecTriple(
+            toBeAFailure.name,
+            "${DescriptionCharSequenceExpectation.VALUE.getDefault()}: \"oh no...\"",
+            { apply { toBeAFailure.invoke(this) { messageToContain("oh no...") } } },
+            { apply { toBeAFailure.invoke(this) {} } }
+        )
+    ) {})
+
+    fun describeFun(vararg pairs: SpecPair<*>, body: Suite.() -> Unit) =
+        describeFunTemplate(describePrefix, pairs.map { it.name }.toTypedArray(), body = body)
+
+    val resultSuccess = Result.success(1)
+    val resultFailure = Result.failure<Int>(IllegalArgumentException("oh no..."))
+    val resultNullSuccess = Result.success(null as Int?)
+    val resultNullableFailure = Result.failure<Int?>(IllegalArgumentException("oh no nullable..."))
+
+    val isNotSuccessDescr = DescriptionResultExpectation.IS_NOT_SUCCESS.getDefault()
+    val isNotFailureDescr = DescriptionResultExpectation.IS_NOT_FAILURE.getDefault()
+    val exceptionDescr = DescriptionResultExpectation.EXCEPTION.getDefault()
+
+    describeFun(toBeASuccessFeature, toBeASuccess, toBeASuccessFeatureNullable, toBeASuccessNullable, toBeAFailureFeature, toBeAFailure) {
+        val successFunctions = uncheckedToNonNullable(
+            unifySignatures(toBeASuccessFeature, toBeASuccess),
+            unifySignatures(toBeASuccessFeatureNullable, toBeASuccessNullable)
+        )
+        val failureFunctions = unifySignatures(toBeAFailureFeature, toBeAFailure)
+
+        context("subject is $resultSuccess") {
+            successFunctions.forEach { (name, toBeASuccessFun, _) ->
+                it("$name - can perform sub-assertion which holds") {
+                    expect(resultSuccess).toBeASuccessFun { toEqual(1) }
+                }
+                it("$name - can perform sub-assertion which fails, throws AssertionError") {
+                    expect {
+                        expect(resultSuccess).toBeASuccessFun { toEqual(2) }
+                    }.toThrow<AssertionError> {
+                        messageToContain("value: 1", "$toEqualDescr: 2")
+                    }
+                }
+            }
+
+            failureFunctions.forEach { (name, toBeAFailureFun, hasExtraHint) ->
+                it("$name - throws AssertionError showing the expected type" + if (hasExtraHint) " and the expected message" else "") {
+                    expect {
+                        expect(resultSuccess).toBeAFailureFun { messageToContain("oh yes...") }
+                    }.toThrow<AssertionError> {
+                        messageToContain(
+                            "exception: $isNotFailureDescr",
+                            "$toBeAnInstanceOfDescr: ${IllegalArgumentException::class.simpleName}"
+                        )
+                        if (hasExtraHint) {
+                            messageToContain(
+                                DescriptionCharSequenceExpectation.TO_CONTAIN.getDefault(),
+                                "${DescriptionCharSequenceExpectation.VALUE.getDefault()}: \"oh yes...\""
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+        context("subject is $resultFailure") {
+            successFunctions.forEach { (name, toBeASuccessFun, hasExtraHint) ->
+                it("$name throws AssertionError" + showsSubAssertionIf(hasExtraHint)) {
+                    expect {
+                        expect(resultFailure).toBeASuccessFun { toEqual(1) }
+                    }.toThrow<AssertionError> {
+                        messageToContain("value: $isNotSuccessDescr")
+                        if (hasExtraHint) messageToContain("$toEqualDescr: 1")
+                    }
+                }
+            }
+
+            //TODO 0.20.0 activate once we have the workaround for #1234 implemented
+            failureFunctions.forEach { (name, toBeAFailureFun, _) ->
+                it("$name - can perform sub-assertion which holds") {
+                    expect(resultFailure).toBeAFailureFun { messageToContain("oh no...") }
+                }
+                it("$name - can perform sub-assertion which fails, throws AssertionError") {
+                    expect {
+                        expect(resultFailure).toBeAFailureFun { messageToContain("oh yes...") }
+                    }.toThrow<AssertionError> {
+                        messageToContain(
+                            "$exceptionDescr: ${IllegalArgumentException::class.fullName}",
+                            DescriptionCharSequenceExpectation.TO_CONTAIN.getDefault(), "${DescriptionCharSequenceExpectation.VALUE.getDefault()}: \"oh yes...\""
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    describeFun(toBeASuccessFeatureNullable, toBeASuccessNullable) {
+        val successFunctions = unifySignatures(toBeASuccessFeatureNullable, toBeASuccessNullable)
+
+        successFunctions.forEach { (name, toBeASuccessFun, hasExtraHint) ->
+            context("subject is $resultNullSuccess") {
+                it("$name - can perform sub-assertion which holds") {
+                    expect(resultNullSuccess).toBeASuccessFun { toEqual(null) }
+                }
+                it("$name - can perform sub-assertion which fails, throws AssertionError") {
+                    expect {
+                        expect(resultNullSuccess).toBeASuccessFun { toEqual(2) }
+                    }.toThrow<AssertionError> {
+                        messageToContain("value: null", "$toEqualDescr: 2")
+                    }
+                }
+            }
+
+            context("subject is $resultNullableFailure") {
+                it("${toBeASuccessFeature.name} throws AssertionError" + showsSubAssertionIf(hasExtraHint)) {
+                    expect {
+                        expect(resultNullableFailure).toBeASuccessFun { toEqual(1) }
+                    }.toThrow<AssertionError> {
+                        messageToContain("value: $isNotSuccessDescr")
+                        if (hasExtraHint) messageToContain("$toEqualDescr: 1")
+                    }
+                }
+            }
+        }
+    }
 })
