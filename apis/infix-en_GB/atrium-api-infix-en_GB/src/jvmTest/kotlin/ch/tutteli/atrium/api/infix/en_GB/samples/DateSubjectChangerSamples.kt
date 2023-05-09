4173d7ae36a02b752890c4da8c027cea0ce08081
Jujusp <jf.rubio@uniandes.edu.co>
2023-05-09 18:11:59 -0500

    Added infix implementation for asLocalDateTime

--- apis/infix-en_GB/atrium-api-infix-en_GB/src/jvmTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/DateSubjectChangerSamples.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB/src/jvmTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/DateSubjectChangerSamples.kt
@@ -5,6 +5,7 @@ import ch.tutteli.atrium.api.verbs.internal.expect
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 import kotlin.test.Test
 
 class DateSubjectChangerSamples {
@@ -40,6 +41,39 @@ class DateSubjectChangerSamples {
                 it toBeAfter(LocalDate.parse("2025-07-17"))            // fails
                 it toBeBefore(LocalDate.parse("1994-07-17"))           // still evaluated even though `toBeAfter` already fails
                                                                        // use ` asLocalDate().` if you want a fail fast behaviour
+            }
+        }
+    }
+
+    @Test
+    fun asLocalDateTimeFeature() {
+        val date = formatter.parse("1995-07-17")
+
+        expect(date) asLocalDateTime o toEqual(LocalDateTime.parse("1995-07-17T00:00:00")) // Subject is now of type LocalDateTime
+
+        fails {
+            expect(date) asLocalDateTime
+                o toBeAfter(LocalDateTime.parse("2025-07-17T00:00:00")) toBeBefore(LocalDateTime.parse("1996-07-17T00:00:00"))
+        }
+    }
+
+    @Test
+    fun asLocalDateTime() {
+        val date = formatter.parse("1995-07-17")
+
+        expect(date) asLocalDateTime { // subject within this expectation-group is of type LocalDateTime
+            it toBeAfterOrTheSamePointInTimeAs(LocalDateTime.parse("1994-07-17T00:00:00"))
+            it toBeBeforeOrTheSamePointInTimeAs(LocalDateTime.parse("1996-07-17T00:00:00"))
+        } // subject here is back to type java.util.Date
+
+        fails {
+            // all expectations inside an expectation-group are evaluated together; for more details see:
+            // https://github.com/robstoll/atrium#define-single-expectations-or-an-expectation-group
+
+            expect(date) asLocalDateTime {
+                it toBeAfter(LocalDateTime.parse("2025-07-17T00:00:00"))            // fails
+                it toBeBefore(LocalDateTime.parse("1994-07-17T00:00:00"))           // still evaluated even though `toBeAfter` already fails
+                // use ` asLocalDateTime().` if you want a fail fast behaviour
             }
         }
     }
