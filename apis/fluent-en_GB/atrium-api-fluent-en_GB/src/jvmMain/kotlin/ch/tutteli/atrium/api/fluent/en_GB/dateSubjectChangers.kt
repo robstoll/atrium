72d2587cd11dfb0b76fdfa6c43b82431ced3f681
jf.rubio <45725531+Jujusp@users.noreply.github.com>
2023-05-15 15:44:37 -0500

    Issue 1286 add asLocalDateTime to java.util.Date  (#1431)

--- apis/fluent-en_GB/atrium-api-fluent-en_GB/src/jvmMain/kotlin/ch/tutteli/atrium/api/fluent/en_GB/dateSubjectChangers.kt
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB/src/jvmMain/kotlin/ch/tutteli/atrium/api/fluent/en_GB/dateSubjectChangers.kt
@@ -9,6 +9,7 @@ import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.logic._logic
 import ch.tutteli.atrium.logic.changeSubject
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.Date
 
@@ -39,3 +40,32 @@ fun <T : Date> Expect<T>.asLocalDate(): Expect<LocalDate> =
  */
 fun <T : Date> Expect<T>.asLocalDate(assertionCreator: Expect<LocalDate>.() -> Unit): Expect<T> =
     apply { asLocalDate()._logic.appendAsGroup(assertionCreator) }
+
+
+/**
+ * Turns `Expect<Date>` into `Expect<LocalDateTime>`.
+ *
+ * The transformation as such is not reflected in reporting.
+ *
+ * @return The newly created [Expect] for the transformed subject.
+ *
+ * @sample ch.tutteli.atrium.api.fluent.en_GB.samples.DateSubjectChangerSamples.asLocalDateTimeFeature
+ *
+ * @since 1.0.0
+ */
+fun <T : Date> Expect<T>.asLocalDateTime(): Expect<LocalDateTime> =
+    _logic.changeSubject.unreported { it.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime() }
+
+/**
+ * Expects that the subject of `this` expectation holds all assertions the given [assertionCreator] creates for the subject as[Date].
+ *
+ * The transformation as such is not reflected in reporting.
+ *
+ * @return an [Expect] for the subject of `this` expectation.
+ *
+ * @sample ch.tutteli.atrium.api.fluent.en_GB.samples.DateSubjectChangerSamples.asLocalDateTime
+ *
+ * @since 1.0.0
+ */
+fun <T : Date> Expect<T>.asLocalDateTime(assertionCreator: Expect<LocalDateTime>.() -> Unit): Expect<T> =
+    apply { asLocalDateTime()._logic.appendAsGroup(assertionCreator) }
--- apis/fluent-en_GB/atrium-api-fluent-en_GB/src/jvmTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/DateSubjectChangerSamples.kt
+++ apis/fluent-en_GB/atrium-api-fluent-en_GB/src/jvmTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/DateSubjectChangerSamples.kt
@@ -5,6 +5,7 @@ import ch.tutteli.atrium.api.verbs.internal.expect
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 import kotlin.test.Test
 
 class DateSubjectChangerSamples {
@@ -24,7 +25,7 @@ class DateSubjectChangerSamples {
                 .asLocalDate()                                  // subject is now of type LocalDate
                 .toBeAfter(LocalDate.parse("2025-07-17"))       // fails
                 .toBeBefore(LocalDate.parse("1996-07-17"))      // not evaluated/reported because `toBeAfter` already fails
-                                                                // use `.asLocalDate { ... }` if you want all assertions evaluated
+                //                                                      use `.asLocalDate { ... }` if you want all assertions evaluated
         }
     }
 
@@ -44,7 +45,45 @@ class DateSubjectChangerSamples {
             expect(date).asLocalDate {
                 toBeAfter(LocalDate.parse("2025-07-17"))            // fails
                 toBeBefore(LocalDate.parse("1994-07-17"))           // still evaluated even though `toBeAfter` already fails
-                                                                    // use `.asLocalDate().` if you want a fail fast behaviour
+                //                                                          use `.asLocalDate().` if you want a fail fast behaviour
+            }
+        }
+    }
+
+    @Test
+    fun asLocalDateTimeFeature() {
+        val date = formatter.parse("1995-07-17")
+
+        expect(date)
+            .asLocalDateTime() // subject is now of type LocalDateTime
+            .toEqual(LocalDateTime.parse("1995-07-17T00:00:00"))
+
+        fails {
+            expect(date)
+                .asLocalDateTime()                                  // subject is now of type LocalDateTime
+                .toBeAfter(LocalDateTime.parse("2025-07-17T00:00:00"))       // fails
+                .toBeBefore(LocalDateTime.parse("1996-07-17T00:00:00"))      // not evaluated/reported because `toBeAfter` already fails
+                //                                                                   use `.asLocalDateTime { ... }` if you want all assertions evaluated
+        }
+    }
+
+    @Test
+    fun asLocalDateTime() {
+        val date = formatter.parse("1995-07-17")
+
+        expect(date).asLocalDateTime { // subject within this expectation-group is of type LocalDateTime
+            toBeAfterOrTheSamePointInTimeAs(LocalDateTime.parse("1994-07-17T00:00:00"))
+            toBeBeforeOrTheSamePointInTimeAs(LocalDateTime.parse("1996-07-17T00:00:00"))
+        } // subject here is back to type java.util.Date
+
+        fails {
+            // all expectations inside an expectation-group are evaluated together; for more details see:
+            // https://github.com/robstoll/atrium#define-single-expectations-or-an-expectation-group
+
+            expect(date).asLocalDateTime {
+                toBeAfter(LocalDateTime.parse("2025-07-17T00:00:00"))            // fails
+                toBeBefore(LocalDateTime.parse("1994-07-17T00:00:00"))           // still evaluated even though `toBeAfter` already fails
+                //                                                                       use `.asLocalDateTime().` if you want a fail fast behaviour
             }
         }
     }
