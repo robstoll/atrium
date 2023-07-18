59c19b48e9e0a137267cf89ae6cfe4c096c13a17
Robert Stoll <rstoll@tutteli.ch>
2023-07-18 22:43:09 +0200

    move JS-IR related TODOs to v1.2.0

--- misc/tools/atrium-samples-test/build.gradle.kts
+++ misc/tools/atrium-samples-test/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("build-logic.gradle-conventions")
 }
 
-//TODO 1.1.0 include ...-kotlin-test again once we use IR for JS
+//TODO 1.2.0 include ...-kotlin-test again once we use IR for JS
 val samples = listOf("junit5", "spek")//, "js-kotlin-test", "multiplatform-kotlin-test")
 
 tasks.register("build") {
