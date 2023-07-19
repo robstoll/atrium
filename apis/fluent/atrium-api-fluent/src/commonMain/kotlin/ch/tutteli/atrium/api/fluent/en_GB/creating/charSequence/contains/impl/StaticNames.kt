eef280dd3744685ac9a6dbe708b7a5df23b9a4b1
Robert Stoll <rstoll@tutteli.ch>
2023-07-19 21:26:01 +0200

    move deprecated StaticNames into charSequence dir for windows
    because windows has a case-insensitive file system and seems to merge
    the two dirs during checkout 🙈

--- apis/fluent/atrium-api-fluent/src/commonMain/kotlin/ch/tutteli/atrium/api/fluent/en_GB/creating/charSequence/contains/impl/StaticNames.kt
+++ apis/fluent/atrium-api-fluent/src/commonMain/kotlin/ch/tutteli/atrium/api/fluent/en_GB/creating/charSequence/contains/impl/StaticNames.kt
@@ -1,54 +1,57 @@
-package ch.tutteli.atrium.api.infix.en_GB.creating.charsequence.contains.impl
+// needed for Windows which cannot deal with two directories which only distinguish its name in case
+// hence we had to move it into charSequence
+@file:Suppress("PackageDirectoryMismatch")
 
-import ch.tutteli.atrium.api.infix.en_GB.*
-import ch.tutteli.atrium.api.infix.en_GB.creating.Values
+package ch.tutteli.atrium.api.fluent.en_GB.creating.charsequence.contains.impl
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.logic.creating.charsequence.contains.CharSequenceContains
 import ch.tutteli.atrium.logic.creating.charsequence.contains.steps.AtLeastCheckerStep
-import kotlin.reflect.KFunction2
+import kotlin.reflect.KFunction3
 
 @Deprecated(
     "Use the one from package toContain, will be removed with 1.2.0",
-    ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticNames")
+    ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticNames")
 )
 internal object StaticNames {
     @Deprecated(
         "Use the one from package toContain, will be removed with 1.2.0",
-        ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticName.notToContainValuesFun")
+        ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticName.notToContainValuesFun")
     )
     val notToContainValuesFun = run {
-        val f: KFunction2<Expect<CharSequence>, Values<Any>, Expect<CharSequence>> =
+        val f: KFunction3<Expect<CharSequence>, Any, Array<out Any>, Expect<CharSequence>> =
             Expect<CharSequence>::notToContain
-        "`${f.name} values`"
+        f.name
     }
 
     @Deprecated(
         "Use the one from package toContain, will be removed with 1.2.0",
-        ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticName.atLeast")
+        ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticName.atLeast")
     )
     val atLeast = CharSequenceContains.EntryPointStep<CharSequence, *>::atLeast.name
 
     @Deprecated(
         "Use the one from package toContain, will be removed with 1.2.0",
-        ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticName.butAtMost")
+        ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticName.butAtMost")
     )
     val butAtMost = AtLeastCheckerStep<CharSequence, *>::butAtMost.name
 
     @Deprecated(
         "Use the one from package toContain, will be removed with 1.2.0",
-        ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticName.atMost")
+        ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticName.atMost")
     )
     val atMost = CharSequenceContains.EntryPointStep<CharSequence, *>::atMost.name
 
     @Deprecated(
         "Use the one from package toContain, will be removed with 1.2.0",
-        ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticName.exactly")
+        ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticName.exactly")
     )
     val exactly = CharSequenceContains.EntryPointStep<CharSequence, *>::exactly.name
 
     @Deprecated(
         "Use the one from package toContain, will be removed with 1.2.0",
-        ReplaceWith("ch.tutteli.atrium.api.infix.en_GB.creating.charSequence.toContain.impl.StaticName.notOrAtMost")
+        ReplaceWith("ch.tutteli.atrium.api.fluent.en_GB.creating.charSequence.toContain.impl.StaticName.notOrAtMost")
     )
     val notOrAtMost = CharSequenceContains.EntryPointStep<CharSequence, *>::notOrAtMost.name
 }
