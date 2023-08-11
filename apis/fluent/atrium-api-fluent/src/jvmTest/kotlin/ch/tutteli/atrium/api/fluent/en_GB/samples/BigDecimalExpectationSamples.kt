a2b8e2b8782ad36110e79d71c0c19541d6a751d0
Robert Stoll <rstoll@tutteli.ch>
2023-08-11 23:10:54 +0200

    discourage usage of expect(BigDecimal).notToEqual except for null
    moreover:
    - improve the samples, include a `fails` case

--- apis/fluent/atrium-api-fluent/src/jvmTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/BigDecimalExpectationSamples.kt
+++ apis/fluent/atrium-api-fluent/src/jvmTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/BigDecimalExpectationSamples.kt
@@ -8,25 +8,26 @@ import kotlin.test.Test
 class BigDecimalExpectationSamples {
 
     @Test
-    fun toEqual() {
-        // only use toEqual to check against null, otherwise use toEqualNumerically or toEqualIncludingScale
+    fun toEqualNull() {
+        // Use toEqual only to check against null if your subject is a BigDecimal
+        // Use toEqualNumerically or toEqualIncludingScale if you want to compare it against another BigDecimal
         expect(null as BigDecimal?).toEqual(null)
-    }
 
+        fails {
+            expect(BigDecimal("-12345.6789") as BigDecimal?).toEqual(null)
+        }
+    }
 
     @Test
-    fun notToEqual() {
-        // only use notToEqual to check against null, otherwise use notToEqualNumerically or notToEqualIncludingScale
-        expect(null as BigDecimal?).notToEqual(BigDecimal("-12345.6789"))
-        expect(null as BigDecimal?).notToEqual(BigDecimal("1.0"))
-        expect(null as BigDecimal?).notToEqual(BigDecimal("25.0"))
+    fun notToEqualNull() {
+        // Use notToEqual only to check against null if your subject is a BigDecimal
+        // Use notToEqualNumerically or notToEqualIncludingScale if you want to compare it against another BigDecimal
+        expect(BigDecimal("-12345.6789") as BigDecimal?).notToEqual(null)
 
-
-        expect(null as BigDecimal?).notToEqual(BigDecimal(-12345.6789))
-        expect(null as BigDecimal?).notToEqual(BigDecimal(1.0))
-        expect(null as BigDecimal?).notToEqual(BigDecimal(25.0))
+        fails {
+            expect(null as BigDecimal?).notToEqual(null)
+        }
     }
-
 
     @Test
     fun toEqualNumerically() {
@@ -38,6 +39,10 @@ class BigDecimalExpectationSamples {
         expect(BigDecimal(10)).toEqualNumerically(BigDecimal(10.0))
         expect(BigDecimal(1213669989183)).toEqualNumerically(BigDecimal(1213669989183))
         expect(BigDecimal(1238126387123)).toEqualNumerically(BigDecimal(1238126387123))
+
+        fails {
+            expect(BigDecimal(10)).toEqualNumerically(BigDecimal(10.1))
+        }
     }
 
 
@@ -50,6 +55,12 @@ class BigDecimalExpectationSamples {
         expect(BigDecimal(10)).notToEqualNumerically(BigDecimal(1213669989183))
         expect(BigDecimal(1213669989183)).notToEqualNumerically(BigDecimal(10))
         expect(BigDecimal(1238126387123)).notToEqualNumerically(BigDecimal(-1.0))
+
+        fails {
+            // because is numerically the same
+            // use notToEqualIncludingScale if you want to compare the scale as well
+            expect(BigDecimal("10")).notToEqualNumerically(BigDecimal("10.0"))
+        }
     }
 
 
@@ -61,6 +72,11 @@ class BigDecimalExpectationSamples {
 
         expect(BigDecimal(10.0)).toEqualIncludingScale(BigDecimal(10.0))
         expect(BigDecimal(456.0)).toEqualIncludingScale(BigDecimal(456.0))
+
+        fails {
+            // because scale is different, use toEqualNumerically if you want to ignore the scale
+            expect(BigDecimal("10")).toEqualIncludingScale(BigDecimal("10.0"))
+        }
     }
 
 
@@ -73,7 +89,9 @@ class BigDecimalExpectationSamples {
         expect(BigDecimal(10)).notToEqualIncludingScale(BigDecimal(10.01))
         expect(BigDecimal(456.01)).notToEqualIncludingScale(BigDecimal(456))
         expect(BigDecimal(-1.456)).notToEqualIncludingScale(BigDecimal(-1.45))
+
+        fails {
+            expect(BigDecimal("10.0")).notToEqualIncludingScale(BigDecimal("10.0"))
+        }
     }
-
-
 }
