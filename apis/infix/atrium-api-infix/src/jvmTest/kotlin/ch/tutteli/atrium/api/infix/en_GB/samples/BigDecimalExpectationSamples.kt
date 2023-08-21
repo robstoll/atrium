12dfc61498b6e2a989c8a34ae90efe5ef71c47a1
enishetty.v <enishetty.v@HMDVENISHETTYV.local>
2023-08-22 00:39:22 +0530

    Added samples for functions which are not deprecated in ch.tutteli.atrium.api.infix.en_GB.samples.BigDecimalExpectationSamples

--- apis/infix/atrium-api-infix/src/jvmTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/BigDecimalExpectationSamples.kt
+++ apis/infix/atrium-api-infix/src/jvmTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/BigDecimalExpectationSamples.kt
@@ -3,6 +3,7 @@ package ch.tutteli.atrium.api.infix.en_GB.samples
 import ch.tutteli.atrium.api.infix.en_GB.*
 import ch.tutteli.atrium.api.verbs.expect
 import java.math.BigDecimal
+import java.math.MathContext
 import kotlin.test.Test
 
 class BigDecimalExpectationSamples {
@@ -26,6 +27,46 @@ class BigDecimalExpectationSamples {
 
         fails {
             expect(null as BigDecimal?) notToEqual null
+        }
+    }
+
+    @Test
+    fun toEqualNumerically(){
+        // Use toEqualNumerically to compare subject and target are numerically equal
+        expect(BigDecimal("-12345.67890")) toEqualNumerically BigDecimal("-12345.6789")
+
+        fails{
+            expect(BigDecimal("-12345.678")) toEqualNumerically BigDecimal("-12345.6789")
+        }
+    }
+
+    @Test
+    fun notToEqualNumerically(){
+        // Use notToEqualNumerically to compare subject and target are not numerically equal
+        expect(BigDecimal("-12345.678")) notToEqualNumerically BigDecimal("-12345.6789")
+
+        fails{
+            expect(BigDecimal("-12345.67890")) notToEqualNumerically BigDecimal("-12345.6789")
+        }
+    }
+
+    @Test
+    fun toEqualIncludingScale(){
+        // Use toEqualIncludingScale to compare subject and target are numerically equal including scale
+        expect(BigDecimal("-12345.678912", MathContext(9))) toEqualIncludingScale BigDecimal("-12345.6789")
+
+        fails{
+            expect(BigDecimal("-12345.67890")) toEqualIncludingScale BigDecimal("-12345.6789")
+        }
+    }
+
+    @Test
+    fun notToEqualIncludingScale(){
+        // Use notToEqualIncludingScale to compare subject and target are not numerically and scaling equal
+        expect(BigDecimal("-12345.67890")) notToEqualIncludingScale BigDecimal("-12345.6789")
+
+        fails{
+            expect(BigDecimal("-12345.678912", MathContext(9))) notToEqualIncludingScale BigDecimal("-12345.6789")
         }
     }
 }
