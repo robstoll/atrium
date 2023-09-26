d4933f1d7dd867ed84f1a35e8a4cfbd463dc778c
Robert Stoll <rstoll@tutteli.ch>
2023-09-26 22:20:49 +0200

    prepare iterableLike and mapLike toContain samples

---
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
@@ -0,0 +1,10 @@
+package ch.tutteli.atrium.api.fluent.en_GB.samples
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
+import ch.tutteli.atrium.api.verbs.expect
+import kotlin.test.Test
+
+class IterableLikeToContainCheckerSamples {
+
+
+}
---
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/MapLikeToContainInOrderOnlyCreatorSamples.kt
@@ -0,0 +1,9 @@
+package ch.tutteli.atrium.api.fluent.en_GB.samples
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
+import ch.tutteli.atrium.api.verbs.expect
+import kotlin.test.Test
+
+class MapLikeToContainInOrderOnlyCreatorSamples {
+
+}
