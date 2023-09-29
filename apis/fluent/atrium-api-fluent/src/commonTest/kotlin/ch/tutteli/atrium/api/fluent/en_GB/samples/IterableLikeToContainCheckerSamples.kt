a621e4d0c7a6148bfa005a9b2821e7ea132b73fa
Nikos-Tsiougranas <n.tsiougkranas@zupan.io>
2023-09-29 21:42:10 +0200

    Add samples for Iterable.toContain.exactly

--- apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
@@ -5,6 +5,26 @@ import ch.tutteli.atrium.api.verbs.expect
 import kotlin.test.Test
 
 class IterableLikeToContainCheckerSamples {
+    @Test
+    fun exactly() {
+        expect(listOf("A", "B", "A")).toContain.inAnyOrder.exactly(2).entry {
+            toEqual("A")
+        }
 
+        expect(listOf(1, 2, 3)).toContain.inAnyOrder.exactly(2).entry {
+            toBeGreaterThan(1)
+        }
 
+        fails {
+            expect(listOf("A", "B", "A", "C")).toContain.inAnyOrder.exactly(2).entry {
+                toEqual("B")
+            }
+        }
+
+        fails {
+            expect(listOf(1, 2, 3)).toContain.inAnyOrder.exactly(2).entry {
+                toBeLessThanOrEqualTo(1)
+            }
+        }
+    }
 }
