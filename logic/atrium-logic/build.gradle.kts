7274248113ba8eb8dc18e9b2f960a8e2c2f34c9e
Robert Stoll <rstoll@tutteli.ch>
2023-10-18 21:54:52 +0200

    update tutteli-gradle-plugins to 4.11.0

--- logic/atrium-logic/build.gradle.kts
+++ logic/atrium-logic/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.dokka.gradle.AbstractDokkaLeafTask
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     id("build-logic.published-kotlin-multiplatform")
@@ -40,6 +41,22 @@ tasks.configureEach<AbstractDokkaLeafTask> {
         reportUndocumented.set(false)
     }
 }
+
+// TODO 1.3.0 some bug in dokka, maybe fixed in a never version? We should be able to suppress jvmMain for js and common
+// see https://github.com/Kotlin/dokka/issues/3209
+project.afterEvaluate {
+    tasks.named<DokkaTask>("dokkaHtmlJs") {
+        dokkaSourceSets.named("jvmMain") {
+            suppress.set(false)
+        }
+    }
+    tasks.named<DokkaTask>("dokkaHtmlKotlinMultiplatform") {
+        dokkaSourceSets.named("jvmMain") {
+            suppress.set(false)
+        }
+    }
+}
+
 
 createGenerateLogicTask(
     includingTarget("jvm"),
