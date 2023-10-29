5b2781570aa2b4c7d93d8dbc54715ceb1ff94372
Fergus Baldwin <u7498672@anu.edu.au>
2023-10-29 14:51:56 +1100

    pre-work of Fergus Baldwin

--- apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
@@ -3,6 +3,7 @@ package ch.tutteli.atrium.api.fluent.en_GB.samples
 import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.api.verbs.expect
 import kotlin.test.Test
+import kotlin.collections.*
 
 class IterableLikeToContainCheckerSamples {
     @Test
@@ -52,5 +53,62 @@ class IterableLikeToContainCheckerSamples {
             }
         }
 
+    }
+
+    @Test
+    fun atMost() {
+        expect(listOf("A,A,B,C,A,B,B")).toContain.inAnyOrder.atMost(2).entry{
+            toEqual("C")
+        }
+
+        expect(listOf(1,2,3,2,3,3)).toContain.inAnyOrder.atMost(2).entry{
+            toBeLessThanOrEqualTo(2)
+        }
+
+        fails {
+            expect(listOf("A,A,B,B,C,C")).toContain.inAnyOrder.atMost(1).entry{
+                toEqual("A")
+            }
+        }
+
+        fails {
+            expect(listOf(1,2,3,2,3,3)).toContain.inAnyOrder.atMost(2).entry{
+                toBeGreaterThan(2)
+            }
+        }
+    }
+
+    @Test
+    fun butAtMost() {
+        expect(listOf("A,B,C,A,B,B")).toContain.inAnyOrder.atLeast(2).butAtMost(2).entry{
+            toEqual("A")
+        }
+
+        expect(listOf(1,2,3,4,5,6,4,5,5,7,7,7,7)).toContain.inAnyOrder.atLeast(3).butAtMost(4).entry{
+            toEqual(5)
+        }
+
+        fails {
+            expect(listOf("A,B,B,B,B,B,C,C")).toContain.inAnyOrder.atLeast(3).butAtMost(4).entry{
+                toEqual("A")
+            }
+        }
+    }
+
+    @Test
+    fun atLeast() {
+        expect(listOf("A,B,C,A,B,B")).toContain.inAnyOrder.atLeast(3).entry{
+            toEqual("B")
+        }
+
+        expect(listOf(1,2,3,4,5,6,4)).toContain.inAnyOrder.atLeast(2).entry{
+            toEqual(4)
+        }
+
+        fails {
+            expect(listOf("A,B,C")).toContain.inAnyOrder.atLeast(2).entry{
+                toEqual("A")
+            }
+        }
     }
 }
