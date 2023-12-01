064b8385cda8b91cb8e566ff0350b798b9f4eb48
Zuretto <mikzur4@gmail.com>
2023-12-01 18:42:27 +0100

    add comments for samples of Iterable.toContain.notOrAtMost

--- apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainCheckerSamples.kt
@@ -35,16 +35,18 @@ class IterableLikeToContainCheckerSamples {
         }
 
         expect(listOf(1, 2, 3)).toContain.inAnyOrder.notOrAtMost(2).entry {
-            toBeGreaterThan(1)
+            // none fulfils the expectation which is fine because we use notOrAtMost
+            // use atMost if you want that at least one element matches
+            toBeGreaterThan(4)
         }
 
-        fails {
+        fails { // because "B" is 3 times in the List
             expect(listOf("A", "B", "B", "B")).toContain.inAnyOrder.notOrAtMost(2).entry {
                 toEqual("B")
             }
         }
 
-        fails {
+        fails { // because there are 3 items greater than 1
             expect(listOf(1, 2, 3, 3)).toContain.inAnyOrder.notOrAtMost(2).entry {
                 toBeGreaterThan(1)
             }
