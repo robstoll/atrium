1df46ed4d7261de0ba8c1c23a4675650c0cab5cc
Robert Stoll <rstoll@tutteli.ch>
2023-12-05 22:38:39 +0100

    Update apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainInOrderOnlyCreatorSamples.kt

--- apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainInOrderOnlyCreatorSamples.kt
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableLikeToContainInOrderOnlyCreatorSamples.kt
@@ -11,9 +11,21 @@ class IterableLikeToContainInOrderOnlyCreatorSamples {
             listOf("A","B","C")
         )
 
-        fails {
+        fails { // although same elements but not in same order
             expect(listOf("A","B","C")).toContain.inOrder.only.elementsOf(
                 listOf("A","C","B")
+            )
+        }
+        
+         fails { // because not all elements found
+            expect(listOf("A","B","C")).toContain.inOrder.only.elementsOf(
+                listOf("A","B")
+            )
+        }
+        
+        fails { // because more elements expected than found
+            expect(listOf("A","B","C")).toContain.inOrder.only.elementsOf(
+                listOf("A","B","C","D")
             )
         }
     }
