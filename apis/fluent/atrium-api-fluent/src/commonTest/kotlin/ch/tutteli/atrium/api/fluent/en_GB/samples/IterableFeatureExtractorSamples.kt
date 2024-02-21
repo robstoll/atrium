32be65d5afc43a3e43d6661b307b8bb271d1dc7b
Constantine Zubrilin <5437573+constantine-zubrilin@users.noreply.github.com>
2024-02-21 22:20:24 +0100

    Comments added https://github.com/robstoll/atrium/issues/1569

--- apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableFeatureExtractorSamples.kt
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/samples/IterableFeatureExtractorSamples.kt
@@ -13,50 +13,76 @@ class IterableFeatureExtractorSamples {
     @Test
     fun minFeature() {
         val iterable = sequenceOf(1, -2, 3).asIterable()
-        expect(iterable).min().toEqual(-2)
-        expect(iterable).min().toBeLessThan(0).toBeGreaterThan(-10)
+
+        expect(iterable).min().toEqual(-2) // subject is -2
+
+        expect(iterable).min()
+            .toBeLessThan(0) // subject is -2
+            .toBeGreaterThan(-10) // subject is still -2
+
         fails {
-            expect(iterable).min().toEqual(1)
+            expect(iterable).min() // subject is -2
+                .toBeLessThan(-5) // fails
+                .toBeGreaterThan(-10) // skipped because previous expectation failed
         }
     }
 
     @Test
     fun min() {
         val iterable = sequenceOf(1, -2, 3).asIterable()
-        expect(iterable).min {
-            toBeLessThan(0)
-            toBeGreaterThanOrEqualTo(-2)
+
+        expect(iterable).min { // subject is -2
+            toBeLessThan(0) // subject is still -2
+            toBeGreaterThanOrEqualTo(-2) // subject is still -2
         }
+
         fails {
-            expect(iterable).min {
-                toBeLessThan(-2)
-                toEqual(-10)
-            }
+            // all expectations are evaluated inside an expectation-group block; for more details:
+            // https://github.com/robstoll/atrium#define-single-expectations-or-an-expectation-group
+
+            expect(iterable).min {// subject within this expectation-group is of type Int (actually -2)
+                toBeLessThan(-2) // fails
+                toEqual(-10) // still evaluated, even though  `toBeLessThan` already fails,
+                //                      use `.min.` if you want a fail fast behaviour
+            } // subject here is back type Iterable
         }
     }
 
     @Test
     fun maxFeature() {
         val iterable = sequenceOf(1, 2, 0).asIterable()
-        expect(iterable).max().toEqual(2)
-        expect(iterable).max().toBeLessThan(10).toBeGreaterThan(1)
+
+        expect(iterable).max().toEqual(2) // subject is 2
+
+        expect(iterable).max()
+            .toBeLessThan(10) // subject is 2
+            .toBeGreaterThan(1) // subject is still 2
+
         fails {
-            expect(iterable).max().toEqual(1)
+            expect(iterable).max() // subject is 2
+                .toBeLessThan(2) // fails
+                .toEqual(1) // skipped because previous expectation failed
         }
     }
 
     @Test
     fun max() {
         val iterable = sequenceOf(1, 2, 0).asIterable()
+
         expect(iterable).max {
-            toBeGreaterThan(0)
-            toBeGreaterThanOrEqualTo(2)
+            toBeGreaterThan(0) // subject is 2
+            toBeGreaterThanOrEqualTo(2) // subject is still 2
         }
+
         fails {
+            // all expectations are evaluated inside an expectation-group block; for more details:
+            // https://github.com/robstoll/atrium#define-single-expectations-or-an-expectation-group
+
             expect(iterable).max {
-                toBeGreaterThan(10)
-                toEqual(2)
-            }
+                toBeGreaterThan(10) // fails
+                toEqual(2) // still evaluated, even though  `toBeGreaterThan` already fails,
+                //                      use `.max.` if you want a fail fast behaviour
+            } // subject here is back type Iterable
         }
     }
 }
--- apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/IterableFeatureExtractorSamples.kt
+++ apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/IterableFeatureExtractorSamples.kt
@@ -15,46 +15,66 @@ class IterableFeatureExtractorSamples {
     @Test
     fun minFeature() {
         val iterable = sequenceOf(1, -2, 3).asIterable()
-        expect(iterable) min o toEqual -2
-        expect(iterable) min o toBeLessThan 0 toBeGreaterThan -10
+
+        expect(iterable) min o toEqual -2 // subject is -2
+
+        expect(iterable) min o toBeLessThan 0 toBeGreaterThan -10 // subject is -2 and passes all expectations
+
         fails {
-            expect(iterable) min o toEqual 1
+            expect(iterable) min o toBeLessThan -5 toBeGreaterThan -10 // subject is -2 and fails the first expectation,
         }
     }
 
     @Test
     fun min() {
         val iterable = sequenceOf(1, -2, 3).asIterable()
+
         expect(iterable) min {
-            it toBeLessThan 0 toBeGreaterThanOrEqualTo -2
+            it toBeLessThan 0 toBeGreaterThanOrEqualTo -2 // subject is -2
         }
+
         fails {
-            expect(iterable) min {
-                it toBeLessThan -2 toEqual -10
-            }
+            // all expectations are evaluated inside an expectation-group block; for more details:
+            // https://github.com/robstoll/atrium#define-single-expectations-or-an-expectation-group
+
+            expect(iterable) min { // subject within this expectation-group is of type Int (actually -2)
+                it toBeLessThan -2 // fails
+                it toEqual -10 // still evaluated, even though  `toBeLessThan` already fails,
+                //                use `it toBeLessThan -2 toEqual -10 ` if you want a fail fast behaviour
+            } // subject here is back type Iterable
         }
     }
 
     @Test
     fun maxFeature() {
         val iterable = sequenceOf(1, 2, 0).asIterable()
-        expect(iterable) max o toEqual 2
-        expect(iterable) max o toBeLessThan 10 toBeGreaterThan 1
+
+        expect(iterable) max o toEqual 2 // subject is 2
+
+        expect(iterable) max o toBeLessThan 10 toBeGreaterThan 1 // subject is 2 and passes all expectations
+
         fails {
-            expect(iterable) max o toEqual 1
+            expect(iterable) max o toBeLessThan 2 toBeGreaterThan 0 // subject is 2 and fails the first expectation, the second is not evaluated
         }
     }
 
     @Test
     fun max() {
         val iterable = sequenceOf(1, 2, 0).asIterable()
+
         expect(iterable) max {
-            it toBeGreaterThan 0 toBeGreaterThanOrEqualTo 2
+            it toBeGreaterThan 0 toBeGreaterThanOrEqualTo 2 // subject is 2 and passes all expectations
         }
+
         fails {
-            expect(iterable) max {
-                it toBeGreaterThan 10 toEqual 2
-            }
+            // all expectations are evaluated inside an expectation-group block; for more details:
+            // https://github.com/robstoll/atrium#define-single-expectations-or-an-expectation-group
+
+            expect(iterable) max { // subject within this expectation-group is of type Int (actually 2)
+                it toBeGreaterThan 10 // fails
+                it toEqual 2 // still evaluated, even though  `toBeGreaterThan` already fails,
+                //              use `it toBeGreaterThan 10 toEqual 2` if you want a fail fast behaviour
+            } // subject here is back type Iterable
         }
     }
 }
