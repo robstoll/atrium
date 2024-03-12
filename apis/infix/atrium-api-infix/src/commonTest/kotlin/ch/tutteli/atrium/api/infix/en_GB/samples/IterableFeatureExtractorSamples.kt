c49887b4f3df2ae8e99879a37118385493c4bba5
Robert Stoll <rstoll@tutteli.ch>
2024-03-12 09:26:15 +0100

    Update apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/IterableFeatureExtractorSamples.kt

--- apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/IterableFeatureExtractorSamples.kt
+++ apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/IterableFeatureExtractorSamples.kt
@@ -88,7 +88,7 @@ class IterableFeatureExtractorSamples {
         expect(iterable) last o toBeGreaterThan(2) toBeLessThan(4) // subject is 3 and passes all expectations
 
         fails {
-            expect(list) last o  toBeGreaterThan (3) toBeLessThan (4) // subject is 3, fails on first expectation, second is skipped
+            expect(iterable) last o  toBeGreaterThan (3) toBeLessThan (4) // subject is 3, fails on first expectation, second is skipped
         }
 
         fails {
