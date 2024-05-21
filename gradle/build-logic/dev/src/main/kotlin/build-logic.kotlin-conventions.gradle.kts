eea44b30337e7088de31222b6ff63f03ad075ae6
Robert Stoll <rstoll@tutteli.ch>
2024-05-21 22:45:50 +0200

    suppress experimental feature warning about expect/actual

--- gradle/build-logic/dev/src/main/kotlin/build-logic.kotlin-conventions.gradle.kts
+++ gradle/build-logic/dev/src/main/kotlin/build-logic.kotlin-conventions.gradle.kts
@@ -20,6 +20,8 @@ tasks.configureEach<KotlinCompilationTask<*>> {
         freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
         // suppress warnings about kotlin 1.4 being deprecated
         freeCompilerArgs.add("-Xsuppress-version-warnings")
+        // suppress warnings about expect/actual being an experimental feature
+        freeCompilerArgs.add("-Xexpect-actual-classes")
 
         val kotlinVersion = KotlinVersion.fromVersion(buildParameters.kotlin.version)
         languageVersion.set(kotlinVersion)
