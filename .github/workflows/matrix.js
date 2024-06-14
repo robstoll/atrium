2f35e51a42118ae4959c73376ea3a19b69398840
BereKanters <berekanters@live.nl>
2024-06-14 22:28:42 +0200

    ###feat/Integrate custom random matrix generation and merge Ubuntu and Windows workflows updated based on feedback
    - Added custom matrix generation scripts (`matrix_builder.js` and `matrix.js`) to dynamically generate the matrix for CI jobs.
    - Configured the GitHub Actions workflow (`ci.yml`) to use the custom matrix generator.
    - Renamed the `setup` job to `dexer` and included the step to check if Atrium's -jvm.jar can be dexed.

---
+++ .github/workflows/matrix.js
@@ -0,0 +1,29 @@
+let { MatrixBuilder } = require('./matrix_builder');
+const matrix = new MatrixBuilder();
+
+// Add axes for the matrix
+matrix.addAxis({
+    name: 'os',
+    title: x => x.replace('-latest', ''),
+    values: ['ubuntu-latest', 'windows-latest']
+});
+matrix.addAxis({
+    name: 'java_version',
+    values: ['11', '17']
+});
+
+// Ensure at least one windows and at least one linux job is present (macos is almost the same as linux)
+matrix.generateRow({ os: 'windows-latest' });
+matrix.generateRow({ os: 'ubuntu-latest' });
+
+// Generate more rows, no duplicates would be generated
+const include = matrix.generateRows(process.env.MATRIX_JOBS || 5);
+if (include.length === 0) {
+    throw new Error('Matrix list is empty');
+}
+// Sort jobs by name, however, numeric parts are sorted appropriately
+// For instance, 'windows 8' would come before 'windows 11'
+include.sort((a, b) => a.name.localeCompare(b.name, undefined, { numeric: true }));
+
+console.log(include);
+console.log('::set-output name=matrix::' + JSON.stringify({ include }));
