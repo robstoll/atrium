d400a45f9636ad7c880cf0e060ef6fe87bbd1d36
BereKanters <berekanters@live.nl>
2024-06-16 23:19:00 +0200

    ###Fix/Added Requires java to module for sqlExceptionFeatureExtractors
    - Added Requires java to module-info.java

--- apis/fluent/atrium-api-fluent/src/jvmMain/java/module-info.java
+++ apis/fluent/atrium-api-fluent/src/jvmMain/java/module-info.java
@@ -3,6 +3,7 @@ module ch.tutteli.atrium.api.fluent.en_GB {
     requires            ch.tutteli.atrium.logic;
     requires            kotlin.stdlib;
     requires            ch.tutteli.kbox;
+    requires java.sql;
 
     exports ch.tutteli.atrium.api.fluent.en_GB;
 }
