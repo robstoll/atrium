61f96823e4a42ce8ae19805e9cb29778c09c5e40
tanaka <177859314+hubtanaka@users.noreply.github.com>
2024-08-17 15:31:13 +0900

    extend IteratorAssertions with a function next, and implement it in DefaultIteratorAssertions

--- logic/atrium-logic/src/generated/commonMain/ch/tutteli/atrium/logic/iterator.kt
+++ logic/atrium-logic/src/generated/commonMain/ch/tutteli/atrium/logic/iterator.kt
@@ -9,10 +9,12 @@ package ch.tutteli.atrium.logic
 import ch.tutteli.atrium.assertions.Assertion
 import ch.tutteli.atrium.creating.AssertionContainer
 import ch.tutteli.atrium.core.ExperimentalNewExpectTypes
+import ch.tutteli.atrium.logic.creating.transformers.FeatureExtractorBuilder
 import ch.tutteli.atrium.logic.impl.DefaultIteratorAssertions
 
 fun <E, T : Iterator<E>> AssertionContainer<T>.hasNext(): Assertion = impl.hasNext(this)
 fun <E, T : Iterator<E>> AssertionContainer<T>.hasNotNext(): Assertion = impl.hasNotNext(this)
+fun <E, T : Iterator<E>> AssertionContainer<T>.next(): FeatureExtractorBuilder.ExecutionStep<T, E> = impl.next(this)
 
 @OptIn(ExperimentalNewExpectTypes::class)
 private inline val <T> AssertionContainer<T>.impl: IteratorAssertions
