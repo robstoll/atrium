2081da975de9c29c0a22c30b6b7aba51caa7a256
Robert Stoll <rstoll@tutteli.ch>
2024-08-29 08:47:40 +0200

    row and column, map assertion to proof and unwrap invisible group

--- atrium-core/src/commonTest/kotlin/ch/tutteli/atrium/reporting/ProofBuilderTest.kt
+++ atrium-core/src/commonTest/kotlin/ch/tutteli/atrium/reporting/ProofBuilderTest.kt
@@ -9,20 +9,65 @@ import ch.tutteli.atrium.creating.proofs.buildProof
 import kotlin.test.Test
 
 class ProofBuilderTest {
+    val core = expect(1)._core
 
     @Test
     fun createOneProof_does_not_wrap_in_invisible() {
-        val subject = expect(1)._core.buildProof {
+        val subject = core.buildProof {
             simpleProof(Text("a"), 1) { true }
         }
-        expect(subject).toBeAnInstanceOf<Text>()
+        expect(subject).toBeAnInstanceOf<SimpleProof>()
+    }
+
+    @Test
+    fun createTwoProof_wraps_into_invisible() {
+        val subject = core.buildProof {
+            simpleProof(Text("a"), 1) { true }
+            simpleProof(Text("b"), 1) { true }
+        }
+        expect(subject).toBeAnInstanceOf<InvisibleProofGroup> {
+            feature { f(it::children) } toContainExactly entries(
+                { it.toBeAnInstanceOf<SimpleProof>() },
+                { it.toBeAnInstanceOf<SimpleProof>() },
+            )
+        }
     }
 
     @Test
     fun createProofAndReportable_wraps_into_invisible() {
-        val subject = expect(1)._core.buildProof {
+        val subject = core.buildProof {
             simpleProof(Text("a"), 1) { true }
             add(Text("bla"))
+        }
+        expect(subject).toBeAnInstanceOf<InvisibleProofGroup> {
+            feature { f(it::children) } toContainExactly entries(
+                { it.toBeAnInstanceOf<SimpleProof>() },
+                { it.toBeAnInstanceOf<Text>() },
+            )
+        }
+    }
+
+    @Test
+    fun createInvisibleWithAnInvisibleWithASingleProof_unwraps() {
+        val subject = core.buildProof {
+            invisibleGroup {
+                invisibleGroup {
+                    simpleProof(Text("a"), 1) { true }
+                }
+            }
+        }
+        expect(subject).toBeAnInstanceOf<SimpleProof>()
+    }
+
+    @Test
+    fun createInvisibleWithAnInvisibleWithAProofAndAReportable_keepsOnly1Group() {
+        val subject = core.buildProof {
+            invisibleGroup {
+                invisibleGroup {
+                    simpleProof(Text("a"), 1) { true }
+                    add(Text("bla"))
+                }
+            }
         }
         expect(subject).toBeAnInstanceOf<InvisibleProofGroup> {
             feature { f(it::children) } toContainExactly entries(
