fd6a397a124401b4be08caccc4fbccdcfa1f4d75
Robert Stoll <robert.stoll@tegonal.com>
2024-10-28 18:08:33 +0100

    submit dependencies only on main

--- gradle/build-logic/basics/src/main/kotlin/build-logic.gradle-conventions.gradle.kts
+++ gradle/build-logic/basics/src/main/kotlin/build-logic.gradle-conventions.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     // plugins we use in all gradle projects
     id("build-logic.build-params")
     id("com.github.vlsi.gradle-extensions")
-    id("com.github.vlsi.crlf")
     // enable to analyse task dependencies
 //    id("com.dorongold.task-tree")
 }
