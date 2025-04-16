5e906f6dd9660b541d08358ed712096efde22de0
Robert Stoll <robert.stoll@tegonal.com>
2025-04-16 21:57:32 +0200

    make check-dexer more lazily

--- gradle/build-logic/check-dexer/src/main/kotlin/DexerUtils.kt
+++ gradle/build-logic/check-dexer/src/main/kotlin/DexerUtils.kt
@@ -5,46 +5,43 @@ import org.gradle.api.attributes.Attribute
 import org.gradle.api.attributes.Usage
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
-import org.gradle.process.CommandLineArgumentProvider
+import org.gradle.kotlin.dsl.repositories
 import java.io.File
 import java.io.OutputStream
 
-fun configureDexerForProject(project: Project, dexerPreCheckTask: Provider<Task>) {
+fun Project.configureDexerForProject(dexerPreCheckTask: Provider<Task>): TaskProvider<JavaExec> {
 
-    project.repositories.apply {
+    repositories {
         google()
         mavenCentral()
     }
 
-    configureR8(project)
+    configureR8()
+    configureD8()
 
-    configureD8(project)
-
-    registerCheckDexerTask(project, dexerPreCheckTask)
+    return registerCheckDexerTask(dexerPreCheckTask)
 }
 
-private fun configureR8(project: Project) {
-    if (project.configurations.findByName("r8") == null) {
-        project.configurations.create("r8")
-        project.dependencies.add("r8", "com.android.tools:r8:8.7.18")
+private fun Project.configureR8() {
+    if (configurations.findByName("r8") == null) {
+        configurations.create("r8")
+        dependencies.add("r8", "com.android.tools:r8:8.7.18")
     }
 }
 
 
-private fun configureD8(project: Project) {
-    if (project.configurations.findByName("d8") == null) {
-        val d8Configuration = project.configurations.create("d8") {
+private fun Project.configureD8() {
+    if (configurations.findByName("d8") == null) {
+        val d8Configuration = configurations.create("d8") {
             attributes {
-                attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.JAVA_RUNTIME))
-                attribute(
-                    Attribute.of("org.jetbrains.kotlin.platform.type", String::class.java),
-                    "jvm"
-                )
+                attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+                attribute(Attribute.of("org.jetbrains.kotlin.platform.type", String::class.java), "jvm")
             }
         }
-        project.dependencies.add(d8Configuration.name, project)
+        dependencies.add(d8Configuration.name, this)
     }
 }
 
@@ -61,58 +58,52 @@ private fun getAndroidJarArgsForD8(): List<String> {
  * Registers and configures the checkDexer task that compiles JVM bytecode
  * into DEX bytecode using the D8 compiler.
  */
-private fun registerCheckDexerTask(project: Project, dexerPreCheckTask: Provider<Task>) {
-    project.tasks.register<JavaExec>("checkDexer") {
+private fun Project.registerCheckDexerTask(dexerPreCheckTask: Provider<Task>) =
+    tasks.register<JavaExec>(AtriumDexerExtension.TASK_NAME) {
         description = "Compiles android class files into dex bytecode"
         inputs.property(AtriumDexerExtension.ATRIUM_ANDROID_JAR, System.getenv(AtriumDexerExtension.ATRIUM_ANDROID_JAR))
 
         val outputPath = project.layout.buildDirectory.dir("d8")
         outputs.dir(outputPath)
 
-        val jvmJar = project.tasks.named<org.gradle.jvm.tasks.Jar>("jvmJar").get()
+        val jvmJar = tasks.named<org.gradle.jvm.tasks.Jar>("jvmJar").get()
         dependsOn(jvmJar)
         dependsOn(dexerPreCheckTask)
         jvmJar.mustRunAfter(dexerPreCheckTask)
 
-        classpath(project.configurations.named("r8").get().asPath)
+        classpath(project.configurations.named("r8"))
         mainClass.set("com.android.tools.r8.D8")
 
-        // Build the D8 command line arguments
-        argumentProviders.add(CommandLineArgumentProvider {
-            val d8Classpath = getD8ClasspathArgs(project)
-            val androidJarArgs = getAndroidJarArgsForD8()
+        configureErrorOutput()
 
-            d8Classpath + androidJarArgs + listOf(
-                "--output", outputPath.get().asFile.absolutePath,
-                jvmJar.archiveFile.get().asFile.absolutePath
-            )
-        })
-
-        configureErrorOutput(this)
+        // retrieve Providers after configuration to keep it lazily, hence inside doFirst block
+        doFirst {
+            args(project.getD8ClasspathArgs())
+            args(getAndroidJarArgsForD8())
+            args(listOf("--output", outputPath.get().asFile.absolutePath))
+            args(jvmJar.archiveFile.get().asFile.absolutePath)
+        }
     }
-}
 
-
-private fun getD8ClasspathArgs(project: Project): List<String> {
-    return project.configurations.named("d8").get().files.flatMap {
+private fun Project.getD8ClasspathArgs(): List<String> =
+    project.configurations.named("d8").get().files.flatMap {
         listOf("--classpath", it.absolutePath)
     }
-}
 
-private fun configureErrorOutput(task: JavaExec) {
+private fun JavaExec.configureErrorOutput() {
     val oldStream = System.err
     var outputWritten = false
 
-    task.errorOutput = object : OutputStream() {
+    errorOutput = object : OutputStream() {
         override fun write(b: Int) {
             outputWritten = true
             oldStream?.write(b) ?: System.out.write(b)
         }
     }
 
-    task.doLast {
+    doLast {
         if (outputWritten) {
-            throw GradleException("checkDexer failed due to output, see above.")
+            throw GradleException("${AtriumDexerExtension.TASK_NAME} failed due to output, see above.")
         }
     }
 }
