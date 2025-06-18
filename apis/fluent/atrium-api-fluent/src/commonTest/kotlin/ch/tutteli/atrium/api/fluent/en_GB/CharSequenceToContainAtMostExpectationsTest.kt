f1d0e15d78e7fdb856b3e79e517c5af77795fd29
Anson Ng <914anson5@gmail.com>
2025-06-19 00:45:39 +0800

    migrate CharSequenceToContainAtMostExpectationsTest to kotlin-test

--- apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/CharSequenceToContainAtMostExpectationsTest.kt
+++ apis/fluent/atrium-api-fluent/src/commonTest/kotlin/ch/tutteli/atrium/api/fluent/en_GB/CharSequenceToContainAtMostExpectationsTest.kt
@@ -2,28 +2,29 @@ package ch.tutteli.atrium.api.fluent.en_GB
 
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.integration.AbstractCharSequenceToContainAtMostExpectationsTest
+import kotlin.test.Test
 
-
-class CharSequenceToContainAtMostExpectationsTest :
-    AbstractCharSequenceToContainAtMostExpectationsTest(
-        getAtMostTriple(),
-        getAtMostIgnoringCaseTriple(),
-        getNotToContainPair(),
-        getExactlyPair()
-    ) {
+class CharSequenceToContainAtMostExpectationsTest : AbstractCharSequenceToContainAtMostExpectationsTest(
+    getAtMostTriple(),
+    getAtMostIgnoringCaseTriple(),
+    getNotToContainPair(),
+    getExactlyPair()
+) {
+    @Test
+    fun trigger_run_gutter() = 1
 
     companion object : CharSequenceToContainSpecBase() {
 
         private fun getAtMostTriple() =
             { what: String, times: String -> "$toContain $what $atMost $times" } to
-                ("$toContain.$atMost.$value/$values" to Companion::toContainAtMost)
+                    ("$toContain.$atMost.$value/$values" to Companion::toContainAtMost)
 
         private fun toContainAtMost(expect: Expect<CharSequence>, atMost: Int, a: Any, aX: Array<out Any>) =
             expect.toContain.atMost(atMost).values(a, *aX)
 
         private fun getAtMostIgnoringCaseTriple() =
             { what: String, times: String -> "$toContain $ignoringCase $what $atMost $times" } to
-                ("$toContain.$ignoringCase.$atMost.$value/$values" to Companion::toContainAtMostIgnoringCase)
+                    ("$toContain.$ignoringCase.$atMost.$value/$values" to Companion::toContainAtMostIgnoringCase)
 
         private fun toContainAtMostIgnoringCase(expect: Expect<CharSequence>, atMost: Int, a: Any, aX: Array<out Any>) =
             expect.toContain.ignoringCase.atMost(atMost).values(a, *aX)
