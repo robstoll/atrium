89555990b8743210d53f880b17fbc2ffeaec7fca
ryo.saito <greentea2smiley@gmail.com>
2025-07-26 05:45:23 +0900

    Fix DefaultRangeAssertions.kt

--- logic/atrium-logic/src/commonMain/kotlin/ch/tutteli/atrium/logic/impl/DefaultRangeAssertions.kt
+++ logic/atrium-logic/src/commonMain/kotlin/ch/tutteli/atrium/logic/impl/DefaultRangeAssertions.kt
@@ -6,64 +6,58 @@ import ch.tutteli.atrium.assertions.builders.withHelpOnFailureBasedOnDefinedSubj
 import ch.tutteli.atrium.creating.AssertionContainer
 import ch.tutteli.atrium.logic.RangeAssertions
 import ch.tutteli.atrium.logic.toExpect
+import ch.tutteli.atrium.translations.DescriptionComparableExpectation
 import ch.tutteli.atrium.reporting.translating.TranslatableWithArgs
-import ch.tutteli.atrium.translations.DescriptionComparableExpectation.*
 
 class DefaultRangeAssertions : RangeAssertions {
 
     override fun <T : Comparable<T>> toBeInRange(
         container: AssertionContainer<T>,
         range: ClosedRange<T>
-    ): Assertion = toBeInRange(
-        container,
-        range.start,
-        range.endInclusive
-    ) { subject ->
+    ): Assertion = toBeInRange(container, range) { subject ->
         listOf(
             createLowerBoundAssertion(subject, range.start),
             createUpperBoundAssertion(subject, range.endInclusive)
         )
     }
 
-    private fun <T : Comparable<T>> toBeInRange(
-        container: AssertionContainer<out T>,
-        lower: T,
-        upper: T,
-        explanatoryAssertionCreator: (T) -> List<Assertion>
-    ): Assertion =
+    private fun <T : Comparable<T>> createLowerBoundAssertion(subject: T, lower: T): Assertion =
         assertionBuilder.descriptive
-            .withTest(container.toExpect()) { it >= lower && it <= upper }
-            .withHelpOnFailureBasedOnDefinedSubject(container.toExpect()) { subject ->
-                assertionBuilder.explanatoryGroup
-                    .withDefaultType
-                    .withAssertions(explanatoryAssertionCreator(subject))
-                    .build()
-            }
+            .withTest { subject >= lower }
             .withDescriptionAndRepresentation(
-                TranslatableWithArgs(TO_BE_IN_RANGE, "$lower..$upper"),
-                "$lower..$upper"
+                DescriptionComparableExpectation.TO_BE_GREATER_THAN_OR_EQUAL_TO,
+                lower
             )
             .build()
 
-    private fun <T : Comparable<T>> createLowerBoundAssertion(
-        subject: T,
-        lower: T
-    ): Assertion = assertionBuilder.descriptive
-        .withTest { subject >= lower }
-        .withDescriptionAndRepresentation(
-            TO_BE_GREATER_THAN_OR_EQUAL_TO,
-            lower
-        )
-        .build()
-
-    private fun <T : Comparable<T>> createUpperBoundAssertion(
-        subject: T,
-        upper: T
-    ): Assertion = assertionBuilder.descriptive
-        .withTest { subject <= upper }
-        .withDescriptionAndRepresentation(
-            TO_BE_LESS_THAN_OR_EQUAL_TO,
-            upper
-        )
-        .build()
+    private fun <T : Comparable<T>> createUpperBoundAssertion(subject: T, upper: T): Assertion =
+        assertionBuilder.descriptive
+            .withTest { subject <= upper }
+            .withDescriptionAndRepresentation(
+                DescriptionComparableExpectation.TO_BE_LESS_THAN_OR_EQUAL_TO,
+                upper
+            )
+            .build()
 }
+
+internal fun <T : Comparable<T>> toBeInRange(
+    container: AssertionContainer<out T>,
+    range: ClosedRange<T>,
+    explanatoryAssertionCreator: (T) -> List<Assertion>
+): Assertion =
+    assertionBuilder.descriptive
+        .withTest(container.toExpect()) { it >= range.start && it <= range.endInclusive }
+        .withHelpOnFailureBasedOnDefinedSubject(container.toExpect()) { subject ->
+            assertionBuilder.explanatoryGroup
+                .withDefaultType
+                .withAssertions(explanatoryAssertionCreator(subject))
+                .build()
+        }
+        .withDescriptionAndRepresentation(
+            TranslatableWithArgs(
+                DescriptionComparableExpectation.TO_BE_IN_RANGE,
+                "${range.start}..${range.endInclusive}"
+            ),
+            range
+        )
+        .build()
