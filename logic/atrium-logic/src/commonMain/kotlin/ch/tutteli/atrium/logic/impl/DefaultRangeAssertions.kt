1c7d10e2d9da3d2ddad192f8eae1381312f78b04
ryo.saito <greentea2smiley@gmail.com>
2025-07-26 20:22:51 +0900

    - Fix DefaultRangeAssertions.kt - Execute ./gradlew generateLogic

--- logic/atrium-logic/src/commonMain/kotlin/ch/tutteli/atrium/logic/impl/DefaultRangeAssertions.kt
+++ logic/atrium-logic/src/commonMain/kotlin/ch/tutteli/atrium/logic/impl/DefaultRangeAssertions.kt
@@ -14,50 +14,12 @@ class DefaultRangeAssertions : RangeAssertions {
     override fun <T : Comparable<T>> toBeInRange(
         container: AssertionContainer<T>,
         range: ClosedRange<T>
-    ): Assertion = toBeInRange(container, range) { subject ->
-        listOf(
-            createLowerBoundAssertion(subject, range.start),
-            createUpperBoundAssertion(subject, range.endInclusive)
-        )
-    }
-
-    private fun <T : Comparable<T>> createLowerBoundAssertion(subject: T, lower: T): Assertion =
+    ): Assertion =
         assertionBuilder.descriptive
-            .withTest { subject >= lower }
+            .withTest(container.toExpect()) { it >= range.start && it <= range.endInclusive }
             .withDescriptionAndRepresentation(
-                DescriptionComparableExpectation.TO_BE_GREATER_THAN_OR_EQUAL_TO,
-                lower
-            )
-            .build()
-
-    private fun <T : Comparable<T>> createUpperBoundAssertion(subject: T, upper: T): Assertion =
-        assertionBuilder.descriptive
-            .withTest { subject <= upper }
-            .withDescriptionAndRepresentation(
-                DescriptionComparableExpectation.TO_BE_LESS_THAN_OR_EQUAL_TO,
-                upper
+                DescriptionComparableExpectation.TO_BE_IN_RANGE,
+                range,
             )
             .build()
 }
-
-internal fun <T : Comparable<T>> toBeInRange(
-    container: AssertionContainer<out T>,
-    range: ClosedRange<T>,
-    explanatoryAssertionCreator: (T) -> List<Assertion>
-): Assertion =
-    assertionBuilder.descriptive
-        .withTest(container.toExpect()) { it >= range.start && it <= range.endInclusive }
-        .withHelpOnFailureBasedOnDefinedSubject(container.toExpect()) { subject ->
-            assertionBuilder.explanatoryGroup
-                .withDefaultType
-                .withAssertions(explanatoryAssertionCreator(subject))
-                .build()
-        }
-        .withDescriptionAndRepresentation(
-            TranslatableWithArgs(
-                DescriptionComparableExpectation.TO_BE_IN_RANGE,
-                "${range.start}..${range.endInclusive}"
-            ),
-            range
-        )
-        .build()
---
+++ logic/atrium-logic/src/generated/commonMain/ch/tutteli/atrium/logic/range.kt
@@ -0,0 +1,18 @@
+// @formatter:off
+//---------------------------------------------------
+//  Generated content, modify:
+//  buildSrc/generation.kt
+//  if necessary - enjoy the day 🙂
+//---------------------------------------------------
+package ch.tutteli.atrium.logic
+
+import ch.tutteli.atrium.assertions.Assertion
+import ch.tutteli.atrium.creating.AssertionContainer
+import ch.tutteli.atrium.core.ExperimentalNewExpectTypes
+import ch.tutteli.atrium.logic.impl.DefaultRangeAssertions
+
+fun <T : Comparable<T>> AssertionContainer<T>.toBeInRange(range: ClosedRange<T>): Assertion = impl.toBeInRange(this, range)
+
+@OptIn(ExperimentalNewExpectTypes::class)
+private inline val <T> AssertionContainer<T>.impl: RangeAssertions
+    get() = getImpl(RangeAssertions::class) { DefaultRangeAssertions() }
