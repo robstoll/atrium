bc16d31ef5856e52297f79a2f568b8766b8d91de
ryo.saito <greentea2smiley@gmail.com>
2025-07-28 21:26:15 +0900

    Add rangeExpectations.kt in atrium-api-infix, Add test and sample for it, Execute ./gradlew apiDump & KOTLIN_VERSION=1.9 ./gradlew apiDump,

---
+++ apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/samples/RangeExpectationSamples.kt
@@ -0,0 +1,22 @@
+package ch.tutteli.atrium.api.infix.en_GB.samples
+
+import ch.tutteli.atrium.api.infix.en_GB.toBeInRange
+import kotlin.test.Test
+import ch.tutteli.atrium.api.verbs.expect
+
+class RangeExpectationSamples {
+    @Test
+    fun rangeExpectationSampleInt() {
+        expect(3) toBeInRange (1..5)
+    }
+
+    @Test
+    fun rangeExpectationSampleLong() {
+        expect(100L) toBeInRange (50L..150L)
+    }
+
+    @Test
+    fun rangeExpectationSampleChar() {
+        expect('c') toBeInRange ('a'..'f')
+    }
+}
