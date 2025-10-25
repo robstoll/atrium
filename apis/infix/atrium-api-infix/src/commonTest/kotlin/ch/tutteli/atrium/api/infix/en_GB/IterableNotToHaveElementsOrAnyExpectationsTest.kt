8f29c833a3f927ef5c7fa2bc9a88805f28512f9c
Tomer Ben-Rachel <tomerpacific@gmail.com>
2025-10-25 19:47:05 +0300

    issue-2016 replacing subjectless tests and assertioncreator tests
    issue-2016 migrating rest of abstract class
    
    issue-2016 migrating tests classes
    
    issue-2016 removing unnecessary path
    
    issue-2016 renaming variable and adding null
    
    issue-2016 correcting method
    
    issue-2016 adding joinToString
    
    issue-2016 adding joinToString

--- apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/IterableNotToHaveElementsOrAnyExpectationsTest.kt
+++ apis/infix/atrium-api-infix/src/commonTest/kotlin/ch/tutteli/atrium/api/infix/en_GB/IterableNotToHaveElementsOrAnyExpectationsTest.kt
@@ -1,29 +1,36 @@
 package ch.tutteli.atrium.api.infix.en_GB
 
+import ch.tutteli.atrium.api.verbs.internal.expect
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.fun1
-import ch.tutteli.atrium.specs.notImplemented
 import ch.tutteli.atrium.specs.withNullableSuffix
+import kotlin.test.Test
 
-class IterableNotToHaveElementsOrAnyExpectationsTest : ch.tutteli.atrium.specs.integration.AbstractIterableNotToHaveElementsOrAnyExpectationsTest(
-    fun1(Expect<Iterable<Double>>::notToHaveElementsOrAny),
-    fun1(Expect<Iterable<Double?>>::notToHaveElementsOrAny).withNullableSuffix()
-) {
+class IterableNotToHaveElementsOrAnyExpectationsTest :
+    ch.tutteli.atrium.specs.integration.AbstractIterableNotToHaveElementsOrAnyExpectationsTest(
+        fun1(Expect<Iterable<Double>>::notToHaveElementsOrAny),
+        fun1(Expect<Iterable<Double?>>::notToHaveElementsOrAny).withNullableSuffix()
+    ) {
 
-    @Suppress("unused", "UNUSED_VALUE")
-    private fun ambiguityTest() {
-        var list: Expect<List<Number>> = notImplemented()
-        var nList: Expect<Set<Number?>> = notImplemented()
-        var subList: Expect<ArrayList<out Number>> = notImplemented()
-        var star: Expect<Collection<*>> = notImplemented()
+    @Suppress("AssignedValueIsNeverRead", "UNUSED_VALUE")
+    @Test
+    fun ambiguityTest() {
+        var list: Expect<List<Number>> = expect(listOf(1.1))
+        var nSet: Expect<Set<Number?>> = expect(setOf(null, 1.1))
+        var subList: Expect<ArrayList<out Number>> = expect(arrayListOf(1.1))
+        var star: Expect<Collection<*>> = expect(listOf(1.1, "asdf"))
 
-        list = list notToHaveElementsOrAny {}
-        nList = nList notToHaveElementsOrAny {}
-        subList = subList notToHaveElementsOrAny {}
-        star = star notToHaveElementsOrAny {}
-
-        nList = nList notToHaveElementsOrAny null
-        subList = subList notToHaveElementsOrAny null
-        star = star notToHaveElementsOrAny null
+        list = list notToHaveElementsOrAny {
+            it notToEqual 2.1
+        }
+        nSet = nSet notToHaveElementsOrAny {
+            it notToEqual 2.1
+        }
+        subList = subList notToHaveElementsOrAny {
+            it toEqual 1.1
+        }
+        star = star notToHaveElementsOrAny {
+            it notToEqual 2.1
+        }
     }
 }
--- misc/atrium-specs/src/commonMain/kotlin/ch/tutteli/atrium/specs/integration/AbstractIterableNotToHaveElementsOrAnyExpectationsTest.kt
+++ misc/atrium-specs/src/commonMain/kotlin/ch/tutteli/atrium/specs/integration/AbstractIterableNotToHaveElementsOrAnyExpectationsTest.kt
@@ -1,120 +1,129 @@
 package ch.tutteli.atrium.specs.integration
 
 import ch.tutteli.atrium.api.fluent.en_GB.*
-import ch.tutteli.atrium.api.verbs.internal.expect
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.specs.*
+import ch.tutteli.atrium.specs.integration.IterableToContainEntriesSpecBase.Companion.toEqualFun
+import ch.tutteli.atrium.specs.integration.IterableToContainSpecBase.Companion.fluentEmpty
+import ch.tutteli.atrium.specs.integration.IterableToContainSpecBase.Companion.oneToSeven
+import ch.tutteli.atrium.specs.integration.IterableToContainSpecBase.Companion.oneToSevenNullable
+import ch.tutteli.atrium.specs.integration.IterableToContainSpecBase.Companion.separator
+import ch.tutteli.atrium.specs.integration.utils.ExpectationCreatorTestData
+import ch.tutteli.atrium.specs.integration.utils.SubjectLessTestData
+import ch.tutteli.atrium.testfactories.TestFactory
 import ch.tutteli.atrium.translations.DescriptionIterableLikeExpectation
 
+@Suppress("FunctionName")
 abstract class AbstractIterableNotToHaveElementsOrAnyExpectationsTest(
-    notToHaveElementsOrAny: Fun1<Iterable<Double>, Expect<Double>.() -> Unit>,
-    notToHaveElementsOrAnyNullable: Fun1<Iterable<Double?>, (Expect<Double>.() -> Unit)?>,
-    describePrefix: String = "[Atrium] "
-) : IterableToContainEntriesSpecBase({
+    private val notToHaveElementsOrAnySpec: Fun1<Iterable<Double>, Expect<Double>.() -> Unit>,
+    private val notToHaveElementsOrAnyNullableSpec: Fun1<Iterable<Double?>, (Expect<Double>.() -> Unit)?>
+) : ExpectationFunctionBaseTest() {
 
-    include(object : SubjectLessSpec<Iterable<Double>>(describePrefix,
-        notToHaveElementsOrAny.forSubjectLessTest { toEqual(2.5) }
-    ) {})
-    include(object : SubjectLessSpec<Iterable<Double?>>(
-        describePrefix,
-        notToHaveElementsOrAnyNullable.forSubjectLessTest(null)
-    ) {})
+    @TestFactory
+    fun subjectLessTest() = subjectLessTestFactory(
+        SubjectLessTestData(
+            notToHaveElementsOrAnySpec.forSubjectLessTest { toEqual(2.5) }
+        ),
+        SubjectLessTestData(
+            notToHaveElementsOrAnyNullableSpec.forSubjectLessTest(null)
+        )
+    )
 
-    include(object : AssertionCreatorSpec<Iterable<Double>>(
-        describePrefix, oneToSeven().toList().asIterable(),
-        notToHaveElementsOrAny.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") { toBeGreaterThan(1.1) }
-    ) {})
-    include(object : AssertionCreatorSpec<Iterable<Double?>>(
-        "$describePrefix[nullable Element] ", oneToSeven().toList().asIterable(),
-        notToHaveElementsOrAnyNullable.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") { toBeGreaterThan(1.1) }
-    ) {})
+    @TestFactory
+    fun expectationCreatorTest() = expectationCreatorTestFactory(
+        ExpectationCreatorTestData(
+            oneToSeven().toList().asIterable(),
+            notToHaveElementsOrAnySpec.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") { toBeGreaterThan(1.1) }
+        ),
+        ExpectationCreatorTestData(
+            oneToSeven().toList().asIterable(),
+            notToHaveElementsOrAnyNullableSpec.forExpectationCreatorTest("$toBeGreaterThanDescr: 1.1") {
+                toBeGreaterThan(
+                    1.1
+                )
+            }
+        )
+    )
 
     val notToHaveElementsOrAnyDescr = DescriptionIterableLikeExpectation.NOT_TO_HAVE_ELEMENTS_OR_ANY.getDefault()
     val noSuchElementDescr = DescriptionIterableLikeExpectation.NEITHER_EMPTY_NOR_ELEMENT_FOUND.getDefault()
 
-    nonNullableCases(
-        describePrefix,
-        notToHaveElementsOrAny,
-        notToHaveElementsOrAnyNullable
-    ) { notToHaveElementsOrAnyFun ->
 
-        context("empty collection") {
-            it("does not throw") {
-                expect(fluentEmpty()).notToHaveElementsOrAnyFun { toBeLessThan(1.1) }
+    @TestFactory
+    fun empty_collection__does_not_throw() = nonNullableCases(
+        notToHaveElementsOrAnySpec,
+        notToHaveElementsOrAnyNullableSpec
+    ) { notToHaveElementsOrAnyFun ->
+        expect(fluentEmpty()).notToHaveElementsOrAnyFun { toBeLessThan(1.1) }
+    }
+
+    @TestFactory
+    fun one_to_7__throws() = nonNullableCases(
+        notToHaveElementsOrAnySpec,
+        notToHaveElementsOrAnyNullableSpec
+    ) { notToHaveElementsOrAnyFun ->
+        expect {
+            expect(oneToSeven()).notToHaveElementsOrAnyFun { toBeGreaterThan(1.1); toBeLessThan(2.1) }
+        }.toThrow<AssertionError> {
+            messageToContain(
+                "$rootBulletPoint$notToHaveElementsOrAnyDescr: $separator",
+                "$toBeGreaterThanDescr: 1.1",
+                "$toBeLessThanDescr: 2.1",
+                noSuchElementDescr
+            )
+        }
+    }
+
+    @TestFactory
+    fun one_to_7__does_not_throw() = nonNullableCases(
+        notToHaveElementsOrAnySpec,
+        notToHaveElementsOrAnyNullableSpec
+    ) { notToHaveElementsOrAnyFun ->
+        expect(oneToSeven()).notToHaveElementsOrAnyFun { toBeGreaterThan(1.1); toBeLessThan(2.2) }
+    }
+
+
+    @TestFactory
+    fun nullable_cases() = testFactory(notToHaveElementsOrAnyNullableSpec) { notToHaveElementsOrAnyFun ->
+        describe("iterable ${oneToSevenNullable().toList().joinToString(",")}") {
+            describe("happy cases (do not throw)") {
+                it("$toEqualFun(1.1)") {
+                    expect(oneToSevenNullable()).notToHaveElementsOrAnyFun { toEqual(1.1) }
+                }
+                it("null") {
+                    expect(oneToSevenNullable()).notToHaveElementsOrAnyFun(null)
+                }
             }
         }
-
-        context("iterable ${oneToSeven().toList()}") {
-            context("search for entry which needs $toBeGreaterThanFun(1.1) and $toBeLessThanFun(2.1)") {
-                it("throws AssertionError containing both assumptions in one expectation") {
-                    expect {
-                        expect(oneToSeven()).notToHaveElementsOrAnyFun { toBeGreaterThan(1.1); toBeLessThan(2.1) }
-                    }.toThrow<AssertionError> {
-                        messageToContain(
+        describe("failing cases") {
+            it("$toEqualFun(2.1)") {
+                expect {
+                    expect(oneToSevenNullable()).notToHaveElementsOrAnyFun { toEqual(2.1) }
+                }.toThrow<AssertionError> {
+                    message {
+                        toContain.exactly(1).values(
                             "$rootBulletPoint$notToHaveElementsOrAnyDescr: $separator",
-                            "$toBeGreaterThanDescr: 1.1",
-                            "$toBeLessThanDescr: 2.1",
+                            "$toEqualDescr: 2.1",
                             noSuchElementDescr
                         )
                     }
                 }
             }
-
-            context("search for entry which $toBeGreaterThanFun(1.1) and $toBeLessThanFun(2.2)") {
-                it("does not throw an exception") {
-                    expect(oneToSeven()).notToHaveElementsOrAnyFun { toBeGreaterThan(1.1); toBeLessThan(2.2) }
-                }
-            }
         }
-    }
-
-    nullableCases(describePrefix) {
-
-        describeFun(notToHaveElementsOrAnyNullable) {
-            val notToHaveElementsOrAnyFun = notToHaveElementsOrAnyNullable.lambda
-
-            context("iterable ${oneToSevenNullable().toList()}") {
-                context("happy cases (do not throw)") {
-                    it("$toEqualFun(1.1)") {
-                        expect(oneToSevenNullable()).notToHaveElementsOrAnyFun { toEqual(1.1) }
-                    }
-                    it("null") {
-                        expect(oneToSevenNullable()).notToHaveElementsOrAnyFun(null)
-                    }
-                }
-
-                context("failing cases") {
-                    it("$toEqualFun(2.1)") {
-                        expect {
-                            expect(oneToSevenNullable()).notToHaveElementsOrAnyFun { toEqual(2.1) }
-                        }.toThrow<AssertionError> {
-                            message {
-                                toContain.exactly(1).values(
-                                    "$rootBulletPoint$notToHaveElementsOrAnyDescr: $separator",
-                                    "$toEqualDescr: 2.1",
-                                    noSuchElementDescr
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-
-            context("iterable ${oneToSeven().toList()}") {
-                it("null, throws an AssertionError") {
-                    expect {
-                        expect(oneToSeven() as Iterable<Double?>).notToHaveElementsOrAnyFun(null)
-                    }.toThrow<AssertionError> {
-                        message {
-                            toContain.exactly(1).values(
-                                "$rootBulletPoint$notToHaveElementsOrAnyDescr: $separator",
-                                "$toEqualDescr: null",
-                                noSuchElementDescr
-                            )
-                        }
+        describe("iterable ${oneToSeven().toList().joinToString(",")}") {
+            it("null, throws an AssertionError") {
+                expect {
+                    expect(oneToSeven() as Iterable<Double?>).notToHaveElementsOrAnyFun(null)
+                }.toThrow<AssertionError> {
+                    message {
+                        toContain.exactly(1).values(
+                            "$rootBulletPoint$notToHaveElementsOrAnyDescr: $separator",
+                            "$toEqualDescr: null",
+                            noSuchElementDescr
+                        )
                     }
                 }
             }
         }
     }
-})
+}
