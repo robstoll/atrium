970f4fafcdd40f491d8665c89a3f4498f471057a
WesleyJammer <wr.tobias@ad-academie.nl>
2026-01-19 13:35:35 +0100

    Exclude UsageHintGroup from proof explanation rendering
    Updated DefaultProofExplanationTextPreRenderer to filter out UsageHintGroup children when rendering proof explanations. Added a test to verify that usage hints are not included in the output for proof explanations.

--- atrium-core/src/commonMain/kotlin/ch/tutteli/atrium/reporting/prerendering/text/impl/DefaultProofExplanationTextPreRenderer.kt
+++ atrium-core/src/commonMain/kotlin/ch/tutteli/atrium/reporting/prerendering/text/impl/DefaultProofExplanationTextPreRenderer.kt
@@ -5,6 +5,7 @@ import ch.tutteli.atrium.reporting.prerendering.text.TextPreRenderControlObject
 import ch.tutteli.atrium.reporting.prerendering.text.TypedTextPreRenderer
 import ch.tutteli.atrium.reporting.reportables.Icon
 import ch.tutteli.atrium.reporting.reportables.ProofExplanation
+import ch.tutteli.atrium.reporting.reportables.UsageHintGroup
 
 internal class DefaultProofExplanationTextPreRenderer :
     TypedTextPreRenderer<ProofExplanation>(ProofExplanation::class) {
@@ -17,7 +18,7 @@ internal class DefaultProofExplanationTextPreRenderer :
             prefix = Icon.PROOF_EXPLANATION_BULLET_POINT,
             explainsProof = true
         )
-        return OutputNode.singleWithoutColumnsNotOwnLevel(children = reportable.children.flatMap { child ->
+        return OutputNode.singleWithoutColumnsNotOwnLevel(children = reportable.children.filterNot { it is UsageHintGroup }.flatMap { child ->
             controlObject.transformChildIncludingIndentationAndPrefix(child, newControlObject)
         })
     }
